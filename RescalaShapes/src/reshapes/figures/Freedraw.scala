f6715e638877ff70fc80b6ac5428551cadc9095d
flom <florian.mladitsch@googlemail.com>
2013-03-02 17:29:17 +0100

    improved freedraw line rendering

--- RescalaShapes/src/reshapes/figures/Freedraw.scala
+++ RescalaShapes/src/reshapes/figures/Freedraw.scala
@@ -5,6 +5,10 @@ import java.awt.Graphics2D
 class Freedraw extends Movable {
 
   def doDraw(g: Graphics2D) = {
-    path map (point => g.fillOval(point.x, point.y, strokeWidth + 2, strokeWidth + 2))
+    var lastPoint = path.first
+    for (point <- path.tail) {
+      g.drawLine(lastPoint.x, lastPoint.y, point.x, point.y)
+      lastPoint = point
+    }
   }
 }
