1120ba1bf857686ffd1b51b603d5019c24863e7d
pweisenburger <pascal.weisenburger@web.de>
2013-05-07 16:47:43 +0200

    comments

--- src/reswing/ImperativeSignal.scala
+++ src/reswing/ImperativeSignal.scala
@@ -3,6 +3,18 @@ package reswing
 import scala.events.ImperativeEvent
 import scala.events.behaviour.Signal
 
+/**
+ * Provides a signal that is used to update Swing values, but also
+ * changes its own value whenever the corresponding Swing value changes.
+ * 
+ * A signal can be set which will cause the Swing setter method to be called
+ * each time the signal changes.
+ * The signal this class provides changes its value according to the value
+ * retrieved from the Swing getter method.
+ * 
+ * This way, it is possible to set a property reactively using a signal
+ * and to use a property in signal expressions.
+ */
 class ImperativeSignal[T] private (private[reswing] val inputSignal: Signal[T]) {
   private val event = new ImperativeEvent[T]
   private val signal = if (inputSignal != null)
