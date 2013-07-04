bb954f982d5afd598859d801efba2c47bfd605ec
guidosalva <guido.salva@gmail.com>
2013-07-04 15:20:36 +0200

    small changes

--- ReactiveScalaReader_AI_ReScalaLib/src/reader/data/FeedStore.scala
+++ ReactiveScalaReader_AI_ReScalaLib/src/reader/data/FeedStore.scala
@@ -15,7 +15,7 @@ class FeedStore (
   val channels = addChannel.fold(Map.empty[RSSChannel, Signal[Set[RSSItem]]]) { (map, channel) => //#SIG //#IF
 	  map + (channel ->
 	    (addItem && (_.srcChannel.isDefined) && (_.srcChannel.get == channel)). //#EF //#EF
-	      fold(Set.empty[RSSItem])(_ + _)) //#SIG
+	      fold(Set.empty[RSSItem])(_ + _)) //#IF
     }
   
   val itemAdded: Event[RSSItem] = addItem && { item => //#EVT //#EF
