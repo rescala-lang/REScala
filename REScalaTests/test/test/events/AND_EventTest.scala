e5f895333803379a9701b2eb4b75cf101a97831e
guidosalva <guido.salva@gmail.com>
2013-07-08 19:16:44 +0200

    added and event test

---
+++ REScalaTests/test/test/events/AND_EventTest.scala
@@ -0,0 +1,81 @@
+package test.events
+
+
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+import org.mockito.Mockito.verify
+import org.mockito.Mockito.times
+import org.scalatest.junit.AssertionsForJUnit
+import org.scalatest.mock.MockitoSugar
+
+import scala.collection.immutable.Set
+
+
+import react.events._
+//import eevents.lib._
+import react._
+
+
+
+
+class AND_EventTest extends AssertionsForJUnit with MockitoSugar {
+  
+
+  @Test def handlerOf_AND_IsNOTExecutedIfEventsFireSingularly = {
+    var test = 0
+    val e1 = new ImperativeEvent[Int]()
+    val e2 = new ImperativeEvent[Int]()
+    val e1_AND_e2 = e1 && e2
+    e1_AND_e2 += ((x: (Int,Int)) => { test += 1 })
+    
+    e1(10)
+    e2(10)
+    assert(test == 0)
+     
+  }
+  
+  @Test def handlerOf_AND_DoesNotRememberOldRounds = {
+    var test = 0
+    val e1 = new ImperativeEvent[Int]()
+    val e2 = new ImperativeEvent[Int]()
+    val e1_AND_e2 = e1 && e2
+    e1_AND_e2 += ((x: (Int,Int)) => { test += 1 })
+    
+    e1(10)
+    e2(10)
+    e1(10)
+    e2(10)
+    assert(test == 0)
+     
+  }
+  
+   @Test def handlerOf_AND_IsExecutedIfBothEventsFire = {
+    
+    var test = 0
+    val e1 = new ImperativeEvent[Int]()
+    val e2 = e1 map ((x: Int) => (x * 2))
+    val e3 = e1 map ((x: Int) => (x * 2))
+    val e2_AND_e3 = e2 && e3
+    e1 += ( (x: Int) => { test += 1 })
+    e2 += ( (x: Int) => { test += 1 })
+    e3 += ( (x: Int) => { test += 1 })
+    e2_AND_e3 += ((x: (Int,Int)) => { test += 1 })
+    
+    e1(10)
+    assert(test == 4)
+  }
+  
+}
+
+
+
+
+
+
+
+
+
+
+
+
