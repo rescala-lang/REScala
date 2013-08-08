c03387456702a6691bc983f67a7f7937ac134c27
Gerold <gerold.hintz@gmail.com>
2013-08-08 14:17:02 +0200

    committing missing files

---
+++ src/millgame/versions/signalonly/MillBoard.scala
@@ -0,0 +1,100 @@
+package millgame.versions.signalonly
+import millgame.types._
+import react.events._
+import react.SignalSynt
+import react.Var
+import react.Signal
+import macro.SignalMacro.{SignalM => Signal}
+
+object MillBoard {
+  
+  	val Borders = (0 to 23 by 2).map(init => 
+	  	List.iterate(init, 3)(x => (x + 1) -
+	  	   (if ((x + 1) % ( (init / 8 + 1) * 8) == 0) 8 else 0)
+	  	)
+	)
+	val Crosses = (1 to 8 by 2).map(List.iterate(_, 3)(x => x + 8))
+	
+	val Lines = Borders ++ Crosses
+}
+
+class MillBoard {
+  
+    /* wrap stones Var, to have the same interface as other versions */
+    def stones = stonesVar.getVal
+  
+	/* spiral-indexed board slots, starting innermost lower left, going clockwise */
+    val stonesVar: Var[Vector[Slot]] = Var(Vector.fill(24)(Empty)) //#VAR
+	
+	/* slots by the 16 lines of the game */
+	val lines = Signal { //#SIG
+      MillBoard.Lines.map(line => line.map(stonesVar()(_))) 
+    }
+	
+	/* lines mapped to owners */
+	val lineOwners: Signal[Vector[Slot]] = Signal { //#SIG
+	  lines().map(line => if(line.forall(_ == line.head)) line.head else Empty).toVector
+	}
+	
+	/* observers */
+	var millClosedListerer: List[(Slot => Unit)] = Nil
+	var numStonesChangedListener: List[(((Slot, Int)) => Unit)] = Nil
+	def addMillClosedListener(l: (Slot => Unit)) = millClosedListerer ::= l
+	def addStonesChangedListenerlistener(l: (((Slot, Int)) => Unit)) = numStonesChangedListener ::= l
+	def notifyMillClosed(s: Slot) = millClosedListerer.foreach(_(s))
+	def notifyStonesChanged(c: (Slot, Int)) = numStonesChangedListener.foreach(_(c))
+	
+	/* access slot state by index */
+	def apply(i: Int) = stonesVar.getVal(i)	
+	def update(i: Int, color: Slot) = {
+	  stonesVar.setVal(stonesVar.getVal.updated(i, color))
+	}
+	
+	/* several test methods*/
+	def canPlace(i: Int) = this(i) == Empty
+	def canRemove(i: Int) = this(i) != Empty
+	def canJump(i: Int, j: Int) = canRemove(i) && canPlace(j)
+	def canMove(i: Int, j: Int) = canJump(i, j) && (
+		math.abs(i - j) == 1 || 
+		(math.abs(i - j) == 8 && i % 2 != 0 && j % 2 != 0) ||
+		math.abs(i - j) == 7 && math.min(i, j) % 8 == 0
+		)
+
+	def place(i: Int, color: Slot) {
+	  val previousOwners = lineOwners.getValue
+	  this(i) = color
+	  val mill = previousOwners.zip(lineOwners.getValue).collectFirst { case (a, b) if a != b => b }
+	  
+	  // possible fire mill closed event
+	  mill.foreach(notifyMillClosed(_))
+	  
+	  // fire numStonesChanged event
+	  notifyStonesChanged((color, numStones.getValue(color)))
+	}
+	
+	def remove(i: Int) = {
+	  val color = stones(i)
+	  this(i) = Empty
+
+	  // fire numStonesChanged event
+	  notifyStonesChanged((color, numStones.getValue(color)))
+	}
+	
+	def move(i: Int, j: Int) = { 
+	  val color = stones(i)
+	  this(i) = Empty
+	  place(j, color)
+	}
+	
+	val numStones: Signal[(Slot => Int)] = Signal { //#SIG
+	  val stones = stonesVar()
+	  (color: Slot) => stones.count(_ == color)
+	}
+}
+
+object Test extends Application {
+  	val mill = new MillBoard
+  	mill(0) = Black
+  	mill(1) = Black
+  	mill(2) = Black
+}
---
+++ src/millgame/versions/signalonly/MillGame.scala
@@ -0,0 +1,145 @@
+package millgame.versions.signalonly
+
+import millgame.types._
+import millgame.versions.signalonly._
+import react.events._
+import react.SignalSynt
+import react.Var
+import react.Signal
+import macro.SignalMacro.{SignalM => Signal}
+import millgame.types.Empty
+
+abstract class Gamestate {
+  def getPlayer: Slot
+  def text: String
+}
+
+case class PlaceStone(val player: Slot) extends Gamestate {
+  def getPlayer = player
+  def text = player + " to PLACE a stone"
+}
+
+case class RemoveStone(val player: Slot) extends Gamestate {
+  assert(player != Empty)
+  def getPlayer = player
+  def color = player.other
+  def text = player + " to REMOVE a " + color + " stone"
+}
+
+case class MoveStoneSelect(val player: Slot) extends Gamestate {
+  def getPlayer = player
+  def text = player + " to MOVE a stone"
+}
+
+case class MoveStoneDrop(val player: Slot, index: Int) extends Gamestate {
+  def getPlayer = player
+  def text = player + " to select destination for stone " + index
+}
+
+case class JumpStoneSelect(val player: Slot) extends Gamestate {
+  def getPlayer = player
+  def text = player + " to JUMP with a stone"
+}
+case class JumpStoneDrop(val player: Slot, index: Int) extends Gamestate {
+  def getPlayer = player
+  def text = player + " to select destination for stone " + index
+}
+
+case class GameOver(winner: Slot) extends Gamestate {
+  def getPlayer = Empty
+  def text = "Game over. " + winner + " wins."
+}
+
+class MillGame {
+
+  val board = new MillBoard
+
+  val state: Var[Gamestate] = Var(PlaceStone(White))  //#VAR
+  val remainCount: Var[Map[Slot, Int]] = Var(Map(Black -> 9, White -> 9)) //#VAR
+  
+  /* We keep those events, because the GUI interacts with them */
+  val remainCountChanged = Signal{remainCount()}.changed //#EVT //#IF
+  val stateChanged = Signal{ state()}.changed //#EVT //#IF
+  val gameEnd = stateChanged && ((_: Gamestate) match {case GameOver(_) => true; case _ => false}) //#EVT
+  val gameWon: Event[Slot] = gameEnd.map {(_: Gamestate) match {case GameOver(w) => w; case _ => null}} //#EVT
+  def stateText = state().text
+  
+  
+  /* Event based game logic: */
+  board.addMillClosedListener({ color =>
+    state() = RemoveStone(color)
+  })
+  
+  board.addStonesChangedListenerlistener( {
+    case (color, n) =>
+      if (remainCount.getValue(color) == 0 && n < 3) {
+        state() = GameOver(color.other) 
+      }
+  })
+  
+
+
+  private def nextState(player: Slot): Gamestate =
+    if (remainCount()(player) > 0) PlaceStone(player)
+    else if (board.numStones.getVal(player) == 3) JumpStoneSelect(player)
+    else MoveStoneSelect(player)
+
+  private def decrementCount(player: Slot) {
+    val currentCount = remainCount.getValue
+    remainCount() = currentCount.updated(player, currentCount(player) - 1)
+  }
+
+  def playerInput(i: Int): Boolean = state.getValue match {
+
+    case PlaceStone(player) =>
+      if (board.canPlace(i)) {
+        state() = (nextState(player.other))
+        decrementCount(player)
+        board.place(i, player)
+        true
+      } else false
+
+    case remove @ RemoveStone(player) =>
+      if (board(i) == remove.color) {
+        state() = (nextState(player.other))
+        /// NOTE: Removing the stone can trigger events which change the state
+        /// therefore, remove has to be called after the change state
+        board.remove(i)
+        true
+      } else false
+
+    case MoveStoneSelect(player) =>
+      if (board(i) == player) {
+        state() = (MoveStoneDrop(player, i))
+        true
+      } else false
+
+    case MoveStoneDrop(player, stone) =>
+      if (board.canMove(stone, i)) {
+        state() = (nextState(player.other))
+        board.move(stone, i)
+        true
+      } else {
+        state() = (MoveStoneSelect(player))
+        false
+      }
+
+    case JumpStoneSelect(player) =>
+      if (board(i) == player) {
+        state() = (JumpStoneDrop(player, i))
+        true
+      } else false
+
+    case JumpStoneDrop(player, stone) =>
+      if (board.canJump(stone, i)) {
+        state() = (nextState(player.other))
+        board.move(stone, i)
+        true
+      } else {
+        state() = (MoveStoneSelect(player))
+        false
+      }
+
+    case _ => false
+  }
+}
