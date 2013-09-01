89b4160a25be313f5d4a80e78e6957be69529539
Gerold <gerold.hintz@gmail.com>
2013-09-01 16:09:42 +0200

    Moved depricated implementations to Depricated.scala

--- REScala/src/react/Reactives.scala
+++ REScala/src/react/Reactives.scala
@@ -69,42 +69,8 @@ trait Var[T] extends DepHolder {
 }
 
 
-class StaticVar[T](initval: T) extends DepHolder with Var[T] {
-  private[this] var value: T = initval
-  def setVal(newval: T): Unit = {
-    val old = value
-    if(newval != old) {
-    value = newval // .asInstanceOf[T] // to make it covariant ?
-    TS.nextRound  // Testing
-    timestamps += TS.newTs // testing
-
-    notifyDependents(value)
-    ReactiveEngine.startEvaluation
-    }
-    else {
-      timestamps += TS.newTs // testing
-    }
-  }  
-  def getValue = value
-  def getVal = value
-  
-  def update(v: T) = setVal(v)
-  
-  def apply(s: SignalSynt[_]) = {
-    if (level >= s.level) s.level = level + 1
-    s.reactivesDependsOnCurrent += this 
-    getVal
-  }
-  
-  def apply = getVal
-  
-  def toSignal = StaticSignal(this){this.getVal}
-  
-  /* Testing */
-  val timestamps = ListBuffer[Stamp]()
-}
 object Var {
-  def apply[T](initval: T) = new StaticVar(initval)
+  def apply[T](initval: T) = new VarSynt(initval)
 }
 
 
@@ -157,77 +123,6 @@ trait Signal[+T] extends Dependent with DepHolder {
 }
 
 
-/* A dependent reactive value */
-class StaticSignal[+T](reactivesDependsOn: List[DepHolder])(expr: =>T)
-  extends Dependent with DepHolder with Signal[T] {
-  
-  var inQueue = false
-
-  private[this] var currentValue = expr
-  
-  def getValue = currentValue
-  def getVal = currentValue
-  
-  def apply(): T = currentValue
-  def apply(s: SignalSynt[_]) = {
-    if (level >= s.level) s.level = level + 1
-    s.reactivesDependsOnCurrent += this 
-    getVal
-  }
-  
-  reactivesDependsOn.foreach( r => {
-    if (r.level >= level) level = r.level + 1 // For glitch freedom  
-    r.addDependent(this) // To be notified in the future
-  }) // check
-  dependOn ++= reactivesDependsOn
-  
-  
-  def triggerReevaluation() = reEvaluate
-  
-  def reEvaluate(): T = {
-    inQueue = false
-    val tmp = expr
-    if (tmp != currentValue) {
-      currentValue = tmp
-      timestamps += TS.newTs // Testing
-      notifyDependents(currentValue)
-    } else {
-      timestamps += TS.newTs // Testing
-    }
-    tmp
-  }
-  override def dependsOnchanged(change: Any,dep: DepHolder) = {
-    if(!inQueue){
-      inQueue = true
-      ReactiveEngine.addToEvalQueue(this)
-    }    
-  }
-  
-  def change[U >: T]: Event[(U, U)] = new ChangedEventNode[(U, U)](this)
- 
-  /* Testing */
-  val timestamps = ListBuffer[Stamp]()
-}
-
-
-
-/**
- * Create a Signal
- */
-object StaticSignal {
-  
-  def apply[T](reactivesDependsOn: List[DepHolder])(expr: => T) =
-    new StaticSignal(reactivesDependsOn)(expr)
-  
-  type DH = DepHolder
-  def apply[T]()(expr: =>T): Signal[T] = apply(List())(expr)
-  def apply[T](r1: DH)(expr: =>T): Signal[T] = apply(List(r1))(expr)
-  def apply[T](r1: DH,r2: DH)(expr: =>T): Signal[T] = apply(List(r1,r2))(expr)
-  def apply[T](r1: DH,r2: DH,r3: DH)(expr: =>T): Signal[T] = apply(List(r1,r2,r3))(expr)
-  def apply[T](r1: DH,r2: DH,r3: DH,r4: DH)(expr: =>T): Signal[T] = apply(List(r1,r2,r3,r4))(expr)
-  def apply[T](r1: DH,r2: DH,r3: DH,r4: DH,r5: DH)(expr: =>T): Signal[T] = apply(List(r1,r2,r3,r4,r5))(expr)
-}
-
 
 /* A callback called when a signal changes */
 class Handler[T] (exp: =>T) extends Dependent {
@@ -266,7 +161,6 @@ object ReactiveEngine {
     }
   }
 }
-
 
 
 // TODO: check the use of these classes. Originally was only for testing
