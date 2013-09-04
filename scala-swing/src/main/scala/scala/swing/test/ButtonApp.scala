9ee44a36a634b615725eab229a937848dfc7b29a
guidosalva <guido.salva@gmail.com>
2013-09-04 15:26:30 +0200

    added scala-wing

---
+++ scala-swing/src/main/scala/scala/swing/test/ButtonApp.scala
@@ -0,0 +1,25 @@
+
+package main.scala.scala.swing.test
+
+import java.awt.Dimension
+
+import swing._
+import swing.event._
+
+object ButtonApp extends SimpleSwingApplication {
+  def top = new MainFrame {
+    title = "My Frame"
+    contents = new GridPanel(2, 2) {
+      hGap = 3
+      vGap = 3
+      contents += new Button {
+        text = "Press Me!"
+        reactions += {
+          case ButtonClicked(_) => text = "Hello Scala"
+        }
+      }
+    }
+    size = new Dimension(300, 80)
+  }
+}
+
