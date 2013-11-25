372a050f06e6d3230d511010d3c44039fe164ff6
Yannick Schroecker <yannickschroecker@googlemail.com>
2013-11-25 12:54:09 +0100

    Introduced fixture to ReactiveSetSpec

--- src/test/collections/ReactiveSetSpec.scala
+++ src/test/collections/ReactiveSetSpec.scala
@@ -5,25 +5,34 @@ import main.collections._
 import react._
 
 class ReactiveSetSpec extends FunSpec {
+    
+    def fixture = new {
+        val collection = new ReactiveHashSet[Int]
+        val contains2 = collection.contains(2)
+        val contains3 = collection.contains(3)
+    }
+    
 	describe("ReactiveHashSet") {
 	    it("should allow fixed manipulation") {
-	        val collection = new ReactiveHashSet[Int]
+	        val f = fixture
+	        import f._
 	        assertResult(false)(collection.contains(3)())
 	        collection += 3
 	        assertResult(true)(collection.contains(3)())
 	    }
 	    
 	    it("should allow reactive checks") {
-	        val collection = new ReactiveHashSet[Int]
-	        val contains3 = collection.contains(3)
+	        val f = fixture
+	        import f._
+	        
 	        assertResult(false)(contains3())
 	        collection += 3
 	        assertResult(true)(contains3())
 	    }
 	    
 	    it("should allow reactive inserts") {
-	        val collection = new ReactiveHashSet[Int]
-	        val contains3 = collection.contains(3)
+	        val f = fixture
+	        import f._
 	        val three = Var(2)
 	        collection += three.toSignal
 	        assertResult(false)(contains3())
@@ -34,8 +43,8 @@ class ReactiveSetSpec extends FunSpec {
 	    }
 	    
 	    it("should allow mixing inserts") {
-	        val collection = new ReactiveHashSet[Int]
-	        val contains3 = collection.contains(3)
+	        val f = fixture
+	        import f._
 	        val three = Var(2)
 	        
 	        collection += three.toSignal
@@ -49,8 +58,8 @@ class ReactiveSetSpec extends FunSpec {
 	    }
 	    
 	    it("should allow mixing inserts with fix removal") {
-	        val collection = new ReactiveHashSet[Int]
-	        val contains3 = collection.contains(3)
+	        val f = fixture
+	        import f._
 	        val three = Var(2)
 	        collection += three.toSignal
 	        assertResult(false)(contains3())
@@ -61,8 +70,8 @@ class ReactiveSetSpec extends FunSpec {
 	    }
 	    
 	    it("should allow reactive removal") {
-	        val collection = new ReactiveHashSet[Int]
-	        val contains3 = collection.contains(3)
+	        val f = fixture
+	        import f._
 	        collection += 3
 	        val remove = Var(2)
 	        collection -= remove.toSignal
@@ -76,9 +85,8 @@ class ReactiveSetSpec extends FunSpec {
 	    
 	    describe("should allow mixing inserts with reactive removal") {
 	        it("when a reactively added component gets removed before it gets inserted") {
-	            val collection = new ReactiveHashSet[Int]
-	            val contains2 = collection.contains(2)
-	            val contains3 = collection.contains(3)
+	            val f = fixture
+	            import f._
 	            val add = Var(2)
 	            val remove = Var(2)
 	            
@@ -99,9 +107,9 @@ class ReactiveSetSpec extends FunSpec {
 	        }
 	        
 	        it("when a reactively added component gets removed after it gets inserted") {
-	            val collection = new ReactiveHashSet[Int]
-	            val contains2 = collection.contains(2)
-	            val contains3 = collection.contains(3)
+	            val f = fixture
+	            import f._
+	            
 	            val add = Var(2)
 	            val remove = Var(2)
 	            
