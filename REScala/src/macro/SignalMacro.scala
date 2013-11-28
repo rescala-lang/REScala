b0e4e2a5e9dc2be78f93d91782b635d978f5fd50
pweisenburger <pascal.weisenburger@web.de>
2013-11-28 17:00:55 +0100

    "Signal { ... }" expression macro implementation extended to support nested method calls that return signals, e.g. "Signal { (event latest 0)() + 42 }"

--- REScala/src/macro/SignalMacro.scala
+++ REScala/src/macro/SignalMacro.scala
@@ -1,78 +1,82 @@
 package macro
 
-
-import scala.reflect.macros.Context
-import scala.language.experimental.macros
-import scala.reflect.runtime.{universe => ru}
-
-import react._
-
 import scala.collection.mutable.ListBuffer
+import scala.language.experimental.macros
+import scala.reflect.macros.Context
 
-
+import react.DepHolder
+import react.Signal
+import react.SignalSynt
 
 object SignalMacro {
-
+  
   def SignalM[A](expression: A): Signal[A] = macro SignalMacro[A]
-
-  def SignalMacro[A: c.WeakTypeTag](c: Context)(expression: c.Expr[A]): c.Expr[SignalSynt[A]] = {
+  
+  def SignalMacro[A: c.WeakTypeTag](c: Context)(expression: c.Expr[A]): c.Expr[Signal[A]] = {
     import c.universe._
     
-
-    //val out = new java.io.FileWriter(
-    //    "/home/guido/Desktop/darmstadt/NREScala_workspace/GuidoScalaReactiveMacro/debugfile.txt",true)
-
-    // extracting sub trees with type Reactive[_]
-    val extractedReactives = expression.tree.filter(t => t.tpe <:< typeOf[DepHolder])
+//    val out = new java.io.FileWriter(
+//        "/home/pascal/Desktop/debugfile.txt", true)
     
-    val sName = newTermName(c.fresh("s$"))
-
-    object tracingTransformer extends Transformer {
-      override def transform(tree: Tree): Tree = {
+//    out.append(showRaw(reify { { val a = 0; a } }) + "\n\n\n\n")
+    
+//    extracting sub trees with type Reactive[_]
+//    val extractedReactives = expression.tree.filter(t => t.tpe <:< typeOf[DepHolder])
+    
+    val signalSyntName = newTermName(c.fresh("s$"))
+    val signalValues = ListBuffer.empty[ValDef]
+    
+    object transformer extends Transformer {
+      override def transform(tree: Tree): Tree =
         tree match {
-          case Apply(
-                 Select(x,y: TermName), 
-                 List()
-                ) if y.decoded == "apply" &&
-                  x.tpe <:< typeOf[DepHolder]
-          =>      
-            val newTree = Apply(
-                 Select(x,y), 
-                 List( c.Expr[Signal[A]](Ident(sName)).tree )
-                )
-            // out.append("Intermediate:  " + ru.showRaw(newTree) + "\n")
-            //out.write(ru.show(y.decoded ) + "\n")
-            super.transform(tree)
-            //val result = super.transform(tree)
-            newTree
+          case Apply(Select(depHolder, apply), List())
+              if depHolder.tpe <:< typeOf[DepHolder] && apply.decoded == "apply" =>
+            Apply(super.transform(Select(depHolder, newTermName("apply"))), List(Ident(signalSyntName)))
+          
+          case depHolder if depHolder.tpe <:< typeOf[DepHolder] =>
+            depHolder match {
+              case Apply(_, args) =>
+                if (args exists { _ exists { _.isInstanceOf[IdentContextApi] } })
+                  c.error(depHolder.pos, "Another Signal cannot be used inside a Signal expression " +
+                                         "if it is depends on local values")
+                
+                val signalName = newTermName(c.fresh("s$"))
+                signalValues += ValDef(Modifiers(), signalName, TypeTree(), depHolder)
+                Ident(signalName)
+              case _ =>
+                super.transform(tree)
+            }
+          
           case _ =>
             super.transform(tree)
         }
-      }
     }
-
-    val t = tracingTransformer.transform(expression.tree)
-    //out.append("Manip:  " + ru.showRaw(c.Expr[A](c.resetAllAttrs(t)).splice) + "\n")	
-
-    val function = Function(
-      List(ValDef(Modifiers(), sName, TypeTree(weakTypeOf[SignalSynt[A]]), EmptyTree)),
-      t)
-
-    val body = reify {
-      SignalSynt[A] { c.Expr[SignalSynt[A] => A](function).splice }
-    }.tree             
-                 
-                 
-   // out.append(" --> " + ru.showRaw(body) + "\n")
-  
-    //out.close
-
-    c.Expr[SignalSynt[A]](c.resetAllAttrs(body))
     
+    val tree = transformer transform expression.tree
+    
+    val function =
+      Function(
+        List(ValDef(Modifiers(), signalSyntName, TypeTree(weakTypeOf[SignalSynt[A]]), EmptyTree)),
+        tree)
+    
+    val body =
+      Apply(
+        TypeApply(
+          Select(
+            Select(
+              Select(
+                Ident(nme.ROOTPKG),
+                newTermName("react")),
+              newTermName("SignalSynt")),
+            newTermName("apply")),
+          List(TypeTree())),
+        List(function))
+    
+    val block = Typed(Block(signalValues.toList, body), TypeTree(weakTypeOf[Signal[A]]))
+    
+//    out.append((c resetAllAttrs block) + "\n\n")
+//    out.close
+    
+    c.Expr[Signal[A]](c resetAllAttrs block)
   }
-
 }
-
-
-
-
