4f16ba6f870b2a82fc3bab2ea06ba9b2646a1826
Yannick Schroecker <yannickschroecker@googlemail.com>
2013-12-05 14:42:20 +0100

    Added further higher-order methods to ReactiveSetLike

--- src/main/collections/ReactiveSetLike.scala
+++ src/main/collections/ReactiveSetLike.scala
@@ -10,7 +10,7 @@ trait ReactiveSetLike[A, ConcreteType[A]] {
 	type InternalType[A] <: SetLike[A, InternalType[A]] with Set[A]
 	
 	protected val collectionSignal: Var[Signal[InternalType[A]]] 
-	protected def wrapSignal(signal: Signal[InternalType[A]]): ConcreteType[A]
+	protected def wrapSignal[B](signal: Signal[InternalType[B]]): ConcreteType[B]
 	
 	def +=(elem: A) {
 	    (+=)(Signal(elem))
@@ -30,20 +30,33 @@ trait ReactiveSetLike[A, ConcreteType[A]] {
 	
 	def -=(elem: Signal[A]) {
 	    val signal = collectionSignal() 
-	    collectionSignal() = SignalSynt[InternalType[A]](signal) {
-	        (x: SignalSynt[InternalType[A]]) => signal(x) - elem(x)
-	    }
+	    collectionSignal() = Signal( signal() - elem() )
+	    
 	}
 	
 	protected def signal[B] = SignalSynt[B](collectionSignal) _ //why don't I need the current collectionSignal()? 
 	
-	
-	def filter(f: A => Boolean): ConcreteType[A] = wrapSignal(signal[InternalType[A]] {
-	    (x: SignalSynt[InternalType[A]]) => collectionSignal(x)(x).filter(f)
-	})
-	
-	def contains(elem: A): Signal[Boolean] = signal[Boolean] {
-	    (x: SignalSynt[Boolean]) => collectionSignal(x)(x).contains(elem)
+	def wrapHigherOrderFunction[B, C, F](hf: InternalType[A] => F => C)(f: F) = signal[C] {
+	    (x: SignalSynt[C]) => hf(collectionSignal(x)(x))(f)
 	}
-	//lazy val size = Signal(internalCollection()().size) //TODO: abstract these
+	
+	
+	val filter = wrapHigherOrderFunction(_.filter)_ andThen wrapSignal
+	
+	type CBF[B] = CanBuildFrom[InternalType[A], B, InternalType[B]]
+	def map[B](f: A => B)(implicit cbf: CBF[B]): ConcreteType[B] = 
+	    (wrapHigherOrderFunction(_.map[B, InternalType[B]])_ andThen wrapSignal)(f)
+	    
+	def flatMap[B](f: A => GenTraversableOnce[B])(implicit cbf: CBF[B]) = 
+	    (wrapHigherOrderFunction(_.flatMap[B, InternalType[B]])_ andThen wrapSignal)(f)
+	
+	def foldLeft[B](z: B)(f: (B, A) => B): Signal[B] = foldLeft(Signal(z))(f)
+	    
+	def foldLeft[B](z: Signal[B])(f: (B, A) => B): Signal[B] = signal[B] {
+	   (x: SignalSynt[B]) => collectionSignal(x)(x).foldLeft[B](z(x))(f)
+	}
+	
+	
+	def contains(elem: A): Signal[Boolean] = Signal(collectionSignal()().contains(elem))
+	lazy val size = signal[Int]((x: SignalSynt[Int]) => collectionSignal(x)(x).size) 
 }
--- src/test/collections/ReactiveSetSpec.scala
+++ src/test/collections/ReactiveSetSpec.scala
@@ -187,5 +187,86 @@ class ReactiveSetSpec extends FunSpec {
 	            assertResult(false)(filteredContains2())
 	        }
 	    }
+	    describe("'s map method") {
+	    	it("should return a fully reactive collection") {
+	    	    val f = fixture
+	            import f._
+	            
+	            val a = Var(3)
+	            collection += 1
+	            collection += a.toSignal
+	            
+	            val transformed = collection.map((_: Int) * 2)
+	            val transformedContains2 = transformed.contains(2)
+	            val transformedContains4 = transformed.contains(4)
+	            val transformedContains6 = transformed.contains(6)
+	            
+	            assertResult(true)(transformedContains2())
+	            assertResult(false)(transformedContains4())
+	            assertResult(true)(transformedContains6())
+	            
+	            a() = 2
+	            assertResult(true)(transformedContains2())
+	            assertResult(true)(transformedContains4())
+	            assertResult(false)(transformedContains6())
+	            
+	            
+	            transformed -= collection.size
+	            collection -= 2
+	            assertResult(true)(transformedContains2())
+	            assertResult(false)(transformedContains4())
+	            assertResult(false)(transformedContains6())
+	            
+	            collection += 10
+	            assertResult(false)(transformedContains2())
+	    	}
+	    }
+	    
+    	it("should have a functioning foldLeft method") { 
+    	    val f = fixture
+            import f._
+            
+            val a = Var(3)
+            val b = Var(88)
+            collection += 1
+            collection += a.toSignal
+            collection -= b.toSignal
+            
+            val sum = collection.foldLeft(0)(_+_)
+            
+            assertResult(4)(sum())
+            
+            a() = 2
+            assertResult(3)(sum())
+            
+            collection += 3
+            assertResult(6)(sum())
+            
+            b() = 1
+            assertResult(5)(sum())
+    	}
+    
+    	it("should have a functioning flatMap method") { 
+    	    val f = fixture
+            import f._
+            
+            val a = Var(3)
+            val b = Var(9)
+            collection += 1
+            collection += a.toSignal
+            collection -= b.toSignal
+            
+            val moments = collection.flatMap(x => List(x,x*x))
+            val momentContains1 = moments.contains(1)
+            val momentContains9 = moments.contains(9)
+            val momentContains11 = moments.contains(11)
+            
+            assertResult(true)(momentContains1())
+            assertResult(true)(momentContains9())
+            assertResult(false)(momentContains11())
+            
+            b() = 3
+            assertResult(false)(momentContains9())
+    	}
 	}
 }
