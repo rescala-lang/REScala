58d3cc0c96f3f09896ffcd4a0e5be3852283c2ca
pweisenburger <pascal.weisenburger@web.de>
2014-03-17 20:40:42 +0100

    replaced Scala Swing components and signal/event adapters with ReSwing library components

--- ReactiveScalaReader.Reactive/src/reader/gui/ReactiveSwing.scala
+++ ReactiveScalaReader.Reactive/src/reader/gui/ReactiveSwing.scala
@@ -1,59 +1,20 @@
 package reader.gui
 
-import scala.swing.Button
-import scala.swing.CheckBox
-import scala.swing.Label
-import scala.swing.ListView
-import scala.swing.event.ButtonClicked
-import scala.swing.event.SelectionChanged
-
 import macro.SignalMacro.{SignalM => Signal}
-import react.Signal
-import react.SignalSynt
-import react.Var
-import react.events.ImperativeEvent
-import react.events.Event
+import reswing.ReListView
+import reswing.ReSwingValue
 
-class ReButton(text: String) extends Button(text) {
-  val pressed = new ImperativeEvent[Button] //#EVT
-  reactions += { case ButtonClicked(_) => pressed(this) } //#HDL  
-}
-
-class ReCheckBox(text: String) extends CheckBox(text) {
-  private val selectedVar = Var(false) //#VAR
-  val checked: Signal[Boolean] = Signal { selectedVar() } //#SIG
+class ReListViewEx[A](
+    listData: ReSwingValue[Seq[A]],
+    visibleRowCount: ReSwingValue[Int] = ())
+  extends
+    ReListView[A](listData, visibleRowCount) {
   
-  val activated: Event[Boolean] = checked.changed && (v => v) //#EVT //#IF //#EF
-  val deactivated: Event[Boolean] = checked.changed && (! _) //#EVT //#IF //#EF
-  
-  reactions += { case _ => selectedVar() = selected }
-}
-
-class ReListView[A](s: Signal[Iterable[A]]) extends ListView[A] { // ? use of s
-  private val selectedItemVar = Var[Option[A]](None)  //#VAR
-  val selectedItem = Signal { selectedItemVar() }   //#SIG
-  
-  s.changed += { data => listData = data.toSeq } //#HDL //#IF
-  
-  private def getSelectedItem: Option[A] = {
-    val i = selection.leadIndex
-    if (i >= 0 && i < listData.size) Some(listData(i)) else None
+  val selectedIndex = selection.leadIndex snapshot selection.changed //#SIG //#IF
+  val selectedItem = Signal { //#SIG
+    if (selectedIndex() >= 0 && selectedIndex() < listData().size)
+      Some(listData()(selectedIndex()))
+    else
+      None
   }
-  
-  override def listData_=(items: Seq[A]) {
-    val selected = selection.indices.headOption
-    super.listData = items
-    for (index <- selected)
-      selectIndices(index)
-  }
-  
-  listenTo(selection)
-  
-  reactions += {
-    case SelectionChanged(_) => selectedItemVar() = getSelectedItem
-  }
-}
-
-class ReText[A](s: Signal[A]) extends Label {
-  s.changed += { v => text = v.toString } //#HDL //#IF
 }
