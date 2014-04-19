afe80a436d72e8f5dd24c4d28004bb68163a80e7
pweisenburger <pascal.weisenburger@web.de>
2014-04-19 19:52:43 +0200

    rewrite of the signal macro to get rid of the type checker workaround, hopefully this will solve some problems with using the macro like this one: https://github.com/guidosalva/examples/pull/4/files#r11724000

--- REScala/src/macro/SignalMacro.scala
+++ REScala/src/macro/SignalMacro.scala
@@ -4,53 +4,105 @@ import scala.collection.mutable.ListBuffer
 import scala.language.experimental.macros
 import scala.reflect.macros.Context
 
-import react.DepHolder
 import react.Signal
 import react.SignalSynt
+import react.Var
 
 object SignalMacro {
   
   def SignalM[A](expression: A): Signal[A] = macro SignalMacro[A]
   
-  def SignalMacro[A: c.WeakTypeTag](c: Context)(expression: c.Expr[A]): c.Expr[Signal[A]] = {
+  def SignalMacro[A: c.WeakTypeTag](c: Context)(expression: c.Expr[A]):
+      c.Expr[Signal[A]] = {
     import c.universe._
     
     
 //    val out = new java.io.FileWriter(
 //        "/home/pascal/Desktop/debugfile.txt", true)
     
+//    out.append(showRaw(expression.tree) + "\n\n\n\n")
 //    out.append(showRaw(reify { { val a = 0; a } }) + "\n\n\n\n")
     
 //    extracting sub trees with type Reactive[_]
-//    val extractedReactives = expression.tree.filter(t => t.tpe <:< typeOf[DepHolder])
+//    val extractedReactives = expression.tree filter {
+//      t => t.tpe <:< typeOf[Reactive]
+//    }
     
     
-    // the argument that is used by the SignalSynt class to assemble dynamic dependencies
+    // all symbols that are defined within the macro expression
+    val definedSymbols = (expression.tree collect {
+      case defTree: DefTree => defTree.symbol -> defTree
+    }).toMap
+    
+    // generate warning for some common cases where called functions are 
+    // either unnecessary (if free of side effects) or have side effects
+    expression.tree foreach {
+      case Block(stats, _) =>
+        stats foreach {
+          case stat @ Apply(_, _) 
+            if (stat exists {
+              case stat @ Apply(fun, _) =>
+                !(fun exists {
+                  case t @ Select(_, nme.CONSTRUCTOR) => true
+                  case tree: SymTree => definedSymbols contains tree.symbol
+                  case _ => false
+                })
+              case _ => false
+            }) =>
+            c.warning(stat.pos,
+                "Statement is either unnecessary or has side effects. " +
+                "Signal expressions should have no side effects.")
+          case _ =>
+        }
+      case _ =>
+    }
+    
+    // the argument that is used by the SignalSynt class to assemble dynamic
+    // dependencies
     // every Signal { ... } macro instance gets expanded into a SignalSynt
-    val signalSyntName = newTermName(c.fresh("s$"))
+    val signalSyntArgName = newTermName(c.fresh("s$"))
+    val signalSyntArgIdent = Ident(signalSyntArgName)
+    signalSyntArgIdent.tpe = weakTypeOf[SignalSynt[A]]
     
-    // the signal values that will be pulled out of the Signal expression
+    // the signal values that will be cut out of the Signal expression
     val signalValues = ListBuffer.empty[ValDef]
     
     object transformer extends Transformer {
-      override def transform(tree: Tree): Tree =
+      private def treeTypeNullWarning =
+        c.warning(c.enclosingPosition,
+            "internal warning: tree type was null, " + 
+            "this should not happen but the signal may still work")
+      
+      private def potentialReactiveConstructionWarning(pos: Position) =
+        c.warning(pos,
+            "expression should not be placed inside a signal expression " +
+            "since it potentially creates a new reactive every time the " +
+            "signal is evaluated which can lead to unintentional behavior")
+      
+      private def isReactive(tree: Tree) =
+        if (tree.tpe == null) { treeTypeNullWarning; false }
+        else tree.tpe <:< typeOf[Signal[_]] || tree.tpe <:< typeOf[Var[_]]
+      
+      override def transform(tree: Tree) =
         tree match {
-          // pass the SignalSynt argument to every dependency holder
+          // pass the SignalSynt argument to every reactive
           // to obtain dynamic dependencies
           //
           // for example, this step transforms
           //   Signal { a() + b() }
           // to
           //   SignalSynt { s => a(s) + b(s) }
-          case Apply(Select(depHolder, apply), List())
-              if depHolder.tpe <:< typeOf[DepHolder] && apply.decoded == "apply" =>
-            super.transform(Apply(Select(depHolder, apply), List(Ident(signalSyntName))))
+          case tree @ Apply(Select(reactive, apply), List())
+              if isReactive(reactive) && apply.decoded == "apply" =>
+            val reactiveApply = Select(reactive, newTermName("apply"))
+            reactiveApply.tpe = tree.tpe
+            Apply(super.transform(reactiveApply), List(signalSyntArgIdent))
           
-          // pull signal values out of the signal expression, that could potentially
-          // create a new signal object for every access
-          // it is assumed that such function are pure in the sense that they will
-          // create an equivalent signal for each call with the same arguments
-          // so the function value has to be calculated just once
+          // cut signal values out of the signal expression, that could
+          // potentially create a new signal object for every access
+          // it is assumed that such functions are pure in the sense that they
+          // will create an equivalent signal for each call with the same
+          // arguments so the function value has to be calculated just once
           //
           // for example, this step transforms
           //   Signal { event.count() }
@@ -58,55 +110,61 @@ object SignalMacro {
           //   Signal { s() }
           // and creates a signal value
           //   val s = event.count
-          case depHolder if depHolder.tpe != null && depHolder.tpe <:< typeOf[DepHolder] =>
-            depHolder match {
-              // only if the reactive value could be retrieved from function
-              // and the object which the function is called on is not a reactive value
-              case Apply(_, _) | Select(_, _)
-                  if !(depHolder exists {
-                        _ match {
-                          case Apply(Select(depHolder, apply), List())
-                            if depHolder.tpe <:< typeOf[DepHolder] && apply.decoded == "apply" => true
-                          case _ => false
-                        }
-                      }) =>
-                
-                // if the reactive value is defined in the very same class
-                // it is possible that it is not yet constructed
-                // so it is simple declared to be lazy-initialized
-                val modifiers = depHolder match {
-                  case Select(This(_), _) => Modifiers(Flag.LAZY)
-                  case _ => Modifiers()
-                }
-                val signalName = newTermName(c.fresh("s$"))
-                val signalDef = ValDef(modifiers, signalName, TypeTree(), depHolder)
-                
-//                // instead of using the type checker below, inspecting the
-//                // symbol and term positions would be a cleaner solution,
-//                // but the position does not always give us the range
-//                // (but only a single point) which is needed here
-//                val macroExpressionLocalIdentifier = depHolder match {
-//                  case Select(a @ Ident(_), _) =>
-//                    expression.tree.pos.includes(a.symbol.asTerm.pos)
-//                  case _ => false
-//                }
-                
-                // ensure that the signal value can be pulled out of the Signal expression
-                // by invoking the type-checker to see if it can stand on its own
-                // outside the signal expression
-                try {
-                  c typeCheck (c resetAllAttrs signalDef)
-                  signalValues += signalDef
-                  Ident(signalName)
-                }
-                catch {
-                  case _: Throwable =>
-                    super.transform(tree)
-                }
-              
-              case _ =>
-                super.transform(tree)
-            }
+          case reactive @ (Apply(_, _) | Select(_, _))
+              if isReactive(reactive) &&
+              // make sure that the expression e to be cut out
+              // - refers to a term that is not a val or var
+              //   or an accessor for a field
+              // - is not a reactive value resulting from a function that is
+              //   itself called on a reactive value
+              //   (so the expression does not contained "chained" reactives)
+              // - does not reference definitions that are defined within the
+              //   macro expression but not within e
+              //   (note that such a case can lead to unintentional behavior)
+              reactive.symbol.isTerm &&
+              !reactive.symbol.asTerm.isVal &&
+              !reactive.symbol.asTerm.isVar &&
+              !reactive.symbol.asTerm.isAccessor &&
+              !(reactive exists {
+                case Apply(Select(chainedReactive, apply), List())
+                    if isReactive(chainedReactive) && apply.decoded == "apply" =>
+                  def reactiveMethodObjectType(tree: Tree = reactive): Type =
+                    if (tree.symbol != reactive.symbol)
+                      tree.tpe
+                    else if (tree.children.nonEmpty)
+                      reactiveMethodObjectType(tree.children.head)
+                    else
+                      NoType
+                  
+                  // issue no warning if the reactive is retrieved from a container
+                  // determined by the generic type parameter
+                  reactiveMethodObjectType() match {
+                    case TypeRef(_, _, args) if !(args contains reactive.tpe) =>
+                      potentialReactiveConstructionWarning(reactive.pos)
+                    case _ =>
+                  }
+                  
+                  true
+                case tree: SymTree =>
+                  definedSymbols get tree.symbol match {
+                    case Some(defTree) if !(reactive exists { _ == defTree }) =>
+                      potentialReactiveConstructionWarning(reactive.pos)
+                      true
+                    case _ => false
+                  }
+                case _ => false
+              }) =>
+            
+            // create the signal definition to be cut out of the
+            // macro expression and its substitution variable
+            val signalName = newTermName(c.fresh("s$"))
+            
+            val signalDef = ValDef(Modifiers(), signalName, TypeTree(), reactive)
+            signalValues += signalDef
+            
+            val ident = Ident(signalName)
+            ident.tpe = reactive.tpe
+            ident
           
           case _ =>
             super.transform(tree)
@@ -118,7 +176,10 @@ object SignalMacro {
     // SignalSynt argument function
     val function =
       Function(
-        List(ValDef(Modifiers(), signalSyntName, TypeTree(weakTypeOf[SignalSynt[A]]), EmptyTree)),
+        List(
+          ValDef(
+            Modifiers(), signalSyntArgName,
+            TypeTree(weakTypeOf[SignalSynt[A]]), EmptyTree)),
         tree)
     
     // create SignalSynt object
@@ -136,9 +197,11 @@ object SignalMacro {
           List(TypeTree())),
         List(function))
     
-    // assemble the SignalSynt object and the signal values that are accessed by the object
-    // but were pulled out of the signal expression during the code transformation
-    val block = Typed(Block(signalValues.toList, body), TypeTree(weakTypeOf[Signal[A]]))
+    // assemble the SignalSynt object and the signal values that are accessed
+    // by the object, but were cut out of the signal expression during the code
+    // transformation
+    val block =
+      Typed(Block(signalValues.toList, body), TypeTree(weakTypeOf[Signal[A]]))
     
     
 //    out.append((c resetLocalAttrs block) + "\n\n")
