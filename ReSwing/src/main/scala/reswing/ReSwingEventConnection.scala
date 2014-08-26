57355c1bee4eb1243e828b9baacb114b16a25199
Pascal Weisenburger <pascal.weisenburger@web.de>
2014-08-26 19:26:48 +0200

    fix deprecation warnings for Scala 2.11

--- ReSwing/src/main/scala/reswing/ReSwingEventConnection.scala
+++ ReSwing/src/main/scala/reswing/ReSwingEventConnection.scala
@@ -10,58 +10,58 @@ import scala.swing.UIElement
 /**
  * Introduces methods to connect a [[ReSwingEvent]] to the corresponding
  * `Swing` reaction of the underlying `Swing` component.
- * 
+ *
  * Classes that extend this trait can use a declarative syntax to specify
  * the connections between a [[ReSwingEvent]] and the `Swing` event, e.g.:
- * 
+ *
  * {{{
  * abstract class ReWindow
  * extends ReSwingEventConnection {
  *   protected def peer: Window
- *   
+ *
  *   val windowActivated = ReSwingEvent using classOf[WindowActivated]
  * }
  * }}}
- * 
+ *
  * Also introduces methods to connect a [[react.events.Event]] to the
  * underlying `Swing` component that will execute a specific method whenever
  * the event fires:
- * 
+ *
  * {{{
  * abstract class ReUIElement(
  *   repaint: ReSwingEvent[Unit] = ())
  * extends ReSwingEventConnection {
  *   protected def peer: UIElement
- *   
+ *
  *   repaint using peer.repaint _
  * }
  * }}}
  */
 private[reswing] abstract trait ReSwingEventConnection {
   protected def peer: UIElement
-  
+
   final protected implicit class EventConnector[T] private[ReSwingEventConnection]
       (value: ReSwingEvent[T]) {
     def using(setter: T => Unit): ReSwingEvent[T] = {
       if (value.isInstanceOf[ReSwingEventIn[_]])
-        delayedInitEvents += { _ =>
+        delayedInitEvents += { () =>
           value += { v => inSyncEDT { setter(v) } }
         }
       value
     }
     def using(setter: () => Unit): ReSwingEvent[T] = {
       if (value.isInstanceOf[ReSwingEventIn[_]])
-        delayedInitEvents += { _ =>
+        delayedInitEvents += { () =>
           value += { _ => inSyncEDT { setter() } }
         }
       value
     }
   }
-  
+
   protected object ReSwingEvent {
     def using[T](reaction: Class[T]): ReSwingEvent[T] =
       using(peer, reaction)
-    
+
     def using[T](publisher: Publisher, reaction: Class[T]): ReSwingEvent[T]  = {
       val event = new ReSwingEventOut[T]({ event =>
         inSyncEDT {
@@ -75,10 +75,10 @@ private[reswing] abstract trait ReSwingEventConnection {
       event
     }
   }
-  
+
   private val reactor = new Reactor { }
-  private val delayedInitEvents = ListBuffer.empty[Unit => Unit]
-  
+  private val delayedInitEvents = ListBuffer.empty[() => Unit]
+
   peer.peer.addHierarchyListener(new HierarchyListener {
     def hierarchyChanged(e: HierarchyEvent) =
       if ((e.getChangeFlags & HierarchyEvent.DISPLAYABILITY_CHANGED) != 0) {
@@ -86,7 +86,7 @@ private[reswing] abstract trait ReSwingEventConnection {
         peer.peer.removeHierarchyListener(this)
       }
   })
-  
+
   protected def initReSwingEventConnection {
     for (init <- delayedInitEvents)
       init()
--- ReSwing/src/main/scala/reswing/ReSwingValueConnection.scala
+++ ReSwing/src/main/scala/reswing/ReSwingValueConnection.scala
@@ -14,55 +14,55 @@ import scala.swing.event.Event
 /**
  * Introduces methods to connect a ''Reactive Swing'' value ([[ReSwingValue]])
  * to the corresponding properties and events of the underlying `Swing` component.
- * 
+ *
  * For each `ReSwing` value, you can specify
  *  - a `Swing` getter
  *  - a `Swing` setter optionally
  *  - `Swing` events that indicate a value change which should be represented
  *    in the reactive value
- * 
+ *
  * Classes that extend this trait can use a declarative syntax to specify
  * the connections between a [[ReSwingValue]] and the `Swing` properties and
  * events, e.g.:
- * 
+ *
  * {{{
  * abstract class ReUIElement(
  *   val preferredSize: ReSwingValue[Dimension] = ())
  * extends ReSwingValueConnection {
  *   protected def peer: UIElement
- *   
+ *
  *   preferredSize using (peer.preferredSize _, peer.preferredSize_= _, "preferredSize")
  * }
  * }}}
- * 
+ *
  * If a value should be read-only, the following syntax can be used:
- * 
+ *
  * {{{
  * abstract class ReComponent
  * extends ReSwingValueConnection {
  *   protected def peer: Component
- *   
+ *
  *   val hasFocus = ReSwingValue using (peer.hasFocus _, classOf[FocusGained], classOf[FocusLost])
  * }
  * }}}
  */
 private[reswing] abstract trait ReSwingValueConnection {
   protected def peer: UIElement
-  
+
   protected implicit def toReSwingValueConnector[T](signal: ReSwingValue[T]) =
     new ReSwingValueConnector(signal)
-  
+
   protected implicit def toChangingProperty(name: String) =
     Left(name): ChangingProperty
-  
+
   protected implicit def toChangingProperty(reaction: (Publisher, Class[_])) =
     Right(reaction): ChangingProperty
-  
+
   protected implicit def toChangingProperty(reaction: Class[_]) =
     Right((peer, reaction)): ChangingProperty
-  
+
   protected def ReSwingValue[T] = (): ReSwingValue[T]
-  
+
   /**
    * Represents a `Swing` property that is used to react on value changes
    * to update the reactive value accordingly.
@@ -72,10 +72,10 @@ private[reswing] abstract trait ReSwingValueConnection {
    * event-publishing component and the event type.
    */
   protected type ChangingProperty = Either[String, (Publisher, Class[_])]
-  
+
   final protected class ReSwingValueConnector[T] private[ReSwingValueConnection]
       (value: ReSwingValue[T]) {
-    
+
     /**
      * Connects the [[ReSwingValue]] object to the given `Swing` getter.
      * The value will be updated when any of the given properties change.
@@ -83,7 +83,7 @@ private[reswing] abstract trait ReSwingValueConnection {
      */
     def using(getter: () => T, names: ChangingProperty*): ReSwingValue[T] =
       using(getter, None, names)
-    
+
     /**
      * Connects the [[ReSwingValue]] object to the given `Swing` getter and setter.
      * The value will be updated when any of the given properties change and
@@ -92,46 +92,46 @@ private[reswing] abstract trait ReSwingValueConnection {
      */
     def using(getter: () => T, setter: T => Unit, names: ChangingProperty*): ReSwingValue[T] =
       using(getter, Some(setter), names)
-    
+
     private def using(getter: () => T, setter: Option[T => Unit],
         names: Seq[ChangingProperty]): ReSwingValue[T] = {
       if (value != null)
         inSyncEDT {
           value() = getter()
           value initLazily { _ => inSyncEDT { initReSwingValueConnection } }
-          
-          delayedInitValues += { _ =>
+
+          delayedInitValues += { () =>
             if (setter.isDefined) {
               val set = setter.get
               value use { v => inSyncEDT { set(v) } }
-              
+
               if (value.fixed)
                 for (name <- names)
                   name match {
                     case Left(name) =>
-                      changingProperties getOrElseUpdate (name, ListBuffer()) += { _ =>
+                      changingProperties getOrElseUpdate (name, ListBuffer()) += { () =>
                         if (getter() != value.get)
                           Swing onEDT { if (getter() != value.get) set(value.get) } }
-                    
+
                     case Right((publisher, reaction)) =>
                       reactor listenTo publisher
-                      changingReactions getOrElseUpdate (reaction, ListBuffer()) += { _ =>
+                      changingReactions getOrElseUpdate (reaction, ListBuffer()) += { () =>
                         if (getter() != value.get)
                           Swing onEDT { if (getter() != value.get) set(value.get) } }
                   }
             }
-            
+
             if (!value.fixed) {
               value() = getter()
               for (name <- names)
                 name match {
                   case Left(name) =>
-                    changingProperties getOrElseUpdate (name, ListBuffer()) += { _ =>
+                    changingProperties getOrElseUpdate (name, ListBuffer()) += { () =>
                       value() = getter() }
-                  
+
                   case Right((publisher, reaction)) =>
                     reactor listenTo publisher
-                    changingReactions.getOrElseUpdate(reaction, ListBuffer()) += { _ =>
+                    changingReactions.getOrElseUpdate(reaction, ListBuffer()) += { () =>
                       value() = getter() }
                 }
             }
@@ -139,7 +139,7 @@ private[reswing] abstract trait ReSwingValueConnection {
         }
       value
     }
-    
+
     /**
      * Forces the property given by `name` the keep the given value using the
      * given setter, if the [[ReSwingValue]] is set to hold a fixed value,
@@ -149,24 +149,24 @@ private[reswing] abstract trait ReSwingValueConnection {
       if (value != null && value.fixed)
         inSyncEDT {
           setter(forcedValue)
-          enforcedProperties += name -> { _ => Swing onEDT { setter(forcedValue) } }
+          enforcedProperties += name -> { () => Swing onEDT { setter(forcedValue) } }
         }
       value
     }
   }
-  
-  private val delayedInitValues = ListBuffer.empty[Unit => Unit]
-  private val changingReactions = Map.empty[Class[_], ListBuffer[Unit => Unit]]
-  private val changingProperties = Map.empty[String, ListBuffer[Unit => Unit]]
-  private val enforcedProperties = Map.empty[String, Unit => Unit]
-  
+
+  private val delayedInitValues = ListBuffer.empty[() => Unit]
+  private val changingReactions = Map.empty[Class[_], ListBuffer[() => Unit]]
+  private val changingProperties = Map.empty[String, ListBuffer[() => Unit]]
+  private val enforcedProperties = Map.empty[String, () => Unit]
+
   private val reactor = new Reactor {
     reactions += { case e: Event =>
       for (signals <- changingReactions get e.getClass; signal <- signals)
         signal()
     }
   }
-  
+
   peer.peer.addPropertyChangeListener(new java.beans.PropertyChangeListener {
     def propertyChange(e: java.beans.PropertyChangeEvent) {
       enforcedProperties get e.getPropertyName match {
@@ -178,7 +178,7 @@ private[reswing] abstract trait ReSwingValueConnection {
       }
     }
   })
-  
+
   peer.peer.addHierarchyListener(new HierarchyListener {
     def hierarchyChanged(e: HierarchyEvent) =
       if ((e.getChangeFlags & HierarchyEvent.DISPLAYABILITY_CHANGED) != 0) {
@@ -186,7 +186,7 @@ private[reswing] abstract trait ReSwingValueConnection {
         peer.peer.removeHierarchyListener(this)
       }
   })
-  
+
   protected def initReSwingValueConnection {
     for (init <- delayedInitValues)
       init()
