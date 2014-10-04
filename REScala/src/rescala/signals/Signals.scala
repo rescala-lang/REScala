6ed9897fb2e158673073b9a28a750194000ce353
ragnar <ragnar@rmgk.de>
2014-10-04 18:28:45 +0200

    parameter name cleanup, import optimise

--- REScala/src/rescala/signals/Signals.scala
+++ REScala/src/rescala/signals/Signals.scala
@@ -1,7 +1,6 @@
 package rescala.signals
 
 import rescala.events.Event
-import rescala.propagation.Pulse.{Diff, NoChange}
 import rescala.propagation._
 
 object Signals {
@@ -36,13 +35,13 @@ object Signals {
     }(_)
   }
 
-  def mapping[T](dependencies: Reactive*)(expr: Turn => T)(implicit maybe: MaybeTurn): Signal[T] = maybe { initialTurn =>
-    makeStatic(dependencies.toSet, expr(initialTurn))((turn, _) => expr(turn))(initialTurn)
+  def mapping[T](dependencies: Reactive*)(fun: Turn => T)(implicit maybe: MaybeTurn): Signal[T] = maybe { initialTurn =>
+    makeStatic(dependencies.toSet, fun(initialTurn))((turn, _) => fun(turn))(initialTurn)
   }
 
-  def lift[A1, A2, B](s1: Stateful[A1], s2: Stateful[A2])(expr: (A1, A2) => B)(implicit maybe: MaybeTurn): Signal[B] =
-    mapping(s1, s2)(t => expr(s1.getValue(t), s2.getValue(t)))
-  def lift[A1, A2, A3, B](s1: Stateful[A1], s2: Stateful[A2], s3: Stateful[A3])(expr: (A1, A2, A3) => B)(implicit maybe: MaybeTurn): Signal[B] =
-    mapping(s1, s2)(t => expr(s1.getValue(t), s2.getValue(t), s3.getValue(t)))
+  def lift[A1, A2, B](s1: Stateful[A1], s2: Stateful[A2])(fun: (A1, A2) => B)(implicit maybe: MaybeTurn): Signal[B] =
+    mapping(s1, s2)(t => fun(s1.getValue(t), s2.getValue(t)))
+  def lift[A1, A2, A3, B](s1: Stateful[A1], s2: Stateful[A2], s3: Stateful[A3])(fun: (A1, A2, A3) => B)(implicit maybe: MaybeTurn): Signal[B] =
+    mapping(s1, s2)(t => fun(s1.getValue(t), s2.getValue(t), s3.getValue(t)))
 
 }
