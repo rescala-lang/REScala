814669e8ff89f3bcaf7bea0c4417b3b0d4b562b0
ragnar <ragnar@rmgk.de>
2014-11-12 17:56:13 +0100

    add turn lock

--- REScala/src/rescala/propagation/TurnLock.scala
+++ REScala/src/rescala/propagation/TurnLock.scala
@@ -1,5 +1,21 @@
 package rescala.propagation
 
-class TurnLock {
+import java.util.concurrent.locks.ReentrantLock
 
+class TurnLock {
+  val relock: ReentrantLock = new ReentrantLock()
+  var owner: Option[Turn] = None
+  def lock()(implicit turn: Turn): Unit = {
+    relock.lock()
+    owner = Some(turn)
+  }
+  def tryLock()(implicit turn: Turn): Boolean = {
+    val res = relock.tryLock()
+    if (res) owner = Some(turn)
+    res
+  }
+  def unlock(): Unit = {
+    owner = None
+    relock.unlock()
+  }
 }
