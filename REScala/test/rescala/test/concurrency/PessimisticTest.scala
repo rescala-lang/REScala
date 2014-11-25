95f8bd3f9173868bbb94102830d7076de14d3f09
ragnar <ragnar@rmgk.de>
2014-11-25 22:18:30 +0100

    add some tests that fails at testing

--- REScala/test/rescala/test/concurrency/PessimisticTest.scala
+++ REScala/test/rescala/test/concurrency/PessimisticTest.scala
@@ -56,8 +56,8 @@ class PessimisticTest extends AssertionsForJUnit {
     val s21 = v2.map{ v => if (v) {latch.countDown(); latch.await(1, TimeUnit.SECONDS)} else false}
     val s22 = Signals.dynamic(s21){ t => if (s21(t)) v1(t) else false }
     var results = List[Boolean]()
-    s12.changed.+={v => println(v); results ::= v}
-    s22.changed.+={v => println(v); results ::= v}
+    s12.changed.+={v => results ::= v}
+    s22.changed.+={v => results ::= v}
 
 
     assert(results === Nil)
@@ -68,6 +68,23 @@ class PessimisticTest extends AssertionsForJUnit {
 
     assert(results === List(true))
     assert(latch.getCount === 0)
+  }
+
+  @Test def addAndRemoveDependencyInOneTurn(): Unit = {
+    val b0 = Var(false)
+    val b2 = b0.map(identity).map(!_)
+    val i0 = Var(11)
+    var reeval = 0
+    val i1_3 = Signals.dynamic(b0) { t => reeval += 1; if (b0(t) && b2(t)) i0(t) else 42}
+    //TODO: so how do we test, that b2 gets added and removed as a dependency?
+
+    assert(i1_3.now === 42)
+    assert(reeval === 1)
+
+    b0.set(true)
+    assert(i1_3.now === 42)
+    assert(reeval === 3)
+
   }
 
 }
