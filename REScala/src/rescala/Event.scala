a11f99f79af9a1a782268b66a84bf052ad499821
ragnar <ragnar@rmgk.de>
2014-12-09 18:23:03 +0100

    mark all event and signal interface methods as final

--- REScala/src/rescala/Event.scala
+++ REScala/src/rescala/Event.scala
@@ -9,99 +9,99 @@ import scala.collection.immutable.Queue
 trait Event[+T] extends Pulsing[T] {
 
   /** add an observer */
-  def +=(react: T => Unit)(implicit maybe: Ticket): Observe = observe(react)(maybe)
-  def observe(react: T => Unit)(implicit maybe: Ticket): Observe = Observe(this)(react)
+  final def +=(react: T => Unit)(implicit maybe: Ticket): Observe = observe(react)(maybe)
+  final def observe(react: T => Unit)(implicit maybe: Ticket): Observe = Observe(this)(react)
 
 
   /**
    * Events disjunction.
    */
-  def ||[U >: T](other: Event[U])(implicit maybe: Ticket): Event[U] = Events.or(this, other)
+  final def ||[U >: T](other: Event[U])(implicit maybe: Ticket): Event[U] = Events.or(this, other)
 
   /**
    * Event filtered with a predicate
    */
-  def &&(pred: T => Boolean)(implicit maybe: Ticket): Event[T] = Events.filter(this)(pred)
-  def filter(pred: T => Boolean)(implicit maybe: Ticket): Event[T] = &&(pred)
+  final def &&(pred: T => Boolean)(implicit maybe: Ticket): Event[T] = Events.filter(this)(pred)
+  final def filter(pred: T => Boolean)(implicit maybe: Ticket): Event[T] = &&(pred)
 
   /**
    * Event is triggered except if the other one is triggered
    */
-  def \[U](other: Event[U])(implicit maybe: Ticket): Event[T] = Events.except(this, other)
+  final def \[U](other: Event[U])(implicit maybe: Ticket): Event[T] = Events.except(this, other)
 
   /**
    * Events conjunction
    */
-  def and[U, R](other: Event[U], merge: (T, U) => R)(implicit maybe: Ticket): Event[R] = Events.and(this, other, merge)
+  final def and[U, R](other: Event[U], merge: (T, U) => R)(implicit maybe: Ticket): Event[R] = Events.and(this, other, merge)
 
   /**
    * Event conjunction with a merge method creating a tuple of both event parameters
    */
-  def &&[U](other: Event[U])(implicit maybe: Ticket): Event[(T, U)] = Events.and(this, other, (p1: T, p2: U) => (p1, p2))
+  final def &&[U](other: Event[U])(implicit maybe: Ticket): Event[(T, U)] = Events.and(this, other, (p1: T, p2: U) => (p1, p2))
 
   /**
    * Transform the event parameter
    */
-  def map[U](mapping: T => U)(implicit maybe: Ticket): Event[U] = Events.map(this)(mapping)
+  final def map[U](mapping: T => U)(implicit maybe: Ticket): Event[U] = Events.map(this)(mapping)
 
   /**
    * Drop the event parameter; equivalent to map((_: Any) => ())
    */
-  def dropParam(implicit maybe: Ticket): Event[Unit] = Events.map(this)(_ => ())
+  final def dropParam(implicit maybe: Ticket): Event[Unit] = Events.map(this)(_ => ())
 
 
   /** folds events with a given fold function to create a Signal */
-  def fold[A](init: A)(fold: (A, T) => A)(implicit maybe: Ticket): Signal[A] = Signals.fold(this, init)(fold)
+  final def fold[A](init: A)(fold: (A, T) => A)(implicit maybe: Ticket): Signal[A] = Signals.fold(this, init)(fold)
 
   /** Iterates a value on the occurrence of the event. */
-  def iterate[A](init: A)(f: A => A)(implicit maybe: Ticket): Signal[A] = fold(init)((acc, _) => f(acc))
+  final def iterate[A](init: A)(f: A => A)(implicit maybe: Ticket): Signal[A] = fold(init)((acc, _) => f(acc))
 
   /**
    * Counts the occurrences of the event. Starts from 0, when the event has never been
    * fired yet. The argument of the event is simply discarded.
    */
-  def count()(implicit maybe: Ticket): Signal[Int] = fold(0)((acc, _) => acc + 1)
+  final def count()(implicit maybe: Ticket): Signal[Int] = fold(0)((acc, _) => acc + 1)
 
   /**
    * Calls f on each occurrence of event e, setting the Signal to the generated value.
    * The initial signal is obtained by f(init)
    */
-  def set[B >: T, A](init: B)(f: (B => A))(implicit maybe: Ticket): Signal[A] = fold(f(init))((_, v) => f(v))
+  final def set[B >: T, A](init: B)(f: (B => A))(implicit maybe: Ticket): Signal[A] = fold(f(init))((_, v) => f(v))
 
   /** returns a signal holding the latest value of the event. */
-  def latest[S >: T](init: S)(implicit maybe: Ticket): Signal[S] = fold(init)((_, v) => v)
+  final def latest[S >: T](init: S)(implicit maybe: Ticket): Signal[S] = fold(init)((_, v) => v)
 
   /** Holds the latest value of an event as an Option, None before the first event occured */
-  def latestOption()(implicit maybe: Ticket): Signal[Option[T]] = fold(None: Option[T]) { (_, v) => Some(v) }
+  final def latestOption()(implicit maybe: Ticket): Signal[Option[T]] = fold(None: Option[T]) { (_, v) => Some(v) }
 
   /** calls factory on each occurrence of event e, resetting the Signal to a newly generated one */
-  def reset[S >: T, A](init: S)(factory: S => Signal[A])(implicit maybe: Ticket): Signal[A] = set(init)(factory).flatten()
+  final def reset[S >: T, A](init: S)(factory: S => Signal[A])(implicit maybe: Ticket): Signal[A] = set(init)(factory).flatten()
 
   /**
    * Returns a signal which holds the last n events in a list. At the beginning the
    * list increases in size up to when n values are available
    */
-  def last(n: Int)(implicit maybe: Ticket): Signal[LinearSeq[T]] =
+  final def last(n: Int)(implicit maybe: Ticket): Signal[LinearSeq[T]] =
     fold(Queue[T]()) { (queue: Queue[T], v: T) =>
       if (queue.length >= n) queue.tail.enqueue(v) else queue.enqueue(v)
     }
 
   /** collects events resulting in a variable holding a list of all values. */
-  def list()(implicit maybe: Ticket): Signal[List[T]] = fold(List[T]())((acc, v) => v :: acc)
+  final def list()(implicit maybe: Ticket): Signal[List[T]] = fold(List[T]())((acc, v) => v :: acc)
 
   /** Switch back and forth between two signals on occurrence of event e */
-  def toggle[A](a: Signal[A], b: Signal[A])(implicit maybe: Ticket): Signal[A] = maybe { implicit turn =>
+  final def toggle[A](a: Signal[A], b: Signal[A])(implicit maybe: Ticket): Signal[A] = maybe { implicit turn =>
     val switched: Signal[Boolean] = iterate(false) { !_ }
     Signals.dynamic(switched, a, b) { s => if (switched(s)) b(s) else a(s) }
   }
 
   /** Return a Signal that is updated only when e fires, and has the value of the signal s */
-  def snapshot[A](s: Signal[A])(implicit maybe: Ticket): Signal[A] = maybe { turn =>
+  final def snapshot[A](s: Signal[A])(implicit maybe: Ticket): Signal[A] = maybe { turn =>
     Signals.makeStatic(Set(this, s), s.get(turn))((t, current) => this.pulse(t).fold(current, _ => s.get(t)))(turn)
   }
 
   /** Switch to a new Signal once, on the occurrence of event e. */
-  def switchOnce[A](original: Signal[A], newSignal: Signal[A])(implicit maybe: Ticket): Signal[A] = maybe { implicit turn =>
+  final def switchOnce[A](original: Signal[A], newSignal: Signal[A])(implicit maybe: Ticket): Signal[A] = maybe { implicit turn =>
     val latest = latestOption
     Signals.dynamic(latest, original, newSignal) { t =>
       latest(t) match {
@@ -116,7 +116,7 @@ trait Event[+T] extends Pulsing[T] {
    * return value is set to the original signal. When the event fires,
    * the result is a constant signal whose value is the value of the event.
    */
-  def switchTo[S >: T](original: Signal[S])(implicit maybe: Ticket): Signal[S] = {
+  final def switchTo[S >: T](original: Signal[S])(implicit maybe: Ticket): Signal[S] = {
     val latest = latestOption
     Signals.dynamic(latest, original) { s =>
       latest(s) match {
@@ -127,7 +127,7 @@ trait Event[+T] extends Pulsing[T] {
   }
 
   /** Like latest, but delays the value of the resulting signal by n occurrences */
-  def delay[S >: T](init: S, n: Int)(implicit maybe: Ticket): Signal[S] = {
+  final def delay[S >: T](init: S, n: Int)(implicit maybe: Ticket): Signal[S] = {
     val history: Signal[LinearSeq[T]] = last(n + 1)
     Signals.mapping(history) { s =>
       val h = history(s)
--- REScala/src/rescala/Signal.scala
+++ REScala/src/rescala/Signal.scala
@@ -7,45 +7,45 @@ import rescala.graph.Stateful
 trait Signal[+A] extends Stateful[A] {
 
   /** add an observer */
-  def observe(react: A => Unit)(implicit maybe: Ticket): Observe = Observe(this)(react)
+  final def observe(react: A => Unit)(implicit maybe: Ticket): Observe = Observe(this)(react)
 
   /** Return a Signal with f applied to the value */
-  def map[B](f: A => B)(implicit maybe: Ticket): Signal[B] = Signals.mapping(this) { turn => f(get(turn)) }
+  final def map[B](f: A => B)(implicit maybe: Ticket): Signal[B] = Signals.mapping(this) { turn => f(get(turn)) }
 
   /** flatten the inner signal */
-  def flatten[B]()(implicit ev: A <:< Signal[B], maybe: Ticket) = Signals.dynamic(this) { s => this(s)(s) }
+  final def flatten[B]()(implicit ev: A <:< Signal[B], maybe: Ticket) = Signals.dynamic(this) { s => this(s)(s) }
 
   /** Return a Signal that gets updated only when e fires, and has the value of this Signal */
-  def snapshot(e: Event[_])(implicit maybe: Ticket): Signal[A] = e.snapshot(this)
+  final def snapshot(e: Event[_])(implicit maybe: Ticket): Signal[A] = e.snapshot(this)
 
   /** Switch to (and keep) event value on occurrence of e */
-  def switchTo[U >: A](e: Event[U])(implicit maybe: Ticket): Signal[U] = e.switchTo(this)
+  final def switchTo[U >: A](e: Event[U])(implicit maybe: Ticket): Signal[U] = e.switchTo(this)
 
   /** Switch to (and keep) event value on occurrence of e */
-  def switchOnce[V >: A](e: Event[_])(newSignal: Signal[V])(implicit maybe: Ticket): Signal[V] = e.switchOnce(this, newSignal)
+  final def switchOnce[V >: A](e: Event[_])(newSignal: Signal[V])(implicit maybe: Ticket): Signal[V] = e.switchOnce(this, newSignal)
 
   /** Switch back and forth between this and the other Signal on occurrence of event e */
-  def toggle[V >: A](e: Event[_])(other: Signal[V])(implicit maybe: Ticket): Signal[V] = e.toggle(this, other)
+  final def toggle[V >: A](e: Event[_])(other: Signal[V])(implicit maybe: Ticket): Signal[V] = e.toggle(this, other)
 
   /** Delays this signal by n occurrences */
-  def delay(n: Int)(implicit maybe: Ticket): Signal[A] = maybe { implicit turn => changed.delay(get, n) }
+  final def delay(n: Int)(implicit maybe: Ticket): Signal[A] = maybe { implicit turn => changed.delay(get, n) }
 
   /** Unwraps a Signal[Event[E]] to an Event[E] */
-  def unwrap[E](implicit evidence: A <:< Event[E], maybe: Ticket): Event[E] = Events.wrapped(this.map(evidence))
+  final def unwrap[E](implicit evidence: A <:< Event[E], maybe: Ticket): Event[E] = Events.wrapped(this.map(evidence))
 
   /**
    * Create an event that fires every time the signal changes. It fires the tuple
    * (oldVal, newVal) for the signal. The first tuple is (null, newVal)
    */
-  def change(implicit maybe: Ticket): Event[(A, A)] = Events.change(this)
+  final def change(implicit maybe: Ticket): Event[(A, A)] = Events.change(this)
 
   /**
    * Create an event that fires every time the signal changes. The value associated
    * to the event is the new value of the signal
    */
-  def changed(implicit maybe: Ticket): Event[A] = change.map(_._2)
+  final def changed(implicit maybe: Ticket): Event[A] = change.map(_._2)
 
   /** Convenience function filtering to events which change this reactive to value */
-  def changedTo[V](value: V)(implicit maybe: Ticket): Event[Unit] = (changed && { _ == value }).dropParam
+  final def changedTo[V](value: V)(implicit maybe: Ticket): Event[Unit] = (changed && { _ == value }).dropParam
 
 }
