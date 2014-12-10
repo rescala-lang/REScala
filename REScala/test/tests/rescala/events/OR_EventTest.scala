d04cd1a6bd89084e19e8e79afc0ae398a91502fb
ragnar <ragnar@rmgk.de>
2014-12-10 09:34:04 +0100

    prettify OR_EventTest observers

--- REScala/test/tests/rescala/events/OR_EventTest.scala
+++ REScala/test/tests/rescala/events/OR_EventTest.scala
@@ -15,7 +15,7 @@ class OR_EventTest extends AssertionsForJUnit with MockitoSugar {
     val e1 = Evt[Int]()
     val e2 = Evt[Int]()
     val e1_OR_e2 = e1 || e2
-    e1_OR_e2 += ((x: Int) => { test += 1 })
+    e1_OR_e2 += { _ => test += 1 }
 
     e1(10)
     e2(10)
@@ -27,13 +27,13 @@ class OR_EventTest extends AssertionsForJUnit with MockitoSugar {
 
     var test = 0
     val e1 = Evt[Int]()
-    val e2 = e1 map ((x: Int) => x * 2)
-    val e3 = e1 map ((x: Int) => x * 2)
+    val e2 = e1 map (_ * 2)
+    val e3 = e1 map (_ * 2)
     val e2_OR_e3 = e2 || e3
-    e1 += ((x: Int) => { test += 1 })
-    e2 += ((x: Int) => { test += 1 })
-    e3 += ((x: Int) => { test += 1 })
-    e2_OR_e3 += ((x: Int) => { test += 1 })
+    e1 += { _ => test += 1 }
+    e2 += { _ => test += 1 }
+    e3 += { _ => test += 1 }
+    e2_OR_e3 += { _ => test += 1 }
 
     e1(10)
     assert(test == 4)
