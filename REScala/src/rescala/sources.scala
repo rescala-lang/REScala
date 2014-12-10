7c5195ca6338dc9190e333849e1def5b0fe31671
ragnar <ragnar@rmgk.de>
2014-12-10 12:15:28 +0100

    add common source interface

--- REScala/src/rescala/sources.scala
+++ REScala/src/rescala/sources.scala
@@ -3,19 +3,23 @@ package rescala
 import rescala.turns.{Turn, Engine}
 import rescala.graph.{EvaluationResult, Pulse}
 
+sealed trait Source[T] {
+  def admit(value: T)(implicit turn: Turn): Unit
+}
 
 /**
  * An implementation of an imperative event
  */
-final class Evt[T]() extends Event[T] {
+final class Evt[T]() extends Event[T] with Source[T] {
 
   /** Trigger the event */
-  def apply(v: T)(implicit fac: Engine): Unit = fac.startNew { turn =>
+  def apply(value: T)(implicit fac: Engine): Unit = fac.startNew { admit(value)(_) }
+
+  def admit(value: T)(implicit turn: Turn): Unit =
     turn.admit(this) {
-      pulses.set(Pulse.change(v))(turn)
+      pulses.set(Pulse.change(value))(turn)
       true
     }
-  }
 
   override protected[rescala] def reevaluate()(implicit turn: Turn): EvaluationResult =
     EvaluationResult.Static(changed = true)
@@ -28,17 +32,15 @@ object Evt {
 
 
 /** A root Reactive value without dependencies which can be set */
-final class Var[T](initval: T) extends Signal[T] {
+final class Var[T](initval: T) extends Signal[T] with Source[T] {
   pulses.current = Pulse.unchanged(initval)
 
-  def update(newValue: T)(implicit fac: Engine): Unit = set(newValue)
-  def set(newValue: T)(implicit fac: Engine): Unit = fac.startNew { turn =>
-    admit(newValue)(turn)
-  }
+  def update(value: T)(implicit fac: Engine): Unit = set(value)
+  def set(value: T)(implicit fac: Engine): Unit = fac.startNew { admit(value)(_) }
 
-  def admit(newValue: T)(implicit turn: Turn): Unit =
+  def admit(value: T)(implicit turn: Turn): Unit =
     turn.admit(this) {
-      val p = Pulse.diff(newValue, get)
+      val p = Pulse.diff(value, get)
       if (p.isChange) {
         pulses.set(p)
         true
