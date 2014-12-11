2ebd0d197d5bb86da15ddc7d03bc35cd30bc5e9e
ragnar <ragnar@rmgk.de>
2014-12-11 22:22:56 +0100

    be sure that a lock is valid in both directions

--- REScala/src/rescala/synchronization/SyncUtil.scala
+++ REScala/src/rescala/synchronization/SyncUtil.scala
@@ -1,6 +1,5 @@
 package rescala.synchronization
 
-import java.util.concurrent.ConcurrentHashMap
 import java.util.concurrent.atomic.AtomicInteger
 
 import scala.annotation.tailrec
@@ -10,17 +9,28 @@ object SyncUtil {
   /** locks the given locks in a global deterministic order */
   @tailrec
   def lockLanes[R](lo: Key*)(f: => R): R = {
+
+    sealed trait LaneResult
+    object Further extends LaneResult
+    object Retry extends LaneResult
+    case class Good(r: R) extends LaneResult
+
+
     val heads = lo.map(laneHead)
 
     heads.sortBy(_.id).foreach(_.keyLock.lock())
     (try {
       val afterHeads = heads.map(laneHead)
-      if (afterHeads == heads) Some(f)
-      else None
+      if (afterHeads == heads) {
+        if (heads zip lo forall (controls _).tupled) Good(f)
+        else Retry
+      }
+      else Further
     }
     finally heads.foreach(_.keyLock.unlock())) match {
-      case None => lockLanes(heads: _*)(f)
-      case Some(r) => r
+      case Further => lockLanes(heads: _*)(f)
+      case Retry => lockLanes(lo: _*)(f)
+      case Good(r) => r
     }
   }
 
@@ -29,6 +39,14 @@ object SyncUtil {
     case None => k
     case Some(p) => laneHead(p)
   }
+
+  @tailrec
+  def controls(pointer: Key, target: Key): Boolean =
+    if (pointer == target) true
+    else pointer.subsequent match {
+      case None => false
+      case Some(next) => controls(next, target)
+    }
 
 
   val counter = new AtomicInteger(0)
