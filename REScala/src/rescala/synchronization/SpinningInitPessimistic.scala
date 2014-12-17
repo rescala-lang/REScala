2aa90ad9a70b292e5027ca37b04975fc3b03b652
ragnar <ragnar@rmgk.de>
2014-12-17 19:21:17 +0100

    clean spinningInit

--- REScala/src/rescala/synchronization/SpinningInitPessimistic.scala
+++ REScala/src/rescala/synchronization/SpinningInitPessimistic.scala
@@ -1,18 +1,14 @@
 package rescala.synchronization
 
 import rescala.graph.Reactive
-import rescala.propagation.{TurnImpl, LevelQueue}
-import rescala.turns.{Engines, Turn, Engine}
-
-import scala.collection.SortedSet
+import rescala.turns.{Engine, Engines, Turn}
 
 class SpinningInitPessimistic extends Pessimistic {
   /** returns the engine of this turn */
   override val engine: Engine[Turn] = Engines.spinningInit
 
   override def lockPhase(initialWrites: List[Reactive]): Unit = SyncUtil.lockReachable(initialWrites, acquireWrite )
-
-
+  
   def acquireWrite(reactive: Reactive): Boolean =
     if (reactive.lock.tryLock(key) eq key) true
     else {
