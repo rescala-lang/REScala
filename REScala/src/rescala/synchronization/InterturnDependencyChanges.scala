0b53010bae481fa5cf4ec974e37b3ada87edd687
ragnar <ragnar@rmgk.de>
2014-12-17 18:42:55 +0100

    move InterturnDependencyChanges into own trait

---
+++ REScala/src/rescala/synchronization/InterturnDependencyChanges.scala
@@ -0,0 +1,30 @@
+package rescala.synchronization
+
+import rescala.graph.Reactive
+import rescala.turns.Turn
+
+trait InterturnDependencyChanges extends Turn {
+  self: Pessimistic =>
+  /** registering a dependency on a node we do not personally own does require some additional care.
+    * we move responsibility to the commit phase */
+  abstract override def register(sink: Reactive)(source: Reactive): Unit = {
+    source.lock.acquireDynamic(key)
+    val owner = source.lock.getOwner
+    if ((owner ne key) && !source.dependants.get.contains(sink)) {
+      owner.turn.register(sink)(source)
+      owner.turn.admit(sink)
+    }
+    else super.register(sink)(source)
+  }
+
+  /** this is for cases where we register and then unregister the same dependency in a single turn */
+  abstract override def unregister(sink: Reactive)(source: Reactive): Unit = {
+    source.lock.acquireDynamic(key)
+    val owner = source.lock.getOwner
+    if (owner ne key) {
+      owner.turn.forget(sink)
+      owner.turn.unregister(sink)(source)
+    }
+    else super.unregister(sink)(source)
+  }
+}
