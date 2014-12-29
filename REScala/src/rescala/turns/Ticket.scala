4d1e759559b0137cc741c5c3f8e39fefa32f8531
ragnar <ragnar@rmgk.de>
2014-12-29 18:00:20 +0100

    add implicitNotFound for ticket

--- REScala/src/rescala/turns/Ticket.scala
+++ REScala/src/rescala/turns/Ticket.scala
@@ -1,7 +1,12 @@
 package rescala.turns
 
+import scala.annotation.implicitNotFound
 import scala.language.implicitConversions
 
+@implicitNotFound(msg = "could not generate a ticket." +
+  " tickets are available whenever an implicit turn is available" +
+  " (in which case that turn is used) or if no turn is present," +
+  " an implicit engine will be used to generate new tickets")
 final case class Ticket(self: Either[Turn, Engine[Turn]]) extends AnyVal {
   def apply[T](f: Turn => T): T = self match {
     case Left(turn) => f(turn)
