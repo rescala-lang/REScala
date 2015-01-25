dd4129b1e0ff06fffa2f06da7fb576bb45396daa
ragnar <ragnar@rmgk.de>
2015-01-25 17:33:52 +0100

    acquireShared now returns the owner

--- REScala/src/rescala/synchronization/TurnLock.scala
+++ REScala/src/rescala/synchronization/TurnLock.scala
@@ -34,35 +34,38 @@ final class TurnLock(val guarded: Reactive) {
   }
 
   @tailrec
-  def acquireShared(requester: Key): Unit = {
+  def acquireShared(requester: Key): Key = {
     val oldOwner = tryLock(requester)
 
-    if (oldOwner eq requester) Keychains.Done(Unit)
-    else {
-      Keychains.lockKeychains(requester, oldOwner) {
-        synchronized {
-          tryLock(requester) match {
-            // make sure the other owner did not unlock before we got his master lock
-            case _ if owner eq requester => Keychains.Done(Unit)
-            case _ if owner ne oldOwner => Keychains.Retry
-            case _ if requester.keychain eq owner.keychain => Done(Unit)
-            case _ =>
-              shared = shared.enqueue(requester)
-              owner.keychain.append(requester.keychain)
-              Await
+    val res =
+      if (oldOwner eq requester) Keychains.Done(requester)
+      else {
+        Keychains.lockKeychains(requester, oldOwner) {
+          synchronized {
+            tryLock(requester) match {
+              // make sure the other owner did not unlock before we got his master lock
+              case _ if owner eq requester => Keychains.Done(requester)
+              case _ if owner ne oldOwner => Keychains.Retry
+              case _ if requester.keychain eq owner.keychain => Done(owner)
+              case _ =>
+                shared = shared.enqueue(requester)
+                owner.keychain.append(requester.keychain)
+                Await
+            }
           }
         }
       }
-    } match {
+    res match {
       case Keychains.Await =>
         Keychains.await(requester)
         synchronized {
           val (k, r) = shared.dequeue
           assert(k == requester, s"resolved await in wrong order got $k expected $requester remaining $r")
           shared = r
+          requester
         }
       case Keychains.Retry => acquireShared(requester)
-      case Keychains.Done(_) =>
+      case Keychains.Done(o) => o
     }
   }
 
