5b6c1878541229f5de584b52aecd7a11e816b418
Andre Pacak <andre.pacak@googlemail.com>
2015-05-26 14:21:32 +0200

    Adjusted values and used new method to get continuous.Elevator working

--- examples/src/main/scala/examples/continuous/Elevator.scala
+++ examples/src/main/scala/examples/continuous/Elevator.scala
@@ -16,21 +16,21 @@ class Elevator(val nFloors : Int) {
 	val FloorHeight = 120
 	val FloorStart = 10
 	val FloorPos = Iterable.iterate(FloorStart, nFloors)(_ + FloorHeight).toList
-	
+
 	// these need to be fine tuned for the system to be 'stable'
-	val MinSpeed = 0.5
-	val MaxSpeed = 50.0
-	val MaxAccel = 50.0
-	val BreakDist = 27.5	
+	val MinSpeed = 1
+	val MaxSpeed = 20
+	val MaxAccel = 10
+	val BreakDist = 20
 
 	// expose this event to the outside
 	val callToFloor = new ImperativeEvent[Int]
 	val queue = SQueue(1)
 	callToFloor += {queue enqueue _}
-	
+
 	// create a timer
-	val time = Timer(0)
-	
+	val time = Timer(Time.ms(1))
+
 	// Define Signals describing state and behavior of the elevator
 	val destination = Signal {
 	  queue.head() match {
@@ -38,15 +38,15 @@ class Elevator(val nFloors : Int) {
 	    case Some(target) => FloorPos(target)
 	  }
 	}
-	val speed = time integrate acceleration() 
-	val pos = time integrate speed()
+	val speed = time integrateWithInterval acceleration()
+	val pos = time integrateWithInterval speed()
 	val position = Signal {pos().asInstanceOf[Int]} // discretize position for drawing
 	val distance = Signal { destination() - position() }
 	val direction = Signal { math.signum(distance()) }
 	val break = Signal { math.abs(distance()) <= BreakDist }
 	val stopped = Signal { math.abs(speed()) < MinSpeed }
-	val reached = Signal { stopped() && position() == destination() }
-	
+	val reached = Signal { stopped() && math.abs(distance()) <= 1 }
+
 	val acceleration : Signal[Double] = Signal {
 	  if(break()){
 	    if(stopped()) 0
@@ -55,21 +55,18 @@ class Elevator(val nFloors : Int) {
 	  else if(math.abs(speed()) > MaxSpeed) 0
 	  else MaxAccel  * direction()
 	}
-	
-	speed.changed += {println(_)}
-	
-	val currentFloor = Signal { 
+
+	val currentFloor = Signal {
 	  val p = position()
 	  FloorPos.indexOf(FloorPos.sortBy(f => math.abs(f - p)).head)
 	}
-	
-	val reachedFloor : Event[Int] = reached.changed && {_ == true} map 
+
+	val reachedFloor : Event[Int] = reached.changed && {_ == true} map
 		{(_ : Boolean) => currentFloor()}
-	
-	
+
 	reachedFloor += {_ => queue.dequeue} // move to the next destination
-	
-	
+
+
 	def nameOfFloor(i : Int) = (nFloors - i) match {
 	    case 1 => "E"
 	    case n => "" + n
