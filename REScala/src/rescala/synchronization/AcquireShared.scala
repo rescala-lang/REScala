31300c7e9769d50a000c2b5c84449c7dab79262d
ragnar <ragnar@rmgk.de>
2015-05-28 16:34:05 +0200

    add comment why not synchronizing might work

--- REScala/src/rescala/synchronization/AcquireShared.scala
+++ REScala/src/rescala/synchronization/AcquireShared.scala
@@ -16,12 +16,15 @@ object AcquireShared {
       if (oldOwner eq requester) Done(requester)
       else {
         Keychains.lockKeychains(requester, oldOwner) {
+          // be aware that the owner of the lock could change at any time.
+          // but it can not change when the owner is the requester or old owner,
+          // because the keychain protects unlocking.ü
           lock.tryLock(requester) match {
             // make sure the other owner did not unlock before we got his master lock
             case owner if owner eq requester => Done(requester)
             case owner if owner ne oldOwner => Retry
             case owner if requester.keychain eq owner.keychain => Done(owner)
-            case owner =>
+            case owner => // owner here must be equal to the oldOwner, whose keychain is locked
               lock.share(requester)
               owner.keychain.append(requester.keychain)
               Await
