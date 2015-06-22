4ee60e6572fe9f041b96f5ab53968f2f504d26e4
ragnar <ragnar@rmgk.de>
2015-06-22 17:04:59 +0200

    add benchmark main method with simple profiling

---
+++ Microbench/src/main/scala/benchmarks/Main.scala
@@ -0,0 +1,20 @@
+package benchmarks
+
+import org.openjdk.jmh.profile.{HotspotRuntimeProfiler, GCProfiler, StackProfiler}
+import org.openjdk.jmh.runner.Runner
+import org.openjdk.jmh.runner.options.{VerboseMode, OptionsBuilder}
+
+object Main {
+  def main(args: Array[String]): Unit = {
+    val opt = new OptionsBuilder()
+      .include(".*" + classOf[TurnCreation].getSimpleName() + ".*")
+      .param("engineName", "parrp")
+      .jvmArgsAppend("-Djmh.stack.lines=1")
+      .jvmArgsAppend("-Djmh.stack.period=1")
+      //.jvmArgsAppend("-Djmh.stack.detailLine=true")
+
+      .addProfiler(classOf[StackProfiler])
+      .build()
+    new Runner(opt).run()
+  }
+}
