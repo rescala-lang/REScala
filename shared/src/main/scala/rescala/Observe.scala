ce02fd0ac44fbe1b102fb38a53775235885b2408
ragnar <ragnar@rmgk.de>
2015-07-13 18:08:30 +0200

    extract Obs into named class

--- shared/src/main/scala/rescala/Observe.scala
+++ shared/src/main/scala/rescala/Observe.scala
@@ -10,16 +10,18 @@ trait Observe[S <: Spores] {
 
 object Observe {
 
+  private class Obs[T, S <: Spores](bud: S#Bud, dependency: Pulsing[T, S], fun: T => Unit) extends Base[S](bud, Set(dependency)) with Reactive[S] with Observe[S] {
+    override protected[rescala] def reevaluate()(implicit turn: Turn[S]): ReevaluationResult[S] = {
+      turn.schedule(once(this, dependency.pulse.toOption, fun))
+      ReevaluationResult.Static(changed = false)
+    }
+    override def remove()(implicit maybe: Ticket[S]): Unit = maybe(_.unregister(this)(dependency))
+    override protected[rescala] def incoming(implicit turn: Turn[S]): Set[Reactive[S]] = staticIncoming
+  }
+
   def apply[T, S <: Spores](dependency: Pulsing[T, S])(fun: T => Unit)(implicit maybe: Ticket[S]): Observe[S] =
     maybe(initTurn => initTurn.create(Set(dependency)) {
-      val obs = new Base[S](initTurn.bufferFactory.bud(), Set(dependency)) with Reactive[S] with Observe[S] {
-        override protected[rescala] def reevaluate()(implicit turn: Turn[S]): ReevaluationResult[S] = {
-          turn.schedule(once(this, dependency.pulse.toOption, fun))
-          ReevaluationResult.Static(changed = false)
-        }
-        override def remove()(implicit maybe: Ticket[S]): Unit = maybe(_.unregister(this)(dependency))
-        override protected[rescala] def incoming(implicit turn: Turn[S]): Set[Reactive[S]] = staticIncoming
-      }
+      val obs = new Obs(initTurn.bufferFactory.bud(), dependency, fun)
       initTurn.schedule(once(obs, dependency.pulse(initTurn).keep.current, fun))
       obs
     })
