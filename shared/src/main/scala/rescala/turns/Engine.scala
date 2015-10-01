5906fc67f67dfe21aabd957f9e115d35da0da831
ragnar <ragnar@rmgk.de>
2015-10-01 16:17:06 +0200

    make engine aliases final

--- shared/src/main/scala/rescala/turns/Engine.scala
+++ shared/src/main/scala/rescala/turns/Engine.scala
@@ -1,8 +1,8 @@
 package rescala.turns
 
-import rescala.{Events, Signals, Signal}
-import rescala.graph.{Reactive, Spores}
+import rescala.graph.Spores
 import rescala.macros.ReactiveMacros
+import rescala.{Events, Signals}
 
 import scala.annotation.implicitNotFound
 import scala.language.experimental.macros
@@ -11,21 +11,21 @@ import scala.language.experimental.macros
 @implicitNotFound(msg = "could not find a propagation engine, select one from Engines")
 trait Engine[S <: Spores, +TTurn <: Turn[S]] {
 
-  type Signal[+A] = rescala.Signal[A, S]
-  type Event[+A] = rescala.Event[A, S]
-  type Var[A] = rescala.Var[A, S]
-  type Evt[A] = rescala.Evt[A, S]
-  type Spores = S
-  type Turn = rescala.turns.Turn[S]
-  type Ticket = rescala.turns.Ticket[S]
-  type Reactive = rescala.graph.Reactive[S]
-  def Evt[A](): Evt[A] = rescala.Evt[A, S]()(this)
-  def Var[A](v: A): Var[A] = rescala.Var[A, S](v)(this)
-  def dynamic[T](dependencies: Reactive*)(expr: Turn => T)(implicit ticket: Ticket): Signal[T] = Signals.dynamic(dependencies: _*)(expr)
-  def dynamicE[T](dependencies: Reactive*)(expr: Turn => Option[T])(implicit ticket: Ticket): Event[T] = Events.dynamic(dependencies: _*)(expr)
+  final type Signal[+A] = rescala.Signal[A, S]
+  final type Event[+A] = rescala.Event[A, S]
+  final type Var[A] = rescala.Var[A, S]
+  final type Evt[A] = rescala.Evt[A, S]
+  final type Spores = S
+  final type Turn = rescala.turns.Turn[S]
+  final type Ticket = rescala.turns.Ticket[S]
+  final type Reactive = rescala.graph.Reactive[S]
+  final def Evt[A](): Evt[A] = rescala.Evt[A, S]()(this)
+  final def Var[A](v: A): Var[A] = rescala.Var[A, S](v)(this)
+  final def dynamic[T](dependencies: Reactive*)(expr: Turn => T)(implicit ticket: Ticket): Signal[T] = Signals.dynamic(dependencies: _*)(expr)
+  final def dynamicE[T](dependencies: Reactive*)(expr: Turn => Option[T])(implicit ticket: Ticket): Event[T] = Events.dynamic(dependencies: _*)(expr)
 
-  def Signal[A](expression: A): Signal[A] = macro ReactiveMacros.SignalMacro[A, S]
-  def Event[A](expression: Option[A]): Event[A] = macro ReactiveMacros.EventMacro[A, S]
+  final def Signal[A](expression: A): Signal[A] = macro ReactiveMacros.SignalMacro[A, S]
+  final def Event[A](expression: Option[A]): Event[A] = macro ReactiveMacros.EventMacro[A, S]
 
 
   /** used for the creation of state inside reactives */
