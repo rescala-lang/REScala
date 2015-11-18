056f92ddcca807b4888dd1351948379265f77a8a
ragnar <ragnar@rmgk.de>
2015-11-18 14:14:46 +0100

    replace scala queue with java queue

--- jvm/src/main/scala/rescala/synchronization/Keychain.scala
+++ jvm/src/main/scala/rescala/synchronization/Keychain.scala
@@ -1,8 +1,8 @@
 package rescala.synchronization
 
-import rescala.graph.Globals
+import java.util
 
-import scala.collection.immutable.Queue
+import rescala.graph.Globals
 
 class Keychain(init: Key) {
 
@@ -10,7 +10,9 @@ class Keychain(init: Key) {
   override def toString = s"Keychain($id)"
 
   /** synchronized on this */
-  private var keys: Queue[Key] = Queue(init)
+  private val keys: util.ArrayDeque[Key] = new util.ArrayDeque[Key](2)
+  keys.add(init)
+
   private var fallthrough: Map[Key, Int] = Map()
   def addFallthrough(key: Key, amount: Int = 1): Unit = synchronized { fallthrough = fallthrough.updated(key, fallthrough.getOrElse(key, 0) + amount) }
   def removeFallthrough(key: Key): Unit = synchronized {
@@ -23,22 +25,22 @@ class Keychain(init: Key) {
     assert(this ne other, s"tried to append $this to itself")
     assert(Thread.holdsLock(this), s"tried to append $this and $other without holding lock on $this")
     assert(Thread.holdsLock(other), s"tried to append $this and $other without holding lock on $other")
-    other.keys.foreach { k =>
-      k.keychain = this
+    val it = other.keys.iterator()
+    while (it.hasNext()) {
+      it.next().keychain = this
     }
     other.fallthrough.foreach { case (k, a) => addFallthrough(k, a) }
-    keys = keys.enqueue(other.keys)
+    keys.addAll(other.keys)
   }
 
   def release(key: Key) = {
     assert(Thread.holdsLock(this), s"tried to release $key without holding $this")
-    val (head, rest) = keys.dequeue
+    val head = keys.poll()
     assert(head eq key, s"tried to drop $key from $this but is not head! ($keys)")
-    keys = rest
     val locks = key.grabLocks().distinct
     if (keys.isEmpty) locks.foreach(_.transfer(null, key))
     else {
-      val target = keys.head
+      val target = keys.peek()
       locks.foreach(_.transfer(target, key, transferWriteSet = fallthrough.nonEmpty))
       fallthrough -= target
       target.continue()
@@ -46,3 +48,4 @@ class Keychain(init: Key) {
   }
 
 }
+
