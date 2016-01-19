7de143991d601a0775c1e62cd1c635f9cd0e4c14
ragnar <ragnar@rmgk.de>
2016-01-19 18:08:01 +0100

    add STMBank benchmark

---
+++ Microbench/src/main/scala/benchmarks/STMBank/BankAccounts.scala
@@ -0,0 +1,154 @@
+package benchmarks.STMBank
+
+import java.util.concurrent.locks.{ReentrantLock, Lock}
+import java.util.concurrent.{ThreadLocalRandom, TimeUnit}
+import java.util.concurrent.atomic.AtomicInteger
+
+import benchmarks.{EngineParam, Workload}
+import org.openjdk.jmh.annotations._
+import org.openjdk.jmh.infra.{Blackhole, BenchmarkParams, ThreadParams}
+import rescala._
+import rescala.graph.Spores
+import rescala.turns.{Engine, Ticket, Turn}
+
+import scala.concurrent.stm.{atomic, Ref}
+
+
+@State(Scope.Benchmark)
+class ReactiveState[S <: Spores] {
+
+
+  @Param(Array("64"))
+  var numberOfAccounts: Int = _
+
+  @Param(Array("0.1"))
+  var globalReadChance: Double = _
+
+  var engine: Engine[S, Turn[S]] = _
+  var accounts: Array[Var[Int, S]] = _
+
+  var locks: Array[Lock] = null
+
+  @Setup(Level.Iteration)
+  def setup(params: BenchmarkParams, engine: EngineParam[S]) = {
+    this.engine = engine.engine
+    val threads = params.getThreads
+    implicit val e = this.engine
+
+    accounts = Array.fill(numberOfAccounts)(Var(0))
+    if (engine.engineName == "unmanaged") locks = Array.fill(numberOfAccounts)(new ReentrantLock())
+  }
+}
+
+
+@State(Scope.Benchmark)
+class STMState {
+
+
+  @Param(Array("64"))
+  var numberOfAccounts: Int = _
+
+  @Param(Array("0.1"))
+  var globalReadChance: Double = _
+
+  var accounts: Array[Ref[Int]] = _
+
+  @Setup(Level.Iteration)
+  def setup(params: BenchmarkParams) = {
+    val threads = params.getThreads
+    accounts = Range(0, numberOfAccounts).map(_ => Ref(0)).toArray
+  }
+}
+
+
+@State(Scope.Benchmark)
+@BenchmarkMode(Array(Mode.Throughput))
+@OutputTimeUnit(TimeUnit.MILLISECONDS)
+@Warmup(iterations = 5, time = 1000, timeUnit = TimeUnit.MILLISECONDS)
+@Measurement(iterations = 5, time = 1000, timeUnit = TimeUnit.MILLISECONDS)
+@Fork(1)
+@Threads(2)
+class BankAccounts[S <: Spores] {
+
+  @Benchmark
+  def reactive(rs: ReactiveState[S], bh: Blackhole) = {
+    if (rs.locks == null) {
+      val tlr = ThreadLocalRandom.current()
+      if (tlr.nextDouble() < rs.globalReadChance) {
+        rs.engine.plan(rs.accounts: _*) { t =>
+          val sum = rs.accounts.foldLeft(0)((acc, v) => acc + v.get(t))
+          bh.consume(sum)
+          assert(sum == 0)
+        }
+      }
+      else {
+        val a1 = tlr.nextInt(rs.numberOfAccounts)
+        val a2 = tlr.nextInt(rs.numberOfAccounts)
+        val account1 = rs.accounts(a1)
+        val account2 = rs.accounts(a2)
+        rs.engine.plan(account1, account2) { t =>
+          account1.admit(account1.get(t) + 4817)(t)
+          account2.admit(account2.get(t) - 4817)(t)
+        }
+      }
+    }
+    else {
+      val tlr = ThreadLocalRandom.current()
+      if (tlr.nextDouble() < rs.globalReadChance) {
+        rs.locks.foreach(_.lock())
+        try {
+          rs.engine.plan(rs.accounts: _*) { t =>
+            val sum = rs.accounts.foldLeft(0)((acc, v) => acc + v.get(t))
+            bh.consume(sum)
+            assert(sum == 0)
+          }
+        }
+        finally { rs.locks.foreach(_.unlock()) }
+      }
+      else {
+        val a1 = tlr.nextInt(rs.numberOfAccounts)
+        val a2 = tlr.nextInt(rs.numberOfAccounts)
+        val first = Math.min(a1, a2)
+        val second = Math.max(a1, a2)
+        rs.locks(first).lock()
+        rs.locks(second).lock()
+        try {
+          val account1 = rs.accounts(a1)
+          val account2 = rs.accounts(a2)
+          rs.engine.plan(account1, account2) { t =>
+            account1.admit(account1.get(t) + 4817)(t)
+            account2.admit(account2.get(t) - 4817)(t)
+          }
+        } finally {
+          rs.locks(first).unlock()
+          rs.locks(second).unlock()
+        }
+      }
+    }
+  }
+
+
+  @Benchmark
+  def stm(rs: STMState, bh: Blackhole) = {
+    val tlr = ThreadLocalRandom.current()
+    if (tlr.nextDouble() < rs.globalReadChance) {
+      atomic { t =>
+        val sum = rs.accounts.foldLeft(0)((acc, v) => acc + v.get(t))
+        bh.consume(sum)
+        assert(sum == 0)
+      }
+    }
+    else {
+      val a1 = tlr.nextInt(rs.numberOfAccounts)
+      val a2 = tlr.nextInt(rs.numberOfAccounts)
+      val account1 = rs.accounts(a1)
+      val account2 = rs.accounts(a2)
+      atomic { t =>
+        account1.set(account1.get(t) + 4817)(t)
+        account2.set(account2.get(t) - 4817)(t)
+      }
+    }
+  }
+
+
+}
