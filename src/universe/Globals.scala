2e4b4f273f7ac75ab06d9f02cc5ca76be48e0492
ragnar <ragnar@rmgk.de>
2016-01-22 17:43:57 +0100

    make engine configurable without recompiling

--- src/universe/Globals.scala
+++ src/universe/Globals.scala
@@ -1,8 +1,8 @@
 package universe
 
-import rescala.graph.{STMSpores, ParRPSpores}
+import rescala.graph.{Spores, STMSpores, ParRPSpores}
 import rescala.synchronization.{STMSync, ParRP}
-import rescala.turns.Engine
+import rescala.turns.{Turn, Engine}
 import rescala.turns.Engines.NoLockEngine
 
 
@@ -10,8 +10,13 @@ import scala.collection.parallel.ForkJoinTaskSupport
 
 object Globals {
   //implicit val engine: NoLockEngine = rescala.turns.Engines.synchron
-  implicit val engine: Engine[ParRPSpores.type, ParRP] = rescala.synchronization.Engines.parrp
-  //implicit val engine: Engine[STMSpores.type, STMSync] = rescala.synchronization.Engines.stm
+  //implicit val engine: Engine[ParRPSpores.type, ParRP] = rescala.synchronization.Engines.parrp
+  implicit val engine: Engine[Spores, Turn[Spores]] = rescala.synchronization.Engines.byName[Spores](System.getProperty("engineName", "parrp"))
+
+  val engineName =
+    if (engine == rescala.synchronization.Engines.parrp) "parrp" else
+    if (engine == rescala.synchronization.Engines.stm) "stm" else
+    if (engine == rescala.turns.Engines.synchron) "synchron" else "unknown"
 
   var taskSupport: ForkJoinTaskSupport = _
   def setParallelism(n: Int) = taskSupport = new ForkJoinTaskSupport(new scala.concurrent.forkjoin.ForkJoinPool(n))
--- src/universe/RunConsole.scala
+++ src/universe/RunConsole.scala
@@ -4,27 +4,32 @@ import java.nio.file.{StandardOpenOption, Paths, Files}
 
 import universe.Globals.engine
 
-// test
 object RunConsole {
   def main(args: Array[String]): Unit = {
     val nAnimals = 100
     val nPlants = 300
+    val width = 100
+    val height = 100
+    val repetitions = 5
+    val threadCounts = Range.inclusive(1,16)
 
-    for (repetition <- 0 to 5; threads <- 1 to 16) {
+    val outfile = s"universe-${Globals.engineName}.csv"
+
+    Files.write(Paths.get(outfile),
+      s""""Threads","Score","Param: engineName","Benchmark","Param: height","Param: width","Param: animals","Param: plants"${"\n"}""".getBytes(),
+      StandardOpenOption.WRITE, StandardOpenOption.CREATE)
+
+    for (repetition <- 0 to repetitions; threads <- threadCounts) {
       println(s"rep: $repetition, threads: $threads")
 
       Globals.setParallelism(threads)
 
-      val world = new World
+      val world = new World(width, height)
 
       world batchSpawn(nAnimals, nPlants)
 
       val start = System.nanoTime()
       while (world.time.week.now < 2) {
-        //    println(world.time)
-        //    println(world.status)
-        //    println(world.board.dump) // dumping the board is a bottleneck!
-
         world.tick()
         world.runPlan()
       }
@@ -33,7 +38,9 @@ object RunConsole {
       //    println(world.board.dump)
       val duration = (System.nanoTime() - start) / 1000000000.0D
       //    println(s"duration = ${duration}s")
-      if (repetition > 0) Files.write(Paths.get(args(0) + s"-threads$threads.txt"), s"$duration\n".getBytes(), StandardOpenOption.APPEND, StandardOpenOption.CREATE)
+      if (repetition > 0) Files.write(Paths.get(outfile),
+        s"""$threads,$duration,"${Globals.engineName}","UniverseCaseStudy",$height,$width,$nAnimals,$nPlants${"\n"}""".getBytes(),
+        StandardOpenOption.APPEND)
     }
   }
 }
