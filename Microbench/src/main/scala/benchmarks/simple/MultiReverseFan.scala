9a5728faf2a951e31f24cbddba0e0808d939ae36
ragnar <ragnar@rmgk.de>
2016-01-25 15:57:38 +0100

    add MultiReverseFan

--- Microbench/src/main/scala/benchmarks/simple/MultiReverseFan.scala
+++ Microbench/src/main/scala/benchmarks/simple/MultiReverseFan.scala
@@ -14,12 +14,12 @@ import rescala.turns.{Engine, Turn}
 @Fork(1)
 @Threads(4)
 @State(Scope.Benchmark)
-class ReverseFan[S <: rescala.graph.Spores] {
+class MultiReverseFan[S <: rescala.graph.Spores] {
 
   implicit var engine: Engine[S, Turn[S]] = _
 
   var sources: Array[rescala.Var[Int, S]] = _
-  var result: rescala.Signal[Int, S] = _
+  var results: Array[rescala.Signal[Int, S]] = _
 
   @Setup
   def setup(params: BenchmarkParams, size: Size, step: Step, engineParam: EngineParam[S], work: Workload) = {
@@ -27,8 +27,10 @@ class ReverseFan[S <: rescala.graph.Spores] {
     val localEngine = engine; import localEngine._
     val threads = params.getThreads
     sources = Array.fill(threads)(Var(step.get()))
-    val intermediate = sources.map(_.map{ v => {work.consume(); v + 1}})
-    result = rescala.Signals.static(intermediate.toSeq : _*) { t => val r = intermediate.foldLeft(0)((a, v) => v.get(t) + a);  work.consume(); r }
+    val intermediate = sources.map(_.map{ v => {work.consume(); v + 1}}).grouped(threads / step.at())
+    results = intermediate.map { sigs =>
+      rescala.Signals.static(sigs.toSeq: _*) { t => val r = sigs.foldLeft(0)((a, v) => v.get(t) + a); work.consume(); r }
+    }.toArray
   }
 
   @Benchmark
