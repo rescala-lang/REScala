8fde60b5ef4fd732f4b4db50a8de7a554f90a3b8
ragnar <ragnar@rmgk.de>
2016-01-26 17:12:03 +0100

    fix reverse fan

--- Microbench/src/main/scala/benchmarks/simple/MultiReverseFan.scala
+++ Microbench/src/main/scala/benchmarks/simple/MultiReverseFan.scala
@@ -1,6 +1,7 @@
 package benchmarks.simple
 
 import java.util.concurrent.TimeUnit
+import java.util.concurrent.locks.{ReentrantLock, Lock}
 
 import benchmarks.{Workload, EngineParam, Size, Step}
 import org.openjdk.jmh.annotations._
@@ -20,19 +21,37 @@ class MultiReverseFan[S <: rescala.graph.Spores] {
 
   var sources: Array[rescala.Var[Int, S]] = _
   var results: Array[rescala.Signal[Int, S]] = _
+  var locks: Array[Lock] = null
+  var groupSize: Int = _
 
   @Setup
   def setup(params: BenchmarkParams, size: Size, step: Step, engineParam: EngineParam[S], work: Workload) = {
     engine = engineParam.engine
-    val localEngine = engine; import localEngine._
+    val localEngine = engine;
+    import localEngine._
     val threads = params.getThreads
+
     sources = Array.fill(threads)(Var(step.get()))
-    val intermediate = sources.map(_.map{ v => {work.consume(); v + 1}}).grouped(threads / size.size)
+    groupSize = if (threads > size.size) threads / size.size else 1
+
+    if (engineParam.engineName == "unmanaged") locks = Array.fill(threads % groupSize)(new ReentrantLock())
+
+    val intermediate = sources.map(_.map { v => {work.consume(); v + 1} }).grouped(groupSize)
     results = intermediate.map { sigs =>
       rescala.Signals.static(sigs.toSeq: _*) { t => val r = sigs.foldLeft(0)((a, v) => v.get(t) + a); work.consumeSecondary(); r }
     }.toArray
   }
 
   @Benchmark
-  def run(step: Step, params: ThreadParams): Unit = sources(params.getThreadIndex).set(step.run())
+  def run(step: Step, params: ThreadParams): Unit = {
+    val index = params.getThreadIndex
+    if (locks == null) sources(index).set(step.run())
+    else {
+      locks(index / groupSize).lock()
+      try {
+        sources(index).set(step.run())
+      }
+      finally locks(index / groupSize).unlock()
+    }
+  }
 }
