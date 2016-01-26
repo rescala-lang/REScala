61790e66e267896de67ea2e9d62daf3f73103c6f
ragnar <ragnar@rmgk.de>
2016-01-26 15:26:55 +0100

    fix escaping

--- Microbench/manage.pl
+++ Microbench/manage.pl
@@ -69,7 +69,7 @@ sub init {
   mkdir $OUTDIR;
   chdir "..";
   system('sbt', 'set scalacOptions in ThisBuild ++= List("-Xdisable-assertions", "-Xelide-below", "9999999")', 'project microbench', 'clean', 'stage', 'compileJmh');
-  qx[perl -p -i -e 's#exec java \$JAVA_OPTS -cp "#exec java \$JAVA_OPTS -cp "\$PROJECT_DIR/Microbench/target/scala-2.11/jmh-classes:#g' ./Microbench/target/start];
+  qx[perl -p -i -e 's#exec java \\\$JAVA_OPTS -cp "#exec java \\\$JAVA_OPTS -cp "\\\$PROJECT_DIR/Microbench/target/scala-2.11/jmh-classes:#g' ./Microbench/target/start];
   #system('sbt','clean', 'jmh:compile', 'jmh:stage');
   chdir $MAINDIR;
 }
