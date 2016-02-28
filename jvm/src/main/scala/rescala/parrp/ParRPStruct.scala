a69bd902cdf1822ab096b4e98a125d7666b671a8
ragnar <ragnar@rmgk.de>
2016-02-28 14:29:31 +0100

    move parrp spore to outer scope

--- jvm/src/main/scala/rescala/parrp/ParRPStruct.scala
+++ jvm/src/main/scala/rescala/parrp/ParRPStruct.scala
@@ -13,19 +13,22 @@ object ParRPStruct extends LevelStruct {
     new ParRPSporeP[P, R](initialValue, transient, lock, initialIncoming)
   }
 
-  class ParRPSporeP[P, R](current: Pulse[P], transient: Boolean, val lock: TurnLock, initialIncoming: Set[R]) extends LevelSporeImpl[P, R](current, transient, initialIncoming) {
-    override def set(value: Pulse[P])(implicit turn: Turn[_]): Unit = {
-      assert(turn match {
-        case pessimistic: ParRP =>
-          val wlo: Option[Key] = Option(lock).map(_.getOwner)
-          assert(wlo.fold(true)(_ eq pessimistic.key),
-            s"buffer owned by $owner, controlled by $lock with owner ${wlo.get}" +
-              s" was written by $turn who locks with ${pessimistic.key}, by now the owner is ${lock.getOwner}")
-          true
-        case _ =>
-          throw new IllegalStateException(s"parrp buffer used with wrong turn")
-      })
-      super.set(value)
-    }
+
+}
+
+
+class ParRPSporeP[P, R](current: Pulse[P], transient: Boolean, val lock: TurnLock, initialIncoming: Set[R]) extends LevelSporeImpl[P, R](current, transient, initialIncoming) {
+  override def set(value: Pulse[P])(implicit turn: Turn[_]): Unit = {
+    assert(turn match {
+      case pessimistic: ParRP =>
+        val wlo: Option[Key] = Option(lock).map(_.getOwner)
+        assert(wlo.fold(true)(_ eq pessimistic.key),
+          s"buffer owned by $owner, controlled by $lock with owner ${wlo.get}" +
+            s" was written by $turn who locks with ${pessimistic.key}, by now the owner is ${lock.getOwner}")
+        true
+      case _ =>
+        throw new IllegalStateException(s"parrp buffer used with wrong turn")
+    })
+    super.set(value)
   }
 }
