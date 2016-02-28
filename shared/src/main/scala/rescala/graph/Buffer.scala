ec36a4fb0d5955bbd1c9933de2c69d8e0a1f8777
ragnar <ragnar@rmgk.de>
2016-02-28 13:19:19 +0100

    simple buffer is no longer used

--- shared/src/main/scala/rescala/graph/Buffer.scala
+++ shared/src/main/scala/rescala/graph/Buffer.scala
@@ -20,38 +20,3 @@ trait Buffer[A] {
   def base(implicit turn: Turn[_]): A
   def get(implicit turn: Turn[_]): A
 }
-
-final class SimpleBuffer[A](initialValue: A, initialStrategy: (A, A) => A) extends Buffer[A] with Committable {
-
-  var current: A = initialValue
-  private var update: Option[A] = None
-  private var owner: Turn[_] = null
-  private val commitStrategy: (A, A) => A = initialStrategy
-
-  override def transform(f: (A) => A)(implicit turn: Turn[_]): A = synchronized {
-    val value = f(get)
-    set(value)
-    value
-  }
-
-  override def set(value: A)(implicit turn: Turn[_]): Unit = synchronized {
-    assert(owner == null || owner == turn, s"buffer owned by $owner written by $turn")
-    update = Some(value)
-    owner = turn
-    turn.schedule(this)
-  }
-
-  override def base(implicit turn: Turn[_]): A = synchronized(current)
-
-  override def get(implicit turn: Turn[_]): A = synchronized {if (turn eq owner) update.getOrElse(current) else current}
-
-  override def release(implicit turn: Turn[_]): Unit = synchronized {
-    update = None
-    owner = null
-  }
-
-  override def commit(implicit turn: Turn[_]): Unit = synchronized {
-    current = commitStrategy(current, get)
-    release(turn)
-  }
-}
