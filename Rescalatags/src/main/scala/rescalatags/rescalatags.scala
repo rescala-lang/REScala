8b004d888749717a17fa6d6a2bc37c12b9ae46f9
ragnar <ragnar@rmgk.de>
2016-08-09 17:08:20 +0200

    use extension method to convert signal to scalatags

--- Rescalatags/src/main/scala/rescalatags/rescalatags.scala
+++ Rescalatags/src/main/scala/rescalatags/rescalatags.scala
@@ -6,42 +6,35 @@ import scalatags.JsDom.all._
 
 package object rescalatags {
 
-  /**
-    * Wraps reactive fragments in spans, so they can be referenced/replaced
-    * when the Signal changes.
-    */
-  implicit def fragment[T](signal: Signal[T])(implicit f: T => Frag): Frag = {
-    htmlTag(signal.map(span(_)))
-  }
+  implicit class SignalToScalatags(signal: Signal[Tag]) {
+    /**
+      * converts a Signal of a scalatags Tag to a scalatags Frag which automatically reflects changes to the signal in the dom
+      */
+    implicit def asFragment: Frag = {
+      new Frag {
+        val rendered = signal.map(_.render)
 
-  /**
-    * Sticks some Signal into a Scalatags fragment, which means hooking up an
-    * changed event handler to propagate changes into the DOM via the element's
-    * ID. Monkey-patches the handler onto the element itself so we have a
-    * reference to kill it when the element leaves the DOM (e.g. gets deleted).
-    */
-  implicit def htmlTag(signal: Signal[Tag]): Frag = {
-    new Frag {
-      val rendered = signal.map(_.render)
-
-      rendered.change.observe { case (lastTag, newTag) =>
-        if (lastTag.parentNode != null && !scalajs.js.isUndefined(lastTag.parentNode)) {
-          lastTag.parentNode.replaceChild(newTag, lastTag)
+        rendered.change.observe { case (lastTag, newTag) =>
+          if (lastTag.parentNode != null && !scalajs.js.isUndefined(lastTag.parentNode)) {
+            lastTag.parentNode.replaceChild(newTag, lastTag)
+          }
         }
-      }
 
-      def applyTo(t: dom.Element) = t.appendChild(rendered.now)
-      def render = rendered.now
+        def applyTo(t: dom.Element) = t.appendChild(rendered.now)
+        def render = rendered.now
+      }
     }
   }
 
-  implicit def attrValue[T: AttrValue] = new AttrValue[Signal[T]] {
+
+
+  implicit def attrValue[T: AttrValue]: AttrValue[Signal[T]] = new AttrValue[Signal[T]] {
     def apply(t: dom.Element, a: Attr, signal: Signal[T]): Unit = {
       signal.observe { value => implicitly[AttrValue[T]].apply(t, a, value) }
     }
   }
 
-  implicit def styleValue[T: StyleValue] = new StyleValue[Signal[T]] {
+  implicit def styleValue[T: StyleValue]: StyleValue[Signal[T]] = new StyleValue[Signal[T]] {
     def apply(t: dom.Element, s: Style, signal: Signal[T]): Unit = {
       signal.observe { value => implicitly[StyleValue[T]].apply(t, s, value) }
     }
