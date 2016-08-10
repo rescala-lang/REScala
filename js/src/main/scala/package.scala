3b7f0d3fb4ce84b1b256856b939117e1954fbd0c
ragnar <ragnar@rmgk.de>
2016-08-10 17:50:36 +0200

    add signals and events to package objects

--- js/src/main/scala/package.scala
+++ js/src/main/scala/package.scala
@@ -2,4 +2,7 @@ import rescala.engines.EngineImpl
 import rescala.engines.Engines.SimpleNoLock
 import rescala.graph.SimpleStruct
 
-package object rescala extends EngineImpl[SimpleStruct, SimpleNoLock](new SimpleNoLock())
+package object rescala extends EngineImpl[SimpleStruct, SimpleNoLock](new SimpleNoLock()) {
+  val Events = reactives.Events
+  val Signals = reactives.Signals
+}
