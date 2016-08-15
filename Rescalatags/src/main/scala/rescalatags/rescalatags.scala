c0feb7712551da712198bb75dd03e4a22cfc16ad
ragnar <ragnar@rmgk.de>
2016-08-15 18:08:54 +0200

    allow scalatags to be used with any engine

--- Rescalatags/src/main/scala/rescalatags/rescalatags.scala
+++ Rescalatags/src/main/scala/rescalatags/rescalatags.scala
@@ -1,42 +1,51 @@
 import org.scalajs.dom
-import rescala._
+import rescala.engines.{Engine, Ticket}
+import rescala.graph.Struct
+import rescala.propagation.Turn
+import rescala.reactives.Signal
 
 import scala.language.{higherKinds, implicitConversions}
 import scalatags.JsDom.all._
 
 package object rescalatags {
 
-  implicit class SignalToScalatags(signal: Signal[Tag]) {
+  implicit class SignalToScalatags[S <: Struct](signal: Signal[Tag, S]) {
     /**
       * converts a Signal of a scalatags Tag to a scalatags Frag which automatically reflects changes to the signal in the dom
       */
-    def asFrag: Frag = {
-      new Frag {
-        val rendered: Signal[dom.Node] = signal
+    def asFrag(implicit ticket: Ticket[S], engine: Engine[S, Turn[S]]): Frag = {
+      val rendered: Signal[dom.Node, S] = ticket { implicit turn =>
+
+        val result: Signal[dom.Node, S] = signal
           .map(_.render)
           .recover(t => span(t.toString).render)
           .withDefault(() => "".render)
 
-        rendered.change.observe { case (lastTag, newTag) =>
+        result.change.observe { case (lastTag, newTag) =>
           if (lastTag.parentNode != null && !scalajs.js.isUndefined(lastTag.parentNode)) {
             lastTag.parentNode.replaceChild(newTag, lastTag)
           }
         }
-
-        def applyTo(t: dom.Element) = t.appendChild(rendered.now)
-        def render: dom.Node = rendered.now
+        result
       }
+
+      new REFrag(rendered)
+    }
+
+    class REFrag(rendered: Signal[dom.Node, S])(implicit engine: Engine[S, Turn[S]]) extends Frag {
+      def applyTo(t: dom.Element) = t.appendChild(rendered.now)
+      def render: dom.Node = rendered.now
     }
   }
 
 
-  implicit def attrValue[T: AttrValue, Sig[T2] <: Signal[T2]]: AttrValue[Sig[T]] = new AttrValue[Sig[T]] {
+  implicit def attrValue[T: AttrValue, S <: Struct, Sig[T2] <: Signal[T2, S]](implicit engine: Engine[S, Turn[S]]): AttrValue[Sig[T]] = new AttrValue[Sig[T]] {
     def apply(t: dom.Element, a: Attr, signal: Sig[T]): Unit = {
       signal.observe { value => implicitly[AttrValue[T]].apply(t, a, value) }
     }
   }
 
-  implicit def styleValue[T: StyleValue, Sig[T2] <: Signal[T2]]: StyleValue[Sig[T]] = new StyleValue[Sig[T]] {
+  implicit def styleValue[T: StyleValue, S <: Struct, Sig[T2] <: Signal[T2, S]](implicit engine: Engine[S, Turn[S]]): StyleValue[Sig[T]] = new StyleValue[Sig[T]] {
     def apply(t: dom.Element, s: Style, signal: Sig[T]): Unit = {
       signal.observe { value => implicitly[StyleValue[T]].apply(t, s, value) }
     }
---
+++ Rescalatags/src/test/scala/test/SimpleTest.scala
@@ -0,0 +1,68 @@
+package test
+
+import org.scalajs.dom.Node
+import org.scalajs.dom.html.Span
+import rescala.engines.Engine
+import rescala.graph.Struct
+import rescala.propagation.Turn
+import rescalatags._
+
+import scalatags.JsDom.all._
+
+class SimpleTest extends org.scalatest.FunSuite {
+
+  rescala.engines.Engines.all.foreach(runWithEngine(_))
+
+  def runWithEngine[S <: Struct](implicit engine: Engine[S, Turn[S]]): Unit = {
+    import engine._
+
+    test(s"$engine: put var into dom") {
+      val v = Var.empty[Tag]
+      val frag: Node = SignalToScalatags[S](v).asFrag.render
+      assert(frag.textContent === "", "empty var gives empty frag")
+
+      val outer = div(v.asFrag)
+      val outerR = outer.render
+
+      assert(outerR.innerHTML === "", "empty var into dom is empty")
+
+      v.set(span("hallo welt"))
+      assert(outerR.innerHTML === "<span>hallo welt</span>", "setting var changes rendered outer tag")
+
+      v.set(div("hallo div"))
+      assert(outerR.innerHTML === "<div>hallo div</div>", "resetting var changes rendered outer tag")
+
+    }
+
+    test(s"$engine: put style into dom") {
+      val v = Var.empty[String]
+
+      span.render.style.setProperty("backgroundColor", "green")
+
+      val ourTag: Span = span(backgroundColor := v).render
+
+      assert(ourTag.style.getPropertyValue("backgroundColor") === "green", "empty color does not render")
+
+      v.set("red")
+      assert(ourTag.style.getPropertyValue("backgroundColor") === "red", "changing var changes color")
+
+      v.set("blue")
+      assert(ourTag.style.getPropertyValue("backgroundColor") === "blue", "changing var changes color again")
+    }
+
+    test(s"$engine: put attribute into dom") {
+      val v = Var.empty[String]
+
+      val ourTag = a(href := v).render
+
+      assert(ourTag.outerHTML === a.render.outerHTML, "empty href does not render")
+
+      v.set("www.rescala-lang.com")
+      assert(ourTag.outerHTML === a(href := "www.rescala-lang.com").render.outerHTML, "changing var changes href")
+
+      v.set("index.html")
+      assert(ourTag.outerHTML === a(href := "index.html").render.outerHTML, "changing var changes href again")
+
+    }
+  }
+}
