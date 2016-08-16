4916502cd6bd0060e785c8cd30674a996d5e63db
ragnar <ragnar@rmgk.de>
2016-08-16 14:03:18 +0200

    clean rescalatags examples

--- Rescalatags/src/test/scala/test/RescalatagsTest.scala
+++ Rescalatags/src/test/scala/test/RescalatagsTest.scala
@@ -2,67 +2,67 @@ package test
 
 import org.scalajs.dom.Node
 import org.scalajs.dom.html.Span
-import rescala.engines.Engine
-import rescala.graph.Struct
-import rescala.propagation.Turn
+import org.scalatest.FunSpec
+import org.scalatest.prop.TableDrivenPropertyChecks
 import rescalatags._
 
 import scalatags.JsDom.all._
 
-class RescalatagsTest extends org.scalatest.FunSuite {
+class RescalatagsTest extends FunSpec with TableDrivenPropertyChecks {
 
-  rescala.engines.Engines.all.foreach(runWithEngine(_))
-
-  def runWithEngine[S <: Struct](implicit engine: Engine[S, Turn[S]]): Unit = {
+  forAll(Table("engine", rescala.engines.Engines.all: _*)) { engine =>
     import engine._
 
-    test(s"$engine: put var into dom") {
-      val v = Var.empty[Tag]
-      val frag: Node = SignalToScalatags[S](v).asFrag.render
-      assert(frag.textContent === "", "empty var gives empty frag")
+    describe(s"$engine") {
 
-      val outer = div(v.asFrag)
-      val outerR = outer.render
+      it(s"put var into dom") {
+        val v = Var.empty[Tag]
+        val frag: Node = v.asFrag.render
+        assert(frag.textContent === "", "empty var gives empty frag")
 
-      assert(outerR.innerHTML === "", "empty var into dom is empty")
+        val outer = div(v.asFrag)
+        val outerR = outer.render
 
-      v.set(span("hallo welt"))
-      assert(outerR.innerHTML === "<span>hallo welt</span>", "setting var changes rendered outer tag")
+        assert(outerR.innerHTML === "", "empty var into dom is empty")
 
-      v.set(div("hallo div"))
-      assert(outerR.innerHTML === "<div>hallo div</div>", "resetting var changes rendered outer tag")
+        v.set(span("hallo welt"))
+        assert(outerR.innerHTML === "<span>hallo welt</span>", "setting var changes rendered outer tag")
 
-    }
+        v.set(div("hallo div"))
+        assert(outerR.innerHTML === "<div>hallo div</div>", "resetting var changes rendered outer tag")
 
-    test(s"$engine: put style into dom") {
-      val v = Var.empty[String]
+      }
+      it(s"put style into dom") {
+        val v = Var.empty[String]
 
-      span.render.style.setProperty("backgroundColor", "green")
+        // we do this because this tends to be globally set from other tests …
+        span.render.style.setProperty("backgroundColor", "green")
 
-      val ourTag: Span = span(backgroundColor := v).render
+        val ourTag: Span = span(backgroundColor := v).render
 
-      assert(ourTag.style.getPropertyValue("backgroundColor") === "green", "empty color does not render")
+        assert(ourTag.style.getPropertyValue("backgroundColor") === "green", "empty color does not render")
 
-      v.set("red")
-      assert(ourTag.style.getPropertyValue("backgroundColor") === "red", "changing var changes color")
+        v.set("red")
+        assert(ourTag.style.getPropertyValue("backgroundColor") === "red", "changing var changes color")
 
-      v.set("blue")
-      assert(ourTag.style.getPropertyValue("backgroundColor") === "blue", "changing var changes color again")
-    }
+        v.set("blue")
+        assert(ourTag.style.getPropertyValue("backgroundColor") === "blue", "changing var changes color again")
+      }
 
-    test(s"$engine: put attribute into dom") {
-      val v = Var.empty[String]
+      it(s"put attribute into dom") {
+        val v = Var.empty[String]
 
-      val ourTag = a(href := v).render
+        val ourTag = a(href := v).render
 
-      assert(ourTag.outerHTML === a.render.outerHTML, "empty href does not render")
+        assert(ourTag.outerHTML === a.render.outerHTML, "empty href does not render")
 
-      v.set("www.rescala-lang.com")
-      assert(ourTag.outerHTML === a(href := "www.rescala-lang.com").render.outerHTML, "changing var changes href")
+        v.set("www.rescala-lang.com")
+        assert(ourTag.outerHTML === a(href := "www.rescala-lang.com").render.outerHTML, "changing var changes href")
 
-      v.set("index.html")
-      assert(ourTag.outerHTML === a(href := "index.html").render.outerHTML, "changing var changes href again")
+        v.set("index.html")
+        assert(ourTag.outerHTML === a(href := "index.html").render.outerHTML, "changing var changes href again")
 
+      }
     }
   }
 }
