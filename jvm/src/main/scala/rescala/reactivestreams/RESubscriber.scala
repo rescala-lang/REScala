d82d46c068b210de44c339bebc023f8461ddd22d
ragnar <ragnar@rmgk.de>
2016-08-18 11:44:24 +0200

    addd RESubscriber

---
+++ jvm/src/main/scala/rescala/reactivestreams/RESubscriber.scala
@@ -0,0 +1,29 @@
+package rescala.reactivestreams
+
+import java.util.Objects
+
+import org.reactivestreams.{Subscriber, Subscription}
+import rescala.engines.Engine
+import rescala.graph.{Pulse, Struct}
+import rescala.propagation.Turn
+import rescala.reactives.Evt
+
+class RESubscriber[T, S <: Struct](evt: Evt[T, S], fac: Engine[S, Turn[S]]) extends Subscriber[T] {
+
+  var subscription: Subscription = _
+
+  override def onError(thrw: Throwable): Unit = synchronized {
+    Objects.requireNonNull(thrw)
+    fac.plan(evt){implicit turn => evt.admitPulse(Pulse.Exceptional(thrw))}
+  }
+  override def onSubscribe(s: Subscription): Unit = synchronized {
+    subscription = s
+    subscription.request(1)
+  }
+  override def onComplete(): Unit = {}
+  override def onNext(value: T): Unit = synchronized {
+    Objects.requireNonNull(value)
+    evt.fire(value)(fac)
+    subscription.request(1)
+  }
+}
