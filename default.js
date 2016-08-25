8cfbe360d333f4c258af1da2f7bdc620565c37ed
ragnar <ragnar@rmgk.de>
2016-08-25 16:53:48 +0200

    use anchors for scrolling to manual items

--- default.js
+++ default.js
@@ -5,14 +5,14 @@ window.onload = function () {
 		if (element === undefined || element.classList === undefined || !element.classList.contains(".dropdown_content"))
 		document.querySelectorAll(".dropdown_content.show_content").forEach(function(element) {element.classList.remove("show_content")});
 	}, true);
-	
+
 	var manualTOCitems = document.body.querySelectorAll("#main-content>h1,#main-content>h2,#main-content>h3,#main-content>h4");
 	var manualTOC = document.body.querySelector("#toc");
 	var currentList = manualTOC;
 	var currentDepth = 1;
 	manualTOCitems.forEach(function (headline) {
 		var depth = headline.nodeName == "H1" ? 1 : headline.nodeName == "H2" ? 2 : headline.nodeName == "H3" ? 3 : 4
-		
+
 		while (currentDepth < depth) {
 			var subList = document.createElement("ol");
 			currentList.appendChild(wrapInLi(subList));
@@ -26,13 +26,10 @@ window.onload = function () {
 		// now we can assume that currentDepth === depth
 		var button = document.createElement("a");
 		button.innerHTML = headline.innerHTML;
-		button.href = "javascript:void(0);";
-		button.addEventListener("click", function(element) {
-			scrollIntoView(headline);
-		}, true);
+		button.href = "#" + headline.id;
 		currentList.appendChild(wrapInLi(button));
 	});
-	
+
 	window.onscroll = function () {
 		updateArrowVisibility();
 	};
