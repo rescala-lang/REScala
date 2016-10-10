ed1ffc298ed322b42ad3d6f86617c4f09d917cb4
Nico Ritschel <n.ritschel@icloud.com>
2016-10-10 22:50:14 +0200

    Meta pointers are now resolved through a lookup table in the meta graph, which should solve most previous co-/contravariance issues

--- shared/src/main/scala/rescala/meta/DataFlowGraph.scala
+++ shared/src/main/scala/rescala/meta/DataFlowGraph.scala
@@ -5,9 +5,11 @@ import scala.collection.mutable
 class DataFlowGraph {
   private val _nodes : mutable.Set[DataFlowNode[_]] = mutable.Set()
   private val _log : mutable.Queue[MetaLog[_]] = mutable.Queue()
+  private val _pointers : mutable.Map[DataFlowPointer[_], DataFlowNode[_]] = mutable.Map()
 
   protected[meta] def nodes = _nodes.toSet
   protected[meta] def log = _log.toList
+  protected[meta] def pointers = _pointers.toList
 
   def numNodes = _nodes.size
 
@@ -18,6 +20,12 @@ class DataFlowGraph {
     l
   }
 
+  protected[meta] def resolvePointer[T](pointer: DataFlowPointer[T]): Option[DataFlowNode[T]] = _pointers.get(pointer) match {
+    case Some(p) => Some(p.asInstanceOf[DataFlowNode[T]])
+    case None => None
+  }
+  protected[meta] def registerPointer[T](pointer: DataFlowPointer[T], node: DataFlowNode[T]) = _pointers += (pointer -> node)
+
   protected[meta] def moveNodes(moveNodes : Set[DataFlowNode[_]], newGraph : DataFlowGraph): Unit = {
     if (moveNodes.exists(!_nodes.contains(_)))
       throw new IllegalArgumentException("Cannot move nodes from other graphs!")
@@ -27,11 +35,15 @@ class DataFlowGraph {
     _nodes --= moveNodes
     val extractedLogs = _log.filter(l => moveNodes.contains(l.node))
     val remainingLogs = _log.filter(l => !extractedLogs.contains(l))
+    val extractedPointers = _pointers.filter(n => moveNodes.contains(n._2))
+    _pointers --= extractedPointers.keys
     _log.clear()
     _log ++= remainingLogs
     newGraph._nodes ++= moveNodes
     moveNodes.foreach(_.graph = newGraph)
     newGraph._log ++= extractedLogs
+    _pointers.keys.foreach(_.graph = newGraph)
+    newGraph._pointers ++= extractedPointers
   }
 
   def createVar[A]() : VarPointer[A] = new VarPointer(VarSignalNode[A](this))
--- shared/src/main/scala/rescala/meta/DataFlowPointer.scala
+++ shared/src/main/scala/rescala/meta/DataFlowPointer.scala
@@ -4,63 +4,93 @@ import rescala.graph.Struct
 import rescala.reactives.Signals
 
 trait DataFlowPointer[+T] {
-  def node : DataFlowNode[T]
+  protected[meta] var graph : DataFlowGraph
+  def node : Option[DataFlowNode[T]]
 
-  def disconnect() = node.disconnect()
+  def disconnect() = node.get.disconnect()
 }
 
 trait ReactivePointer[+T] extends DataFlowPointer[T] {
-  override def node : ReactiveNode[T]
+  override def node : Option[ReactiveNode[T]]
 
-  def observe[U >: T](onSuccess: (U) => Unit, onFailure: (Throwable) => Unit = t => throw t): ObservePointer[U] = new ObservePointer(node.observe(onSuccess, onFailure))
-  def reify[S <: Struct](reifier: Reifier[S]) = node.reify(reifier)
+  def observe[U >: T](onSuccess: (U) => Unit, onFailure: (Throwable) => Unit = t => throw t): ObservePointer[U] = new ObservePointer(node.get.observe(onSuccess, onFailure))
+  def reify[S <: Struct](reifier: Reifier[S]) = node.get.reify(reifier)
 }
 
-class EventPointer[+T](protected[this] var _node : EventNode[T]) extends ReactivePointer[T] {
-  override def node: EventNode[T] = _node
-  override def reify[S <: Struct](reifier: Reifier[S]) = node.reify(reifier)
+class EventPointer[+T](_node : EventNode[T]) extends ReactivePointer[T] {
+  override protected[meta] var graph : DataFlowGraph = _node.graph
+  graph.registerPointer(this, _node)
 
-  def +=[X >: T](react: X => Unit): ObservePointer[X] = new ObservePointer(node += react)
-  def ||[U >: T](others: EventPointer[U]*): EventPointer[U] = new EventPointer(node||(others.map(_.node):_*))
-  def &&[U >: T](pred: (U) => Boolean): EventPointer[U] = new EventPointer(node && pred)
-  def \[U](other: EventPointer[U]): EventPointer[T] = new EventPointer(node \ other.node)
-  def and[X >: T, U, R](other: EventPointer[U])(merger: (X, U) => R): EventPointer[R] = new EventPointer(node.and(other.node)(merger))
-  def zip[U](other: EventPointer[U]): EventPointer[(T, U)] = new EventPointer(node.zip(other.node))
-  def map[X >: T, U](mapping: (X) => U): EventPointer[U] = new EventPointer(node.map(mapping))
-  def fold[X >: T, A](init: A)(fold: (A, X) => A): SignalPointer[A] = new SignalPointer(node.fold(init)(fold))
-  def toggle[A](a: SignalPointer[A], b: SignalPointer[A]): SignalPointer[A] = new SignalPointer(node.toggle(a.node, b.node))
-  def snapshot[A](s: SignalPointer[A]): SignalPointer[A] = new SignalPointer(node.snapshot(s.node))
-  def switchOnce[A](original: SignalPointer[A], newSignal: SignalPointer[A]): SignalPointer[A] = new SignalPointer(node.switchOnce(original.node, newSignal.node))
-  def switchTo[A >: T](original: SignalPointer[A]): SignalPointer[A] = new SignalPointer(node.switchTo(original.node))
-  def flatMap[X >: T, B](f: (X) => EventPointer[B]): EventPointer[B] = new EventPointer(node.flatMap({x : X => f(x).node}))
+  override def node: Option[EventNode[T]] = graph.resolvePointer(this) match {
+    case Some(n) => Some(n.asInstanceOf[EventNode[T]])
+    case None => None
+  }
+
+  override def reify[S <: Struct](reifier: Reifier[S]) = node.get.reify(reifier)
+
+  def +=[X >: T](react: X => Unit): ObservePointer[X] = new ObservePointer(node.get += react)
+  def ||[U >: T](others: EventPointer[U]*): EventPointer[U] = new EventPointer(node.get||(others.map(_.node.get):_*))
+  def &&[U >: T](pred: (U) => Boolean): EventPointer[U] = new EventPointer(node.get && pred)
+  def \[U](other: EventPointer[U]): EventPointer[T] = new EventPointer(node.get \ other.node.get)
+  def and[X >: T, U, R](other: EventPointer[U])(merger: (X, U) => R): EventPointer[R] = new EventPointer(node.get.and(other.node.get)(merger))
+  def zip[U](other: EventPointer[U]): EventPointer[(T, U)] = new EventPointer(node.get.zip(other.node.get))
+  def map[X >: T, U](mapping: (X) => U): EventPointer[U] = new EventPointer(node.get.map(mapping))
+  def fold[X >: T, A](init: A)(fold: (A, X) => A): SignalPointer[A] = new SignalPointer(node.get.fold(init)(fold))
+  def toggle[A](a: SignalPointer[A], b: SignalPointer[A]): SignalPointer[A] = new SignalPointer(node.get.toggle(a.node.get, b.node.get))
+  def snapshot[A](s: SignalPointer[A]): SignalPointer[A] = new SignalPointer(node.get.snapshot(s.node.get))
+  def switchOnce[A](original: SignalPointer[A], newSignal: SignalPointer[A]): SignalPointer[A] = new SignalPointer(node.get.switchOnce(original.node.get, newSignal.node.get))
+  def switchTo[A >: T](original: SignalPointer[A]): SignalPointer[A] = new SignalPointer(node.get.switchTo(original.node.get))
+  def flatMap[X >: T, B](f: (X) => EventPointer[B]): EventPointer[B] = new EventPointer(node.get.flatMap({x : X => f(x).node.get}))
 }
 
 class EvtPointer[T](protected[this] var __node : EvtEventNode[T]) extends EventPointer(__node) {
-  override def node: EvtEventNode[T] = __node
-  override def reify[S <: Struct](reifier: Reifier[S]) = node.reify(reifier)
+  override def node: Option[EvtEventNode[T]] = graph.resolvePointer(this) match {
+    case Some(n) => Some(n.asInstanceOf[EvtEventNode[T]])
+    case None => None
+  }
 
-  def fire(value: T) = node.fire(value)
+  override def reify[S <: Struct](reifier: Reifier[S]) = node.get.reify(reifier)
+
+  def fire(value: T) = node.get.fire(value)
 
 }
 
-class SignalPointer[+A](protected[this] var _node : SignalNode[A]) extends ReactivePointer[A] {
-  override def node: SignalNode[A] = _node
-  override def reify[S <: Struct](reifier: Reifier[S]) = node.reify(reifier)
+class SignalPointer[+A](_node : SignalNode[A]) extends ReactivePointer[A] {
+  override protected[meta] var graph : DataFlowGraph = _node.graph
+  graph.registerPointer(this, _node)
 
-  def delay(n: Int): SignalPointer[A] = new SignalPointer(node.delay(n))
-  def map[X >: A, B](f: (X) => B): SignalPointer[B] = new SignalPointer(node.map(f))
-  def change: EventPointer[Signals.Diff[A]] = new EventPointer(node.change)
-  def changed: EventPointer[A] = new EventPointer(node.changed)
+  override def node: Option[SignalNode[A]] = graph.resolvePointer(this) match {
+    case Some(n) => Some(n.asInstanceOf[SignalNode[A]])
+    case None => None
+  }
+
+  override def reify[S <: Struct](reifier: Reifier[S]) = node.get.reify(reifier)
+
+  def delay(n: Int): SignalPointer[A] = new SignalPointer(node.get.delay(n))
+  def map[X >: A, B](f: (X) => B): SignalPointer[B] = new SignalPointer(node.get.map(f))
+  def change: EventPointer[Signals.Diff[A]] = new EventPointer(node.get.change)
+  def changed: EventPointer[A] = new EventPointer(node.get.changed)
 }
 
-class VarPointer[T](protected[this] var __node : VarSignalNode[T]) extends SignalPointer(__node) {
-  override def node: VarSignalNode[T] = __node
-  override def reify[S <: Struct](reifier: Reifier[S]) = node.reify(reifier)
+class VarPointer[A](__node : VarSignalNode[A]) extends SignalPointer(__node) {
+  override def node: Option[VarSignalNode[A]] = graph.resolvePointer(this) match {
+    case Some(n) => Some(n.asInstanceOf[VarSignalNode[A]])
+    case None => None
+  }
 
-  def set(value: T) = node.set(value)
+  override def reify[S <: Struct](reifier: Reifier[S]) = node.get.reify(reifier)
+
+  def set(value: A) = node.get.set(value)
 }
 
-class ObservePointer[T](protected[this] var _node : ObserveNode[T]) extends DataFlowPointer[Unit] {
-  override def node: ObserveNode[T] = _node
-  def reify[S <: Struct](reifier: Reifier[S]) = node.reify(reifier)
+class ObservePointer[T](_node : ObserveNode[T]) extends DataFlowPointer[Unit] {
+  override protected[meta] var graph : DataFlowGraph = _node.graph
+  graph.registerPointer(this, _node)
+
+  override def node: Option[ObserveNode[T]] = graph.resolvePointer(this) match {
+    case Some(n) => Some(n.asInstanceOf[ObserveNode[T]])
+    case None => None
+  }
+
+  def reify[S <: Struct](reifier: Reifier[S]) = node.get.reify(reifier)
 }
