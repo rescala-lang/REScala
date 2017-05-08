3f66272c9d63dfe4fcf3aa083ccda054a25a7ba5
Joscha Drechsler <drechsler@cs.tu-darmstadt.de>
2017-05-08 09:46:00 +0200

    merged

--- Research/Multiversion/src/main/scala/rescala/fullmv/FramingBranchResult.scala
+++ Research/Multiversion/src/main/scala/rescala/fullmv/FramingBranchResult.scala
@@ -3,26 +3,26 @@ package rescala.fullmv
 import rescala.graph.Reactive
 
 sealed trait FramingBranchResult[+R] {
-  def processBranching(txn: FullMVTurn, taskPool: TaskPool): Unit
+  val branchDelta: Int
+  def submitToPool(txn: FullMVTurn, taskPool: TaskPool): Unit
 }
 
 object FramingBranchResult {
   case object FramingBranchEnd extends FramingBranchResult[Nothing] {
-    override def processBranching(txn: FullMVTurn, taskPool: TaskPool): Unit = {
-//      txn.branches(-1)
-    }
+    override val branchDelta = -1
+    override def submitToPool(txn: FullMVTurn, taskPool: TaskPool): Unit = {}
   }
 
   case class FramingBranchOut[R](out: Set[Reactive[FullMVStruct]]) extends FramingBranchResult[R] {
-    override def processBranching(txn: FullMVTurn, taskPool: TaskPool): Unit = {
-//      if (out.size != 1) txn.branches(out.size - 1)
+    override val branchDelta: Int = out.size - 1
+    override def submitToPool(txn: FullMVTurn, taskPool: TaskPool): Unit = {
       out.foreach { node => taskPool.addFraming(node, txn) }
     }
   }
 
   case class FramingBranchOutSuperseding[R](out: Set[Reactive[FullMVStruct]], supersede: FullMVTurn) extends FramingBranchResult[R] {
-    override def processBranching(txn: FullMVTurn, taskPool: TaskPool): Unit = {
-//      if (out.size != 1) txn.branches(out.size - 1)
+    override val branchDelta: Int = out.size - 1
+    override def submitToPool(txn: FullMVTurn, taskPool: TaskPool): Unit = {
       out.foreach { node => taskPool.addSupersedingFraming(node, txn, supersede) }
     }
   }
--- Research/Multiversion/src/main/scala/rescala/fullmv/FullMVTurn.scala
+++ Research/Multiversion/src/main/scala/rescala/fullmv/FullMVTurn.scala
@@ -1,7 +1,10 @@
 package rescala.fullmv
 
+import java.util.concurrent.atomic.AtomicInteger
+
 import rescala.engine.{EngineImpl, InitializationImpl, ValuePersistency}
 import rescala.graph.{Pulsing, Reactive, Struct}
+import rescala.fullmv.FramingBranchResult.FramingBranchEnd
 
 trait FullMVStruct extends Struct {
   override type State[P, S <: Struct] = NodeVersionHistory[P]
@@ -14,18 +17,27 @@ class FullMVEngine extends EngineImpl[FullMVStruct, FullMVTurn] {
 }
 
 class FullMVTurn(val sgt: SerializationGraphTracking) extends InitializationImpl[FullMVStruct] {
+  val activeBranches = new AtomicInteger()
+
   def incrementFrame(node: Reactive[FullMVStruct]): Unit = {
-    // TODO
-    //val branching = node.state.incrementFrame(this)
+    processBranching(node.state.incrementFrame(this))
   }
+
   def incrementSupersedeFrame(node: Reactive[FullMVStruct], superseded: FullMVTurn): Unit = {
-    // TODO
-    //val branching = node.state.incrementSupersedeFrame(this, superseded)
+    processBranching(node.state.incrementSupersedeFrame(this, superseded))
+  }
+
+  def processBranching(result: FramingBranchResult[Reactive[FullMVStruct]]): Unit = {
+    activeBranches.getAndAdd(result.branchDelta)
+    result match {
+      case FramingBranchEnd =>
+      case FramingBranchOut()
+    }
   }
 
   def notify(node: Reactive[FullMVStruct], changed: Boolean, maybeFollowFrame: Option[FullMVTurn]): Unit = {
+    val notificationResultAction = node.state.notify(this, changed, maybeFollowFrame)
     // TODO
-    //val notificationResultAction = node.state.notify(this, changed, maybeFollowFrame)
   }
 
   override protected def makeStructState[P](valuePersistency: ValuePersistency[P]): NodeVersionHistory[P] = new NodeVersionHistory(sgt, this, valuePersistency.initialValue)
@@ -34,8 +46,6 @@ class FullMVTurn(val sgt: SerializationGraphTracking) extends InitializationImpl
   override private[rescala] def dynamicDependencyInteraction(reactive: Reactive[FullMVStruct]) = reactive.state.ensureReadVersion(this)
   override private[rescala] def before[P](pulsing: Pulsing[P, FullMVStruct]) = pulsing.state.before(this)
   override private[rescala] def after[P](pulsing: Pulsing[P, FullMVStruct]) = pulsing.state.after(this)
-
-
 
   override def observe(f: () => Unit): Unit = f()
 }
