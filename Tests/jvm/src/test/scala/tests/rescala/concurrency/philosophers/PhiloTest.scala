970da22756bf674479e1ab272b62486ff963942c
Joscha Drechsler <drechsler@cs.tu-darmstadt.de>
2017-05-29 16:38:30 +0200

    stabilizing a couple of tests against engines with branch-parallelism and better debug output for PhiloTest in case of thread failures

--- Tests/jvm/src/test/scala/tests/rescala/concurrency/philosophers/PhiloTest.scala
+++ Tests/jvm/src/test/scala/tests/rescala/concurrency/philosophers/PhiloTest.scala
@@ -7,6 +7,9 @@ import rescala.fullmv.FullMVEngine
 import rescala.graph.Struct
 import tests.rescala.concurrency.Spawn
 
+import scala.concurrent.TimeoutException
+import scala.util.{Failure, Success, Try}
+
 class PhiloTest extends FunSuite {
   def `eat!`[S <: Struct](engine: Engine[S, Turn[S]], dynamic: Boolean): Unit = {
     val size = 3
@@ -26,8 +29,20 @@ class PhiloTest extends FunSuite {
     println(s"philo party sleeping on $engine (dynamic $dynamic)")
     Thread.sleep(1000)
     cancel = true
-    threads.foreach(_.join(1000))
-    println(s"philo party done sleeping on $engine (dynamic $dynamic)")
+    val threadResults = threads.map(t => (t, Try{t.join(1000)}))
+    println(s"philo party done sleeping on $engine (dynamic $dynamic), score: ${table.eaten.get()}")
+
+    val threadFailures = threadResults.filter{
+      case (thread, Failure(e: TimeoutException)) =>
+        System.err.println(s"Thread $thread timed out.")
+        true
+      case (thread, Failure(e)) =>
+        System.err.println(s"Thread $thread failed:")
+        e.printStackTrace()
+        true
+      case (_, Success(_)) => false
+    }
+    assert(threadFailures.isEmpty, threadFailures.size + " threads failed.")
   }
 
   test("eating Contests Spinning") {`eat!`(Engines.parrp, dynamic = false)}
