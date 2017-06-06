10732d6a2eb2f332462366d62b0e176a997f29d0
ragnar <ragnar@rmgk.de>
2017-06-06 16:02:07 +0200

    change default parameters for creation benchmark

--- Research/Microbenchmarks/src/main/scala/benchmarks/simple/Creation.scala
+++ Research/Microbenchmarks/src/main/scala/benchmarks/simple/Creation.scala
@@ -10,9 +10,9 @@ import rescala.reactives._
 
 @BenchmarkMode(Array(Mode.Throughput))
 @OutputTimeUnit(TimeUnit.MILLISECONDS)
-@Warmup(iterations = 5, time = 1000, timeUnit = TimeUnit.MILLISECONDS)
-@Measurement(iterations = 5, time = 1000, timeUnit = TimeUnit.MILLISECONDS)
-@Fork(1)
+@Warmup(iterations = 3, time = 1000, timeUnit = TimeUnit.MILLISECONDS)
+@Measurement(iterations = 3, time = 1000, timeUnit = TimeUnit.MILLISECONDS)
+@Fork(3)
 @Threads(1)
 @State(Scope.Thread)
 class Creation[S <: rescala.graph.Struct] {
