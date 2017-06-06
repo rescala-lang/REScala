501f5987c0c0b837957f2d72663b2b4b61c73e06
ragnar <ragnar@rmgk.de>
2017-06-06 16:34:55 +0200

    add type annotation

--- Main/shared/src/main/scala/rescala/engine/TicketOrEngine.scala
+++ Main/shared/src/main/scala/rescala/engine/TicketOrEngine.scala
@@ -3,9 +3,9 @@ package rescala.engine
 import rescala.graph.{OutsidePropagationTicket, Struct}
 
 object TicketOrEngine extends LowPriorityTicketOrEngine {
-  implicit def fromTicket[S <: Struct](implicit ticket: OutsidePropagationTicket[S]) = TicketOrEngine(Left(ticket))
+  implicit def fromTicket[S <: Struct](implicit ticket: OutsidePropagationTicket[S]): TicketOrEngine[S] = TicketOrEngine(Left(ticket))
 }
 sealed trait LowPriorityTicketOrEngine {
-  implicit def fromEngine[S <: Struct](implicit engine: Engine[S, _ <: Turn[S]]) = TicketOrEngine(Right(engine))
+  implicit def fromEngine[S <: Struct](implicit engine: Engine[S, _ <: Turn[S]]): TicketOrEngine[S] = TicketOrEngine(Right(engine))
 }
 case class TicketOrEngine[S <: Struct](e: Either[OutsidePropagationTicket[S], Engine[S, _ <: Turn[S]]])
