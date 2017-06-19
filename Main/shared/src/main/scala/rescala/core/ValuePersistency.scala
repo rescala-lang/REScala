78f5328b8d5476eb85a614846cfbc9b12d2ccf89
ragnar <ragnar@rmgk.de>
2017-06-19 18:28:55 +0200

    fix wrong cast in ValuePersistency

--- Main/shared/src/main/scala/rescala/core/ValuePersistency.scala
+++ Main/shared/src/main/scala/rescala/core/ValuePersistency.scala
@@ -14,9 +14,9 @@ object ValuePersistency {
   // any effect, we simply set all events to just be reevaluated upon ignition unconditionally,
   // which ensures that dynamic events work correctly and doesn't hurt others.
   private object _Event extends ValuePersistency[Pulse[Nothing]](Pulse.NoChange, isTransient = true, ignitionRequiresReevaluation = true)
-  def Event[V]: ValuePersistency[V] = _Event.asInstanceOf[ValuePersistency[V]]
+  def Event[V]: ValuePersistency[Pulse[V]] = _Event.asInstanceOf[ValuePersistency[Pulse[V]]]
   private object _DerivedSignal extends ValuePersistency[Pulse.Change[Nothing]](Pulse.empty, isTransient = false, ignitionRequiresReevaluation = true)
-  def DerivedSignal[V]: ValuePersistency[V] = _DerivedSignal.asInstanceOf[ValuePersistency[V]]
+  def DerivedSignal[V]: ValuePersistency[Pulse[V]] = _DerivedSignal.asInstanceOf[ValuePersistency[Pulse[V]]]
 
   case class InitializedSignal[V: ReSerializable](override val initialValue: Pulse.Change[V])
     extends ValuePersistency[Pulse[V]](initialValue, isTransient = false, ignitionRequiresReevaluation = false) {
