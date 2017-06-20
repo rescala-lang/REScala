4368f2c6641f8385af45196a270b945917ba4b3c
ragnar <ragnar@rmgk.de>
2017-06-20 12:09:47 +0200

    remove old comments

--- Main/shared/src/main/scala/rescala/core/Turn.scala
+++ Main/shared/src/main/scala/rescala/core/Turn.scala
@@ -11,37 +11,6 @@ trait Turn[S <: Struct] {
   private[rescala] def makeAdmissionPhaseTicket(): AdmissionTicket[S]
   private[rescala] def makeWrapUpPhaseTicket(): WrapUpTicket[S]
 
-//  /**
-//    * Synchronize for access (i.e., [[before]] or [[after]]) on this node when
-//    * synchronization is unknown. Multiple invocations are redundant, but not harmful outside of an
-//    * implementation-dependent performance penalty.
-//    *
-//    * @param reactive the reactive to be dynamically accessed
-//    */
-//  private[rescala] def dynamicDependencyInteraction(reactive: Reactive[S]): Unit
-//
-//  /**
-//    * Read value from before this turn. Only call this if you know that you are synchronized with this node:
-//    * Reads on dependencies where an edge exists (e.g., reading a static dependency) is always synchronized.
-//    * Reads on other nodes must be synchronized through dynamicDependencyInteraction first.
-//    * @param pulsing the node to be read
-//    * @tparam P the node's storage type
-//    * @return the stored value from before this turn
-//    */
-//  private[rescala] def before[P](pulsing: Pulsing[P, S]): P
-//  /**
-//    * Read value from after this turn. Implementations may return the node's current value, including
-//    * changes already made by this turn but disregarding potential future changes, or may suspend to
-//    * return only the value that is final until the end of this turn.
-//    * Only call this if you know that you are synchronized with this node:
-//    * Reads on dependencies where an edge exists (e.g., reading a static dependency) is always synchronized.
-//    * Reads on other nodes must be synchronized through dynamicDependencyInteraction first.
-//    * @param pulsing the node to be read
-//    * @tparam P the node's storage type
-//    * @return the stored value from after this turn
-//    */
-//  private[rescala] def after[P](pulsing: Pulsing[P, S]): P
-
   /**
     * Registers a new handler function that is called after all changes were written and committed.
     *
@@ -49,7 +18,6 @@ trait Turn[S <: Struct] {
     */
   private[rescala] def observe(f: () => Unit): Unit
 }
-
 
 
 
