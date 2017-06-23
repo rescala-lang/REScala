cd1c9b9c2ba0c5259d7e33cfe794e75c6e007f41
ragnar <ragnar@rmgk.de>
2017-06-23 10:42:37 +0200

    remove ;

--- TestTools/shared/src/main/scala/rescala/testtools/SetAndExtractTransactionHandle.scala
+++ TestTools/shared/src/main/scala/rescala/testtools/SetAndExtractTransactionHandle.scala
@@ -2,7 +2,6 @@ package rescala.testhelper
 
 import rescala.core.{Creation, Engine, Struct}
 import rescala.reactives.Source
-;
 
 object SetAndExtractTransactionHandle {
   def apply[A, S <: Struct](source: Source[A, S], value: A)(implicit engine: Engine[S]): Creation[S] = {
