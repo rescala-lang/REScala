3ec715486f34dcac142236ec510751ea4fd8b092
ragnar <ragnar@rmgk.de>
2017-07-07 18:07:12 +0200

    fiddle more with serializers

--- Main/js/src/main/scala/rescala/package.scala
+++ Main/js/src/main/scala/rescala/package.scala
@@ -1,5 +1,8 @@
+import rescala.core.ReSerializable
 import rescala.levelbased.SimpleStruct
 
 package object rescala extends RescalaDefaultImports[SimpleStruct] {
   override implicit def explicitEngine: Engines.SimpleEngine = rescala.Engines.default
+  implicit def doNotSerialize[T]: ReSerializable[T] = ReSerializable.doNotSerialize
+
 }
--- Main/jvm/src/main/scala/rescala/package.scala
+++ Main/jvm/src/main/scala/rescala/package.scala
@@ -1,6 +1,7 @@
-import rescala.core.Engine
+import rescala.core.{Engine, ReSerializable}
 import rescala.parrp.ParRP
 
 package object rescala extends RescalaDefaultImports[ParRP] {
   override def explicitEngine: Engine[ParRP] = rescala.Engines.parrp
+  implicit def doNotSerialize[T]: ReSerializable[T] = ReSerializable.doNotSerialize
 }
