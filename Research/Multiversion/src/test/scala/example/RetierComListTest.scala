97b66fdcbeafd4d97d5891be0427560db7810d1b
Sören Albrecht <soren.albrecht@gmail.com>
2017-08-23 13:58:34 +0200

    update to RetComListTest

--- Research/Multiversion/src/test/scala/example/RetierComListTest.scala
+++ Research/Multiversion/src/test/scala/example/RetierComListTest.scala
@@ -9,10 +9,13 @@ import scala.concurrent.duration.Duration
 import scala.concurrent.{Await, Future}
 
 import rescala.fullmv.transmitter.SignalTransmittable._
+import rescala.fullmv.transmitter.EventTransmittable._
+
 
 object Bindings1 {
-  val eventBinding = Binding [Evt[Int]]("listAdd")
+  //val eventBinding = Binding [Evt[Int]]("listEvt")
   val variableBinding1 = Binding[Signal[List[Int]]]("variable")
+  val addBinding = Binding [Int => Unit]("listAdd")
 }
 //This method represents the server
 object Server extends App {
@@ -20,15 +23,18 @@ object Server extends App {
   var testList1 = Var( List(1,2,3))
   def add(x: Int) = { testList1()= testList1.now :+ x}
 
-  val e0 =  Evt[Int]
-  e0 += add
-  e0(10)
+
+  //val e0 =  Evt[Int]
+  //e0 += add
+  //e0(10)
+
 
   val registry = new Registry
   registry.listen(TCP(1099))
 
   registry.bind(Bindings1.variableBinding1)(testList1)
-  registry.bind(Bindings1.eventBinding)(e0)
+  //registry.bind(Bindings1.eventBinding)(e0)
+  registry.bind(Bindings1.addBinding)(add)
 
   testList1 observe println
 
@@ -44,12 +50,20 @@ object Client extends App {
 
   import scala.concurrent.ExecutionContext.Implicits._
 
+
   val listOnServer: Signal[List[Int]] = Await result (registry.lookup(Bindings1.variableBinding1, remote), Duration.Inf)
   listOnServer observe println
 
   var input =""
   var continueProgram = true
-  //val e0 :  Future[Evt[Int]] = registry.lookup(Bindings1.eventBinding, remote)
+
+  //val e0: Future[rescala.Evt[Int]] = registry.lookup(Bindings1.eventBinding, remote)
+
+  val add: Int => Future [Unit] =  registry.lookup(Bindings1.addBinding, remote)
+  val e1 = Evt[Int]
+  def add1(x:Int) = {add(x)}
+  e1 += add1
+
   while (continueProgram) {
     println("enter \"add\" to add a vaule or \"end\" to end")
     input = scala.io.StdIn.readLine()
@@ -59,9 +73,9 @@ object Client extends App {
       println("enter a value")
       input = scala.io.StdIn.readLine()
       try
-        {
-          //e0(1)
-        }
+      {
+        e1(input.toInt)
+      }
       catch
       {
         case e:Exception => println("please enter a valid number")
