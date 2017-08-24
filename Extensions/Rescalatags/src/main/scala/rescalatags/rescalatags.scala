44de23bf8c2a4b6f6d0c27e6269fa1efd3e5154c
ragnar <ragnar@rmgk.de>
2017-08-24 14:18:19 +0200

    somme scalatags implicit cleanups

--- Extensions/Rescalatags/src/main/scala/rescalatags/rescalatags.scala
+++ Extensions/Rescalatags/src/main/scala/rescalatags/rescalatags.scala
@@ -1,6 +1,6 @@
 import org.scalajs.dom
-import rescala.core.{CreationTicket, Engine, Struct}
-
+import org.scalajs.dom.Node
+import rescala.core.{Creation, CreationTicket, Engine, Struct}
 import rescala.reactives.Signals.Diff
 import rescala.reactives.{Observe, Signal}
 
@@ -13,27 +13,30 @@ package object rescalatags {
     /**
       * converts a Signal of a scalatags Tag to a scalatags Frag which automatically reflects changes to the signal in the dom
       */
-    def asFrag(implicit ticket: CreationTicket[S], engine: Engine[S]): Frag = {
-      ticket { implicit turn =>
-        val result: Signal[dom.Node, S] = signal
-          .map(_.render)
-          .recover { case t => span(t.toString).render }
-          .withDefault("".render)
-
-        val observer = Observe.weak(result.change)(
-          { case Diff(lastTag, newTag) =>
-            if (lastTag.parentNode != null && !scalajs.js.isUndefined(lastTag.parentNode)) {
-              lastTag.parentNode.replaceChild(newTag, lastTag)
-            }
-          },
-          t => throw t)
-        new REFrag(result, observer)
-      }
+    def asFrag(implicit ct: CreationTicket[S], engine: Engine[S]): Frag = {
+      ct { creation => asFragInner(engine)(creation) }
     }
 
-    class REFrag(rendered: Signal[dom.Node, S], val observe: Observe[S])(implicit engine: Engine[S]) extends Frag {
-      def applyTo(t: dom.Element) = t.appendChild(rendered.now)
-      def render: dom.Node = rendered.now
+    private def asFragInner(engine: Engine[S])(implicit creation: Creation[S]): REFrag = {
+      val result: Signal[Node, S] = signal
+        .map(_.render)
+        .recover { case t => span(t.toString).render }
+        .withDefault("".render)
+
+      val observer = Observe.weak(result.change)(
+        { case Diff(lastTag, newTag) =>
+          if (lastTag.parentNode != null && !scalajs.js.isUndefined(lastTag.parentNode)) {
+            lastTag.parentNode.replaceChild(newTag, lastTag)
+          }
+        },
+        t => throw t)
+      new REFrag(result, observer, engine)
+    }
+
+
+    private class REFrag(rendered: Signal[dom.Node, S], val observe: Observe[S], engine: Engine[S]) extends Frag {
+      def applyTo(t: dom.Element) = t.appendChild(rendered.now(engine, implicitly))
+      def render: dom.Node = rendered.now(engine, implicitly)
     }
   }
 
