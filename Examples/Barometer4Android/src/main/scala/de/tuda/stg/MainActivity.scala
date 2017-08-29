b8e78605c472a38f26833ba77829583244b4a624
I. Dix <isabella.dix@t-online.de>
2017-08-29 12:19:51 +0200

    removed testing code from Main Activity

--- Examples/Barometer4Android/src/main/scala/de/tuda/stg/MainActivity.scala
+++ Examples/Barometer4Android/src/main/scala/de/tuda/stg/MainActivity.scala
@@ -18,9 +18,6 @@ class MainActivity extends AppCompatActivity {
     super.onCreate(savedInstanceState)
     Log.d(TAG, "Started ReSensor MainActivity")
 
-    // nice Activity View, if connected to Monitor
-    //    makeNiceMonitorDisplay()
-
     ReSensorManager.init(this.getApplicationContext)
     // get temperature sensor (requires cast)
     val temperatureSensor: ReTemperatureSensor =
@@ -28,29 +25,16 @@ class MainActivity extends AppCompatActivity {
 
     alphNumDisplay = new ReAlphaNumericDisplay(temperatureSensor.valueChanged)
     alphNumDisplay.init
-
-    //    // turn display off and on again and simulate stop (all for testing reasons)
-    //    import scala.concurrent.Future
-    //    import scala.concurrent.ExecutionContext.Implicits.global
-    //    val future = Future {
-    //      Thread.sleep(3000)
-    //      alphNumDisplay.turnOff
-    //
-    //      Thread.sleep(3000)
-    //      alphNumDisplay.turnOn
-    //
-    //      //      Thread.sleep(3000)
-    //      //      simulateStop()
-    //    }
   }
 
-  // actual method called on stopped activity
+
   override def onDestroy(): Unit = {
     super.onDestroy()
     Log.d(TAG, "Destroying ReSensor MainActivity")
 
     destroy()
   }
+
 
   def destroy(): Unit = {
     // remove sensors
@@ -59,20 +43,4 @@ class MainActivity extends AppCompatActivity {
     // turn off display
     alphNumDisplay.destroy()
   }
-
-  // simulate stopping the activity (only for testing)
-  //  def simulateStop(): Unit = {
-  //    destroy()
-  //  }
-
-  //  private def makeNiceMonitorDisplay(): Unit = {
-  //  import android.graphics.drawable.Animatable
-  //    val vh: TypedViewHolder.main =
-  //      TypedViewHolder.setContentView(this, TR.layout.main).asInstanceOf[TypedViewHolder.main]
-  //    vh.text.setText(s"Hello world, from ${TR.string.app_name.value}")
-  //    vh.image.getDrawable match {
-  //      case a: Animatable => a.start()
-  //      case _ => // not animatable
-  //    }
-  //  }
 }
