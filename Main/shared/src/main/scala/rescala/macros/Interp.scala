95f43704de9df82b68e744b7543f756844d39fc0
ragnar <ragnar@rmgk.de>
2018-02-25 18:39:24 +0100

    accessed type is called A

--- Main/shared/src/main/scala/rescala/macros/Interp.scala
+++ Main/shared/src/main/scala/rescala/macros/Interp.scala
@@ -5,25 +5,25 @@ import rescala.core.{ReSource, Struct}
 import scala.annotation.compileTimeOnly
 
 /** Common macro accessors for [[rescala.reactives.Signal]] and [[rescala.reactives.Event]]
-  * @tparam T return type of the accessor
+  * @tparam A return type of the accessor
   * @groupname accessor Accessor and observers */
-trait Interp[+T, S <: Struct] extends ReSource[S] {
+trait Interp[+A, S <: Struct] extends ReSource[S] {
   /** Makes the enclosing reactive expression depend on the current value of the reactive.
     * Is an alias for [[value]].
     * @group accessor
     * @see value*/
   @compileTimeOnly(s"$this apply can only be used inside of reactive expressions")
-  final def apply(): T = throw new IllegalAccessException(s"$this.apply called outside of macro")
+  final def apply(): A = throw new IllegalAccessException(s"$this.apply called outside of macro")
 
   /** Makes the enclosing reactive expression depend on the current value of the reactive.
     * Is an alias for [[apply]].
     * @group accessor
     * @see apply*/
   @compileTimeOnly("value can only be used inside of reactive expressions")
-  final def value: T = throw new IllegalAccessException(s"$this.value called outside of macro")
+  final def value: A = throw new IllegalAccessException(s"$this.value called outside of macro")
 
   /** Interprets the internal type to the external type
     * @group internal */
-  def interpret(v: Value): T
+  def interpret(v: Value): A
 
 }
