a547a4372a61e62b4422b9a024ff7a26f1636aca
Julian Haas <github@julian-haas.de>
2018-06-19 20:01:05 +0200

    First try for a universal pVar Transmittable (not working)

--- Extensions/crdts/src/main/scala/rescala/crdts/pvars/PSet.scala
+++ Extensions/crdts/src/main/scala/rescala/crdts/pvars/PSet.scala
@@ -1,11 +1,13 @@
 package rescala.crdts.pvars
 
+import loci.transmitter.{Serializable, Transmittable}
 import rescala._
+import rescala.crdts.pvars.Publishable.{PVarFactory, PVarTransmittable}
 import rescala.crdts.statecrdts.sets.ORSet
 
 case class PSet[A](initial: ORSet[A] = ORSet(),
                    internalChanges: Evt[ORSet[A]] = Evt[ORSet[A]],
-                   externalChanges: Evt[ORSet[A]] = Evt[ORSet[A]]) extends Publishable[Set[A],ORSet[A]] {
+                   externalChanges: Evt[ORSet[A]] = Evt[ORSet[A]]) extends Publishable[Set[A], ORSet[A]] {
 
   def add(a: A): Unit = internalChanges.fire(crdtSignal.readValueOnce.add(a))
 
@@ -21,5 +23,13 @@ object PSet {
   def apply[A](values: Set[A]): PSet[A] = {
     val init: ORSet[A] = ORSet().fromValue(values)
     new PSet[A](init)
+  }
+
+  implicit def pSetTransmittable[A, S](implicit transmittable: Transmittable[ORSet[A], S, ORSet[A]],
+                                       serializable: Serializable[S], pVarFactory: PVarFactory[PSet[A]]) = {
+    implicit object PSetFactory extends PVarFactory[PSet[A]] {
+      override def apply(): PSet[A] = PSet[A](Set[A]())
+    }
+    new PVarTransmittable[S, ORSet[A], PSet[A]]
   }
 }
