656a81385be9e5112ee68fc6a9cc5fa532bcb0c8
Julian Haas <github@julian-haas.de>
2018-07-15 11:15:12 +0200

    test vertex serialization and new publishables

--- Extensions/crdts/src/test/scala/rescala/crdts/ManualCRDTTests.scala
+++ Extensions/crdts/src/test/scala/rescala/crdts/ManualCRDTTests.scala
@@ -1,6 +1,8 @@
 package rescala.crdts
 
+import io.circe.Decoder.Result
 import io.circe.generic.auto._
+import io.circe.syntax._
 import loci.communicator.ws.akka.WS
 import loci.registry.{Binding, Registry}
 import loci.serializer.circe._
@@ -10,33 +12,65 @@ import rescala.crdts.pvars.Publishable._
 import scala.concurrent.duration.Duration
 import scala.concurrent.{Await, Future}
 import rescala.crdts.pvars._
-
-import scala.language.higherKinds
-
+import rescala.crdts.statecrdts.sequences._
+import rescala.crdts.statecrdts.StateCRDT
 
 //noinspection ScalaUnusedSymbol
 object testSignalExpressions {
   def main(args: Array[String]): Unit = {
 
 
+    //var r = RGA("1","2")
+    //var s = RGA[String]()
+
+    val p = Vertex(4)
+    val x = Vertex(5)
+    val j = Vertex.start
+    val k = Vertex.start[Int]
+    println(j == k)
+    val l = List(p, x, j)
+    val f = Map[Vertex[Int], Vertex[Int]]() + (k -> x) + (x -> p)
+    val json = f.asJson
+    println(json)
+    val m: Result[Map[Vertex[Int], Vertex[Int]]] = json.as[Map[Vertex[Int], Vertex[Int]]]
+    println(f)
+    println(m.getOrElse("error"))
+
+    def merge[A, F](a: F, b: F)(implicit stateCRDT: StateCRDT[A, F]) = {
+      stateCRDT.merge(a, b)
+    }
+
+    //s = merge(r,s)
+
+    // r = r.addRight(Vertex.start, "4")
+    // s = s.addRight(Vertex.start, "5")
+
+    // println(merge(r,s).value)
 
 
-
-
+    // test all bindings
+    val counterBinding = Binding[PGrowOnlyCounter]("counter")
+    val logBinding = Binding[PGrowOnlyLog[String]]("log")
+    val gsetBinding = Binding[PGrowOnlySet[Int]]("gset")
     val setBinding = Binding[PSet[Int]]("set")
-    /* val counterBinding = Binding[PGrowOnlyCounter]("counter")(BindingBuilder.value[PGrowOnlyCounter](
+    val vertexListBinding = Binding[PVertexList[Int]]("vertexList")
+
+    //Binding[PGrowOnlyLog[Int]]("log")(BindingBuilder.value[PGrowOnlyLog[Int]])
+    // implicitly[Serializable[RGA[Int]]]
+    /*
+    val counterBinding = Binding[PGrowOnlyCounter]("counter")(BindingBuilder.value[PGrowOnlyCounter](
       Marshallable.marshallable[GCounter, Int, GCounter](
-        PGrowOnlyCounter.pGrowOnlyCounterTransmittable.asInstanceOf[Transmittable[GCounter,Int,GCounter]], implicitly[Serializable[Int]]
-      ))
-//    */
+        Publishable.PVarTransmittable[GCounter,PGrowOnlyCounter].asInstanceOf[Transmittable[GCounter,Int,GCounter]], implicitly[Serializable[Int]]
+      )))
+    */
     println("running server")
 
     val (sr, s2) = { //server
       val registry = new Registry
       registry.listen(WS(1099))
 
-      val l1 = PSet(Set(10))
-      registry.bind(setBinding)(l1)
+      val l1 = PGrowOnlyLog(List("a"))
+      registry.bind(logBinding)(l1)
 
       (registry, l1)
     }
@@ -62,23 +96,22 @@ object testSignalExpressions {
       val registry = new Registry
       val connection: Future[RemoteRef] = registry.connect(WS("ws://localhost:1099/"))
       val remote: RemoteRef = Await.result(connection, Duration.Inf)
-      val subscribedSig: Future[PSet[Int]] = registry.lookup(setBinding, remote)
-      val set: PSet[Int] = Await.result(subscribedSig, Duration.Inf)
+      val subscribedSig: Future[PGrowOnlyLog[String]] = registry.lookup(logBinding, remote)
+      val set: PGrowOnlyLog[String] = Await.result(subscribedSig, Duration.Inf)
       (registry, set)
     }
 
     println("done")
-    s2.add(20)
-    c1.add(30)
+    s2.append("b")
+    c1.append("c")
 
     Thread.sleep(1000)
     Thread.sleep(1000)
 
     println("s2: " + s2.value)
     println("c1: " + c1.value)
-    println("s2 == c1? " + (s2.value ==  c1.value))
+    println("s2 == c1? " + (s2.value == c1.value))
     println("slept")
     sr.terminate()
   }
 }
-
