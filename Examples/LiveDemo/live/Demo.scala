79d9bfddcb5601ca11a64a2feebfcfd0d9358d7a
ragnar <ragnar@rmgk.de>
2018-10-30 14:45:18 +0100

    remove unused definitions

--- Examples/LiveDemo/live/Demo.scala
+++ Examples/LiveDemo/live/Demo.scala
@@ -20,27 +20,6 @@ object Demo {
   implicit val storingEngine: LocalStorageStore = new LocalStorageStore()
   import storingEngine._
 
-  implicit val taskDecoder: io.circe.Decoder[Task] = io.circe.Decoder.decodeTuple2[Var[String], Var[Boolean]].map { case (desc, done) =>
-    new Task(desc, done)
-  }
-  implicit val taskEncoder: io.circe.Encoder[Task] = io.circe.Encoder.encodeTuple2[Var[String], Var[Boolean]].contramap{ t =>
-    (t.desc, t.done)
-  }
-
-  class Task(val desc : Var[String], val done : Var[Boolean]) {
-    val editing = Var(false)(ReSerializable.doNotSerialize, implicitly)
-  }
-  object Task {
-    def apply(desc: String, done: Boolean) = {
-      val rn = s"Task(${ThreadLocalRandom.current().nextLong().toHexString})"
-      val descV = Var(desc)(implicitly, rn.toString)
-      val doneV = Var(done)(implicitly, rn.toString + "b")
-      storingEngine.registerSource(descV)
-      storingEngine.registerSource(doneV)
-      new Task(descV, doneV)
-    }
-  }
-
   @JSExportTopLevel("live.Demo.main")
   def main(): Unit = {
     ChromeDebuggerInterface.setup(storingEngine)
@@ -58,7 +37,6 @@ object Demo {
       val aggregated = Signal {
         aggregation.value(history.value)
       }
-
 
       div(
         `class` := "todoapp",
