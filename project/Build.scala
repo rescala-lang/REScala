66e23eb776902f925523fe4f8baf626df8ea23cf
ragnar <ragnar@rmgk.de>
2019-01-31 13:22:12 +0100

    update stuff

--- project/Build.scala
+++ project/Build.scala
@@ -1,10 +1,6 @@
-import sbt._
-import sbt.Keys._
-import org.scalajs.sbtplugin.ScalaJSPlugin
-import org.scalajs.sbtplugin.ScalaJSPlugin.autoImport._
-import sbtcrossproject.CrossPlugin
-import sbtcrossproject.CrossPlugin.autoImport._
 import org.portablescala.sbtplatformdeps.PlatformDepsPlugin.autoImport._
+import sbt.Keys._
+import sbt._
 
 
 object Settings {
@@ -32,6 +28,8 @@ object Settings {
     "-Ywarn-numeric-widen",
     //"-Ywarn-value-discard" ,
     )
+  
+  val compileWithStrictScalacOptions = Compile / compile / scalacOptions ++= strictScalacOptions
 }
 
 object Resolvers {
@@ -41,25 +39,27 @@ object Resolvers {
 }
 
 object Dependencies {
-  val akkaHttp = libraryDependencies ++= Seq("akka-http-core", "akka-http").map(n => "com.typesafe.akka" %% n % "10.1.6")
+  val akkaHttp = libraryDependencies ++= Seq("akka-http-core", "akka-http").map(n => "com.typesafe.akka" %% n % "10.1.7")
   val akkaStream = libraryDependencies += ("com.typesafe.akka" %% "akka-stream" % "2.5.19")
   val betterFiles = libraryDependencies += ("com.github.pathikrit" %% "better-files" % "3.7.0")
-  val circe = libraryDependencies ++= Seq("core", "generic", "generic-extras", "parser").map(n => "io.circe" %%% s"circe-$n" % "0.11.0")
+  val circe = libraryDependencies ++= Seq("core", "generic", "generic-extras", "parser").map(n => "io.circe" %%% s"circe-$n" % "0.11.1")
   val decline = libraryDependencies += ("com.monovore" %% "decline" % "0.5.1")
+  val fastparse = libraryDependencies += "com.lihaoyi" %%% "fastparse" % "2.1.0"
   val fontawesome = libraryDependencies += ("org.webjars" % "font-awesome" % "5.3.1")
   val jsoup = libraryDependencies += ("org.jsoup" % "jsoup" % "1.11.3")
   val lociDependency = Def.setting((n: String) => "de.tuda.stg" %%% s"scala-loci-$n" % "0.2.0")
   val lociCommunication = libraryDependencies ++= Seq("communication", "communicator-ws-akka", "communicator-tcp").map(lociDependency.value)
   val lociCommunicationCirce =  libraryDependencies += lociDependency.value("serializer-circe")
   val lociCommunicationUpickle =  libraryDependencies += lociDependency.value("serializer-upickle")
+  val pprint = libraryDependencies += "com.lihaoyi" %%% "pprint" % "0.5.3"
   val purecss = libraryDependencies += ("org.webjars.npm" % "purecss" % "1.0.0")
   val rmgkLogging = libraryDependencies += ("de.rmgk" %%% "logging" % "0.2.1")
   val scalacheck = libraryDependencies += ("org.scalacheck" %% "scalacheck" % "1.14.0" % "test")
   val scalactic = libraryDependencies += ("org.scalactic" %% "scalactic" % "3.0.5")
   val scalajsdom = libraryDependencies += ("org.scala-js" %%% "scalajs-dom" % "0.9.6")
+  val scalaswing = libraryDependencies += "org.scala-lang.modules" %% "scala-swing" % "2.0.3"
   val scalatags = libraryDependencies += ("com.lihaoyi" %%% "scalatags" % "0.6.7")
   val scalatest = libraryDependencies += ("org.scalatest" %% "scalatest" % "3.0.5" % "test")
-  val scalaswing = libraryDependencies += "org.scala-lang.modules" %% "scala-swing" % "2.0.3"
-  val sourcecode = libraryDependencies += "com.lihaoyi" %%% "sourcecode" % "0.1.4"
   val scalaXml = libraryDependencies += "org.scala-lang.modules" %% "scala-xml" % "1.1.1"
+  val sourcecode = libraryDependencies += "com.lihaoyi" %%% "sourcecode" % "0.1.4"
 }
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -1,14 +1,8 @@
-logLevel := Level.Warn
-
-addSbtPlugin("pl.project13.scala" % "sbt-jmh" % "0.3.4")
-
-addSbtPlugin("org.portable-scala" % "sbt-scalajs-crossproject"      % "0.6.0")
-// addSbtPlugin("org.portable-scala" % "sbt-scala-native-crossproject" % "0.5.0")
-addSbtPlugin("org.scala-js" % "sbt-scalajs" % "0.6.25")
-// addSbtPlugin("org.scala-native"   % "sbt-scala-native"              % "0.3.7")
-
-addSbtPlugin("org.tpolecat" % "tut-plugin" % "0.6.9")
-
-addSbtPlugin("com.typesafe.sbt" % "sbt-native-packager" % "1.3.12")
-
+// https://github.com/dwijnand/sbt-dynver
 addSbtPlugin("com.dwijnand" % "sbt-dynver" % "3.1.0")
+// https://github.com/sbt/sbt-native-packager
+addSbtPlugin("com.typesafe.sbt" % "sbt-native-packager" % "1.3.15")
+addSbtPlugin("org.irundaia.sbt" % "sbt-sassify" % "1.4.12")
+addSbtPlugin("org.portable-scala" % "sbt-scalajs-crossproject"  % "0.6.0")
+addSbtPlugin("org.scala-js" % "sbt-scalajs" % "0.6.26")
+addSbtPlugin("pl.project13.scala" % "sbt-jmh" % "0.3.4")
