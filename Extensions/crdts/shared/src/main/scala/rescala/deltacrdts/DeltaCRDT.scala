10a10ec703a6288ae34ea43a03beba1f196bf00e
Julian Haas <github@julian-haas.de>
2019-03-13 23:01:22 +0100

    implement delta crdt gcounter

---
+++ Extensions/crdts/shared/src/main/scala/rescala/deltacrdts/DeltaCRDT.scala
@@ -0,0 +1,19 @@
+package rescala.deltacrdts
+
+/**
+  *
+  * @tparam A type of the CRDT
+  * @tparam D type of the delta mutations
+  */
+trait DeltaCRDT[A, D] {
+  /**
+    * Applies a delta mutation to a CRDT
+    * @return
+    */
+  def applyΔ(crdt: A, delta: D): A
+}
+
+object DeltaCRDT {
+  def apply[A,D](implicit ev: DeltaCRDT[A,D]): DeltaCRDT[A,D] = ev
+  def applyΔ[A,D](implicit ev: DeltaCRDT[A,D], crdt: A, delta: D) = apply[A,D].applyΔ(crdt, delta)
+}
