3e30e6cfaad16ec8f0b824496b28cac9d63426e2
ragnar <ragnar@rmgk.de>
2019-03-17 17:32:22 +0100

    do not load serviceworker in main script

--- server/src/main/scala/ersir/server/ResourceLoader.scala
+++ server/src/main/scala/ersir/server/ResourceLoader.scala
@@ -7,22 +7,23 @@ import akka.http.scaladsl.server.Route
 import ersir.server.ResourcePaths._
 
 object ResourcePaths {
-  val mappings: Seq[(String, String)] = Seq(
+  val jsm  : Seq[(String, String)] = Seq(
     "web-fastopt-library.js" -> "web-fastopt-library.js.gz",
     "web-fastopt-loader.js" -> "web-fastopt-loader.js.gz",
-    "web-fastopt.js" -> "web-fastopt.js.gz",
+    "web-fastopt.js" -> "web-fastopt.js.gz")
+  val csm  : Seq[(String, String)] = Seq(
+    "style.css" -> "style.css.gz")
+  val other: Seq[(String, String)] = Seq(
     "web-fastopt.js.map" -> "web-fastopt.js.map.gz",
-    "style.css" -> "style.css.gz",
     "style.css.map" -> "style.css.map.gz",
-    "serviceworker.js" -> "serviceworker.js"
-  )
+    "serviceworker.js" -> "serviceworker.js")
 
-  val css: List[String] = mappings.iterator.map(_._1).filter(_.endsWith("css")).toList
-  val js : List[String] = mappings.iterator.map(_._1).filter(_.endsWith("js")).toList
+  val css: List[String] = csm.map(_._1)
+  val js : List[String] = jsm.map(_._1)
 }
 
 class WebResources(resourcePath: Path) {
-  val routes: Route = mappings.map { case (k, v) =>
+  val routes: Route = (jsm ++ csm ++ other).map { case (k, v) =>
     path(k) {getFromFile(resourcePath.resolve(v).toFile)}
   }.reduce(_ ~ _)
 }
