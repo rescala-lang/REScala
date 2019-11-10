08775924df39407fa5ba849afe5593cfe18ded46
ragnar <ragnar@rmgk.de>
2019-11-10 12:20:27 +0100

    new updates

--- project/Build.scala
+++ project/Build.scala
@@ -88,17 +88,17 @@ object Dependencies {
   def ld = libraryDependencies
 
   val betterFiles = ld += "com.github.pathikrit" %% "better-files" % "3.8.0"
-  val cats        = ld += "org.typelevel" %%% "cats-core" % "1.6.0"
-  val decline     = ld += "com.monovore" %% "decline" % "0.6.2"
-  val fastparse   = ld += "com.lihaoyi" %%% "fastparse" % "2.1.2"
+  val cats        = ld += "org.typelevel" %%% "cats-core" % "2.0.0"
+  val decline     = ld += "com.monovore" %%% "decline" % "1.0.0"
+  val fastparse   = ld += "com.lihaoyi" %%% "fastparse" % "2.1.3"
   val jsoup       = ld += "org.jsoup" % "jsoup" % "1.12.1"
-  val kaleidoscope= ld += "com.propensive" %% "kaleidoscope" % "0.1.0"
-  val pprint      = ld += "com.lihaoyi" %%% "pprint" % "0.5.4"
+  val kaleidoscope= ld += "com.propensive" %%% "kaleidoscope" % "0.1.0"
+  val pprint      = ld += "com.lihaoyi" %%% "pprint" % "0.5.6"
   val rmgkLogging = Def.settings(Resolvers.rmgk, ld += "de.rmgk" %%% "logging" % "0.2.1")
   val scalactic   = ld += "org.scalactic" %% "scalactic" % "3.0.7"
-  val scribe      = ld += "com.outr" %%% "scribe" % "2.7.9"
+  val scribe      = ld += "com.outr" %%% "scribe" % "2.7.10"
   val sourcecode  = ld += "com.lihaoyi" %%% "sourcecode" % "0.1.7"
-  val upickle     = ld += "com.lihaoyi" %% "upickle" % "0.7.4"
+  val upickle     = ld += "com.lihaoyi" %% "upickle" % "0.8.0"
 
   val akkaHttp = ld ++= (Seq("akka-http-core",
                              "akka-http")
@@ -119,7 +119,7 @@ object Dependencies {
   val fontawesome  = ld += "org.webjars" % "font-awesome" % "5.10.1"
 
   // tests
-  val scalacheck = ld += "org.scalacheck" %%% "scalacheck" % "1.14.0" % "test"
+  val scalacheck = ld += "org.scalacheck" %%% "scalacheck" % "1.14.2" % "test"
   val scalatest  = ld += "org.scalatest" %%% "scalatest" % "3.0.8" % "test"
 
   // legacy
