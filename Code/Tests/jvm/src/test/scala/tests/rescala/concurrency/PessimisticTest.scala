490b56dc2a3339ddbdf7841f7abe30afc282f964
ragnar <ragnar@rmgk.de>
2020-07-26 14:09:33 +0200

    autoformat

--- Code/Tests/jvm/src/test/scala/tests/rescala/concurrency/PessimisticTest.scala
+++ Code/Tests/jvm/src/test/scala/tests/rescala/concurrency/PessimisticTest.scala
@@ -13,21 +13,21 @@ import scala.concurrent.duration._
 import scala.concurrent.{Await, Future}
 
 class PessimisticTest extends RETests {
-  engines(Interfaces.parrp)("SynchronizedReevaluation should synchronize reevaluations"){ engine =>
+  engines(Interfaces.parrp)("SynchronizedReevaluation should synchronize reevaluations") { engine =>
     import engine._
 
-    val v1 = Var(false)
-    val v2 = Var(false)
+    val v1          = Var(false)
+    val v2          = Var(false)
     val (sync1, s1) = SynchronizedReevaluation(v1)
     val (sync2, s2) = SynchronizedReevaluation(v2)
-    val trackS1 = new ReevaluationTracker(s1)
-    val reached1 = SynchronizedReevaluation.notifyOnceReached(sync1)
-    val autoSync = SynchronizedReevaluation.autoSyncNextReevaluation(sync1, sync2)
+    val trackS1     = new ReevaluationTracker(s1)
+    val reached1    = SynchronizedReevaluation.notifyOnceReached(sync1)
+    val autoSync    = SynchronizedReevaluation.autoSyncNextReevaluation(sync1, sync2)
 
-    val t1 = Spawn{ v1.set(true) }
+    val t1 = Spawn { v1.set(true) }
 
     assert(reached1.await(1000, TimeUnit.MILLISECONDS))
-    trackS1.assertClear(false) // turn did not finish yet
+    trackS1.assertClear(false)      // turn did not finish yet
     assert(autoSync.getCount === 1) // but has reduced latch to only second turn missing
 
     v2.set(true)
@@ -39,81 +39,87 @@ class PessimisticTest extends RETests {
     assert(s2.readValueOnce === true)
   }
 
-  engines(Interfaces.parrp)("Pessimistic Engines should safely execute concurrently admitted updates to summed signals"){ engine =>
+  engines(Interfaces.parrp)(
+    "Pessimistic Engines should safely execute concurrently admitted updates to summed signals"
+  ) { engine =>
     import engine._
 
-    val size = 10
+    val size    = 10
     val sources = List.fill(size)(Var(0))
 
-    val sum = sources.map(_.map(identity)).reduce(Signals.lift(_, _)(_ + _))
+    val sum        = sources.map(_.map(identity)).reduce(Signals.lift(_, _)(_ + _))
     val sumTracker = new ReevaluationTracker(sum)
 
     val latch = new CountDownLatch(size)
-    val threads = sources.map(v => Spawn {
-      latch.countDown()
-      latch.await()
-      v.set(1)
-    })
+    val threads = sources.map(v =>
+      Spawn {
+        latch.countDown()
+        latch.await()
+        v.set(1)
+      }
+    )
 
     val timeout = System.currentTimeMillis() + 1000
     threads.foreach(_.await(math.max(0, timeout - System.currentTimeMillis())))
     assert(latch.getCount == 0)
 
-    sumTracker.assert((0 to size).reverse:_*)
+    sumTracker.assert((0 to size).reverse: _*)
     assert(sum.readValueOnce === size)
   }
 
-  "Pessimistic Engines should correctly execute crossed dynamic discoveries" in { for (_ <- 1 to 1000) {
-    val interface = Interfaces.parrp
-    import interface._
+  "Pessimistic Engines should correctly execute crossed dynamic discoveries" in {
+    for (_ <- 1 to 1000) {
+      val interface = Interfaces.parrp
+      import interface._
 
-    val initA = "init a"
-    val initB = "init b"
+      val initA = "init a"
+      val initB = "init b"
 
-    val staticA = "static a"
-    val staticB = "static b"
+      val staticA = "static a"
+      val staticB = "static b"
 
+      val vA = Var(initA)("var A")
+      val vB = Var(initB)("var B")
 
-    val vA = Var(initA)("var A")
-    val vB = Var(initB)("var B")
+      val (syncInA, syncA) = SynchronizedReevaluation(vA)("sync A")
+      val (syncInB, syncB) = SynchronizedReevaluation(vB)("sync B")
 
-    val (syncInA, syncA) = SynchronizedReevaluation(vA)("sync A")
-    val (syncInB, syncB) = SynchronizedReevaluation(vB)("sync B")
+      // so if syncA becomes true, this adds a dependency on vB
+      val crossA =
+        interface.Signals.dynamic(syncA) { t => if (t.depend(syncA) != initA) t.depend(syncB) else staticA }("crossA")
 
-    // so if syncA becomes true, this adds a dependency on vB
-    val crossA = interface.Signals.dynamic(syncA) { t => if (t.depend(syncA) != initA) t.depend(syncB) else staticA} ("crossA")
+      // this does as above, causing one or the other to access something which will change later
+      val crossB =
+        interface.Signals.dynamic(syncB) { t => if (t.depend(syncB) != initB) t.depend(syncA) else staticB }("crossB")
 
-    // this does as above, causing one or the other to access something which will change later
-    val crossB = interface.Signals.dynamic(syncB) { t => if (t.depend(syncB) != initB) t.depend(syncA) else staticB } ("crossB")
+      val resultsA = new ReevaluationTracker(crossA)("resultsA")
+      val resultsB = new ReevaluationTracker(crossB)("resultsB")
 
-    val resultsA = new ReevaluationTracker(crossA)("resultsA")
-    val resultsB = new ReevaluationTracker(crossB)("resultsB")
+      resultsA.assertClear(staticA)
+      resultsB.assertClear(staticB)
 
-    resultsA.assertClear(staticA)
-    resultsB.assertClear(staticB)
+      // force both turns to start executing before either may perform its dynamic discoveries
+      val syncBothLatch = SynchronizedReevaluation.autoSyncNextReevaluation(syncInA, syncInB)
 
-    // force both turns to start executing before either may perform its dynamic discoveries
-    val syncBothLatch = SynchronizedReevaluation.autoSyncNextReevaluation(syncInA, syncInB)
+      // no evaluations happend yet
+      resultsA.assertClear()
+      resultsB.assertClear()
 
+      val finalA = "final a"
+      val finalB = "final b"
+      val t1     = Spawn(vA.set(finalA), Some("setting A thread"))
+      val t2     = Spawn(vB.set(finalB), Some("setting B thread"))
 
-    // no evaluations happend yet
-    resultsA.assertClear()
-    resultsB.assertClear()
+      assert(syncBothLatch.await(1000, TimeUnit.MILLISECONDS))
 
-    val finalA = "final a"
-    val finalB = "final b"
-    val t1 = Spawn(vA.set(finalA), Some("setting A thread"))
-    val t2 = Spawn(vB.set(finalB), Some("setting B thread"))
+      t1.await(1000)
+      t2.await(1000)
 
-    assert(syncBothLatch.await(1000, TimeUnit.MILLISECONDS))
-
-    t1.await(1000)
-    t2.await(1000)
-
-    // turn 1 used the old value of vB and the retrofitted reevaluation for turn 2 executed and used the new value
-    assert((resultsA.results == List(finalB, initB) && resultsB.results == List(finalA)) ||
-             (resultsA.results == List(finalB) && resultsB.results == List(finalA, initA)))
-  }}
+      // turn 1 used the old value of vB and the retrofitted reevaluation for turn 2 executed and used the new value
+      assert((resultsA.results == List(finalB, initB) && resultsB.results == List(finalA)) ||
+        (resultsA.results == List(finalB) && resultsB.results == List(finalA, initA)))
+    }
+  }
 
   "ParRP should (not?) Add And Remove Dependency In One Turn" in {
     import rescala.Interfaces.parrp._
@@ -121,29 +127,29 @@ class PessimisticTest extends RETests {
     // this behavior is not necessary for correctness; adding and removing the edge (i.e. regs and unregs +=1)
     // would be equally correct. It is implemented purely to discover accidental behavior changes, but should
     // have its exepected results changed upon intentional behavior changes!
-    val b0 = Var(false)
-    val b2 = b0.map(identity).map(!_) // dirty hacks to get il_3 to reevaluate first on levelbased engines
-    val i0 = Var(11)
+    val b0     = Var(false)
+    val b2     = b0.map(identity).map(!_) // dirty hacks to get il_3 to reevaluate first on levelbased engines
+    val i0     = Var(11)
     var reeval = 0
-    val i1_3 = Signals.dynamic(b0) { t => reeval += 1; if (t.depend(b0) && t.depend(b2)) t.depend(i0) else 42 }
+    val i1_3   = Signals.dynamic(b0) { t => reeval += 1; if (t.depend(b0) && t.depend(b2)) t.depend(i0) else 42 }
 
-    var regs = 0
+    var regs   = 0
     var unregs = 0
 
     val mockFac = new TwoVersionScheduler[ParRPStruct, ParRPTransaction] {
       override def schedulerName: String = "Reg/Unreg counting ParRP"
-      override protected def makeTurn(priorTurn: Option[ParRPTransaction]): ParRPTransaction = new ParRPTransaction(new Backoff(), None) {
-        override def discover(source: ReSource, sink: Reactive): Unit = {
-          if (source eq i0) regs += 1
-          super.discover(source, sink)
+      override protected def makeTurn(priorTurn: Option[ParRPTransaction]): ParRPTransaction =
+        new ParRPTransaction(new Backoff(), None) {
+          override def discover(source: ReSource, sink: Reactive): Unit = {
+            if (source eq i0) regs += 1
+            super.discover(source, sink)
+          }
+          override def drop(source: ReSource, sink: Reactive): Unit = {
+            if (source eq i0) unregs += 1
+            super.drop(source, sink)
+          }
         }
-        override def drop(source: ReSource, sink: Reactive): Unit = {
-          if (source eq i0) unregs += 1
-          super.drop(source, sink)
-        }
-      }
     }
-
 
     assert(JVMInfiltrator.unsafeNow(i1_3) === 42)
     assert(reeval === 1)
@@ -175,14 +181,16 @@ class PessimisticTest extends RETests {
     assert(unregs === 0)
   }
 
-  engines(Interfaces.parrp)("Pessimistic Engines should not retrofit a reevaluation for t2, after a dependency might have been added and removed again inside a single t1 While Owned By t2"){ engine =>
+  engines(Interfaces.parrp)(
+    "Pessimistic Engines should not retrofit a reevaluation for t2, after a dependency might have been added and removed again inside a single t1 While Owned By t2"
+  ) { engine =>
     import engine._
 
-    val bl0 = Var(false)
-    val (syncB1, bl1) = SynchronizedReevaluation(bl0)
+    val bl0                  = Var(false)
+    val (syncB1, bl1)        = SynchronizedReevaluation(bl0)
     var bl3: Signal[Boolean] = null // dirty hacks to get b2b3i2 to reevaluate first on non-levelbased engines
-    val il0 = Var(11)
-    val (syncI1, il1) = SynchronizedReevaluation(il0)
+    val il0                  = Var(11)
+    val (syncI1, il1)        = SynchronizedReevaluation(il0)
 
     var reeval = List.empty[engine.Creation]
     // this starts on level 2. when bl0 becomes true bl1 becomes true on level 1
@@ -194,12 +202,13 @@ class PessimisticTest extends RETests {
       if (t.depend(bl1)) {
         if (t.depend(bl3)) {
           val res = t.depend(il1)
-          assert(res === 11, "did not read old value, this may happen spouriosly, probably because of the timing issue in this test")
+          assert(
+            res === 11,
+            "did not read old value, this may happen spouriosly, probably because of the timing issue in this test"
+          )
           res
-        }
-        else 37
-      }
-      else 42
+        } else 37
+      } else 42
     }
     val results = new ReevaluationTracker(b2b3i2)
 
@@ -207,7 +216,7 @@ class PessimisticTest extends RETests {
 
     // this is here, so that we have i lock bl1.
     // we need this to be a dynamic lock to lock just this single reactive and not bl3 etc.
-    val i2b2 = engine.Signals.dynamic(il1)(t => if (t.depend(il1) == 0) t.depend(bl1) else false)
+    val i2b2     = engine.Signals.dynamic(il1)(t => if (t.depend(il1) == 0) t.depend(bl1) else false)
     val results2 = new ReevaluationTracker(i2b2)
 
     // bl0 -> bl1 -> (bl2) -> bl3
@@ -230,11 +239,11 @@ class PessimisticTest extends RETests {
     // we start the rescala.turns …
     // i will try to grab bl1, which is locked by b, so i will start to wait on b
     val t1 = Future { SetAndExtractTransactionHandle(bl0, true) }
-    val t2 = Future {  SetAndExtractTransactionHandle(il0, 0) }
+    val t2 = Future { SetAndExtractTransactionHandle(il0, 0) }
 
     assert(latch.await(1000, TimeUnit.MILLISECONDS))
     assert(latch1.getCount === 1) // b should wait for manual approval only
-    results2.assertClear() // i should not have propagated over the dynamic discovery, despite not being blocked manually
+    results2.assertClear()        // i should not have propagated over the dynamic discovery, despite not being blocked manually
 
     latch1.countDown()
     // which causes b to continue and evaluate b2b3i2
@@ -245,19 +254,20 @@ class PessimisticTest extends RETests {
 
     results.assertClear(37)
     results2.assertClear(true)
-    assert(Set(List(turn1, turn1), List(turn1)).contains(reeval), " -- for reference, turn2 was "+turn2)
+    assert(Set(List(turn1, turn1), List(turn1)).contains(reeval), " -- for reference, turn2 was " + turn2)
   }
 
   engines(Interfaces.parrp)(
     "pessimistic engines should add two dynamic dependencies and remove only one",
-    List(IgnoreOnGithubCiBecause("test failes sometimes …"))) { engine =>
+    List(IgnoreOnGithubCiBecause("test failes sometimes …"))
+  ) { engine =>
     import engine._
 
-    val bl0 = Var(false)
-    val (syncB1, bl1) = SynchronizedReevaluation(bl0)
+    val bl0                  = Var(false)
+    val (syncB1, bl1)        = SynchronizedReevaluation(bl0)
     var bl3: Signal[Boolean] = null // dirty hacks to ensure that b2b3i2 is reevaluated first
-    val il0 = Var(11)
-    val (syncI1, il1) = SynchronizedReevaluation(il0)
+    val il0                  = Var(11)
+    val (syncI1, il1)        = SynchronizedReevaluation(il0)
 
     var reeval = List.empty[Creation]
     // this starts on level 2. when bl0 becomes true bl1 becomes true on level 1
@@ -271,17 +281,15 @@ class PessimisticTest extends RETests {
           val res = t.depend(il0) + t.depend(il1)
           assert(res === 22, "did not read old value")
           res
-        }
-        else t.depend(il0)
-      }
-      else 42
+        } else t.depend(il0)
+      } else 42
     }
     val results = new ReevaluationTracker(b2b3i2)
     bl3 = bl1.map(identity).map(!_) // dirty hacks to get b2b3i2 to reevaluate first on levelbased engines
 
     // this is here, so that we have i lock bl1.
     // we need this to be a dynamic lock to lock just this single reactive and not bl3 etc.
-    val i2b2 = engine.Signals.dynamic(il1)(t => if (t.depend(il1) == 17) t.depend(bl1) else false)
+    val i2b2     = engine.Signals.dynamic(il1)(t => if (t.depend(il1) == 17) t.depend(bl1) else false)
     val results2 = new ReevaluationTracker(i2b2)
 
     // bl0 -> bl1 -> (bl2) -> bl3
@@ -304,11 +312,11 @@ class PessimisticTest extends RETests {
     // we start the rescala.turns …
     // i will try to grab bl1, which is locked by b, so i will start to wait on b
     val t1 = Future { SetAndExtractTransactionHandle(bl0, true) }
-    val t2 = Future { SetAndExtractTransactionHandle(il0, 17)}
+    val t2 = Future { SetAndExtractTransactionHandle(il0, 17) }
 
     assert(latch.await(1000, TimeUnit.MILLISECONDS))
     assert(latch1.getCount === 1) // b should wait for manual approval only
-    results2.assertClear() // i should not have propagated over the dynamic discovery, despite not being blocked manually
+    results2.assertClear()        // i should not have propagated over the dynamic discovery, despite not being blocked manually
 
     latch1.countDown()
     // which causes b to continue and evaluate b2b3i2
