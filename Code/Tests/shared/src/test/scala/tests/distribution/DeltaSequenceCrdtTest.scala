1383bfd12bbf7a554565f7729bd3a7e132779764
ragnar <ragnar@rmgk.de>
2020-07-26 14:34:17 +0200

    vertex creation order must respect insertion order

--- Code/Tests/shared/src/test/scala/tests/distribution/DeltaSequenceCrdtTest.scala
+++ Code/Tests/shared/src/test/scala/tests/distribution/DeltaSequenceCrdtTest.scala
@@ -6,15 +6,15 @@ import rescala.extra.lattices.sequences.{DeltaSequence, Vertex}
 
 class DeltaSequenceCrdtTest extends AnyFreeSpec {
 
-
   "basic interaction" in {
     val ds      = DeltaSequence.empty[String]
     val a       = "a"
+    val vertex1 = Vertex.fresh()
     val vertex2 = Vertex.fresh()
-    val added2  = ds
-      .addRight(a, Vertex.start, Vertex.fresh(), "Hello world!")
+    val added2 = ds
+      .addRight(a, Vertex.start, vertex1, "Hello world!")
       .addRight(a, Vertex.start, vertex2, "Hello world 2!")
-    assert(added2.iterator.toList === List("Hello world 2!", "Hello world!"))
+    assert(added2.iterator.toList === List("Hello world 2!", "Hello world!"), "add right is correctly ordered")
 
     val addDelta = added2.addRightDelta(a, vertex2, Vertex.fresh(), "Hello world after 2!")
 
@@ -27,12 +27,10 @@ class DeltaSequenceCrdtTest extends AnyFreeSpec {
 
     assert(combined2.iterator.toList === List("Hello world 2!", "Hello world after 2 the second!", "Hello world!"))
 
-    val fullCombine = Lattice.merge(combined, combined2)
+    val fullCombine  = Lattice.merge(combined, combined2)
     val deltaCombine = Lattice.merge(combined, addDeltaOtherReplica)
 
     assert(fullCombine.iterator.toList == deltaCombine.iterator.toList)
-
   }
-
 
 }
