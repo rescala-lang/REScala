02184b38bf87c2eb600bab11608b4cf54b7a6077
ragnar <ragnar@rmgk.de>
2020-07-26 14:56:18 +0200

    actually pass tags to test runner …

--- Code/Tests/shared/src/test/scala/tests/rescala/testtools/RETests.scala
+++ Code/Tests/shared/src/test/scala/tests/rescala/testtools/RETests.scala
@@ -7,24 +7,26 @@ import org.scalatest.prop.TableDrivenPropertyChecks
 import rescala.core.Struct
 import rescala.interface.RescalaInterface
 
-
 abstract class RETests extends AnyFreeSpec with TableDrivenPropertyChecks {
 
   type TestStruct <: Struct
 
-  def engines(es: RescalaInterface[_ <: Struct]*)
-             (text: String, tags: List[Tag] = Nil)
-             (testCase: RescalaInterface[TestStruct] => Any)
-             (implicit pos: source.Position): Unit = {
+  def engines(es: RescalaInterface[_ <: Struct]*)(
+      text: String,
+      tags: List[Tag] = Nil
+  )(testCase: RescalaInterface[TestStruct] => Any)(implicit pos: source.Position): Unit = {
     forAll(Table("engine", es: _*)) { e =>
-      s"Testing $e" - {text in testCase(e.asInstanceOf[RescalaInterface[TestStruct]])}
+      val testEngine = e.asInstanceOf[RescalaInterface[TestStruct]]
+      s"Testing $testEngine" - (tags match {
+        case Nil          => text in testCase(testEngine)
+        case head :: tail => text taggedAs (head, tail: _*) in testCase(testEngine)
+      })
     }
   }
 
   def allEngines(text: String)(testCase: RescalaInterface[TestStruct] => Any)(implicit pos: source.Position): Unit = {
     engines(tests.rescala.testtools.TestEngines.all: _*)(text)(testCase)(pos)
   }
-
 
   def multiEngined(block: RescalaInterface[TestStruct] => Any): Unit = {
     for (engine <- tests.rescala.testtools.TestEngines.all) yield {
