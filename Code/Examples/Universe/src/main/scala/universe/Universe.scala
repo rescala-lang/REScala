fbc8a26ba60819ee5e3ae7f060e425d20d408990
ragnar <ragnar@rmgk.de>
2020-11-24 17:28:43 +0100

    make universe case study a bit nicer to use from IDE

--- Code/Examples/Universe/src/main/scala/universe/Universe.scala
+++ Code/Examples/Universe/src/main/scala/universe/Universe.scala
@@ -2,7 +2,8 @@ package universe
 
 import java.nio.file.{Files, Paths, StandardOpenOption}
 
-object RunConsole {
+object Universe {
+
   def main(args: Array[String]): Unit = {
     val nAnimals     = 100
     val nPlants      = 300
@@ -11,9 +12,13 @@ object RunConsole {
     val repetitions  = 10
     val threadCounts = Range.inclusive(1, 8)
 
+    val genCsv = !args.headOption.contains("nocsv")
+
+    println(s"profiling ${Globals.engineName}")
+
     val outfile = s"universe-${Globals.engineName}.csv"
 
-    Files.write(
+    if (genCsv) Files.write(
       Paths.get(outfile),
       s""""Repetition","Threads","Score","Param: engineName","Benchmark","Param: height","Param: width","Param: animals","Param: plants"${"\n"}""".getBytes(),
       StandardOpenOption.WRITE,
@@ -23,8 +28,6 @@ object RunConsole {
     for (repetition <- 0 to repetitions; threads <- threadCounts) {
       println(s"rep: $repetition, threads: $threads")
 
-      // WARN: not sure why the next call was here, not sure what removing it broke, but observers … work differently now
-      //Observe.dereferenceAllStrongObserversWARNINGonlyUseThisIfYouKnowWhatYouAreDoing()
       System.gc()
       Globals.setParallelism(threads)
 
@@ -37,50 +40,16 @@ object RunConsole {
         world.tick()
         world.runPlan()
       }
-      //      println(world.time)
-      //      println(world.status)
-      //      println(world.board.dump)
       val duration = (System.nanoTime() - start) / 1e9d
-      if (repetition > 0)
+      if (!genCsv) println(s"duration: $duration")
+      if (repetition > 0 && genCsv) {
         Files.write(
           Paths.get(outfile),
           s"""$repetition,$threads,$duration,"${Globals.engineName}","UniverseCaseStudy",$height,$width,$nAnimals,$nPlants${"\n"}""".getBytes(),
           StandardOpenOption.APPEND
         )
+      }
     }
-  }
-
-}
-
-object RunPrinting {
-  def main(args: Array[String]): Unit = {
-    val nAnimals = 100
-    val nPlants  = 300
-    val width    = 70
-    val height   = 20
-
-    //WARN: not sure why the next call was here, not sure what removing it broke, but observers … work differently now
-    // Observe.dereferenceAllStrongObserversWARNINGonlyUseThisIfYouKnowWhatYouAreDoing()
-    System.gc()
-    //Globals.setParallelism(1)
-
-    val world = new World(width, height)
-
-    world.batchSpawn(nAnimals, nPlants)
-
-    val start = System.nanoTime()
-    import universe.Globals.engine.implicitScheduler
-    world.time.day.observe { _ =>
-      println(world.time)
-      println(world.status)
-      println(world.board.dump)
-    }
-    while (world.time.week.readValueOnce < 2) {
-      world.tick()
-      world.runPlan()
-
-    }
-    (System.nanoTime() - start) / 1e9d
   }
 
 }
