a4b2d37947315e278b91eb51d64eadb23bfe5226
Vincent Stollenwerk <vstollen@gmail.com>
2021-05-05 12:33:53 +0200

    Test ReactorStage callback

--- Code/Main/jvm/src/test/scala/rescala/core/reactor/ReactorWithoutAPITest.scala
+++ Code/Main/jvm/src/test/scala/rescala/core/reactor/ReactorWithoutAPITest.scala
@@ -28,13 +28,30 @@ class ReactorWithoutAPITest extends RETests {
 
     CustomReactor.once { self =>
       state.set(1)
-      self.next(e1) { res =>
+      self.next(e1) { _ =>
         state.set(2)
       }
     }
 
-    assert(state.now == 1)
+    assert(state.now === 1)
     e1.fire()
-    assert(state.now == 2)
+    assert(state.now === 2)
+  }
+
+  test("ReactorStage callback passes event value") {
+    val state = Var(0)
+    val e1 = Evt[Int]()
+
+    assert(state.now === 0)
+
+    CustomReactor.once( self =>
+      self.next(e1) { res =>
+        state.set(res)
+      }
+    )
+
+    assert(state.now === 0)
+    e1.fire(42)
+    assert(state.now === 42)
   }
 }
