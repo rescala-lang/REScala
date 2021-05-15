f420580b9e3c560f1320be2393dcd7a3838525ed
Vincent Stollenwerk <vstollen@gmail.com>
2021-05-15 19:33:26 +0200

    Implement reactive based reactor

--- Code/Main/jvm/src/test/scala/rescala/core/reactor/ReactorWithoutAPITest.scala
+++ Code/Main/jvm/src/test/scala/rescala/core/reactor/ReactorWithoutAPITest.scala
@@ -1,112 +1,117 @@
 package rescala.core.reactor
 
+import rescala.core.{CreationTicket, Interp, ReName, Scheduler}
 import tests.rescala.testtools.RETests
 
 class ReactorWithoutAPITest extends RETests {
 
   import rescala.default._
 
-  object CustomReactor {
-    def once(body: ReactorStage => Unit): Unit = {
-      body(new ReactorStage)
+  class CustomReactorReactive[T](initState: ReStructure#State[ReactorStage[T], ReStructure])
+      extends Derived
+      with Interp[T, ReStructure] {
+    override type Value = ReactorStage[T]
+
+    /** Interprets the internal type to the external type
+      *
+      * @group internal
+      */
+    override def interpret(v: Value): T = v.run()
+
+    override protected[rescala] def name: ReName = "Custom Reactor"
+
+    /** Return the value of the Reactor after the end of a transaction.
+      *
+      * This method is called at the end of transactions.
+      * The value of [[CustomReactorReactive]] does not change
+      * at the end of a transaction.
+      *
+      * @param base the reactor's current value
+      * @return the value provided by base
+      */
+    override protected[rescala] def commit(base: ReactorStage[T]): ReactorStage[T] = base
+
+    /** called if any of the dependencies ([[ReSource]]s) changed in the current update turn,
+      * after all (known) dependencies are updated
+      */
+    override protected[rescala] def reevaluate(input: ReIn): Rout = {
+      state.current.run()
+
+      if (state.current.nextStage.isEmpty) {
+        return input
+      }
+
+      val nextStage = state.current.nextStage.get
+
+      // Should never be None. Every stage except the
+      // initial Stage has a trigger.
+      val trigger   = nextStage.trigger.get
+
+      val triggered = input.dependStatic(trigger)
+      if (triggered.isEmpty) {
+        return input
+      }
+
+      input.withValue(nextStage)
+    }
+
+    override protected[rescala] def state: State = initState
+  }
+
+  class ReactorStage[T](var trigger: Option[Evt[Unit]], initialValue: T)(body: ReactorStage[T] => Unit) {
+    private var value: T                                    = initialValue
+    private[reactor] var nextStage: Option[ReactorStage[T]] = None
+
+    def next(event: Evt[Unit])(callback: ReactorStage[T] => Unit): Unit = {
+      nextStage = Some(new ReactorStage[T](Some(event), value)(callback))
+    }
+
+    def set(newValue: T): Unit = {
+      value = newValue
+    }
+
+    private[reactor] def run(): T = {
+      body(this)
+      value
     }
   }
 
-  class ReactorStage {
-    private var observer = None: Option[Observe]
+  object CustomReactorReactive {
+    def once[T](initialValue: T, inputs: Set[ReSource])(body: ReactorStage[T] => Unit)(implicit
+        fac: Scheduler[ReStructure]
+    ): CustomReactorReactive[T] = {
+      val customReactor =
+        CreationTicket.fromScheduler(scheduler)
+          .create(
+            inputs,
+            new ReactorStage[T](None, initialValue)(body),
+            inite = true
+          ) { createdState =>
+            new CustomReactorReactive[T](createdState)
+          }
 
-    def next[A](event: Evt[A])(callback: (ReactorStage, A) => Unit): Unit = {
-      observer = Some(event.observe({ x =>
-        removeObserver()
-        callback(new ReactorStage, x)
-      }))
+      customReactor
+    }
+  }
+
+  test("Reactor has value") {
+    val reactor = CustomReactorReactive.once("Initial Value", Set()) { self =>
+      self.set("Value Set!")
     }
 
-    private[this] def removeObserver(): Unit = {
-      observer.foreach( obs =>
-        obs.remove()
-      )
-      observer = None
-    }
+    assert(transaction(reactor) { _.now(reactor) } === "Value Set!")
   }
 
   test("Reactor waits for event when using next") {
-    val state = Var(0)
     val e1 = Evt[Unit]()
-
-    assert(state.now === 0)
-
-    CustomReactor.once { self =>
-      state.set(1)
-      self.next(e1) { (_, _) =>
-        state.set(2)
+    val reactor = CustomReactorReactive.once(42, Set(e1)) { self =>
+      self.next(e1) { self =>
+        self.set(1)
       }
     }
 
-    assert(state.now === 1)
+    assert(transaction(reactor) { _.now(reactor) } === 42)
     e1.fire()
-    assert(state.now === 2)
-  }
-
-  test("ReactorStage callback passes event value") {
-    val state = Var(0)
-    val e1 = Evt[Int]()
-
-    assert(state.now === 0)
-
-    CustomReactor.once( self =>
-      self.next(e1) { (_, res) =>
-        state.set(res)
-      }
-    )
-
-    assert(state.now === 0)
-    e1.fire(42)
-    assert(state.now === 42)
-  }
-
-  test("ReactorStage next only gets triggered once") {
-    val state = Var(0)
-    val e1 = Evt[Unit]()
-
-    assert(state.now === 0)
-
-    CustomReactor.once( self =>
-      self.next(e1) { (_, _) =>
-        state.set(1)
-      }
-    )
-
-    assert(state.now === 0)
-    e1.fire()
-    assert(state.now === 1)
-    state.set(42)
-    assert(state.now === 42)
-    e1.fire()
-    assert(state.now === 42)
-  }
-
-  test("ReactorStages can be nested") {
-    val state = Var(0)
-    val e1 = Evt[Unit]()
-
-    assert(state.now === 0)
-
-    CustomReactor.once { self =>
-      state.set(1)
-
-      self.next(e1) { (self, _) =>
-        state.set(2)
-        self.next(e1) { (_,_) =>
-          state.set(3)
-        }
-      }
-    }
-
-    assert(state.now === 1)
-    e1.fire()
-    assert(state.now === 2)
-    e1.fire()
-    assert(state.now === 3)
+    assert(transaction(reactor) { _.now(reactor) } === 1)
   }
 }
