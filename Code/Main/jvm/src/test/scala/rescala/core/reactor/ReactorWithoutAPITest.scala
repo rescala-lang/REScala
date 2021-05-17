2b7a30d30d867ee4e85907535490a8459ba91442
Vincent Stollenwerk <vstollen@gmail.com>
2021-05-17 19:47:42 +0200

    Add generic event support for reactors
    The new reactive based reactors now support
    generic event types for ReactorStage.next().
    Before they used to only support Evt[Unit] typed
    events.

--- Code/Main/jvm/src/test/scala/rescala/core/reactor/ReactorWithoutAPITest.scala
+++ Code/Main/jvm/src/test/scala/rescala/core/reactor/ReactorWithoutAPITest.scala
@@ -1,105 +1,104 @@
 package rescala.core.reactor
 
-import rescala.core.{CreationTicket, Interp, ReName, Scheduler}
+import rescala.core.{CreationTicket, Interp, ReName}
 import tests.rescala.testtools.RETests
 
 class ReactorWithoutAPITest extends RETests {
 
   import rescala.default._
 
-  abstract class ReactorStage[T](initialValue: T) {
-    protected var value: T                                   = initialValue
-    protected[reactor] var nextStage: Option[NestedStage[T]] = None
-
-    def next(event: Evt[Unit])(callback: NestedStage[T] => Unit): Unit = {
-      nextStage = Some(new NestedStage[T](event, value)(callback))
-    }
-
-    def set(newValue: T): Unit = {
-      value = newValue
-    }
-
-    private[reactor] def run(): T
-  }
-
-  class CustomReactorReactive[T](initState: ReStructure#State[ReactorStage[T], ReStructure])
-      extends Derived
+  class CustomReactorReactive[T](
+      initState: ReStructure#State[ReactorStage[T], ReStructure]
+  ) extends Derived
       with Interp[T, ReStructure] {
+
     override type Value = ReactorStage[T]
 
     /** Interprets the internal type to the external type
       *
       * @group internal
       */
-    override def interpret(v: Value): T = v.run()
+    override def interpret(v: ReactorStage[T]): T = v.value
 
     override protected[rescala] def name: ReName = "Custom Reactor"
-
-    /** Return the value of the Reactor after the end of a transaction.
-      *
-      * This method is called at the end of transactions.
-      * The value of [[CustomReactorReactive]] does not change
-      * at the end of a transaction.
-      *
-      * @param base the reactor's current value
-      * @return the value provided by base
-      */
-    override protected[rescala] def commit(base: ReactorStage[T]): ReactorStage[T] = base
 
     /** called if any of the dependencies ([[ReSource]]s) changed in the current update turn,
       * after all (known) dependencies are updated
       */
     override protected[rescala] def reevaluate(input: ReIn): Rout = {
-      state.current.run()
-
-      if (state.current.nextStage.isEmpty) {
-        return input
-      }
-
-      val nextStage = state.current.nextStage.get
-
-      // Should never be None. Every stage except the
-      // initial Stage has a trigger.
-      val trigger = nextStage.trigger
-
-      val triggered = input.dependStatic(trigger)
-      if (triggered.isEmpty) {
-        return input
-      }
-
-      input.withValue(nextStage)
+      input.withValue(state.current.eval(input))
     }
 
-    override protected[rescala] def state: State = initState: State
+    override protected[rescala] def state: State = initState
+
+    override protected[rescala] def commit(base: ReactorStage[T]): ReactorStage[T] = base
   }
 
-  class InitialStage[T](val initialValue: T)(body: InitialStage[T] => Unit) extends ReactorStage[T](initialValue) {
-    override private[reactor] def run(): T = {
-      body(this)
-      value
+  class ReactorStage[T](initialValue: T, reactor: CustomReactorReactive[T]) {
+    var value: T                                                     = initialValue
+    private var stageHolder: Option[reactor.ReIn => ReactorStage[T]] = None
+
+    def eval(input: reactor.ReIn): ReactorStage[T] = {
+      stageHolder.foreach { holder =>
+        return holder(input)
+      }
+
+      this
+    }
+
+    def set(newValue: T): Unit = {
+      value = newValue
+    }
+
+    def next[E](event: Evt[E])(body: (ReactorStage[T], E) => Unit): Unit = {
+      stageHolder = Some(stageHolder(event, body)(_: reactor.ReIn))
+    }
+
+    private def stageHolder[E](
+        event: Evt[E],
+        body: (ReactorStage[T], E) => Unit
+    )(input: reactor.ReIn): ReactorStage[T] = {
+      val eventDepend = input.dependStatic(event)
+
+      if (eventDepend.isEmpty) {
+        return this
+      }
+
+      val eventValue = eventDepend.get
+
+      val nextStage = new ReactorStage[T](value, reactor)
+      body(nextStage, eventValue)
+
+      nextStage
     }
   }
-  class NestedStage[T](val trigger: Evt[Unit], initialValue: T)(body: NestedStage[T] => Unit)
-      extends ReactorStage[T](initialValue) {
 
-    private[reactor] def run(): T = {
-      body(this)
-      value
+  class BasicReactorStage[T](initialValue: T) extends ReactorStage(initialValue, null) {
+
+    // TODO: Throw errors if other methods are called. Probably UnsupportedOperationExceptions
+
+    def upgrade(reactor: CustomReactorReactive[T]): ReactorStage[T] = {
+      new ReactorStage[T](value, reactor)
     }
   }
 
   object CustomReactorReactive {
-    def once[T](initialValue: T, inputs: Set[ReSource])(body: ReactorStage[T] => Unit)(implicit
-        fac: Scheduler[ReStructure]
-    ): CustomReactorReactive[T] = {
-      CreationTicket.fromScheduler(scheduler)
+    def once[T](initialValue: T, inputs: Set[ReSource])(body: ReactorStage[T] => Unit): CustomReactorReactive[T] = {
+      val initialStage = new BasicReactorStage[T](initialValue)
+      val reactor: CustomReactorReactive[T] = CreationTicket.fromScheduler(scheduler)
         .create(
           inputs,
-          new InitialStage[T](initialValue)(body): ReactorStage[T],
-          inite = true
+          initialStage: ReactorStage[T],
+          inite = false
         ) { createdState: ReStructure#State[ReactorStage[T], ReStructure] =>
           new CustomReactorReactive[T](createdState)
         }
+
+      val reactorStage = initialStage.upgrade(reactor)
+      body(reactorStage)
+      reactor.state.current = reactorStage
+
+      reactor
     }
   }
 
@@ -120,7 +119,7 @@ class ReactorWithoutAPITest extends RETests {
   test("Reactor waits for event when using next") {
     val e1 = Evt[Unit]()
     val reactor = CustomReactorReactive.once(42, Set(e1)) { self =>
-      self.next(e1) { self =>
+      self.next(e1) { (self, _) =>
         self.set(1)
       }
     }
@@ -128,5 +127,38 @@ class ReactorWithoutAPITest extends RETests {
     assert(transaction(reactor) { _.now(reactor) } === 42)
     e1.fire()
     assert(transaction(reactor) { _.now(reactor) } === 1)
+  }
+
+  test("ReactorStage callback passes event value") {
+    val e1 = Evt[Int]()
+
+    val reactor = CustomReactorReactive.once(0, Set(e1)) { self =>
+      self.next(e1) { (self, e) =>
+        self.set(e)
+      }
+    }
+
+    assert(transaction(reactor) { _.now(reactor) } === 0)
+    e1.fire(1)
+    assert(transaction(reactor) { _.now(reactor) } === 1)
+  }
+
+  test("ReactorStages can be nested") {
+    val e1 = Evt[Unit]()
+
+    val reactor = CustomReactorReactive.once(0, Set(e1)) { self =>
+      self.next(e1) { (self, _) =>
+        self.set(1)
+        self.next(e1) { (self, _) =>
+          self.set(2)
+        }
+      }
+    }
+
+    assert(transaction(reactor) { _.now(reactor) } === 0)
+    e1.fire()
+    assert(transaction(reactor) { _.now(reactor) } === 1)
+    e1.fire()
+    assert(transaction(reactor) { _.now(reactor) } === 2)
   }
 }
