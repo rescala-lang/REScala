e488190e11f2272d356e77ed9f8c9d421d16ef22
Christian Kuessner <ckuessner@mailbox.org>
2021-06-29 11:20:42 +0200

    Add EncryptedCrdt with simple test using CounterCrdt

---
+++ src/test/scala/encrdt/experiments/EncryptedCrdtSpec.scala
@@ -0,0 +1,38 @@
+package de.ckuessner
+package encrdt.experiments
+
+import com.google.crypto.tink.aead.AeadConfig
+import com.google.crypto.tink.{Aead, KeyTemplates, KeysetHandle}
+import org.scalatest.flatspec.AnyFlatSpec
+
+import scala.util.Random
+
+class EncryptedCrdtSpec extends AnyFlatSpec {
+  AeadConfig.register()
+  val keyset: KeysetHandle = KeysetHandle.generateNew(KeyTemplates.get("XCHACHA20_POLY1305"))
+  val aead: Aead = keyset.getPrimitive(classOf[Aead])
+
+  "EncryptedCrdt" should "unseal for simple example with CounterCrdt" in {
+    val counterCrdtState = CounterCrdtState(Map(123 -> 12, 42 -> 21), Map(4711 -> 1, 42 -> 12))
+
+    val encCrdt = EncryptedCrdt.from(counterCrdtState, aead).get
+    assertResult(counterCrdtState) {
+      encCrdt.unseal.get
+    }
+  }
+
+  it should "merge for CounterCrdt and unseal to correct Crdt" in {
+    val encCrdt = new EncryptedCrdt[CounterCrdtState](aead)
+    var crdtState = CounterCrdtState()
+
+    val r = new Random()
+    for (_ <- 1 to 10) {
+      val replicaId = r.nextInt().abs
+      crdtState = crdtState.updated(replicaId, r.nextInt())
+      encCrdt.appendState(crdtState)
+      assertResult(crdtState) {
+        encCrdt.unseal.get
+      }
+    }
+  }
+}
