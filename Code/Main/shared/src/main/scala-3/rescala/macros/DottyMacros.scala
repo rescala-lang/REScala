388d8bbd2048997e3053f346c409f18d444e4cc6
ragnar <ragnar@rmgk.de>
2021-07-19 23:36:44 +0200

    small small victories

--- Code/Main/shared/src/main/scala-3/rescala/macros/DottyMacros.scala
+++ Code/Main/shared/src/main/scala-3/rescala/macros/DottyMacros.scala
@@ -3,24 +3,69 @@ package rescala.macros
 import rescala.macros.MacroAccess
 
 import scala.quoted.*
+import rescala.default.*
 
-object FindInterp extends ExprMap {
+class FindInterp extends ExprMap {
+
+  var found: List[Expr[MacroAccess[_, _]]] = Nil
+
   override def transform[T](e: Expr[T])(using Type[T])(using Quotes): Expr[T] = {
     import quotes.reflect.*
     e match {
       case '{(${x}: MacroAccess[_, _]).value} =>
-        println(x.show)
-        '{null.asInstanceOf[T]}
+        found ::= x
+        e
       case _ => transformChildren(e)
     }
 
   }
 }
 
-inline def detectReactives[T](inline expr: T): T =
+inline def dottyEventExpression[T](inline expr: Option[T]): Event[T] =
   ${ detectImpl('expr) }
 
-def detectImpl[T: Type](expr: Expr[T])(using Quotes): Expr[T] =
+def detectImpl[T: Type](expr: Expr[Option[T]])(using Quotes): Expr[Event[T]] =
+  import quotes.reflect.*
+
+  class ReplaceInterp(replacement: Map[Expr[MacroAccess[_, _]], Term], staticTicket: Tree) extends ExprMap {
+
+    override def transform[T](e: Expr[T])(using Type[T])(using Quotes): Expr[T] = {
+      import quotes.reflect.*
+      e match {
+        case '{(${x}: MacroAccess[_, _]).value} =>
+          val wideType = TypeRepr.of[T].widen.asType
+          val term = replacement(x).asExprOf[Interp[T]]
+          println(s"term: ${term.show}")
+          println(s"type: ${TypeRepr.of[T].widen}")
+          '{(${staticTicket.asExprOf[StaticTicket]}.dependStatic(${term}))}
+        case _ => transformChildren(e)
+      }
+    }
+  }
+
+  //val exprSym = Symbol.newMethod(Symbol.spliceOwner, "reactiveExpr", TypeRepr.of[StaticTicket => Option[Any]])
+
+  val exprType = MethodType.apply(List("staticTicket"))((_: MethodType) => List(TypeRepr.of[StaticTicket]), (_ : MethodType) => TypeRepr.of[Option[T]])
+
+  //println(Printer.TreeStructure.show(expr.asTerm))
+
+
+  println(expr.show)
+
   println("transforming!")
-  val res = FindInterp.transformChildren(expr)
+  val fi = FindInterp()
+  fi.transform(expr)
+  val found = fi.found
+  val res = ValDef.let(Symbol.spliceOwner, found.map(_.asTerm)){defs =>
+    defs.foreach(d => println(Printer.TreeStructure.show(d)))
+    val replacementMap = found.zip(defs).toMap
+    //val rdef = DefDef(exprSym, {params =>
+    val rdef = Lambda(Symbol.spliceOwner, exprType, {(sym, params) =>
+      println(params)
+      val staticTicket = params.head
+      ReplaceInterp(replacementMap, staticTicket).transform(expr).asTerm
+    })
+    ('{Events.static(${Expr.ofSeq(defs.toSeq.map(_.asExprOf[ReSource]))} : _*){${rdef.asExprOf[StaticTicket => Option[T]]}}}).asTerm
+  }.asExprOf[Event[T]]
+  println(res.show)
   res
--- Code/Main/shared/src/main/scala-3/rescala/macros/Test.scala
+++ Code/Main/shared/src/main/scala-3/rescala/macros/Test.scala
@@ -1,15 +1,39 @@
 package rescala.macros
-import rescala.default._
+import rescala.default.*
 
 object Main {
   def main(args: Array[String]): Unit = {
     val iAmAnEvt = Evt[Int]()
-    val iAmToo = Evt[Int]()
+    val iAmToo   = Evt[Int]()
     val iAmThree = Evt[Int]()
-    detectReactives( {
-      (iAmAnEvt.value,
-          ((f: Int) => f.toString + iAmToo.value),
-          iAmThree // note, note value)
-    )})
+
+    def iReturnThee = iAmThree
+
+    val derived = dottyEventExpression(iAmAnEvt.value)
+
+    derived.observe(println)
+
+    //dottyEventExpression {
+    //  val dep = iAmToo.value
+    //  dep.map(_.toString)
+    //}
+
+    //Events.dynamic() { dt =>
+    //  {
+    //    implicit def idt: DynamicTicket = dt
+    //    dottyEventExpression({
+    //      (
+    //        iAmAnEvt.value,
+    //        ((f: Int) => f.toString + iAmToo.value),
+    //        iAmThree, // note, note value
+    //        iReturnThee.value,
+    //      )
+    //    })
+    //    println("reevaluated!")
+    //    None
+    //  }
+    //}
+    iAmAnEvt.fire(5)
+    iAmThree.fire(6)
   }
 }
