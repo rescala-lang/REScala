9811399cfc79f2ea0cf8a52dcd3325a80608c98a
Christian Kuessner <ckuessner@mailbox.org>
2021-09-01 15:06:06 +0200

    Update EncryptedCrdtSpec

--- src/test/scala/encrdt/encrypted/EncryptedCrdtSpec.scala
+++ src/test/scala/encrdt/encrypted/EncryptedCrdtSpec.scala
@@ -17,21 +17,21 @@ class EncryptedCrdtSpec extends AnyFlatSpec {
   "EncryptedCrdt" should "unseal for simple example with CounterCrdt" in {
     val counterCrdtState = CounterCrdtLattice(Map("123" -> 12, "42" -> 21), Map("4711" -> 1, "42" -> 12))
 
-    val encCrdt = EncryptedCrdt.from(counterCrdtState, aead).get
+    val encCrdt = EncryptedCrdt.from(counterCrdtState, "encreplica", aead).get
     assertResult(counterCrdtState) {
       encCrdt.unseal.get
     }
   }
 
   it should "merge for CounterCrdt and unseal to correct Crdt" in {
-    val encCrdt = new EncryptedCrdt[CounterCrdtLattice](aead)
+    val encCrdt = new EncryptedCrdt[CounterCrdtLattice](aead, "encreplica")
     var crdtState = CounterCrdtLattice()
 
     val r = new Random()
     for (_ <- 1 to 10) {
       val replicaId = r.nextInt().abs.toString
       crdtState = crdtState.updated(replicaId, r.nextInt())
-      encCrdt.appendState(crdtState)
+      encCrdt.update(crdtState)
       assertResult(crdtState) {
         encCrdt.unseal.get
       }
