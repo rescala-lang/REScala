28435d157ece1d99423ed4d3e9929f98be05b3ed
Christian Kuessner <ckuessner@mailbox.org>
2021-09-01 15:05:38 +0200

    Simplify EncryptedCrdt, use MultiValueRegister

--- src/main/scala/encrdt/encrypted/EncryptedCrdt.scala
+++ src/main/scala/encrdt/encrypted/EncryptedCrdt.scala
@@ -1,23 +1,29 @@
 package de.ckuessner
 package encrdt.encrypted
 
-import com.github.plokhotnyuk.jsoniter_scala.core.{JsonValueCodec, readFromArray, writeToArray}
-import com.github.plokhotnyuk.jsoniter_scala.macros.JsonCodecMaker
-import com.google.crypto.tink.Aead
-import de.ckuessner.encrdt.lattices.interfaces.SemiLattice
+import encrdt.causality.VectorClock
+import encrdt.lattices.MultiValueRegisterLattice
+import encrdt.lattices.interfaces.SemiLattice
 
-import scala.collection.immutable.Queue
+import com.github.plokhotnyuk.jsoniter_scala.core.{JsonValueCodec, readFromArray, writeToArray}
+import com.google.crypto.tink.Aead
+
 import scala.util.{Failure, Success, Try}
 
 class EncryptedCrdt[T: SemiLattice](val aead: Aead,
-                                    initialState: EncryptedCrdtState = EncryptedCrdtState()) {
+                                    replicaId: String,
+                                    initialState: MultiValueRegisterLattice[Array[Byte]] = MultiValueRegisterLattice(Map.empty)) {
 
   private var _state = initialState
 
-  def state: EncryptedCrdtState = _state
+  def state: MultiValueRegisterLattice[Array[Byte]] = _state
+
+  def currentTime: VectorClock =
+    if (state.versions.isEmpty) VectorClock()
+    else state.versions.keys.reduce((a, b) => a.merged(b))
 
   def unseal(implicit jsonValueCodec: JsonValueCodec[T]): Try[T] =
-    state.states map { ciphertext: Array[Byte] =>
+    state.versions.values.map { ciphertext: Array[Byte] =>
       Try {
         // Ignore associated data for now
         aead.decrypt(ciphertext, new Array[Byte](0))
@@ -32,33 +38,18 @@ class EncryptedCrdt[T: SemiLattice](val aead: Aead,
       }
     })
 
-  def appendState(newState: T)(implicit jsonValueCodec: JsonValueCodec[T]): Try[Unit] = Try {
-    val cleartextBytes = writeToArray(newState)
-    val ciphertextBytes = aead.encrypt(cleartextBytes, new Array[Byte](0))
-    _state = state.enqueue(ciphertextBytes)
+  def update(newPlainTextState: T)(implicit jsonValueCodec: JsonValueCodec[T]): Try[Unit] = Try {
+    val ciphertextBytes = aead.encrypt(writeToArray(newPlainTextState), new Array[Byte](0))
+    _state = MultiValueRegisterLattice(Map(currentTime.advance(replicaId) -> ciphertextBytes))
   }
 }
 
 object EncryptedCrdt {
-  def from[T: SemiLattice](crdtState: T, aead: Aead)(implicit codec: JsonValueCodec[T]): Try[EncryptedCrdt[T]] = {
-    val encCrdt = new EncryptedCrdt[T](aead)
-    encCrdt.appendState(crdtState) match {
+  def from[T: SemiLattice](crdtState: T, replicaId: String, aead: Aead)(implicit codec: JsonValueCodec[T]): Try[EncryptedCrdt[T]] = {
+    val encCrdt = new EncryptedCrdt[T](aead, replicaId)
+    encCrdt.update(crdtState) match {
       case Failure(exception) => Failure[EncryptedCrdt[T]](exception)
       case Success(_) => Try(encCrdt)
     }
-  }
-}
-
-case class EncryptedCrdtState(states: Queue[Array[Byte]] = Queue()) {
-  def enqueue(bytes: Array[Byte]): EncryptedCrdtState = EncryptedCrdtState(states.enqueue(bytes))
-}
-
-object EncryptedCrdtState {
-  implicit val jsonCodec: JsonValueCodec[EncryptedCrdtState] = JsonCodecMaker.make
-
-  implicit val semiLattice: SemiLattice[EncryptedCrdtState] = (left: EncryptedCrdtState, right: EncryptedCrdtState) => {
-    // TODO: This won't work for Optimized AWSet
-    // TODO: This only makes sense for deltas (maybe use a Set and a replace Array[Byte] with an object containing meta)
-    EncryptedCrdtState(left.states.enqueueAll(right.states))
   }
 }
