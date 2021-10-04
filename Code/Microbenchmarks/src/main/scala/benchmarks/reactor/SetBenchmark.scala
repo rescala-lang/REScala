8b163200c3c8343062ce1062764b16e0e3bf18c2
Vincent Stollenwerk <vstollen@gmail.com>
2021-10-04 12:17:26 +0200

    Add reactor benchmarks

---
+++ Code/Microbenchmarks/src/main/scala/benchmarks/reactor/SetBenchmark.scala
@@ -0,0 +1,40 @@
+package benchmarks.reactor
+
+import benchmarks.EngineParam
+import org.openjdk.jmh.annotations._
+import rescala.extra.reactor.ReactorBundle
+import rescala.interface.RescalaInterface
+
+import java.util.concurrent.TimeUnit
+
+@BenchmarkMode(Array(Mode.Throughput))
+@OutputTimeUnit(TimeUnit.MILLISECONDS)
+@Warmup(iterations = 3, time = 1000, timeUnit = TimeUnit.MILLISECONDS)
+@Measurement(iterations = 3, time = 1000, timeUnit = TimeUnit.MILLISECONDS)
+@Fork(3)
+@Threads(1)
+@State(Scope.Thread)
+class SetBenchmark {
+  var engine: RescalaInterface = _
+  lazy val stableEngine        = engine
+  lazy val reactorApi          = new ReactorBundle[stableEngine.type](stableEngine)
+  import reactorApi._
+  import stableEngine._
+
+  var reactor: Reactor[Int] = _
+  var trigger: Evt[Unit]    = _
+
+  @Setup
+  def setup(engineParam: EngineParam) = {
+    engine = engineParam.engine
+    trigger = Evt[Unit]()
+    reactor = Reactor.loop(0) {
+      S.next(trigger) {
+        S.set(42)
+      }
+    }
+  }
+
+  @Benchmark
+  def run(): Unit = trigger.fire()
+}
