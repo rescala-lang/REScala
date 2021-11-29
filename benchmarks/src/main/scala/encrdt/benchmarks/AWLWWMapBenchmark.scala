d2dec53bb36c106acbe3e518ba858e7dc9c6f7c7
Christian Kuessner <ckuessner@mailbox.org>
2021-11-29 15:51:04 +0100

    Add more JMH benchmarks

--- benchmarks/src/main/scala/encrdt/benchmarks/AWLWWMapBenchmark.scala
+++ benchmarks/src/main/scala/encrdt/benchmarks/AWLWWMapBenchmark.scala
@@ -6,47 +6,56 @@ import encrdt.causality.VectorClock
 import encrdt.crdts.AddWinsLastWriterWinsMap
 import encrdt.encrypted.statebased.DecryptedState.vectorClockJsonCodec
 
-import com.github.javafaker.Faker
 import com.github.plokhotnyuk.jsoniter_scala.core.writeToArray
-import com.google.crypto.tink.aead.AeadConfig
-import com.google.crypto.tink.{Aead, KeyTemplates, KeysetHandle}
-import org.conscrypt.Conscrypt
+import com.google.crypto.tink.Aead
 import org.openjdk.jmh.annotations._
 import org.openjdk.jmh.infra.Blackhole
 
-import java.security.Security
-import java.util.Random
 import java.util.concurrent.TimeUnit
 
 @OutputTimeUnit(TimeUnit.MILLISECONDS)
 @BenchmarkMode(Array(Mode.AverageTime))
 @State(Scope.Thread)
-@Warmup(iterations = 10, time = 2000, timeUnit = TimeUnit.MILLISECONDS)
-@Measurement(iterations = 10, time = 2000, timeUnit = TimeUnit.MILLISECONDS)
+@Warmup(iterations = 4, time = 1000, timeUnit = TimeUnit.MILLISECONDS)
+@Measurement(iterations = 5, time = 1000, timeUnit = TimeUnit.MILLISECONDS)
 @Fork(5)
 @Threads(1)
 class AWLWWMapBenchmark {
-  var entries: Array[(String, String)] = _
-  var aead: Aead = _
-
   val replicaId = "TestReplica"
 
-  @Param(Array("1000"))
-  var putOperations: Int = _
-
-  @Setup(Level.Trial)
-  def setupTestData(): Unit = {
-    val faker = new Faker(new Random(42))
-    entries = new Array[(String, String)](putOperations)
-    for (i <- entries.indices) {
-      entries(i) = faker.name().fullName() -> faker.address().fullAddress()
-    }
+  @Benchmark
+  def serializeOnly(blackhole: Blackhole, serializeOnlyBenchmarkState: SerializeOnlyBenchmarkState): Unit = {
+    val serialPlaintextState = writeToArray(serializeOnlyBenchmarkState.crdtState)
+    blackhole.consume(serialPlaintextState)
   }
 
   @Benchmark
-  def putAndSerializePlaintext(blackhole: Blackhole): Unit = {
+  @Warmup(iterations = 7)
+  def encryptOnly(blackhole: Blackhole, serializeOnlyBenchmarkState: SerializeOnlyBenchmarkState, aeadState: AeadState): Unit = {
+    val serialEncryptedState = aeadState.aead.encrypt(serializeOnlyBenchmarkState.serialPlaintextState, serializeOnlyBenchmarkState.serialPlaintextVectorClock)
+    blackhole.consume(serialEncryptedState, serializeOnlyBenchmarkState.serialPlaintextState)
+  }
+
+  @Benchmark
+  @Warmup(iterations = 10)
+  def serializeAndEncrypt(blackhole: Blackhole, serializeOnlyBenchmarkState: SerializeOnlyBenchmarkState, aeadState: AeadState): Unit = {
+    val serialPlaintextState = writeToArray(serializeOnlyBenchmarkState.crdtState)
+    val serialPlaintextVectorClock = writeToArray(serializeOnlyBenchmarkState.crdtStateVersionVector)
+    val serialEncryptedState = aeadState.aead.encrypt(serialPlaintextState, serialPlaintextVectorClock)
+    blackhole.consume(serialEncryptedState, serialPlaintextState)
+  }
+
+  @Benchmark
+  def putOnceNoSerialization(benchState: SerializeOnlyBenchmarkState): Unit = {
+    benchState.crdt.put("This is a String", "And so this is too")
+  }
+
+  @Benchmark
+  @Fork(2)
+  @Warmup(iterations = 3, time = 1000, timeUnit = TimeUnit.MILLISECONDS)
+  def putAndSerializeManyTimes(blackhole: Blackhole, putBenchmarkState: PutManyBenchmarkState): Unit = {
     val crdt = new AddWinsLastWriterWinsMap[String, String](replicaId)
-    for (entry <- entries) {
+    for (entry <- putBenchmarkState.dummyKeyValuePairs) {
       // Update crdt
       crdt.put(entry._1, entry._2)
       // Serialize to JSON (as byte array)
@@ -57,12 +66,14 @@ class AWLWWMapBenchmark {
   }
 
   @Benchmark
-  def putAndSerializeAndEncrypt(blackhole: Blackhole, aeadState: AeadState): Unit = {
+  @Fork(2)
+  @Warmup(iterations = 3, time = 1000, timeUnit = TimeUnit.MILLISECONDS)
+  def putAndSerializeAndEncryptManyTimes(blackhole: Blackhole, putBenchmarkState: PutManyBenchmarkState, aeadState: AeadState): Unit = {
     var versionVector: VectorClock = VectorClock()
     val crdt = new AddWinsLastWriterWinsMap[String, String](replicaId)
     val aead = aeadState.aead
 
-    for (entry <- entries) {
+    for (entry <- putBenchmarkState.dummyKeyValuePairs) {
       // Update crdt
       crdt.put(entry._1, entry._2)
       // Track causality information used for encrypted crdt
@@ -75,7 +86,6 @@ class AWLWWMapBenchmark {
       blackhole.consume(encryptedState, serialState)
     }
   }
-
 }
 
 @State(Scope.Thread)
@@ -87,11 +97,56 @@ class AeadState {
 
   @Setup(Level.Trial)
   def setupAead(): Unit = {
-    Conscrypt.checkAvailability()
-    Security.addProvider(Conscrypt.newProvider)
-    AeadConfig.register()
-    val keyset: KeysetHandle = KeysetHandle.generateNew(KeyTemplates.get(keyTemplateString))
-    aead = keyset.getPrimitive(classOf[Aead])
+    aead = Helper.setupAead(keyTemplateString)
+  }
+}
+
+@State(Scope.Thread)
+class SerializeOnlyBenchmarkState {
+  var crdt: AddWinsLastWriterWinsMap[String, String] = _
+  var crdtState: AddWinsLastWriterWinsMap.LatticeType[String, String] = _
+  var crdtStateVersionVector: VectorClock = _
+
+  var serialPlaintextState: Array[Byte] = _
+  var serialPlaintextVectorClock: Array[Byte] = _
+
+  @Param(Array("10", "100", "1000", "10000"))
+  var crdtSizeInElements: Int = _
+
+  @Setup(Level.Trial)
+  def setupCrdtState(): Unit = {
+    val dummyKeyValuePairs = Helper.dummyKeyValuePairs(crdtSizeInElements)
+
+    var versionVector: VectorClock = VectorClock()
+    val replicaId = "TestReplica"
+    val crdt = new AddWinsLastWriterWinsMap[String, String](replicaId)
+
+    for (entry <- dummyKeyValuePairs) {
+      // Update crdt
+      crdt.put(entry._1, entry._2)
+      // Track causality information used for encrypted crdt
+      versionVector = versionVector.advance(replicaId)
+    }
+
+    this.crdt = crdt
+    this.crdtState = crdt.state
+    this.crdtStateVersionVector = versionVector
+
+    this.serialPlaintextState = writeToArray(crdtState)
+    this.serialPlaintextVectorClock = writeToArray(crdtStateVersionVector)
+  }
+}
+
+@State(Scope.Thread)
+class PutManyBenchmarkState {
+  var dummyKeyValuePairs: Array[(String, String)] = _
+
+  @Param(Array("10", "100", "1000"))
+  var crdtSizeInElements: Int = _
+
+  @Setup(Level.Trial)
+  def setupTestData(): Unit = {
+    dummyKeyValuePairs = Helper.dummyKeyValuePairs(crdtSizeInElements)
   }
 }
 
