c07a7c1b54b388406cd47e7af4e95b48b75b8755
ragnar <ragnar@rmgk.de>
2021-12-22 19:15:06 +0100

    fix incorrect tag names …

--- Code/Main/shared/src/test/scala-2/tests/rescala/testtools/TestIgnoreTags.scala
+++ Code/Main/shared/src/test/scala-2/tests/rescala/testtools/TestIgnoreTags.scala
@@ -1,19 +1,19 @@
 package tests.rescala.testtools
 
-import org.scalatest.{Ignore, Tag}
+import org.scalatest.Tag
 
 // workarounds for tests are fun.
 case class IgnoreOnWindowsBecause(description: String)
-    extends Tag(if (System.getProperty("os.name").contains("Windows")) "Ignore" else "")
+    extends Tag(if (System.getProperty("os.name").contains("Windows")) "org.scalatest.Ignore" else "")
 
 case class IgnoreOnGithubCiBecause(description: String)
-    extends Tag(if (Option(System.getenv("GITHUB_WORKFLOW")).exists(_.nonEmpty)) "Ignore" else "")
+    extends Tag(if (Option(System.getenv("GITHUB_WORKFLOW")).exists(_.nonEmpty)) "org.scalatest.Ignore" else "")
 
 case class IgnoreOnGithubWindowsCiBecause(description: String)
     extends Tag(
       if (
         System.getProperty("os.name").contains("Windows") &&
         Option(System.getenv("GITHUB_WORKFLOW")).exists(_.nonEmpty)
-      ) "Ignore"
+      ) "org.scalatest.Ignore"
       else ""
     )
