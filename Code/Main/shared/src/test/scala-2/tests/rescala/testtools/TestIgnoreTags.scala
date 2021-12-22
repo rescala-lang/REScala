5e7c589747951e6224d739dd1512a7c94fc60f56
ragnar <ragnar@rmgk.de>
2021-12-22 18:14:12 +0100

    remove use of reflection not compilable to JS

--- Code/Main/shared/src/test/scala-2/tests/rescala/testtools/TestIgnoreTags.scala
+++ Code/Main/shared/src/test/scala-2/tests/rescala/testtools/TestIgnoreTags.scala
@@ -4,13 +4,16 @@ import org.scalatest.{Ignore, Tag}
 
 // workarounds for tests are fun.
 case class IgnoreOnWindowsBecause(description: String)
-    extends Tag(if (System.getProperty("os.name").contains("Windows")) classOf[Ignore].getName else "")
+    extends Tag(if (System.getProperty("os.name").contains("Windows")) "Ignore" else "")
 
 case class IgnoreOnGithubCiBecause(description: String)
-    extends Tag(if (Option(System.getenv("GITHUB_WORKFLOW")).exists(_.nonEmpty)) classOf[Ignore].getName else "")
+    extends Tag(if (Option(System.getenv("GITHUB_WORKFLOW")).exists(_.nonEmpty)) "Ignore" else "")
 
 case class IgnoreOnGithubWindowsCiBecause(description: String)
-    extends Tag(if (
-      System.getProperty("os.name").contains("Windows") && Option(System.getenv("GITHUB_WORKFLOW")).exists(_.nonEmpty)
-    ) classOf[Ignore].getName
-    else "")
+    extends Tag(
+      if (
+        System.getProperty("os.name").contains("Windows") &&
+        Option(System.getenv("GITHUB_WORKFLOW")).exists(_.nonEmpty)
+      ) "Ignore"
+      else ""
+    )
