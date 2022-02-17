8f3f6e63e37d659a4e95413ad2646dd2b5490790
ragnar <ragnar@rmgk.de>
2022-02-17 20:42:30 +0100

    formatting

--- Code/Extensions/Kofre/src/main/scala/kofre/causality/CContext.scala
+++ Code/Extensions/Kofre/src/main/scala/kofre/causality/CContext.scala
@@ -5,10 +5,9 @@ import kofre.causality.DotStore.DotSet
 import kofre.causality.impl.IntTree
 import kofre.causality.{CContext, Dot}
 
-
-
 /** CContext is the typeclass trait for causal contexts. Causal contexts are used in causal CRDTs to keep track of all the dots that a
   * replica has witnessed.
+  * TODO: modeling this as a typeclass is somewhat overengineered, but changing the API currently would be too much effort
   */
 trait CContext[A] {
   def contains(cc: A, d: Dot): Boolean
@@ -47,16 +46,16 @@ object CContext {
     * data structure to efficiently store large continuous ranges of dots.
     */
   implicit val intTreeCC: CContext[CausalContext] = new CContext[CausalContext] {
-    override def contains(cc: CausalContext, d: Dot): Boolean = cc.contains(d)
-    override def fromSet(dots: Set[Dot]): CausalContext = CausalContext.fromSet(dots)
-    override def empty: CausalContext         = CausalContext.empty
-    override def one(dot: Dot): CausalContext = CausalContext.one(dot)
-    override def toSet(cc: CausalContext): Set[Dot] = cc.toSet
+    override def contains(cc: CausalContext, d: Dot): Boolean                    = cc.contains(d)
+    override def fromSet(dots: Set[Dot]): CausalContext                          = CausalContext.fromSet(dots)
+    override def empty: CausalContext                                            = CausalContext.empty
+    override def one(dot: Dot): CausalContext                                    = CausalContext.one(dot)
+    override def toSet(cc: CausalContext): Set[Dot]                              = cc.toSet
     override def union(left: CausalContext, right: CausalContext): CausalContext = left.merged(right)
-    override def diff(cc: CausalContext, other: Iterable[Dot]): CausalContext = cc.diff(fromSet(other.toSet))
-    override def max(cc: CausalContext, replicaID: String): Option[Dot] = cc.max(replicaID)
+    override def diff(cc: CausalContext, other: Iterable[Dot]): CausalContext    = cc.diff(fromSet(other.toSet))
+    override def max(cc: CausalContext, replicaID: String): Option[Dot]          = cc.max(replicaID)
     override def decompose(cc: CausalContext, exclude: Dot => Boolean): Iterable[CausalContext] = cc.decompose(exclude)
-    override def forall(cc: CausalContext, cond: Dot => Boolean): Boolean = cc.forall(cond)
+    override def forall(cc: CausalContext, cond: Dot => Boolean): Boolean                       = cc.forall(cond)
   }
 
 }
