1d29ab6cb625c89325a348d97a77f8dcd2a03c53
ragnar <ragnar@rmgk.de>
2022-02-18 13:51:07 +0100

    more object sizes

--- Code/Microbenchmarks/src/main/scala/benchmarks/measureSizes/ObjectSizes.scala
+++ Code/Microbenchmarks/src/main/scala/benchmarks/measureSizes/ObjectSizes.scala
@@ -1,8 +1,8 @@
-package benchmarks.measureSizes
+package rescala.benchmarks.measureSizes
 
 import kofre.causality.impl.ArrayRanges
 import org.openjdk.jol.info.GraphLayout
-import rescala.default
+import rescala.{Schedulers, default}
 
 object ObjectSizes {
 
@@ -17,18 +17,28 @@ object ObjectSizes {
     measure("var empty", rescala.default.Var.empty)
     measure("var 5", rescala.default.Var(5))
     measure("default empty signal", rescala.default.Signal {})
-    measure("default empty signal x 10", List.fill(100)(rescala.default.Signal {}): _*)
+    measure("default empty signal x 10", List.fill(100)(rescala.default.Signal {}))
     measure("synchron empty signal", rescala.Schedulers.synchron.Signal {})
 
-    def tx = new default.ParRPTransaction(new rescala.parrp.Backoff(), None)
-    measure("transaction", List.fill(100)(tx))
+    def ptx = new default.ParRPTransaction(new rescala.parrp.Backoff(), None)
+    measure("transaction", List.fill(100)(ptx))
     measure(
       "reev ticket",
-      new rescala.default.ReevTicket(tx, ()) {
+      new rescala.default.ReevTicket(ptx, ()) {
         override protected def staticAccess(reactive: default.ReSource): reactive.Value  = ???
         override protected def dynamicAccess(reactive: default.ReSource): reactive.Value = ???
       }
     )
+
+    def stx = new Schedulers.synchron.SimpleNoLock()
+    measure("nolock transaction", List.fill(100)(stx))
+    measure(
+      "nolock reev ticket",
+      new rescala.Schedulers.synchron.ReevTicket(stx, ()) {
+        override protected def staticAccess(reactive: rescala.Schedulers.synchron.ReSource): reactive.Value  = ???
+        override protected def dynamicAccess(reactive: rescala.Schedulers.synchron.ReSource): reactive.Value = ???
+      }
+      )
   }
 
 }
