baf2f3e1101a3556f93f5e7f5f46d7517ba080aa
ragnar <ragnar@rmgk.de>
2022-02-19 22:44:39 +0100

    bundle doc

--- Code/Main/shared/src/main/scala/rescala/operator/Operators.scala
+++ Code/Main/shared/src/main/scala/rescala/operator/Operators.scala
@@ -1,4 +1,15 @@
 package rescala.operator
 
-trait Operators extends DefaultImplementations with EventBundle with SignalBundle with FlattenApi with Sources
-with ObserveBundle
+/** To support virtual State types, everything is put into the bundle traits.
+  * But because the operators all have cyclic dependencies to each other,
+  * we need this combining bundle, which all other operator bundles use as a self type
+  * this is then the actual combinator of those bundles, ensuring that they
+  * can access each other */
+trait Operators
+    extends AnyRef // to make the below more symmetric
+    with DefaultImplementations
+    with EventBundle
+    with SignalBundle
+    with FlattenApi
+    with Sources
+    with ObserveBundle
