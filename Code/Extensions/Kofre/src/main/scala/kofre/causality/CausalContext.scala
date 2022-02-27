f418afb45a639a1d8e3e9c267c54ea3bd1b21a1d
ragnar <ragnar@rmgk.de>
2022-02-28 00:01:58 +0100

    use braceless syntax for short definitions

--- Code/Extensions/Kofre/src/main/scala/kofre/causality/CausalContext.scala
+++ Code/Extensions/Kofre/src/main/scala/kofre/causality/CausalContext.scala
@@ -6,13 +6,10 @@ import kofre.causality.impl.ArrayCausalContext
 // so, this whole file is a bit of a hack to allow renaming of ArrayCausalContext to just CausalContext
 // the TreeCausalContext can be used here as a drop in replacement for different performance behaviour.
 
-package object causality {
+// the split into package and package object seems necessary for scala 2 interop
+package object causality:
   type CausalContext = ArrayCausalContext
-}
 
-package causality {
-
-  object CausalContext {
+package causality:
+  object CausalContext:
     export ArrayCausalContext.*
-  }
-}
