4a61a416b914d62b10f5aca9aa27fe10ddeec6e5
Julian <github@julian-haas.de>
2022-04-27 18:44:52 +0200

    reformat

--- src/main/scala/lore/Parser.scala
+++ src/main/scala/lore/Parser.scala
@@ -8,7 +8,6 @@ import cats._
 import cats.data.NonEmptyList
 
 object Parser:
-
   // helpers
   val ws: P0[Unit] = sp.rep0.void // whitespace
   val id: P[ID] = alpha.rep.string
@@ -19,21 +18,23 @@ object Parser:
     }
 
   // basic terms
-  val _var: P[TVar] = id.map(TVar(_))
+  val _var: P[TVar] = id.map(TVar(_)) // variables
 
   // bolean expressions
   val quantifierVars: P[NonEmptyList[TArgT]] =
     (argT <* ws).repSep(P.char(',') <* ws)
   val triggers: P0[List[TViper]] = P.unit.as(List[TViper]())
-  val forall: P[TForall] = (((P.string("forall") ~ ws *> quantifierVars) <* P.string(
-    "::"
-  ) ~ ws) ~ triggers ~ booleanExpr).map { case ((vars, triggers), body) =>
-    TForall(vars = vars, triggers = triggers, body = body)
-  }
-  val quantifier: P[TQuantifier] = forall
+  val forall: P[TForall] =
+    (((P.string("forall") ~ ws *> quantifierVars) <* P.string(
+      "::"
+    ) ~ ws) ~ triggers ~ booleanExpr).map { case ((vars, triggers), body) =>
+      TForall(vars = vars, triggers = triggers, body = body)
+    }
+  val exists: P[TExists] = ???
+  val quantifier: P[TQuantifier] = forall | exists
   val implication: P[TImpl] = ???
-  val booleanExpr: P[TBoolean] = (quantifier | implication)
+  val booleanExpr: P[TBoolean] = quantifier | implication
 
   // programs are sequences of terms
   val term: P[Term] = _var
-  val prog: P[NonEmptyList[Term]] = term.repSep(ws) <* P.end
+  val prog: P[NonEmptyList[Term]] = term.repSep(ws).between(ws, P.end)
