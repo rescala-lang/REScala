6c8eb2a63a9bbb8e2f6ac8bf42defb8dcff7abe8
Julian <github@julian-haas.de>
2022-04-27 18:31:47 +0200

    WIP refactor parser

---
+++ src/main/scala/lore/AST.scala
@@ -0,0 +1,79 @@
+package lore
+
+import cats.data.NonEmptyList
+
+/**
+ * The abstract syntax of the LoRe language.
+ */
+object AST:
+    sealed trait Term
+
+    // helper types
+    type ID = String
+    type Type = String
+    type Number = Int
+    case class TArgT(name: ID, _type: Type) extends Term // arguments with type annotation
+
+    // basic terms
+    case class TVar(name: ID) extends Term // variable
+    case class TAbs(name: ID, _type: Type, body: Term) extends Term // abstractions
+    case class TApp(left: Term, right: Term) extends Term // application
+    case class TUnit() extends Term // unit
+
+    // derived forms
+    case class TSeq(left: Term, right: Term) extends Term // sequence
+    case class TArrow(left: Term, right: Term) extends Term // anonymous functions
+    
+    // reactives
+    sealed trait TReactive extends Term
+    case class TSource(body: Term) extends TReactive
+    case class TDerived(body: Term) extends TReactive
+
+    // Viper terms
+    case class TViper() extends Term
+
+    // interactions
+    case class TPre(body: TViper) extends Term
+    case class TPost(body: TViper) extends Term
+    case class TInt(reType: Seq[Type], argType: Seq[Type],
+        mo: Set[ID], pr: Seq[TPre], po: Seq[TPost], ex: Term) extends Term
+
+    // arithmetic expressions
+    // TODO
+    sealed trait TArith extends Term
+    case class TNum(value: Number) extends TArith // numbers
+    case class TDiv(left: Term, right: Term) extends TArith // division
+    case class TMul(left: Term, right: Term) extends TArith // multiplication
+    case class TAdd(left: Term, right: Term) extends TArith // addition
+    case class TSub(left: Term, right: Term) extends TArith // substraction
+
+    // boolean expressions
+    sealed trait TBoolean extends Term
+    case class TTrue() extends TBoolean
+    case class TFalse() extends TBoolean
+    case class TLt(left: Term, right: Term) extends TBoolean
+    case class TGt(left: Term, right: Term) extends TBoolean
+    case class TLeq(left: Term, right: Term) extends TBoolean
+    case class TGeq(left: Term, right: Term) extends TBoolean
+    case class TEq(left: Term, right: Term) extends TBoolean // equality
+    case class TIneq(left: Term, right: Term) extends TBoolean // inequality
+    case class TDisj(left: Term, right: Term) extends TBoolean // disjunction
+    case class TConj(left: Term, right: Term) extends TBoolean // conjunction
+    case class TImpl(left: Term, right: Term) extends TBoolean // implication
+    case class TInSet(left: Term, right: Term) extends TBoolean // in set
+
+    sealed trait TQuantifier extends TBoolean
+    case class TForall(vars: NonEmptyList[TArgT], triggers: Seq[TViper], body: Term) extends TQuantifier
+    case class TExists(vars: Seq[TArgT], triggers: Seq[TViper], body: Term) extends TQuantifier
+
+    // parantheses
+    case class TParens(inner: Term) extends Term
+    
+    // strings
+    case class TString(value: String) extends Term
+
+    // Scala stuff
+    // field access
+    case class TFAcc(parent: Term, field: ID, args: Seq[Term])
+    // function call
+    case class TFunC(name: ID, args: Seq[Term])
---
+++ src/main/scala/lore/Parser.scala
@@ -0,0 +1,39 @@
+package lore
+
+import AST._
+import cats.parse.{Parser => P, Parser0 => P0, Rfc5234}
+import cats.parse.Rfc5234.{alpha, digit, sp}
+import cats.implicits._
+import cats._
+import cats.data.NonEmptyList
+
+object Parser:
+
+  // helpers
+  val ws: P0[Unit] = sp.rep0.void // whitespace
+  val id: P[ID] = alpha.rep.string
+  val number: P[Number] = digit.rep.string.map(Integer.parseInt(_))
+  val argT: P[TArgT] =
+    (((id <* sp.? ~ P.char(':')) <* sp.rep0) ~ id).map { // args with type
+      case (l: ID, r: Type) => TArgT(l, r)
+    }
+
+  // basic terms
+  val _var: P[TVar] = id.map(TVar(_))
+
+  // bolean expressions
+  val quantifierVars: P[NonEmptyList[TArgT]] =
+    (argT <* ws).repSep(P.char(',') <* ws)
+  val triggers: P0[List[TViper]] = P.unit.as(List[TViper]())
+  val forall: P[TForall] = (((P.string("forall") ~ ws *> quantifierVars) <* P.string(
+    "::"
+  ) ~ ws) ~ triggers ~ booleanExpr).map { case ((vars, triggers), body) =>
+    TForall(vars = vars, triggers = triggers, body = body)
+  }
+  val quantifier: P[TQuantifier] = forall
+  val implication: P[TImpl] = ???
+  val booleanExpr: P[TBoolean] = (quantifier | implication)
+
+  // programs are sequences of terms
+  val term: P[Term] = _var
+  val prog: P[NonEmptyList[Term]] = term.repSep(ws) <* P.end
