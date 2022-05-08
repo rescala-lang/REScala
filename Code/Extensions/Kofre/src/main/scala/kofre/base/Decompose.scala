bd438ac71c403029abfb45b30d18de6a0c7f83ab
ragnar <ragnar@rmgk.de>
2022-05-08 13:19:26 +0200

    documentation

--- Code/Extensions/Kofre/src/main/scala/kofre/base/Decompose.scala
+++ Code/Extensions/Kofre/src/main/scala/kofre/base/Decompose.scala
@@ -1,8 +1,13 @@
 package kofre.base
 
+/** This is kinda the reverse of a lattice. */
 trait Decompose[A] {
 
-  /** Decomposes a lattice state into its unique irredundant join decomposition of join-irreducible states */
+  /** Decompose a state into smaller parts.
+    * Note that the goal here is small individual storage size at reasonable computational cost.
+    * Minimalism of returned results is not guaranteed.
+    * It is also not guaranteed that the result does not overlap.
+    */
   def decompose(a: A): Iterable[A]
 
   extension (a: A) def decomposed: Iterable[A] = decompose(a)
