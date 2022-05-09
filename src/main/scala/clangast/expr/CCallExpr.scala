1198f91a46a936253093af606436f823bc683ebc
Philipp Martens <philipp.martens98@gmail.com>
2022-05-09 14:41:57 +0200

    fix CCallExpr.textgen

--- src/main/scala/clangast/expr/CCallExpr.scala
+++ src/main/scala/clangast/expr/CCallExpr.scala
@@ -2,7 +2,7 @@ package clangast.expr
 import scala.quoted.{Expr, Quotes}
 
 case class CCallExpr(callee: CExpr, args: List[CExpr]) extends CExpr {
-  override def textgen: String = s"$callee(${args.map(_.textgen).mkString(", ")})"
+  override def textgen: String = s"${callee.textgen}(${args.map(_.textgen).mkString(", ")})"
 
   override def toExpr(using Quotes): Expr[CCallExpr] = {
     val calleeExpr = callee.toExpr
