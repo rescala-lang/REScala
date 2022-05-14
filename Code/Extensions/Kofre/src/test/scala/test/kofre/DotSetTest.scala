b698bdfca5ef9d2bd45e833da043df2bed02b806
ragnar <ragnar@rmgk.de>
2022-05-14 23:14:10 +0200

    actually use dot set merge

--- Code/Extensions/Kofre/src/test/scala/test/kofre/DotSetTest.scala
+++ Code/Extensions/Kofre/src/test/scala/test/kofre/DotSetTest.scala
@@ -1,9 +1,10 @@
 package test.kofre
 
-import kofre.base.DecomposeLattice
-import kofre.causality.CausalContext
+import kofre.base.{DecomposeLattice, Lattice}
+import kofre.causality.{CausalContext, Dot}
 import kofre.contextual.{AsCausalContext, WithContext}
 import kofre.dotted.DotSet
+import org.scalacheck.Prop
 import org.scalacheck.Prop.*
 import test.kofre.DataGenerator.*
 
@@ -13,17 +14,73 @@ class DotSetTest extends munit.ScalaCheckSuite {
     assert(
       DotSet.empty.isEmpty,
       s"DotSet.empty should be empty, but ${DotSet.empty} is not empty"
-      )
+    )
 
   }
-  property("merge") {
+
+  test("Manual Tests") {
+    val d1 = Dot("1", 1)
+    val d2 = Dot("2", 1)
+
+    val s1 = Set(d1)
+    assert(AsCausalContext[Set[Dot]].dots(s1).contains(d1))
+
+    val s2 = Set(d2)
+    val c1 = Set(d1)
+    val c2 = Set(d1, d2) // d1 is already deleted in the second causal context
+
+    val mergedStore = Lattice.merge(
+      WithContext(s1, CausalContext.fromSet(c1)),
+      WithContext(s2, CausalContext.fromSet(c2))
+    ).store
+
+    assert(!mergedStore.contains(d1))
+    assert(mergedStore.contains(d2))
+  }
+  property("merge 1") {
+    forAll { (s1: Set[Dot], t1: Set[Dot], s2: Set[Dot], t2: Set[Dot]) =>
+      // add all current values to their causal contexts
+      val c1 = s1 ++ t1
+      val c2 = s2 ++ t2
+
+      // commutativity
+      val m1 = Lattice.merge(WithContext(s1, CausalContext.fromSet(c1)), WithContext(s2, CausalContext.fromSet(c2)))
+      val m2 = Lattice.merge(WithContext(s2, CausalContext.fromSet(c2)), WithContext(s1, CausalContext.fromSet(c1)))
+      assert(m1 == m2)
+
+      // check if all elements were added to the new causal context
+      for (e <- c1) yield {
+        assert(m1.context.contains(e))
+      }
+      for (e <- c2) yield {
+        assert(m1.context.contains(e))
+      }
+
+      val deadElements = c1.filter(!s1.contains(_)) ++ c2.filter(!s2.contains(_))
+      val newElements  = (s1 union s2) -- deadElements
+
+      // check that already deleted elements are not added again
+      for (e <- deadElements) yield {
+        assert(!m1.store.contains(e))
+      }
+
+      // check that the new store contains all new elements
+      for (e <- newElements) yield {
+        assert(m1.store.contains(e))
+      }
+
+      Prop.passed
+    }
+  }
+
+  property("merge 2") {
     forAll { (dsA: CausalContext, deletedA: CausalContext, dsB: CausalContext, deletedB: CausalContext) =>
       val ccA = dsA union deletedA
       val ccB = dsB union deletedB
 
-      val WithContext(dsMerged, ccMerged) = DecomposeLattice[WithContext[CausalContext]].merge(
-        WithContext(dsA, ccA),
-        WithContext(dsB, ccB)
+      val WithContext(dsMerged, ccMerged) = DecomposeLattice[WithContext[DotSet]].merge(
+        WithContext(DotSet(dsA), ccA),
+        WithContext(DotSet(dsB), ccB)
       )
 
       assert(
