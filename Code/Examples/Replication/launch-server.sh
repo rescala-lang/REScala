79d0c4f020b754e8af830c13e5449030e276e2c1
ragnar <git@rmgk.de>
2023-01-30 20:05:50 +0100

    launch server to allow tcp connections

--- Code/Examples/Replication/launch-server.sh
+++ Code/Examples/Replication/launch-server.sh
@@ -4,4 +4,4 @@ curr=$(pwd)
 cd ../../..
 sbtn "replicationExamplesJS/deploy; replicationExamplesJVM/stageJars"
 cd $curr
-java --class-path "jvm/target/jars/*" replication.cli conn --webserver-listen-port 3004 --webserver-static-path js/target/scala-3.2.2/replicationexamples-fastopt --northwind-path "northwind.db"
+java --class-path "jvm/target/jars/*" replication.cli conn --webserver-listen-port 3004 --webserver-static-path js/target/scala-3.2.2/replicationexamples-fastopt --northwind-path "northwind.db" --tcp-listen-port 3005
