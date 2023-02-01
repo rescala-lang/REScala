687eaafcd52ba948a42a994d29355f34813fd2a6
Julian Haas <git@julian-haas.de>
2023-02-01 15:40:24 +0100

    refactor and fix viper backend

--- src/main/scala/lore/ViperBackend.scala
+++ src/main/scala/lore/ViperBackend.scala
@@ -2,71 +2,56 @@ package lore
 
 import AST._
 import cats.implicits._
-import java.util.UUID
 
 object ViperBackend:
+  // type definitions
+  private type Graph[R] = Map[String, (R, Type)]
+  private type CompilationResult = (CompilationContext, Seq[String])
+  private case class CompilationContext(
+      ast: Seq[Term]
+  ):
+    val graph: Graph[TReactive] = allReactives(ast)
+    val sources: Graph[TSource] = graph.collect {
+      case (name: String, (s: TSource, t: Type)) => (name, (s, t))
+    }
+
+    val derived: Graph[TDerived] = graph.collect {
+      case (name: String, (d: TDerived, t: Type)) => (name, (d, t))
+    }
+    val invariants: Seq[TInvariant] = ast.collect({ case i: TInvariant =>
+      i
+    })
 
   def toViper(ast: Seq[Term]): String =
-    //// step 1: collect registry of all reactives
-    val graph: Map[String, (TReactive, Type)] = allReactives(ast)
+    //// step 1: build context object that we pass around to subfunctions
+    val ctx = CompilationContext(ast)
 
-    // step 2: analyze invariants //
-    val invariants: Seq[TInvariant] = ast.collect({ case i: TInvariant => i })
+    //// step 2: compile source reactives
+    val (ctx2, sourcesCompiled) = compileSources(ctx)
 
-    // collect relevant reactives per invariant
-    val reactivesPerInvariant: Map[TInvariant, Set[ID]] = invariants
-      .map(i =>
-        // get reactives that this invariant mentions
-        val directDeps = uses(i).filter(name => graph.keySet.contains(name))
-        // collect (transitive) inputs of these reactives
-        val allDeps =
-          directDeps.flatMap(name =>
-            getSubgraph(name, graph.view.mapValues(_._1).toMap)
-          )
-        (i, allDeps)
-      )
-      .toMap
-
-    // compile invariants to macros
+    //// step 3: prepare macros and compile derived reactives
     // replace references to derived reactives with macro calls
-    def transformer(term: Term) = term match
+    def derived2macro(term: Term): Term = term match
       case _var @ TVar(name) =>
         // check if is reference to derived reactive
-        graph.get(name).map(_._1) match
+        ctx2.graph.get(name).map(_._1) match
           case Some(d: TDerived) =>
             val args: Seq[TVar] = uses(d)
               // find every reactive that is used in d
-              .filter(r => graph.keys.toSet.contains(r))
+              .filter(r => ctx2.graph.keys.toSet.contains(r))
               .toSeq
               .sorted
               .map(TVar(_))
             TFunC(name, args)
           case _ => _var
       case _ => term
+    val ctx3 = ctx2.copy(ast = ctx.ast.map(traverseFromNode(_, derived2macro)))
 
-    val invariantsCompiled: Seq[String] = invariants.map(i =>
-      val transformed = traverseFromNode(i, transformer)
-      invariantToMacro(
-        transformed,
-        reactivesPerInvariant(i)
-          .map(name => (name, graph(name)))
-          .collect({ case (name, s): (ID, TSource) => name })
-      )
-    )
+    // compile derived reactives
+    val (ctx4, derivedCompiled) = compileDerived(ctx3)
 
-    // step 3: compile reactives
-    val sourcesCompiled: Seq[String] = graph
-      .collect({ case s @ (name, (_: TSource, _type)) => (name, _type) })
-      .map(
-        sourceToField(_, _)
-      )
-      .toSeq
-
-    val derivedCompiled: Seq[String] = List("TODO derived reactives")
-    // val derivedCompiled: Seq[String] = graph.values
-    //   .collect({ case d: TDerived => d })
-    //   .map(d => derivedToMacro(graph, d))
-    //   .toSeq
+    //// step 4: compile invariants
+    val (ctx5, invariantsCompiled) = compileInvariants(ctx4)
 
 //     // step 4: compile transactions
 //     val transactions: Seq[Transaction] = ast.collect({ case t: Transaction =>
@@ -86,29 +71,83 @@ object ViperBackend:
           |${transactionsCompiled.mkString("\n")}
           |""".stripMargin
 
-  def invariantToMacro(invariant: TInvariant, inputs: Set[ID]): String =
-    val inputsString = inputs.toSeq.sorted.mkString(", ")
-    val id = UUID.nameUUIDFromBytes(TInvariant.toString.getBytes).toString
-    s"define inv_$id($inputsString) ${expressionToViper(invariant.condition)}"
+  private def compileSources(ctx: CompilationContext): CompilationResult =
+    def sourceToField(name: ID, _type: Type): String =
+      s"field $name: ${typeToViper(_type)}"
+    return (
+      ctx,
+      ctx.sources.map { case (name: ID, (source: TSource, _type: Type)) =>
+        sourceToField(name, _type)
+      }.toSeq
+    )
 
-  def sourceToField(name: ID, _type: Type): String =
-    def printType(t: Type): String =
-      t match
-        case SimpleType(name, Nil) => name
-        case SimpleType("Source", inner) =>
-          s"${inner.map(printType).mkString(" ,")}"
-        case SimpleType(name, inner) =>
-          s"$name[${inner.map(printType).mkString(" ,")}]"
-        case TupleType(inner) =>
-          s"(${inner.toList.map(printType).mkString(" ,")})"
-    s"field $name: ${printType(_type)}"
+  private def compileDerived(ctx: CompilationContext): CompilationResult =
+    def derivedToMacro(
+        graph: Map[String, (TReactive, Type)],
+        name: ID
+    ): String =
+      val (d, _) = graph(name)
+      val usedReactives = uses(d).filter(r => graph.keys.toSet.contains(r))
+      val body = expressionToViper(d.body)
+      val argsString = usedReactives.toSeq.sorted.mkString(", ")
 
-  // def derivedToMacro(graph: Map[String, Reactive], d: DerivedReactive): String =
-  //     val usedReactives = uses(d).filter(r => graph.keys.toSet.contains(r))
-  //     val body = expressionToViper(d.body)
-  //     val argsString = usedReactives.toSeq.sorted.mkString(", ")
+      s"define $name($argsString) $body"
 
-  //     s"define ${d.name.name}($argsString) $body"
+    return (
+      ctx,
+      ctx.derived.map { case (name: String, (_, _)) =>
+        derivedToMacro(ctx.graph, name)
+      }.toSeq
+    )
+
+  private def compileInvariants(ctx: CompilationContext): CompilationResult =
+    // collect relevant reactives per invariant
+    val reactivesPerInvariant: Map[TInvariant, Set[ID]] = ctx.invariants
+      .map(i =>
+        // get reactives that this invariant mentions
+        val directDeps = uses(i).filter(name => ctx.graph.keySet.contains(name))
+        // collect (transitive) inputs of these reactives
+        val allDeps =
+          directDeps.flatMap(name =>
+            getSubgraph(name, ctx.graph.view.mapValues(_._1).toMap)
+          )
+        (i, allDeps)
+      )
+      .toMap
+
+    def invariantToMacro(
+        invariant: TInvariant,
+        inputs: Set[ID],
+        id: String
+    ): String =
+      val inputsString = inputs.toSeq.sorted.mkString(", ")
+      s"define inv_$id($inputsString) ${expressionToViper(invariant.condition)}"
+
+    return (
+      ctx,
+      ctx.invariants
+        .zip(1 to ctx.invariants.length + 1)
+        .map((inv: TInvariant, id: Int) =>
+          // replace references to reactives with macro calls
+          invariantToMacro(
+            invariant = inv,
+            inputs = reactivesPerInvariant(inv)
+              .map(name => (name, ctx.graph(name)))
+              .collect({ case (name: ID, (_: TSource, _: Type)) => name }),
+            id = id.toString
+          )
+        )
+    )
+
+  def typeToViper(t: Type): String =
+    t match
+      case SimpleType(name, Nil) => name
+      case SimpleType("Source", inner) =>
+        s"${inner.map(typeToViper).mkString(" ,")}"
+      case SimpleType(name, inner) =>
+        s"$name[${inner.map(typeToViper).mkString(" ,")}]"
+      case TupleType(inner) =>
+        s"(${inner.toList.map(typeToViper).mkString(" ,")})"
 
   // def transactionToMethods(ast: Seq[ParsedExpression], transaction: AST.Transaction): String =
   //     val argsString = transaction.args.map{
@@ -201,7 +240,7 @@ object ViperBackend:
         case TForall(vars, triggers, body) =>
           val varString =
             vars
-              .map { case TArgT(name, t) => s"$name: $t" }
+              .map { case TArgT(name, t) => s"$name: ${typeToViper(t)}" }
               .toList
               .mkString(", ")
           triggers match
@@ -211,7 +250,7 @@ object ViperBackend:
         case TExists(vars, body) =>
           val varString =
             vars
-              .map { case TArgT(name, t) => s"$name: $t" }
+              .map { case TArgT(name, t) => s"$name: ${typeToViper(t)}" }
               .toList
               .mkString(", ")
           s"exists $varString :: ${expressionToViper(body)}"
