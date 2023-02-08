90f7a5d4fd8fd75497c49899347ff6943faf733a
ragnar <git@rmgk.de>
2023-02-08 19:57:36 +0100

    debug adapter outputs reinfo

--- Code/Examples/Todolist/src/main/scala/todo/DebugAdapter.scala
+++ Code/Examples/Todolist/src/main/scala/todo/DebugAdapter.scala
@@ -2,18 +2,19 @@ package todo
 
 import com.github.plokhotnyuk.jsoniter_scala.core.{JsonReader, JsonValueCodec, JsonWriter, writeToString}
 import com.github.plokhotnyuk.jsoniter_scala.macros.JsonCodecMaker
-import rescala.core.{ReSource, Tracing}
+import rescala.core.{ReInfo, ReSource, Tracing}
 
 import scala.scalajs.js.annotation.{JSExport, JSExportTopLevel}
 
 @JSExportTopLevel("DebugAdapter")
 object DebugAdapter {
 
+  implicit val infoCodec: JsonValueCodec[ReInfo] = JsonCodecMaker.make
   implicit val resourceCodec: JsonValueCodec[ReSource] = {
     new JsonValueCodec[ReSource]:
       override def decodeValue(in: JsonReader, default: ReSource): ReSource =
         throw IllegalStateException("deserialization not supported")
-      override def encodeValue(x: ReSource, out: JsonWriter): Unit = out.writeVal(x.info.description)
+      override def encodeValue(x: ReSource, out: JsonWriter): Unit = infoCodec.encodeValue(x.info, out)
       override def nullValue: ReSource                             = null
   }
   implicit val dataCodec: JsonValueCodec[Tracing.Data] = JsonCodecMaker.make
