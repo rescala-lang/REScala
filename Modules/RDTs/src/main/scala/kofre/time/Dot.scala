dd8376ae525c9490aebdf828459459d1dd1ecf0e
ragnar <git@rmgk.de>
2023-03-04 17:47:51 +0100

    Let’s not put more confusion about clocks out there

--- Modules/RDTs/src/main/scala/kofre/time/Dot.scala
+++ Modules/RDTs/src/main/scala/kofre/time/Dot.scala
@@ -4,7 +4,6 @@ import kofre.base.{Uid, Time}
 
 /** A Dot is a globally unique point in time.
   * Dots are partially ordered by their time per replicaId.
-  * Dots are another name for lamport clocks.
   */
 case class Dot(replicaId: Uid, time: Time) {
   def advance: Dot = Dot(replicaId, time + 1)
