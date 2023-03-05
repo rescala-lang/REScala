bb06426bb0b566dd8acb187c0147187e206c3bb6
ragnar <git@rmgk.de>
2023-03-05 13:26:44 +0100

    use with syntax

--- Modules/RDTs/src/main/scala/kofre/datatypes/GenericLastWriterWins.scala
+++ Modules/RDTs/src/main/scala/kofre/datatypes/GenericLastWriterWins.scala
@@ -13,18 +13,17 @@ trait ILastWriterWins[Time, Value] {
 
 object ILastWriterWins {
 
-  given lattice[Time: Ordering, A, LWW <: ILastWriterWins[Time, A]]: Lattice[LWW] =
-    new Lattice[LWW] {
-      override def lteq(left: LWW, right: LWW): Boolean = left.timestamp <= right.timestamp
+  given lattice[Time: Ordering, A, LWW <: ILastWriterWins[Time, A]]: Lattice[LWW] with {
+    override def lteq(left: LWW, right: LWW): Boolean = left.timestamp <= right.timestamp
 
-      override def decompose(state: LWW): Iterable[LWW] = List(state)
+    override def decompose(state: LWW): Iterable[LWW] = List(state)
 
-      override def merge(left: LWW, right: LWW): LWW =
-        Ordering[Time].compare(left.timestamp, right.timestamp) match
-          case 0 =>
-            assert(left.payload == right.payload, s"LWW same timestamp, different value: »$left«, »$right«")
-            left
-          case -1 => right
-          case 1  => left
-    }
+    override def merge(left: LWW, right: LWW): LWW =
+      Ordering[Time].compare(left.timestamp, right.timestamp) match
+        case 0 =>
+          assert(left.payload == right.payload, s"LWW same timestamp, different value: »$left«, »$right«")
+          left
+        case -1 => right
+        case 1  => left
+  }
 }
