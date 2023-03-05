5fdbf20149bfa206f57411f1ad2af265ecc11685
ragnar <git@rmgk.de>
2023-03-05 04:28:05 +0100

    enable some forgotten property tests ...

--- Modules/Example Replication/jvm/src/test/scala/deltaAntiEntropy/tests/GrowMapTest.scala
+++ Modules/Example Replication/jvm/src/test/scala/deltaAntiEntropy/tests/GrowMapTest.scala
@@ -7,29 +7,33 @@ import kofre.datatypes.{AddWinsSet, GrowOnlyMap}
 import replication.JsoniterCodecs.*
 import kofre.datatypes.GrowOnlyMap.given
 
+import org.scalacheck.Prop.*
+
 import scala.collection.mutable
 
 class GrowMapTest extends munit.ScalaCheckSuite {
   implicit val intCodec: JsonValueCodec[Int] = JsonCodecMaker.make
 
-  test("mutateKey/queryKey") { (add: List[Int], k: Int) =>
-    val network = new Network(0, 0, 0)
-    val aea     = new AntiEntropy[GrowOnlyMap[Int, AddWinsSet[Int]]]("a", network, mutable.Buffer())
-    val aeb     = new AntiEntropy[AddWinsSet[Int]]("b", network, mutable.Buffer())
+  property("mutateKey/queryKey") {
+    forAll { (add: List[Int], k: Int) =>
+      val network = new Network(0, 0, 0)
+      val aea     = new AntiEntropy[GrowOnlyMap[Int, AddWinsSet[Int]]]("a", network, mutable.Buffer())
+      val aeb     = new AntiEntropy[AddWinsSet[Int]]("b", network, mutable.Buffer())
 
-    val set = add.foldLeft(AntiEntropyContainer[AddWinsSet[Int]](aeb)) {
-      case (s, e) => s.add(using s.replicaID)(e)
+      val set = add.foldLeft(AntiEntropyContainer[AddWinsSet[Int]](aeb)) {
+        case (s, e) => s.add(using s.replicaID)(e)
+      }
+
+      val map = add.foldLeft(AntiEntropyContainer[GrowOnlyMap[Int, AddWinsSet[Int]]](aea)) {
+        case (m, e) => m.mutateKeyNamedCtx(k, AddWinsSet.empty[Int])((st) => st.add(using m.replicaID)(e))
+      }
+
+      val mapElements = map.queryKey(k).map(_.elements).getOrElse(AddWinsSet.empty[Int])
+
+      assert(
+        mapElements == set.elements,
+        s"Mutating/Querying a key in an ObserveRemoveMap should have the same behavior as modifying a standalone CRDT of that type, but $mapElements does not equal ${set.elements}"
+      )
     }
-
-    val map = add.foldLeft(AntiEntropyContainer[GrowOnlyMap[Int, AddWinsSet[Int]]](aea)) {
-      case (m, e) => m.mutateKeyNamedCtx(k, AddWinsSet.empty[Int])((st) => st.add(using m.replicaID)(e))
-    }
-
-    val mapElements = map.queryKey(k).map(_.elements).getOrElse(AddWinsSet.empty[Int])
-
-    assert(
-      mapElements == set.elements,
-      s"Mutating/Querying a key in an ObserveRemoveMap should have the same behavior as modifying a standalone CRDT of that type, but $mapElements does not equal ${set.elements}"
-    )
   }
 }
--- Modules/Example Replication/jvm/src/test/scala/deltaAntiEntropy/tests/ORMapTest.scala
+++ Modules/Example Replication/jvm/src/test/scala/deltaAntiEntropy/tests/ORMapTest.scala
@@ -5,71 +5,78 @@ import com.github.plokhotnyuk.jsoniter_scala.macros.JsonCodecMaker
 import deltaAntiEntropy.tools.{AntiEntropy, AntiEntropyContainer, Network}
 import kofre.datatypes.{AddWinsSet, ObserveRemoveMap}
 import replication.JsoniterCodecs.*
+import org.scalacheck.Prop.*
 
 import scala.collection.mutable
 
 class ORMapTest extends munit.ScalaCheckSuite {
   implicit val intCodec: JsonValueCodec[Int] = JsonCodecMaker.make
 
-  test("mutateKey/queryKey") { (add: List[Int], remove: List[Int], k: Int) =>
-    val network = new Network(0, 0, 0)
-    val aea     = new AntiEntropy[ObserveRemoveMap[Int, AddWinsSet[Int]]]("a", network, mutable.Buffer())
-    val aeb     = new AntiEntropy[AddWinsSet[Int]]("b", network, mutable.Buffer())
+  property("mutateKey/queryKey") {
+    forAll { (add: List[Int], remove: List[Int], k: Int) =>
 
-    val set = {
-      val added: AntiEntropyContainer[AddWinsSet[Int]] = add.foldLeft(AntiEntropyContainer(aeb)) {
-        case (s, e) => s.add(using s.replicaID)(e)
+      val network = new Network(0, 0, 0)
+      val aea     = new AntiEntropy[ObserveRemoveMap[Int, AddWinsSet[Int]]]("a", network, mutable.Buffer())
+      val aeb     = new AntiEntropy[AddWinsSet[Int]]("b", network, mutable.Buffer())
+
+      val set = {
+        val added: AntiEntropyContainer[AddWinsSet[Int]] = add.foldLeft(AntiEntropyContainer(aeb)) {
+          case (s, e) => s.add(using s.replicaID)(e)
+        }
+
+        remove.foldLeft(added) {
+          case (s, e) => s.remove(e)
+        }
       }
 
-      remove.foldLeft(added) {
-        case (s, e) => s.remove(e)
+      val map = {
+        val added = add.foldLeft(AntiEntropyContainer[ObserveRemoveMap[Int, AddWinsSet[Int]]](aea)) {
+          case (m, e) =>
+            m.mutateKeyNamedCtx(k)(_.add(using m.replicaID)(e))
+        }
+
+        remove.foldLeft(added) {
+          case (m, e) => m.mutateKeyNamedCtx(k)(_.remove(e))
+        }
       }
+
+      val mapElements = map.queryKey(k).elements
+
+      assert(
+        mapElements == set.elements,
+        s"Mutating/Querying a key in an ObserveRemoveMap should have the same behavior as modifying a standalone CRDT of that type, but $mapElements does not equal ${set.elements}"
+      )
     }
-
-    val map = {
-      val added = add.foldLeft(AntiEntropyContainer[ObserveRemoveMap[Int, AddWinsSet[Int]]](aea)) {
-        case (m, e) =>
-          m.mutateKeyNamedCtx(k)(_.add(using m.replicaID)(e))
-      }
-
-      remove.foldLeft(added) {
-        case (m, e) => m.mutateKeyNamedCtx(k)(_.remove(e))
-      }
-    }
-
-    val mapElements = map.queryKey(k).elements
-
-    assert(
-      mapElements == set.elements,
-      s"Mutating/Querying a key in an ObserveRemoveMap should have the same behavior as modifying a standalone CRDT of that type, but $mapElements does not equal ${set.elements}"
-    )
   }
 
-  test("remove") { (add: List[Int], remove: List[Int], k: Int) =>
-    val network = new Network(0, 0, 0)
-    val aea =
-      new AntiEntropy[ObserveRemoveMap[Int, AddWinsSet[Int]]]("a", network, mutable.Buffer())
-    val aeb = new AntiEntropy[AddWinsSet[Int]]("b", network, mutable.Buffer())
+  property("remove") {
+    forAll { (add: List[Int], remove: List[Int], k: Int) =>
+      val network = new Network(0, 0, 0)
+      val aea =
+        new AntiEntropy[ObserveRemoveMap[Int, AddWinsSet[Int]]]("a", network, mutable.Buffer())
+      val aeb = new AntiEntropy[AddWinsSet[Int]]("b", network, mutable.Buffer())
 
-    val empty = AntiEntropyContainer[AddWinsSet[Int]](aeb)
+      val empty = AntiEntropyContainer[AddWinsSet[Int]](aeb)
 
-    val map = {
-      val added = add.foldLeft(AntiEntropyContainer[ObserveRemoveMap[Int, AddWinsSet[Int]]](aea)) {
-        case (m, e) => m.mutateKeyNamedCtx(k)(_.add(using m.replicaID)(e))
+      val map = {
+        val added = add.foldLeft(AntiEntropyContainer[ObserveRemoveMap[Int, AddWinsSet[Int]]](aea)) {
+          case (m, e) => m.mutateKeyNamedCtx(k)(_.add(using m.replicaID)(e))
+        }
+
+        remove.foldLeft(added) {
+          case (m, e) => m.mutateKeyNamedCtx(k)(_.remove(e))
+        }
       }
 
-      remove.foldLeft(added) {
-        case (m, e) => m.mutateKeyNamedCtx(k)(_.remove(e))
-      }
+      val removed = map.observeRemoveMap.remove(k)
+
+      val queryResult = removed.queryKey(k).elements
+
+      assert(
+        queryResult == empty.elements,
+        s"Querying a removed key should produce the same result as querying an empty CRDT, but $queryResult does not equal ${empty.elements}"
+      )
     }
-
-    val removed = map.observeRemoveMap.remove(k)
-
-    val queryResult = removed.queryKey(k).elements
-
-    assert(
-      queryResult == empty.elements,
-      s"Querying a removed key should produce the same result as querying an empty CRDT, but $queryResult does not equal ${empty.elements}"
-    )
   }
+
 }
