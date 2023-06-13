3ef681f7b8f5361f360a20f34ddbbd96d50a3a57
André Wolski <andre.wolski@stud.tu-darmstadt.de>
2023-06-13 23:13:12 +0200

    add testcase

---
+++ Modules/RDTs/src/test/scala/test/kofre/DeltaBufferContainerDottedTest.scala
@@ -0,0 +1,27 @@
+package test.kofre
+
+import kofre.base.Uid
+import kofre.datatypes.experiments.AuctionInterface.AuctionData
+import kofre.dotted.Dotted
+import kofre.syntax.DeltaBuffer
+import kofre.syntax.DeltaBufferContainer
+import kofre.syntax.ReplicaId
+
+class DeltaBufferContainerDottedTest extends munit.FunSuite {
+
+  test("nested context removal") {
+    given ReplicaId = Uid.gen()
+
+    val initial = DeltaBufferContainer[Dotted[AuctionData]](DeltaBuffer(Dotted(AuctionData())))
+
+    assertEquals(initial.result.state.data, AuctionData())
+    assert(initial.result.state.data.bids.isEmpty)
+    assertEquals(initial.result.deltaBuffer, List.empty)
+
+    initial.bid(userId = "Testuser", price = 7)
+
+    assertEquals(initial.result.state.data.bids.size, 1)
+    assertEquals(initial.result.deltaBuffer.size, 1)
+  }
+
+}
