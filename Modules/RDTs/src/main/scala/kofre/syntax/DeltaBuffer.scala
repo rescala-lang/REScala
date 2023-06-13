rescala-lang/REScala

commit e17f7670918563af9606483b4d20c9d0aeada820

add nested delta buffer context permissions

Modules/RDTs/src/main/scala/kofre/syntax/DeltaBuffer.scala
@@ -65,4 +65,14 @@ object DeltaBufferContainer {
       c
     override def query(c: DeltaBufferContainer[L]): L = c.result.state
   }
+
+  trait LowPrio {
+    given nestedPlainPermissions[L](using pm: PermMutate[DeltaBuffer[Dotted[L]], L]): PermMutate[DeltaBufferContainer[Dotted[L]], L] = new {
+      override def mutate(c: DeltaBufferContainer[Dotted[L]], delta: L): DeltaBufferContainer[Dotted[L]] =
+        c.result = pm.mutate(c.result, delta)
+        c
+
+      override def query(c: DeltaBufferContainer[Dotted[L]]): L = c.result.state.data
+    }
+  }
 }

commit 756ede6620f4199c3c0752c801636467a92380ab

DeltaBufferContainer: add missing import
Fixes: e17f76709185 ("add nested delta buffer context permissions")

Modules/RDTs/src/main/scala/kofre/syntax/DeltaBuffer.scala
@@ -43,7 +43,7 @@ class DeltaBufferContainer[State](var result: DeltaBuffer[State]) {
     result = result.applyDelta(delta)
 }
 
-object DeltaBufferContainer {
+object DeltaBufferContainer extends DeltaBufferContainer.LowPrio {
 
   given dottedPermissions[L](using
       pcm: PermCausalMutate[DeltaBuffer[Dotted[L]], L]