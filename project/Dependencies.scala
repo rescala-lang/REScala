d923c5b4c06f1090d8ef569990757a578d9e47a7
ragnar <git@rmgk.de>
2023-06-19 12:13:09 +0200

    dependencies

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -10,9 +10,9 @@ object Dependencies {
   object Versions {
     val directories   = "26"
     val jol           = "0.17"
-    val jsoniterScala = "2.23.1"
+    val jsoniterScala = "2.23.2"
     val jsoup         = "1.16.1"
-    val munit         = "1.0.0-M7"
+    val munit         = "1.0.0-M8"
     val okHttp        = "4.10.0"
     val pprint        = "0.8.0"
     val quicklens     = "1.9.0"
