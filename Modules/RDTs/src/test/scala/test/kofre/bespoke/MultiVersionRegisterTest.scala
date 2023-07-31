a181390829a47751fc15e72fe1cb2346b4a5d333
ragnar <git@rmgk.de>
2023-07-31 21:13:01 +0200

    add bespoke MultiVersionRegisterTest test
    because I do still not quite believe that the has dots versions work

---
+++ Modules/RDTs/src/test/scala/test/kofre/bespoke/MultiVersionRegisterTest.scala
@@ -0,0 +1,37 @@
+package test.kofre.bespoke
+
+import kofre.base.Uid
+import kofre.datatypes.contextual.MultiVersionRegister
+import kofre.datatypes.experiments.BoundedCounter
+import kofre.dotted.Dotted
+import kofre.syntax.TestReplica
+import kofre.time.Dot
+import test.kofre.DataGenerator.ExampleData
+import test.kofre.given
+
+class MultiVersionRegisterTest extends munit.FunSuite {
+
+  test("basic usage") {
+
+    val a = Dotted(MultiVersionRegister.empty[String])
+
+    val alice = Uid.predefined("alice")
+    val bob = Uid.predefined("bob")
+    val charlie = Uid.predefined("charlie")
+
+    val b = a.write(using alice)("hi")
+    val c = a.write(using bob)("ho")
+
+    val m1 = b merge c
+
+    assertEquals(m1.read, Set("hi", "ho"))
+
+    val d = m1.write(using charlie)("lets go!")
+
+    assertEquals(d.read, Set("lets go!"))
+
+    assertEquals(m1 merge d, d merge b)
+
+  }
+
+}
