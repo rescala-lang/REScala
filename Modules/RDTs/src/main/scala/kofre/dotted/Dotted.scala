406dfc57dcbcc11bcb176d02d440f2158469624c
ragnar <git@rmgk.de>
2023-08-01 19:58:09 +0200

    merge perm mutate & perm causal mutate

--- Modules/RDTs/src/main/scala/kofre/dotted/Dotted.scala
+++ Modules/RDTs/src/main/scala/kofre/dotted/Dotted.scala
@@ -25,36 +25,29 @@ object DottedLattice {
   def apply[A](using ev: Lattice[Dotted[A]]): Lattice[Dotted[A]] = ev
 }
 
-object Dotted extends Dotted.LowPrio {
+object Dotted {
 
   def empty[A: Bottom]: Dotted[A] = Dotted(Bottom.empty[A], Dots.empty)
   def apply[A](a: A): Dotted[A]   = Dotted(a, Dots.empty)
-
 
   given lattice[A: HasDots: Bottom: Lattice]: Lattice[Dotted[A]] with {
     def merge(left: Dotted[A], right: Dotted[A]): Dotted[A] =
       val l = left.data.removeDots(right.deletions).getOrElse(Bottom.empty)
       val r = right.data.removeDots(left.deletions).getOrElse(Bottom.empty)
-      Dotted(l merge r, left.context union  right.context)
+      Dotted(l merge r, left.context union right.context)
   }
 
   def liftLattice[A: Lattice]: Lattice[Dotted[A]] = new {
     def merge(left: Dotted[A], right: Dotted[A]): Dotted[A] =
-      Dotted (left.data merge right.data, left.context union right.context)
+      Dotted(left.data merge right.data, left.context union right.context)
   }
 
-  given syntaxPermissions[L](using Lattice[Dotted[L]]): PermCausalMutate[Dotted[L], L] with {
+  given syntaxPermissions[L](using Lattice[Dotted[L]]): PermCausalMutate[Dotted[L], L] with PermMutate[Dotted[L], L]
+    with {
     override def mutateContext(c: Dotted[L], delta: Dotted[L]): Dotted[L] = c merge delta
+    override def mutate(c: Dotted[L], delta: L): Dotted[L]                = c merge Dotted(delta)
     override def query(c: Dotted[L]): L                                   = c.data
     override def context(c: Dotted[L]): Dots                              = c.context
-  }
-
-  trait LowPrio {
-    given identitySyntaxPermissions[L](using Lattice[Dotted[L]]): PermMutate[Dotted[L], L] with {
-      override def mutate(c: Dotted[L], delta: L): Dotted[L] = c merge Dotted(delta)
-
-      override def query(c: Dotted[L]): L = c.data
-    }
   }
 
 }
