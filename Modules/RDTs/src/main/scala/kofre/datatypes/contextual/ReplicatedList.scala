1671055b2127969c25babd82d48b57c301b39d4d
ragnar <git@rmgk.de>
2023-08-03 15:52:37 +0200

    make replicated list return dos of order

--- Modules/RDTs/src/main/scala/kofre/datatypes/contextual/ReplicatedList.scala
+++ Modules/RDTs/src/main/scala/kofre/datatypes/contextual/ReplicatedList.scala
@@ -35,7 +35,7 @@ object ReplicatedList {
   given lattice[E]: Lattice[ReplicatedList[E]] = Lattice.derived[ReplicatedList[E]]
   given hasDots[E]: HasDots[ReplicatedList[E]] with {
     extension (dotted: ReplicatedList[E])
-      def dots: Dots = dotted.meta.dots
+      def dots: Dots = dotted.meta.dots union Dots.from(dotted.order.value.inner.values.map(_.value.payload))
       def removeDots(dots: Dots): Option[ReplicatedList[E]] =
         val nmeta = dotted.meta.repr.map: (k, v) =>
           if dots.contains(k)
