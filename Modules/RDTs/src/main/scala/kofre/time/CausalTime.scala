d3bf3aae65911c45b3df944542b0e32498d3b65f
ragnar <git@rmgk.de>
2023-08-04 02:39:45 +0200

    counter instead of nanoTime

--- Modules/RDTs/src/main/scala/kofre/time/CausalTime.scala
+++ Modules/RDTs/src/main/scala/kofre/time/CausalTime.scala
@@ -15,4 +15,7 @@ object CausalTime:
     Ordering.by[CausalTime, Long](_.time)
       .orElseBy(_.causal)
       .orElseBy(_.random)
-  def now() = CausalTime(Time.current(), 0, System.nanoTime())
+
+  // originally used `System.nanoTime` for the third component, but the Web does not offer high precision timers, so a counter it is!
+  @volatile private var timeCounter = 0L
+  def now() = CausalTime(Time.current(), 0, {timeCounter += 1; timeCounter})
