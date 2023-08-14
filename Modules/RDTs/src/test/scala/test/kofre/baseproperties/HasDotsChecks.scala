be0c7e7dd92516986d2009eba52dacdb91bfdcf0
ragnar <git@rmgk.de>
2023-08-14 16:11:30 +0200

    add exhaustiveness check … bit imprcise

--- Modules/RDTs/src/test/scala/test/kofre/baseproperties/HasDotsChecks.scala
+++ Modules/RDTs/src/test/scala/test/kofre/baseproperties/HasDotsChecks.scala
@@ -13,24 +13,32 @@ import test.kofre.DataGenerator.RGAGen.given
 import scala.collection.immutable.Queue
 import scala.math.Ordering.Implicits.infixOrderingOps
 
-class DotSetHDChecks extends HasDotsChecks[DotSet]
-class CausalStoreHDChecks extends HasDotsChecks[CausalStore[DotFun[ExampleData]]]
+class DotSetHDChecks         extends HasDotsChecks[DotSet]
+class CausalStoreHDChecks    extends HasDotsChecks[CausalStore[DotFun[ExampleData]]]
 class ReplicatedListHDChecks extends HasDotsChecks[ReplicatedList[ExampleData]]
-
 
 // the specification of these tests is nice, but the generators are essentially useless, as it is extremely unlikely
 // that they will produce any kind of comparable values
 abstract class HasDotsChecks[A: Arbitrary: HasDots] extends munit.ScalaCheckSuite {
-  property("remove is precise"):
+  property("remove is precise") {
     forAll: (a: A) =>
       val dots = a.dots
       dots.iterator.foreach: dot =>
         val single = Dots.single(dot)
         val should = dots subtract single
         val remaining = a.removeDots(single) match
-          case None => Dots.empty
+          case None        => Dots.empty
           case Some(value) => value.dots
 
         assertEquals(remaining, should, s"removed $single")
+  }
+
+  property("remove is exhaustive") {
+    forAll: (a: A) =>
+      a.removeDots(a.dots) match
+        case Some(value) => assertEquals(value.dots, Dots.empty)
+        case None => // good
+
+  }
 
 }
