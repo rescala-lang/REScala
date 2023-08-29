0663582946bf75478a455507b66eb0e829878853
André Wolski <andre.wolski@stud.tu-darmstadt.de>
2023-08-29 22:28:05 +0200

    JsoniterCodecs: organize imports

--- Modules/Example Replication/shared/src/main/scala/replication/JsoniterCodecs.scala
+++ Modules/Example Replication/shared/src/main/scala/replication/JsoniterCodecs.scala
@@ -5,11 +5,11 @@ import com.github.plokhotnyuk.jsoniter_scala.macros.{CodecMakerConfig, JsonCodec
 import rdts.base.Uid
 import rdts.datatypes.alternatives.ResettableCounter
 import rdts.datatypes.contextual.{ReplicatedSet, EnableWinsFlag, MultiVersionRegister, ObserveRemoveMap, ReplicatedList}
+import rdts.datatypes.experiments.AuctionInterface.AuctionData
 import rdts.datatypes.{
   Epoch, GrowOnlyCounter, GrowOnlyList, GrowOnlyMap, GrowOnlySet, LastWriterWins, PosNegCounter, TwoPhaseSet
 }
 import rdts.dotted.Dotted
-import rdts.datatypes.experiments.AuctionInterface.AuctionData
 import rdts.time.{ArrayRanges, Dot, Dots, Time}
 
 object JsoniterCodecs {
