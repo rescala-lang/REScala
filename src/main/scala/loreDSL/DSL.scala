3091606f8074e17babddf7caf02547b61a91ffa8
Robin Breitenbach <robin.breitenbach@stud.tu-darmstadt.de>
2023-12-04 17:31:37 +0100

    Basic plugin skeleton

--- src/main/scala/loreDSL/DSL.scala
+++ src/main/scala/loreDSL/DSL.scala
@@ -1,5 +1,33 @@
 package loreDSL
 
-class DSL:
+import dotty.tools.dotc.ast.Trees.*
+import dotty.tools.dotc.ast.tpd
+import dotty.tools.dotc.core.Constants.Constant
+import dotty.tools.dotc.core.Contexts.Context
+import dotty.tools.dotc.core.StdNames.*
+import dotty.tools.dotc.core.Symbols.*
+import dotty.tools.dotc.plugins.{PluginPhase, StandardPlugin}
+import dotty.tools.dotc.report
+import dotty.tools.dotc.transform.{Pickler, Staging}
+import lore.AST.*
+import lore.DSL.*
 
+class DSL extends StandardPlugin:
+  val name: String = "DSL"
+  val description: String = "Constructs a LoRe AST from the given Scala AST"
+
+  override def init(options: List[String]): List[PluginPhase] =
+    (new DSLPhase) :: Nil
 end DSL
+
+class DSLPhase extends PluginPhase:
+  import tpd.*
+  val phaseName: String = "DSL"
+
+  override val runsAfter: Set[String] = Set(Pickler.name)
+  override val runsBefore: Set[String] = Set(Staging.name)
+
+  override def transformValDef(tree: tpd.ValDef)(using Context): tpd.Tree =
+    super.transformValDef(tree)
+  end transformValDef
+end DSLPhase
