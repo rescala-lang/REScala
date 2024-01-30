798b2a9f90b2e2d6444858d69e39c3d71b8202ea
ragnar <git@rmgk.de>
2024-01-30 15:53:42 +0100

    add debug adapter

--- build.sbt
+++ build.sbt
@@ -8,8 +8,7 @@ lazy val chat = project.in(file("."))
       "com.github.rescala-lang.rescala" %%% "kofre"       % "71e3cee000",
       "org.scala-js"                    %%% "scalajs-dom" % "2.8.0",
       "com.lihaoyi"                     %%% "scalatags"   % "0.12.0",
-      // NOTE: everything below was added after the first half of the video, so is not shown in the setup.
-      // These imports are required for the restoration and distribution.
+      // Below imports are required for the restoration and distribution.
       "com.github.scala-loci.scala-loci"      %%% "scala-loci-communicator-webrtc"       % "b9809c9c2d",
       "com.github.scala-loci.scala-loci"      %%% "scala-loci-serializer-jsoniter-scala" % "b9809c9c2d",
       "com.github.plokhotnyuk.jsoniter-scala" %%% "jsoniter-scala-core"                  % "2.27.7",
---
+++ src/main/scala/copl/DebugAdapter.scala
@@ -0,0 +1,66 @@
+package copl
+
+import com.github.plokhotnyuk.jsoniter_scala.core.{JsonReader, JsonValueCodec, JsonWriter, writeToString}
+import com.github.plokhotnyuk.jsoniter_scala.macros.JsonCodecMaker
+import rescala.core.Tracing.RawWrapper
+import rescala.core.{ReInfo, ReSource, Tracing}
+import rescala.structure.Pulse
+import scala.scalajs.js
+
+import scala.scalajs.js.annotation.{JSExport, JSExportTopLevel}
+
+/** This file is to enable integration with the external inspector. */
+@JSExportTopLevel("DebugAdapter")
+object DebugAdapter {
+
+  given infoCodec: JsonValueCodec[ReInfo] = JsonCodecMaker.make
+  given resourceCodec: JsonValueCodec[ReSource] = {
+    new JsonValueCodec[ReSource]:
+      override def decodeValue(in: JsonReader, default: ReSource): ReSource =
+        throw IllegalStateException("deserialization not supported")
+      override def encodeValue(x: ReSource, out: JsonWriter): Unit = infoCodec.encodeValue(x.info, out)
+      override def nullValue: ReSource                             = null
+  }
+
+  def debugPrinter(x: Any): String = x match
+    case v: String                                => s"\"$v\""
+    case v: (Boolean | Char | Short | Int | Long) => v.toString
+    case o: Option[Any]                           => o.map(debugPrinter).toString
+    case s: (Seq[Any])                            => s.iterator.map(debugPrinter).mkString(s"List(", ", ", ")")
+    case m: scalatags.generic.Modifier[_]         => "<some html>"
+    case p: Pulse[Any]                            => p.map(debugPrinter).getOrElse("")
+    case p: Product => Range(0, p.productArity).map(n =>
+        s"${p.productElementName(n)} = ${debugPrinter(p.productElement(n))}"
+      ).mkString(s"${p.getClass.getName}(", ", ", ")")
+    case other => s"<unknown: ${other.getClass}>"
+
+  given JsonValueCodec[Tracing.ValueWrapper] = new JsonValueCodec[Tracing.ValueWrapper]:
+    override def decodeValue(in: JsonReader, default: Tracing.ValueWrapper): Tracing.ValueWrapper =
+      throw IllegalStateException("deserialization not supported")
+    override def encodeValue(x: Tracing.ValueWrapper, out: JsonWriter): Unit =
+      out.writeVal(debugPrinter(x.v))
+    override def nullValue: Tracing.ValueWrapper = null
+
+  given JsonValueCodec[Tracing.RawWrapper] = new JsonValueCodec[Tracing.RawWrapper]:
+    override def decodeValue(in: JsonReader, default: Tracing.RawWrapper): Tracing.RawWrapper =
+      RawWrapper(in.readNullOrError(default, "unexpected"))
+    override def encodeValue(x: Tracing.RawWrapper, out: JsonWriter): Unit = out.writeNull()
+    override def nullValue: Tracing.RawWrapper                             = null
+
+  given dataCodec: JsonValueCodec[Tracing.Data] = JsonCodecMaker.make
+
+  @JSExport
+  def setListener(obs: scalajs.js.Function1[Any, Unit]): Unit = {
+    println(s"setting listener")
+    Tracing.observer = {
+      case raw: Tracing.DomAssociation =>
+        obs(js.Dictionary(
+          "type"     -> "DomAssociation",
+          "reSource" -> writeToString(raw.reSource),
+          "node"     -> raw.node.v
+        ))
+      case data => obs(writeToString(data))
+    }
+  }
+
+}
