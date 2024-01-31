85841e7bd92c1d31375e7fda9b2c2f07ef6d22eb
Robin Breitenbach <robin.breitenbach@stud.tu-darmstadt.de>
2024-01-31 18:34:39 +0100

    Some source generation

--- examples/addition/src/main/scala/loreDSL/additionExample.scala
+++ examples/addition/src/main/scala/loreDSL/additionExample.scala
@@ -4,26 +4,28 @@ import lore.DSL.*
 
 object additionExample:
   @main def main(): Unit =
-    val firstRealVariable: Source[Int] = Source(0)
-    val secondRealVariable: Derived[Int] = Derived { firstRealVariable() + firstRealVariable() }
-    val thirdRealVariable = Interaction[Int, Int]
+    val integerSource: Source[Int] = Source(0)
+    val stringSource: Source[String] = Source("abc")
+    val boolSource: Source[Boolean] = Source(true)
+//    val secondRealVariable: Derived[Int] = Derived { integerSource() + integerSource() }
+//    val thirdRealVariable = Interaction[Int, Int]
 //      .requires((curr, _) => curr < 20)
-//      .modifies(firstRealVariable)
+//      .modifies(integerSource)
 //      .executes((curr, _) => curr + 10)
 //      .requires((curr, _) => curr < 20)
-//      .modifies(firstRealVariable)
+//      .modifies(integerSource)
 //      .requires(() => false)
 //      .executes((curr) => curr + 10)
 
-//    println(s"firstRealVariable: ${firstRealVariable.now}, secondRealVariable: ${secondRealVariable.now}")
+//    println(s"integerSource: ${integerSource.now}, secondRealVariable: ${secondRealVariable.now}")
 
 //    add10(0)
-//    println(s"firstRealVariable: ${firstRealVariable.now}, secondRealVariable: ${secondRealVariable.now}")
+//    println(s"integerSource: ${integerSource.now}, secondRealVariable: ${secondRealVariable.now}")
 
 //    add10(0)
-//    println(s"firstRealVariable: ${firstRealVariable.now}, secondRealVariable: ${secondRealVariable.now}")
+//    println(s"integerSource: ${integerSource.now}, secondRealVariable: ${secondRealVariable.now}")
 
 //    add10(0)
-//    println(s"firstRealVariable: ${firstRealVariable.now}, secondRealVariable: ${secondRealVariable.now}")
+//    println(s"integerSource: ${integerSource.now}, secondRealVariable: ${secondRealVariable.now}")
   end main
 end additionExample
--- src/main/scala/loreDSL/DSL.scala
+++ src/main/scala/loreDSL/DSL.scala
@@ -11,6 +11,8 @@ import dotty.tools.dotc.plugins.{PluginPhase, StandardPlugin}
 import dotty.tools.dotc.report
 import dotty.tools.dotc.transform.{Pickler, Staging}
 
+import scala.util.matching.Regex
+
 class DSL extends StandardPlugin:
   val name: String = "DSL"
   val description: String = "Constructs a LoRe AST from the given Scala AST"
@@ -27,41 +29,53 @@ class DSLPhase extends PluginPhase:
   override val runsBefore: Set[String] = Set(Staging.name)
 
   var loreTerms: List[Term] = List()
-  private val reactiveTypeNames: List[String] = List(
-    "Source", "Derived", "Interaction",
-    "InteractionWithTypes", "InteractionWithRequires",
-    "InteractionWithRequiresAndModifies"
+  private val loreReactives: List[String] = List(
+    "rescala.default.Var", // Source
+    "rescala.default.Signal", // Derived
+    "lore.DSL.InteractionWithTypes",
+    "lore.DSL.InteractionWithRequires",
+    "lore.DSL.InteractionWithRequiresAndModifies"
   )
+  private val reactiveSourcePattern: Regex = """rescala\.default\.Var\[(\w+)\]""".r
+  private val reactiveDerivedPattern: Regex = """rescala\.default\.Signal\[(\w+)\]""".r
+  private val reactiveInteractionPattern: Regex = """lore\.DSL\.InteractionWithTypes\[(\w+), (\w+)\]""".r
 
   override def transformValDef(tree: tpd.ValDef)(using Context): tpd.Tree =
     tree match
-      // Catch any Source, Derived and Interaction definitions
-      case ValDef(name, tpt, rhs) if reactiveTypeNames.contains(tpt.denot.name.toString) =>
-        tpt match
-          // Doesn't work for Interaction, because it gets inlined to InteractionWithTypes etc.
-          // Structure of InteractionWithTypes also seems different, it doesn't have a denotation,
-          // is using a generic TypeTree and then an AppliedType, instead of an AppliedTypeTree.
-          // Unsure how to match that part for type name / type parameters.
-          // Try testing: tpt.tpe.argTypes and copying over the AST file into this project instead of importing lore
-          case AppliedTypeTree(typeName: Ident, typeParameters: List[Ident]) =>
-            report.warning(s"${typeName.name.toString} definition detected", tree.sourcePos)
-            println("-------------")
-            println(s"typeName: $typeName")
-            println(s"typeParameters: ${typeParameters.mkString(", ")}")
-            println(s"argTypes: ${tpt.tpe.argTypes.mkString(", ")}")
-            loreTerms = loreTerms :+ TAbs(name.toString, SimpleType(typeName.name.toString, typeParameters.map(t => SimpleType(t.name.toString, List()))), TNum(39))
-            println("-------")
-            println(s"loreTerms:\n${loreTerms.mkString("\n")}")
-            println("-------")
-          case _ =>
-            ()
+      // Only match ValDefs for LoRe reactives (Source, Derived, Interaction)
+      case ValDef(name, tpt, rhs) if loreReactives.exists(t => tpt.tpe.show.startsWith(t)) =>
+        val typeString = tpt.tpe.show
+        // Match which reactive it actually is, and what its type arguments are
+        typeString match
+          case reactiveSourcePattern(typeArg) =>
+            println(s"Detected Source definition with $typeArg type parameter")
+            // Process the RHS of the definition
+            rhs match
+              case Inlined(call, _, _) =>
+                println(s"Source rhs call: $call")
+                call match
+                  case Apply(_, List(Literal(Constant(value: Number)))) => // E.g. "Source(0)"
+                    println(s"Adding Source reactive with number value $value to term list")
+                    loreTerms = loreTerms :+ TAbs(name.toString, SimpleType("Source", List(SimpleType(typeArg, List()))), TNum(value))
+                  case Apply(_, List(Literal(Constant(value: String)))) => // E.g. "Source("foo")"
+                    println(s"Adding Source reactive with string value $value to term list")
+                    loreTerms = loreTerms :+ TAbs(name.toString, SimpleType("Source", List(SimpleType(typeArg, List()))), TString(value))
+                  // todo: other cases
+              case _ => println(s"Source rhs: $rhs")
+          case reactiveDerivedPattern(typeArg) =>
+            println(s"Detected Derived definition with $typeArg type parameter")
+            // todo
+          case reactiveInteractionPattern(typeArg1, typeArg2) =>
+            println(s"Detected Interaction definition with $typeArg1 and $typeArg2 type parameters")
+            // todo
       case ValDef(name, tpt, rhs) if name.toString.equals("thirdRealVariable") =>
         println("----------")
         println(s"name: $name")
         println("----------------")
         println(s"tpt: $tpt")
         println(s"tpt.tpe: ${tpt.tpe}")
-        println(s"tpt.tpe.argTypes: ${tpt.tpe.argTypes.mkString(", ")}")
+        // Use tpt.tpe.show to get string rep of actual type with type params
+        println(s"tpt.tpe.argTypes: ${tpt.tpe.argTypes.map(t => t.show).mkString(", ")}")
         println("----------------")
         println(s"rhs: $rhs")
 
