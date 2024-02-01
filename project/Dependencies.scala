9f4e0694b26f571940611bdf42d145bba62eb561
ragnar <git@rmgk.de>
2024-02-01 19:23:53 +0100

    import shared dependencies

---
+++ project/Dependencies.scala
@@ -0,0 +1,63 @@
+/* This file is shared between multiple projects
+ * and may contain unused dependencies */
+
+import org.portablescala.sbtplatformdeps.PlatformDepsPlugin.autoImport.*
+import sbt.*
+import sbt.Keys.libraryDependencies
+
+object Dependencies {
+
+  def directories   = libraryDependencies += "dev.dirs"                     % "directories"      % "26"
+  def jol           = libraryDependencies += "org.openjdk.jol"              % "jol-core"         % "0.17"
+  def jsoup         = libraryDependencies += "org.jsoup"                    % "jsoup"            % "1.17.2"
+  def munit         = libraryDependencies += "org.scalameta"              %%% "munit"            % "1.0.0-M10" % Test
+  def munitCheck    = libraryDependencies += "org.scalameta"              %%% "munit-scalacheck" % "1.0.0-M10" % Test
+  def okHttp        = libraryDependencies += "com.squareup.okhttp3"         % "okhttp"           % "4.10.0"
+  def pprint        = libraryDependencies += "com.lihaoyi"                %%% "pprint"           % "0.8.1"
+  def quicklens     = libraryDependencies += "com.softwaremill.quicklens" %%% "quicklens"        % "1.9.0"
+  def scalacheck    = libraryDependencies += "org.scalacheck"             %%% "scalacheck"       % "1.17.0"    % Test
+  def scalaJavaTime = libraryDependencies += "io.github.cquiroz"          %%% "scala-java-time"  % "2.3.0"
+  def scalajsDom    = libraryDependencies += "org.scala-js"               %%% "scalajs-dom"      % "2.8.0"
+  def scalatags     = libraryDependencies += "com.lihaoyi"                %%% "scalatags"        % "0.12.0"
+  def scribe        = libraryDependencies += "com.outr"                   %%% "scribe"           % "3.10.7"
+  def scribeSlf4j   = libraryDependencies += "com.outr"                    %% "scribe-slf4j"     % "3.10.7"
+  def scribeSlf4j2  = libraryDependencies += "com.outr"                    %% "scribe-slf4j2"    % "3.10.7"
+  def sourcecode    = libraryDependencies += "com.lihaoyi"                %%% "sourcecode"       % "0.3.1"
+  def sqliteJdbc    = libraryDependencies += "org.xerial"                   % "sqlite-jdbc"      % "3.44.1.0"
+  def upickle       = libraryDependencies += "com.lihaoyi"                %%% "upickle"          % "3.1.4"
+  def jsoniterScala =
+    libraryDependencies ++= Seq(
+      "com.github.plokhotnyuk.jsoniter-scala" %%% "jsoniter-scala-core"   % "2.27.4",
+      "com.github.plokhotnyuk.jsoniter-scala" %%% "jsoniter-scala-macros" % "2.27.4" % "provided"
+    )
+
+  object slips {
+    def chain   = libraryDependencies += "de.rmgk.slips" %%% "chain"   % "0.5.0"
+    def delay   = libraryDependencies += "de.rmgk.slips" %%% "delay"   % "0.5.0"
+    def logging = libraryDependencies += "de.rmgk.slips" %%% "logging" % "0.5.0"
+    def options = libraryDependencies += "de.rmgk.slips" %%% "options" % "0.7.0"
+    def scip    = libraryDependencies += "de.rmgk.slips" %%% "scip"    % "0.5.0"
+    def script  = libraryDependencies += "de.rmgk.slips" %%% "script"  % "0.8.0"
+  }
+
+  object loci {
+    def generic(n: String) =
+      // use maven (true) jitpack (false)?
+      if (false)
+        libraryDependencies += "io.github.scala-loci" %%% s"scala-loci-$n" % "0.5.0-62-gd313a2f"
+      else
+        libraryDependencies += "com.github.scala-loci.scala-loci" %%% s"scala-loci-$n" % "b9809c9c2d"
+
+    def communication = generic("communication")
+    def circe         = generic("serializer-circe")
+    def tcp           = generic("communicator-tcp")
+    def upickle       = generic("serializer-upickle")
+    def jsoniterScala = generic("serializer-jsoniter-scala")
+    def webrtc        = generic("communicator-webrtc")
+    def wsAkka        = generic("communicator-ws-akka")
+    def wsWeb         = generic("communicator-ws-webnative")
+    def wsJavalin     = generic("communicator-ws-javalin")
+    def wsJetty       = generic("communicator-ws-jetty")
+    def wsJetty12     = generic("communicator-ws-jetty12")
+  }
+}
---
+++ project/Settings.scala
@@ -0,0 +1,171 @@
+/* This file is shared between multiple projects
+ * and may contain unused dependencies */
+
+import com.jsuereth.sbtpgp.PgpKeys.publishSigned
+import org.scalajs.sbtplugin.ScalaJSPlugin.autoImport.jsEnv
+import sbt.*
+import sbt.Keys.*
+
+import scala.math.Ordering.Implicits.infixOrderingOps
+
+object Settings {
+
+  object Versions {
+    val scala211 = "2.11.12"
+    val scala212 = "2.12.17"
+    val scala213 = "2.13.10"
+    val scala3   = "3.3.1"
+  }
+
+  val commonCrossBuildVersions =
+    crossScalaVersions := Seq(Versions.scala211, Versions.scala212, Versions.scala213, Versions.scala3)
+
+  private def cond(b: Boolean, opts: String*) = if (b) opts.toList else Nil
+
+  // these are scoped to compile&test only to ensure that doc tasks and such do not randomly fail for no reason
+  val fatalWarnings = Seq(Compile / compile, Test / compile).map(s =>
+    s / scalacOptions ++= {
+      val version = CrossVersion.partialVersion(scalaVersion.value).get
+      List(
+        cond(version >= (2, 13), "-Werror"),
+        cond(version < (2, 13), "-Xfatal-warnings"),
+      ).flatten
+    }
+  )
+
+  val featureOptions = Seq(
+    scalacOptions ++= {
+      val version = CrossVersion.partialVersion(scalaVersion.value).get
+      List(
+        List("-feature", "-language:higherKinds", "-language:implicitConversions", "-language:existentials"),
+        cond(version == (2, 13), "-Ytasty-reader"),
+        cond(version < (3, 0), "-language:experimental.macros", "-Xsource:3"),
+        cond(version >= (3, 0), "-deprecation"),
+      ).flatten
+    }
+  )
+
+  def unusedWarnings(conf: TaskKey[_]*) = conf.map { c =>
+    c / scalacOptions ++= {
+      val version = CrossVersion.partialVersion(scalaVersion.value).get
+      cond(
+        version._1 == 3,
+        "-Wunused:imports",
+        "-Wunused:privates",
+        "-Wunused:locals",
+        "-Wunused:explicits",
+        "-Wunused:implicits",
+        "-Wunused:params",
+        "-Wunused:all"
+      )
+    }
+  }
+
+  def valueDiscard(conf: TaskKey[_]*) = conf.map { c =>
+    c / scalacOptions ++= {
+      val version = CrossVersion.partialVersion(scalaVersion.value).get
+      cond(version._1 == 3, "-Wvalue-discard")
+    }
+  }
+
+  def nonunitStatements(conf: TaskKey[_]*) = conf.map { c =>
+    c / scalacOptions ++= {
+      val version = CrossVersion.partialVersion(scalaVersion.value).get
+      cond(version._1 == 3, "-Wnonunit-statement")
+    }
+  }
+
+  def explicitNulls(conf: Configuration*) = conf.map { c =>
+    c / scalacOptions ++= {
+      val version = CrossVersion.partialVersion(scalaVersion.value).get
+      cond(version._1 == 3, "-Yexplicit-nulls")
+    }
+  }
+
+  val commonScalacOptions =
+    fatalWarnings ++ featureOptions ++ valueDiscard(Compile / compile)
+
+  // see https://www.scala-js.org/news/2021/12/10/announcing-scalajs-1.8.0/#the-default-executioncontextglobal-is-now-deprecated
+  val jsAcceptUnfairGlobalTasks =
+    Seq(scalacOptions, Test / scalacOptions).map(s =>
+      s ++= cond(!`is 3`(scalaVersion.value), "-P:scalajs:nowarnGlobalExecutionContext")
+    )
+
+  val scalaVersion_211 = Def.settings(
+    scalaVersion := Versions.scala211,
+    commonScalacOptions
+  )
+  val scalaVersion_212 = Def.settings(
+    scalaVersion := Versions.scala212,
+    commonScalacOptions
+  )
+  val scalaVersion_213 = Def.settings(
+    scalaVersion := Versions.scala213,
+    commonScalacOptions
+  )
+  val scalaVersion_3 = Def.settings(
+    scalaVersion := Versions.scala3,
+    commonScalacOptions
+  )
+
+  val scalaVersionFromEnv = scala.sys.env.get("SCALA_VERSION") match {
+    case Some("2.11") => scalaVersion_211
+    case Some("2.12") => scalaVersion_212
+    case Some("2.13") => scalaVersion_213
+    case _            => scalaVersion_3
+  }
+
+  def `is 2.11`(scalaVersion: String): Boolean =
+    CrossVersion.partialVersion(scalaVersion).contains((2, 11))
+  def `is 2.13`(scalaVersion: String): Boolean =
+    CrossVersion.partialVersion(scalaVersion).contains((2, 13))
+  def `is 3`(scalaVersion: String) =
+    CrossVersion.partialVersion(scalaVersion) collect { case (3, _) => true } getOrElse false
+
+  val dottyMigration = List(
+    Compile / compile / scalacOptions ++= List("-rewrite", "-source", "3.0-migration"),
+    Test / compile / scalacOptions ++= List("-rewrite", "-source", "3.0-migration")
+  )
+
+  val resolverJitpack = resolvers += "jitpack" at "https://jitpack.io"
+  val resolverS01     = resolvers += "sonatype staging" at "https://s01.oss.sonatype.org/content/groups/staging/"
+
+  val noPublish = Seq(
+    publishArtifact   := false,
+    packagedArtifacts := Map.empty,
+    publish           := {},
+    publishLocal      := {},
+    publishM2         := {},
+    publishSigned     := {}
+  )
+
+  // this is a tool to analyse memory consumption/layout
+  val jolSettings = Seq(
+    javaOptions += "-Djdk.attach.allowAttachSelf",
+    fork := true,
+    Dependencies.jol
+  )
+
+  // see https://www.scala-js.org/doc/project/js-environments.html
+  val jsEnvDom = jsEnv := new org.scalajs.jsenv.jsdomnodejs.JSDOMNodeJSEnv()
+
+  def sourcemapFromEnv() = {
+    val customSourcePrefix = scala.sys.env.get("RESCALA_SOURCE_MAP_PREFIX")
+    customSourcePrefix match {
+      case Some(targetUrl) if !targetUrl.isEmpty =>
+        Def.settings(
+          scalacOptions += {
+
+            def gitHash = sys.process.Process("git rev-parse HEAD").lineStream_!.head
+            def baseUrl = (LocalRootProject / baseDirectory).value.toURI.toString
+
+            if (`is 3`(scalaVersion.value))
+              s"-scalajs-mapSourceURI:$baseUrl->$targetUrl$gitHash/"
+            else
+              s"-P:scalajs:mapSourceURI:$baseUrl->$targetUrl$gitHash/"
+          }
+        )
+      case _ => Def.settings()
+    }
+  }
+}
