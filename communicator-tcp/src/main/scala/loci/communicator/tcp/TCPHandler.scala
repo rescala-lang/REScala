ba9cb22b4d5fd3b7e62b81026173c53cf791735b
ragnar <git@rmgk.de>
2024-02-01 21:59:18 +0100

    move tcp to single file

--- communicator-tcp/src/main/scala/loci/communicator/tcp/TCPHandler.scala
+++ communicator-tcp/src/main/scala/loci/communicator/tcp/TCPHandler.scala
@@ -3,12 +3,11 @@ package communicator
 package tcp
 
 import java.io.{BufferedInputStream, BufferedOutputStream, IOException}
-import java.net.{Socket, SocketException}
+import java.net.{DatagramSocket, InetAddress, InetSocketAddress, ServerSocket, Socket, SocketException}
 import java.util.concurrent.{Executors, ScheduledFuture, ThreadFactory, TimeUnit}
 import scala.collection.mutable
 import scala.util.{Failure, Success}
 import scala.util.control.NonFatal
-
 import de.rmgk.delay.Async
 
 trait MessageBuffer {
@@ -148,4 +147,42 @@ class TCPConnection(socket: Socket) extends InChan with OutChan {
     }
   }
 
+}
+
+trait TCPListener {
+  def channels: Async[Any, Bidirectional]
+  def close: Async[Any, Unit]
+}
+
+def startListening(port: Int, interface: String): TCPListener = {
+
+  new TCPListener {
+
+    val socket = new ServerSocket
+
+    try socket.setReuseAddress(true)
+    catch {
+      case _: SocketException =>
+      // some implementations may not allow SO_REUSEADDR to be set
+    }
+
+    override def close: Async[Any, Unit] = Async {
+      socket.close()
+    }
+
+    override def channels: Async[Any, Bidirectional] = Async.fromCallback {
+
+      socket.bind(new InetSocketAddress(InetAddress.getByName(interface), port))
+
+      try
+        while (true) {
+          val connection = socket.accept()
+          if connection != null then Async.handler.succeed(makeConnection(connection))
+        }
+      catch {
+        case exception: SocketException =>
+          Async.handler.fail(exception)
+      }
+    }
+  }
 }
