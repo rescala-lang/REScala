e61a94f4dffaf52d8a025c43ee307c39f887bca0
ragnar <git@rmgk.de>
2024-02-01 21:40:02 +0100

    experiment more

--- communicator-tcp/src/test/scala/loci/registry/TCPTest.scala
+++ communicator-tcp/src/test/scala/loci/registry/TCPTest.scala
@@ -1,11 +1,13 @@
 package loci.registry
 
 import de.rmgk.delay.Async
-import loci.communicator.tcp.ArrayMessageBuffer
+import loci.communicator.tcp.{ArrayMessageBuffer, Bidirectional}
 
+import java.text.Bidi
 import java.util.concurrent.TimeUnit
 import scala.concurrent.Await
 import scala.concurrent.duration.{Duration, SECONDS}
+import scala.util.{Failure, Success}
 
 object EchoServerTest {
   def main(args: Array[String]): Unit = {
@@ -13,39 +15,68 @@ object EchoServerTest {
 
     val listener = loci.communicator.tcp.startListening(port, "0")
 
-    def newThread: Async[Any, Unit] = Async.fromCallback:
-      new Thread(() =>
+    def fork: Async[Any, Unit] = Async.fromCallback:
+      val t = new Thread(() =>
         Async.handler.succeed(())
-      ).start()
+      )
+      t.setDaemon(true)
+      println(s"starting thread")
+      t.start()
 
     val echoServer: Async[Any, Unit] = Async:
-      newThread.bind
+      fork.bind
       println(s"serving")
       val channel = listener.channels.bind
       println(s"new connection")
-      newThread.bind
+      fork.bind
       val msg = channel.in.receive.bind
       println(s"echoing")
       channel.out.send(msg).bind
       println(s"done")
 
-    val client: Async[Any, Unit] = Async:
-      newThread.bind
-      println(s"connecting")
-      val bidi = loci.communicator.tcp.connect("localhost", port).bind
-      println(s"sending")
-      bidi.out.send(ArrayMessageBuffer(Array(1, 2, 3, 4))).bind
-      bidi.out.send(ArrayMessageBuffer(Array(5, 6, 7, 8))).bind
-      println(s"receiving")
-      val response = bidi.in.receive.bind
-      println(response.asArray.mkString("[", ", ", "]"))
+    val client: Async[Any, Bidirectional] =
+      val bidiA = Async:
+        fork.bind
+        println(s"connecting")
+        loci.communicator.tcp.connect("localhost", port).bind
+      .runToAsync
+
+      val sending = Async: (_: Any) ?=>
+        val bidi = bidiA.bind
+        println(s"sending")
+        fork.bind
+        bidi.out.send(ArrayMessageBuffer(Array(1, 2, 3, 4))).bind
+        bidi.out.send(ArrayMessageBuffer(Array(5, 6, 7, 8))).bind
+
+      val receiving = Async:
+        val bidi = bidiA.bind
+        fork.bind
+        println(s"receiving")
+        val response = bidi.in.receive.bind
+        println(response.asArray.mkString("[", ", ", "]"))
+
+      Async:
+        sending.bind
+        receiving.bind
+        bidiA.bind
+
+
+
+    var bidi: Bidirectional = null
 
     echoServer.run: res =>
       println(s"echo res: $res")
-    client.run: res =>
-      println(s"client res: $res")
+    client.run:
+      case Success(res) => bidi = res
+      case Failure(e) => throw e
 
-    Thread.sleep(10000)
+    Thread.sleep(1000)
+
+    println(s"done sleeping!")
+
+    listener.close.runToAsync
+    bidi.out.close()
+
   }
 }
 
