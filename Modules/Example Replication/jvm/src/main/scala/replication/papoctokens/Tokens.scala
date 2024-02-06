868410d01b8e285b22d6abd3ae7a7d293c8c5067
ragnar <git@rmgk.de>
2024-02-06 18:12:17 +0100

    add a variant of the token voting

--- Modules/Example Replication/jvm/src/main/scala/replication/papoctokens/Tokens.scala
+++ Modules/Example Replication/jvm/src/main/scala/replication/papoctokens/Tokens.scala
@@ -1,6 +1,7 @@
 package replication.papoctokens
 
 import kofre.base.{Bottom, Lattice, Orderings, Uid}
+import kofre.datatypes.Epoche
 import kofre.datatypes.contextual.ReplicatedSet
 import kofre.datatypes.contextual.ReplicatedSet.syntax
 import kofre.datatypes.experiments.RaftState
@@ -44,15 +45,15 @@ object Token {
     def grant(using ReplicaId): Mutate = mutate:
       if !isOwner then unchanged
       else
-        selectNewOwner match
+        selectFrom(current.wants) match
           case None => unchanged
           case Some(nextOwner) =>
             Token(Ownership(current.os.epoche + 1, nextOwner), ReplicatedSet.empty)
 
-    def selectNewOwner(using ReplicaId, PermQuery): Option[Uid] =
+    def selectFrom(wants: ReplicatedSet[Uid])(using ReplicaId) =
       // We find the “largest” ID that wants the token.
       // This is incredibly “unfair” but does prevent deadlocks in case someone needs multiple tokens.
-      current.wants.elements.maxOption.filter(id => id != replicaId)
+      wants.elements.maxOption.filter(id => id != replicaId)
 
     def isOwner(using ReplicaId, PermQuery): Boolean = replicaId == current.os.owner
   }
@@ -62,3 +63,53 @@ case class ExampleTokens(
     calendarAinteractionA: Token,
     calendarBinteractionA: Token
 )
+
+case class Vote(owner: Uid, voter: Uid)
+case class Voting(rounds: Epoche[ReplicatedSet[Vote]])
+
+object Voting {
+
+  val unchanged: Dotted[Voting] = Dotted(Voting(Epoche.empty))
+
+  given Lattice[Voting] = Lattice.derived
+
+  val participants = 5
+  val threshold    = (participants / 2) + 1
+
+  extension [C, E](container: C)
+    def voting: syntax[C] = syntax(container)
+
+  implicit class syntax[C](container: C) extends OpsSyntaxHelper[C, Voting](container) {
+
+    def request(using ReplicaId): CausalMutate = mutate:
+      if !current.rounds.value.isEmpty then unchanged
+      else voteFor(replicaId)
+
+    def voteFor(replica: Uid)(using ReplicaId, PermCausalMutate): Dotted[Voting] =
+      val newVote = current.rounds.value.inheritContext.add(Vote(replicaId, replicaId))
+      newVote.map(rs => Voting(current.rounds.write(rs)))
+
+    def release(using ReplicaId): Mutate = mutate:
+      Voting(Epoche(current.rounds.counter + 1, ReplicatedSet.empty))
+
+    def vote(using ReplicaId): CausalMutate = mutate:
+      val (id, count)    = leadingCount
+      if checkIfMajorityPossible(count)
+      then voteFor(id)
+      else current.inheritContext.release
+
+    def checkIfMajorityPossible(count: Int)(using PermQuery): Boolean =
+      val totalVotes = current.rounds.value.elements.size
+      val remainingVotes = participants - totalVotes
+      (count + remainingVotes) > threshold
+
+    def leadingCount(using ReplicaId, PermQuery): (Uid, Int) =
+      val votes: Set[Vote] = current.rounds.value.elements
+      votes.groupBy(_.owner).map((o, elems) => (o, elems.size)).maxBy((o, size) => size)
+
+    def isOwner(using ReplicaId, PermQuery): Boolean =
+      val (id, count) = leadingCount
+      id == replicaId && count >= threshold
+
+  }
+}
