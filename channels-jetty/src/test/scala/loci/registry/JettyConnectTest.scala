e86d8358e643dee5b537e7eb4f43979f0402a31d
ragnar <git@rmgk.de>
2024-02-18 01:45:28 +0100

    add custom websocket implementation draft

---
+++ channels-jetty/src/test/scala/loci/registry/JettyConnectTest.scala
@@ -0,0 +1,26 @@
+package loci.registry
+
+import channel.{ArrayMessageBuffer, Ctx}
+import channel.jetty.JettyWsConnection
+import de.rmgk.delay.*
+
+import java.net.URI
+
+object JettyConnectTest {
+
+  val port = 8080
+
+  def main(args: Array[String]): Unit = {
+    val connect = Async[Ctx] {
+      val outgoing = JettyWsConnection.connect(URI.create(s"ws://localhost:$port/registry/")).bind
+      outgoing.send(ArrayMessageBuffer("hello world".getBytes)).bind
+      println(s"send successfull")
+      val answer = outgoing.receive.bind
+      println(new String(answer.asArray))
+    }.run(using Ctx()) { res =>
+      println(s"stopping!")
+      println(res)
+    }
+  }
+
+}
