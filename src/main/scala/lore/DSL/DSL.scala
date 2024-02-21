826b9db4f7c588dbe4aa928e535b7badb75de7de
Svenja Kernig <svenja.kernig@gmx.de>
2024-02-21 13:33:51 +0100

    add prototype invariant DSL

--- src/main/scala/lore/DSL/DSL.scala
+++ src/main/scala/lore/DSL/DSL.scala
@@ -1,4 +1,5 @@
 package lore.DSL
+
 import scala.quoted.*
 import rescala.default.*
 import lore.Parser
@@ -10,7 +11,9 @@ object Source:
   inline def apply[A](inline expr: A): Var[A] = Var(expr)
 
 object Derived:
-  inline def apply[A](inline expr: A): Signal[A] = Signal { expr }
+  inline def apply[A](inline expr: A): Signal[A] = Signal {
+    expr
+  }
 
 // S = source type, A = argument type
 // case class Interaction(
@@ -38,20 +41,77 @@ private class InteractionWithRequires[S, A](requires: (S, A) => Boolean):
 //     InteractionWithRequiresAndModifies(expr, modifies)
 
 private class InteractionWithRequiresAndModifies[S, A](
-    requires: (S, A) => Boolean,
-    modifies: Source[S]
-):
-  inline def executes(inline expr: (S, A) => S): A => Unit =
+                                                        requires: (S, A) => Boolean,
+                                                        modifies: Source[S]
+                                                      ):
+  inline def executes(inline expr: (S, A) => S)(using invariantManager: InvariantManager): A => Unit =
     (arg: A) =>
       modifies.transform(currVal =>
-        if requires(currVal, arg) then expr(currVal, arg)
-        else
+        if !requires(currVal, arg) then
           println(s"Requirement $requires evaluated to false!")
           currVal
+        else
+          if !invariantManager.checkInvariants() then
+            print(s"Invariants pre failed")
+            currVal
+          else
+            val res = expr(currVal, arg)
+
+            if !invariantManager.checkInvariants() then
+              print("Invariants post failed")
+              currVal
+            else
+              res
+
       )
 
+trait Invariant {
+
+  def apply(): Boolean
+
+}
+
+object Invariant {
+  inline def apply(name: String)(using invariantManager: InvariantManager): Invariant = {
+    val invariant = new Invariant {
+
+      override def apply(): Boolean = {
+        println(f"Checking invariant $name")
+
+        true
+      }
+
+    }
+
+    invariantManager.registerInvariant(invariant)
+
+    invariant
+  }
+}
+
+trait InvariantManager {
+
+  def registerInvariant(invariant: Invariant): Unit
+
+  def registeredInvariants: Seq[Invariant]
+
+  def checkInvariants(): Boolean = {
+    registeredInvariants.forall(_.apply())
+  }
+
+}
+
+given defaultInvariantManager: InvariantManager = new InvariantManager {
+  private var invariants: List[Invariant] = List.empty
+  
+  override def registerInvariant(invariant: Invariant): Unit = invariants :+= invariant
+
+  override def registeredInvariants: Seq[Invariant] = invariants
+}
+
 object Interaction:
-  inline def apply[S, A]: InteractionWithTypes[S, A] = InteractionWithTypes[S, A]
+  inline def apply[S, A](using invariantManager: InvariantManager): InteractionWithTypes[S, A] = InteractionWithTypes[S, A]
+
   // inline def modifies[A](source: Source[A]) = InteractionWithModifies(source)
   inline def requires[S, A](b: Boolean): Boolean =
     ${
@@ -61,6 +121,6 @@ object Interaction:
 def makeFromRequires(b: Expr[Boolean])(using Quotes) =
   println(b.show)
   Parser.parse(b.toString) match
-    case Left(err)    => println(err)
+    case Left(err) => println(err)
     case Right(value) => println(value)
   b
--- src/main/scala/lore/DSL/DSLTest.scala
+++ src/main/scala/lore/DSL/DSLTest.scala
@@ -9,6 +9,9 @@ object DSLTest:
   def main(): Unit =
     val a: Source[Int] = Source(0)
     val b: Derived[Int] = Derived { a() + a() }
+    
+    Invariant("one")
+    
     val add10 =
       Interaction[Int, Int]
         .requires((curr, _) => curr < 20)
@@ -18,6 +21,9 @@ object DSLTest:
     //   .modifies(a)
     //   .requires(() => false)
     //   .executes((curr) => curr + 10)
+    
+    Invariant("two")
+    
     add10(0)
     println(s"a: ${a.now}, b: ${b.now}")
     add10(0)
