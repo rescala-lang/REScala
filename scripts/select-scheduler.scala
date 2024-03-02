eb21fcf8ce673e79dbf7960e911ff5b7e98ecd11
ragnar <git@rmgk.de>
2024-03-02 23:08:52 +0100

    update scheduler selection script

--- scripts/select-scheduler.scala
+++ scripts/select-scheduler.scala
@@ -5,32 +5,45 @@
 import java.nio.file.Path
 import java.nio.file.Files
 import de.rmgk.options
+
 import java.nio.file.StandardOpenOption
 import de.rmgk.script.*
+
+import scala.util.matching.Regex
 
 object SelectScheduler {
   def main(args: Array[String]): Unit =
     options.parseArguments(args.toList):
-      val selection = options
-        .positional("scheduler", "default scheduler for all", "default")
-        .value
+      val selection = options.positional[String]("scheduler", "default scheduler for all").value
 
-      val scheduler = selection match {
-        case "fullmv"   => "reactives.fullmv.FullMVUtil.defaultScheduler"
-        case "parrp"    => "reactives.parrp.ParRPDefault.scheduler"
-        case "toposort" => "reactives.scheduler.TopoBundle.TopoScheduler"
-        case "sidup"    => "reactives.scheduler.SynchronizedSidup.scheduler"
-        case "calculus" => "reactives.scheduler.CalculusLike.FScheduler"
-        case _          => "reactives.scheduler.LevelbasedVariants.synchron"
-      }
-      val target = Path.of(
-        "Modules/Reactives/shared/src/main/scala/reactives/scheduler/GeneratedSelection.scala"
-      )
-      Files.writeString(
-        target,
-        s"package reactives.generated { object Scheduler { val selection = $scheduler } }",
-        StandardOpenOption.CREATE,
-        StandardOpenOption.TRUNCATE_EXISTING
-      )
-      val p = process"cs launch scalafmt -- $target".runInherit()
+      val targetFolder =
+        val cwd     = Path.of("").toAbsolutePath
+        val project = if cwd.endsWith("scripts") then cwd.getParent else cwd
+        project.resolve("Modules/Reactives/shared/src/main/scala/reactives/scheduler/")
+
+      val sourceFile = targetFolder.resolve("GlobalCandidate.scala")
+
+      if !Files.isRegularFile(sourceFile) then throw IllegalStateException(s"could not find regular file: $sourceFile")
+
+      val regex = new Regex(raw"""(?x)(?s) case \s+ " (?<candidate> \w+ ) " \s+ =>""")
+
+      val candidates = regex.findAllMatchIn(Files.readString(sourceFile)).map(m => m.group("candidate")).toList
+
+      if !candidates.contains(selection)
+      then
+        println(s"invalid selection: »$selection«, the following are available:")
+        candidates.foreach(println)
+      else
+        val target = targetFolder.resolve("GeneratedSelection.scala")
+
+        Files.writeString(
+          target,
+          s"""package reactives.scheduler
+             |object GeneratedSelection {
+             |  inline val selection: "$selection" = "$selection"
+             |}
+             |""".stripMargin,
+          StandardOpenOption.CREATE,
+          StandardOpenOption.TRUNCATE_EXISTING
+        )
 }
