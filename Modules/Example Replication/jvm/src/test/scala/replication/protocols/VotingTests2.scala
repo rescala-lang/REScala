3d4c77595de14db632b1ccfd374c425ba982ed0f
Julian <git@julian-haas.de>
2024-04-02 18:27:53 +0200

    add voting tests

---
+++ Modules/Example Replication/jvm/src/test/scala/replication/protocols/VotingTests2.scala
@@ -0,0 +1,44 @@
+package replication.protocols
+import rdts.base.Uid
+import rdts.datatypes.contextual.ReplicatedSet
+import rdts.datatypes.{Epoch, LastWriterWins}
+import rdts.syntax.LocalReplicaId
+import rdts.time.Dots
+import replication.papoctokens.{Vote, Voting}
+import replication.papoctokens.Voting.*
+
+class VotingTests2 extends munit.FunSuite {
+  given dots: Dots = Dots.empty
+  // create replicas with for set of 2 participants
+  val id1: LocalReplicaId = LocalReplicaId.gen()
+  val id2: LocalReplicaId = LocalReplicaId.gen()
+  var voting = Voting(
+    rounds = Epoch.empty[ReplicatedSet[Vote]],
+    numParticipants = LastWriterWins.now(2)
+  )
+  test("No initial owner") {
+    assert(!voting.isOwner(using id1))
+  }
+  test("Still not owner after one vote"){
+    voting = voting.merge(voting.voteFor(id1.uid)(using id1).data)
+    assert(!voting.isOwner(using id1))
+  }
+  test("Duplicate vote changes nothing"){
+    assertEquals(voting.merge(voting.voteFor(id1.uid)(using id1).data), voting)
+  }
+  test("Is owner after two votes"){
+    voting = voting.merge(voting.voteFor(id1.uid)(using id2).data)
+    assert(voting.isOwner(using id1))
+    assert(!voting.isOwner(using id2))
+  }
+  test("Is not owner for 4 participants"){
+    voting = voting.merge(Voting(voting.rounds, LastWriterWins.now(4)))
+    assert(!voting.isOwner(using id1))
+    assert(!voting.isOwner(using id2))
+  }
+  test("Is owner for 3 participants"){
+    voting = voting.merge(Voting(voting.rounds, LastWriterWins.now(3)))
+    assert(voting.isOwner(using id1))
+    assert(!voting.isOwner(using id2))
+  }
+}
