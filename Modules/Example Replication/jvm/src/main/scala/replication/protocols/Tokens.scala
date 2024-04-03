74f0a621c543faed56f9b48c1108159853138248
Julian Haas <git@julian-haas.de>
2024-04-03 16:16:38 +0200

    move papocstuff to new package

--- Modules/Example Replication/jvm/src/main/scala/replication/protocols/Tokens.scala
+++ Modules/Example Replication/jvm/src/main/scala/replication/protocols/Tokens.scala
@@ -1,4 +1,4 @@
-package replication.papoctokens
+package replication.protocols
 
 import rdts.base.{Bottom, Lattice, Orderings, Uid}
 import rdts.datatypes.contextual.ReplicatedSet
@@ -53,62 +53,6 @@ case class ExampleTokens(
     calendarAinteractionA: Token,
     calendarBinteractionA: Token
 )
-
-case class Vote(owner: Uid, voter: Uid)
-case class Voting(rounds: Epoch[ReplicatedSet[Vote]], numParticipants: LastWriterWins[Int]) {
-  def threshold: Int = numParticipants.value / 2 + 1
-
-  def isOwner(using LocalReplicaId): Boolean =
-    val (id, count) = leadingCount
-    id == replicaId && count >= threshold
-
-  def request(using LocalReplicaId, Dots): Dotted[Voting] =
-    if !rounds.value.isEmpty then Voting.unchanged
-    else voteFor(replicaId)
-
-  def release(using LocalReplicaId): Voting =
-    if !isOwner
-    then Voting.unchanged.data
-    else Voting(Epoch(rounds.counter + 1, ReplicatedSet.empty), numParticipants)
-
-  def upkeep(using LocalReplicaId, Dots): Dotted[Voting] =
-    val (id, count) = leadingCount
-    if checkIfMajorityPossible(count)
-    then voteFor(id)
-    else Dotted(forceRelease)
-
-  def forceRelease(using LocalReplicaId): Voting =
-    Voting(Epoch(rounds.counter + 1, ReplicatedSet.empty), numParticipants)
-
-  def voteFor(uid: Uid)(using LocalReplicaId, Dots): Dotted[Voting] =
-    if rounds.value.elements.exists { case Vote(_, voter) => voter == replicaId }
-    then Voting.unchanged // already voted!
-    else
-      val newVote = rounds.value.addElem(Vote(uid, replicaId))
-      newVote.map(rs => Voting(rounds.write(rs), numParticipants))
-
-  def checkIfMajorityPossible(count: Int): Boolean =
-    val totalVotes     = rounds.value.elements.size
-    val remainingVotes = numParticipants.value - totalVotes
-    (count + remainingVotes) > threshold
-
-  def leadingCount(using id: LocalReplicaId): (Uid, Int) =
-    val votes: Set[Vote]       = rounds.value.elements
-    val grouped: Map[Uid, Int] = votes.groupBy(_.owner).map((o, elems) => (o, elems.size))
-    if grouped.isEmpty
-    then (replicaId, 0)
-    else grouped.maxBy((o, size) => size)
-  // .maxBy((o, size) => size)
-}
-
-object Voting {
-  given Bottom[Int] with
-    def empty = 0
-  val unchanged: Dotted[Voting] = Dotted(Voting(Epoch.empty, LastWriterWins.empty[Int]))
-
-  given Lattice[Voting] = Lattice.derived
-
-}
 
 case class Exclusive[T: Lattice: Bottom](token: Token, value: T) {
   def transform(f: T => T)(using LocalReplicaId) =
