207bb8e260efbb2c789b701cafb78fc0235f0ab9
Julian Haas <git@julian-haas.de>
2024-04-03 16:16:38 +0200

    move papocstuff to new package

--- Modules/Example Replication/jvm/src/test/scala/replication/protocols/VotingTests2Participants.scala
+++ Modules/Example Replication/jvm/src/test/scala/replication/protocols/VotingTests2Participants.scala
@@ -4,12 +4,10 @@ import rdts.datatypes.contextual.ReplicatedSet
 import rdts.datatypes.{Epoch, LastWriterWins}
 import rdts.syntax.LocalReplicaId
 import rdts.time.Dots
-import replication.papoctokens.{Vote, Voting}
-import replication.papoctokens.Voting.*
 
-class VotingTests2 extends munit.FunSuite {
+class VotingTests2Participants extends munit.FunSuite {
   given dots: Dots = Dots.empty
-  // create replicas with for set of 2 participants
+  // create replicas for set of 2 participants
   val id1: LocalReplicaId = LocalReplicaId.gen()
   val id2: LocalReplicaId = LocalReplicaId.gen()
   var voting = Voting(
@@ -19,24 +17,24 @@ class VotingTests2 extends munit.FunSuite {
   test("No initial owner") {
     assert(!voting.isOwner(using id1))
   }
-  test("Still not owner after one vote"){
+  test("Still not owner after one vote") {
     voting = voting.merge(voting.voteFor(id1.uid)(using id1).data)
     assert(!voting.isOwner(using id1))
   }
-  test("Duplicate vote changes nothing"){
+  test("Duplicate vote changes nothing") {
     assertEquals(voting.merge(voting.voteFor(id1.uid)(using id1).data), voting)
   }
-  test("Is owner after two votes"){
+  test("Is owner after two votes") {
     voting = voting.merge(voting.voteFor(id1.uid)(using id2).data)
     assert(voting.isOwner(using id1))
     assert(!voting.isOwner(using id2))
   }
-  test("Is not owner for 4 participants"){
+  test("Is not owner for 4 participants") {
     voting = voting.merge(Voting(voting.rounds, LastWriterWins.now(4)))
     assert(!voting.isOwner(using id1))
     assert(!voting.isOwner(using id2))
   }
-  test("Is owner for 3 participants"){
+  test("Is owner for 3 participants") {
     voting = voting.merge(Voting(voting.rounds, LastWriterWins.now(3)))
     assert(voting.isOwner(using id1))
     assert(!voting.isOwner(using id2))
