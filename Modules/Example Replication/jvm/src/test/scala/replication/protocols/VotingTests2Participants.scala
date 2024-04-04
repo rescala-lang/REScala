5f862fe5effbbae6e24548e1c8654435308e2fe5
Julian <git@julian-haas.de>
2024-04-04 18:19:42 +0200

    wrap voting in a dotted

--- Modules/Example Replication/jvm/src/test/scala/replication/protocols/VotingTests2Participants.scala
+++ Modules/Example Replication/jvm/src/test/scala/replication/protocols/VotingTests2Participants.scala
@@ -1,42 +1,45 @@
 package replication.protocols
-import rdts.base.Uid
+import rdts.base.{Lattice, Uid}
 import rdts.datatypes.contextual.ReplicatedSet
 import rdts.datatypes.{Epoch, LastWriterWins}
+import rdts.dotted.Dotted
+import rdts.dotted.DottedLattice
 import rdts.syntax.LocalReplicaId
 import rdts.time.Dots
 
 class VotingTests2Participants extends munit.FunSuite {
-  given dots: Dots = Dots.empty
+  given dots: Dots              = Dots.empty
+  given Lattice[Dotted[Voting]] = Lattice.derived
   // create replicas for set of 2 participants
   val id1: LocalReplicaId = LocalReplicaId.gen()
   val id2: LocalReplicaId = LocalReplicaId.gen()
-  var voting = Voting(
+  var voting = Dotted(Voting(
     rounds = Epoch.empty[ReplicatedSet[Vote]],
     numParticipants = LastWriterWins.now(2)
-  )
+  ))
   test("No initial owner") {
-    assert(!voting.isOwner(using id1))
+    assert(!voting.data.isOwner(using id1))
   }
   test("Still not owner after one vote") {
-    voting = voting.merge(voting.voteFor(id1.uid)(using id1).data)
-    assert(!voting.isOwner(using id1))
+    voting = voting.merge(voting.data.voteFor(id1.uid)(using id1))
+    assert(!voting.data.isOwner(using id1))
   }
   test("Duplicate vote changes nothing") {
-    assertEquals(voting.merge(voting.voteFor(id1.uid)(using id1).data), voting)
+    assertEquals(voting.merge(voting.data.voteFor(id1.uid)(using id1)), voting)
   }
   test("Is owner after two votes") {
-    voting = voting.merge(voting.voteFor(id1.uid)(using id2).data)
-    assert(voting.isOwner(using id1))
-    assert(!voting.isOwner(using id2))
+    voting = voting.merge(voting.data.voteFor(id1.uid)(using id2))
+    assert(voting.data.isOwner(using id1))
+    assert(!voting.data.isOwner(using id2))
   }
   test("Is not owner for 4 participants") {
-    voting = voting.merge(Voting(voting.rounds, LastWriterWins.now(4)))
-    assert(!voting.isOwner(using id1))
-    assert(!voting.isOwner(using id2))
+    voting = voting.merge(Dotted(Voting(voting.data.rounds, LastWriterWins.now(4))))
+    assert(!voting.data.isOwner(using id1))
+    assert(!voting.data.isOwner(using id2))
   }
   test("Is owner for 3 participants") {
-    voting = voting.merge(Voting(voting.rounds, LastWriterWins.now(3)))
-    assert(voting.isOwner(using id1))
-    assert(!voting.isOwner(using id2))
+    voting = voting.merge(Dotted(Voting(voting.data.rounds, LastWriterWins.now(3))))
+    assert(voting.data.isOwner(using id1))
+    assert(!voting.data.isOwner(using id2))
   }
 }
