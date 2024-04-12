68feed1018f98e792cd0861fddd6246153564475
Christian Kuessner <17302779+ckuessner@users.noreply.github.com>
2024-04-12 23:12:48 +0200

    Replace encrdt Map, MultiValueReg crdts with ardts

---
+++ RDTs/src/main/scala/com/github/ckuessner/ardt/datatypes/MultiValueRegister.scala
@@ -0,0 +1,35 @@
+package com.github.ckuessner.ardt.datatypes
+
+import com.github.ckuessner.ardt.base.{Causal, Lattice}
+import com.github.ckuessner.ardt.causality.DotStore
+import com.github.ckuessner.ardt.causality.DotStore.DotFun
+import com.github.ckuessner.ardt.causality.impl.ArrayCausalContext
+
+import scala.language.implicitConversions
+
+opaque type MultiValueRegister[V] = Causal[DotFun[V]]
+
+object MultiValueRegister:
+  extension [V](reg: MultiValueRegister[V])
+    def read: Set[V] = {
+      reg.dotStore.values.toSet
+    }
+
+  private[datatypes] given regToCausal[V]: Conversion[MultiValueRegister[V], Causal[DotFun[V]]] = identity
+  private[datatypes] given causalToReg[V]: Conversion[Causal[DotFun[V]], MultiValueRegister[V]] = identity
+
+  given lattice[V]: Lattice[MultiValueRegister[V]] = Causal.CausalWithDotFunLattice[V]
+
+  object mutators:
+    def write[V](register: MultiValueRegister[V], value: V, replicaId: String): MultiValueRegister[V] =
+      val dot = register.causalContext.clockOf(replicaId).advance(replicaId)
+      Causal(
+        Map(dot -> value),
+        ArrayCausalContext.fromSet(register.dotStore.keySet).add(dot.replicaId, dot.time)
+      )
+
+    def clear[V](register: MultiValueRegister[V]): MultiValueRegister[V] =
+      Causal(
+        DotStore[DotFun[V]].bottom,
+        ArrayCausalContext.fromSet(register.dotStore.keySet)
+      )
