9d7504678b6c808cc6896fb50c1446856f8341f2
Christian Kuessner <17302779+ckuessner@users.noreply.github.com>
2024-04-12 11:46:22 +0200

    Remove non-delta encrypted CRDTs

--- RDTs/src/main/scala/com/github/ckuessner/encrdt/encrypted/statebased/Replica.scala
+++
@@ -1,7 +0,0 @@
-package com.github.ckuessner.encrdt.encrypted.statebased
-
-trait Replica {
-  def receive(encryptedState: EncryptedState): Unit
-
-  protected def disseminate(encryptedState: EncryptedState): Unit
-}
