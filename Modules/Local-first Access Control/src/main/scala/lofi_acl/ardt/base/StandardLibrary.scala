b36557ae94a09faa60f8295ec4d1ce041c5741e4
Christian Kuessner <17302779+ckuessner@users.noreply.github.com>
2024-04-26 12:41:21 +0200

    Add access control Filter implementations for Standard Library ARDTs

--- Modules/Local-first Access Control/src/main/scala/lofi_acl/ardt/base/StandardLibrary.scala
+++ Modules/Local-first Access Control/src/main/scala/lofi_acl/ardt/base/StandardLibrary.scala
@@ -1,6 +1,11 @@
 package lofi_acl.ardt.base
 
+import lofi_acl.access.*
+import lofi_acl.access.Permission.{ALLOW, DENY}
+import lofi_acl.access.PermissionTreeValidationException.InvalidPathException
 import rdts.base.{Bottom, Lattice}
+
+import scala.util.{Failure, Success, Try}
 
 object StandardLibrary:
 
@@ -15,6 +20,20 @@ object StandardLibrary:
     given bottom[Nothing]: Bottom[Option[Nothing]] with
       override val empty: Option[Nothing] = None
 
+    given filter[T: Filter]: Filter[Option[T]] with
+      override def filter(delta: Option[T], permission: PermissionTree): Option[T] =
+        if delta.isEmpty
+        then delta
+        else
+          permission match
+            case PermissionTree(DENY, children) if children.isEmpty  => None
+            case PermissionTree(ALLOW, children) if children.isEmpty => delta
+            case PermissionTree(_, children)                         => Some(Filter[T].filter(delta.get, permission))
+
+      override def validatePermissionTree(permissionTree: PermissionTree): Try[PermissionTree] =
+        if permissionTree.children.isEmpty then Success(permissionTree)
+        else Filter[T].validatePermissionTree(permissionTree)
+
   // Set[T] can be treated as a grow only set
   object GrowOnlySet:
     object mutators:
@@ -24,6 +43,17 @@ object StandardLibrary:
 
     given bottom[T]: Bottom[Set[T]] with
       override val empty: Set[T] = Set.empty
+
+    given filter[T]: Filter[Set[T]] with
+      override def filter(delta: Set[T], permission: PermissionTree): Set[T] = permission match
+        case PermissionTree(ALLOW, children) if children.isEmpty => delta
+        case PermissionTree(DENY, children) if children.isEmpty  => Set.empty
+        case PermissionTree(_, _)                                => ??? // TODO: Maybe add support for child filters
+
+      override def validatePermissionTree(permissionTree: PermissionTree): Try[PermissionTree] =
+        if permissionTree.children.isEmpty
+        then Success(permissionTree)
+        else Failure(PermissionTreeValidationException.InvalidPathException(List.empty))
 
   // Map[K, V] can be treated as a grow only map, if V is mergeable (e.g., last-writer-wins register, …)
   object GrowOnlyMap:
@@ -49,3 +79,38 @@ object StandardLibrary:
 
     given bottom[K, V]: Bottom[Map[K, V]] with
       override val empty: Map[K, V] = Map.empty
+
+    given filter[K, V: Filter]: Filter[Map[K, V]] with
+      override def filter(delta: Map[K, V], permission: PermissionTree): Map[K, V] =
+        permission match
+          case PermissionTree(DENY, children) if children.isEmpty   => Map.empty
+          case PermissionTree(ALLOW, children) if children.isEmpty  => delta
+          case PermissionTree(parentPermission, childrenPermission) =>
+            // TODO: Add MapLikeFilter typeclass that extracts this and makes equality check of key explicit
+            delta.flatMap { case key -> value =>
+              // TODO: Replace key.toString based lookup with typeclass based equality
+              childrenPermission.get(key.toString) match
+                case Some(keyPermissionTree) =>
+                  if keyPermissionTree.children.isEmpty then
+                    if keyPermissionTree.permission == DENY
+                    then None
+                    else Some(key -> value)
+                  else Some(key -> Filter[V].filter(value, keyPermissionTree))
+                case None /* No rule specific to mapping -> use parent permission */ =>
+                  if parentPermission == ALLOW
+                  then Some(key -> value)
+                  else None
+            }
+
+      override def validatePermissionTree(permissionTree: PermissionTree): Try[PermissionTree] =
+        if permissionTree.children.isEmpty
+        then Success(permissionTree)
+        else
+          permissionTree.children.foldLeft[(String, Try[PermissionTree])]("" -> Success(permissionTree)) {
+            case (prevKeyPath -> prevResult, keyPath -> pt) =>
+              if (prevResult.isFailure) prevKeyPath -> prevResult
+              else keyPath                          -> Filter[V].validatePermissionTree(pt)
+          } match
+            case keyPath -> Failure(InvalidPathException(subPath)) => Failure(InvalidPathException(keyPath :: subPath))
+            case (_, f @ scala.util.Failure(_))                    => f
+            case _ -> Success(_)                                   => Success(permissionTree)
