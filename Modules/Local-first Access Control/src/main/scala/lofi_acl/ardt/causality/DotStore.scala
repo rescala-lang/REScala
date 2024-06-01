b44f48dc725002b5871314783c84978471752631
ragnar <git@rmgk.de>
2024-06-01 13:54:17 +0200

    make dot store compat … ?

--- Modules/Local-first Access Control/src/main/scala/lofi_acl/ardt/causality/DotStore.scala
+++ Modules/Local-first Access Control/src/main/scala/lofi_acl/ardt/causality/DotStore.scala
@@ -1,6 +1,8 @@
 package lofi_acl.ardt.causality
 
 import lofi_acl.ardt.causality.DotStore.*
+import rdts.base.Bottom
+import rdts.dotted.HasDots
 import rdts.time.{Dot, Dots}
 
 // See: Delta state replicated data types (https://doi.org/10.1016/j.jpdc.2017.08.003)
@@ -17,23 +19,9 @@ object DotStore {
 
   inline def apply[D](using dotStore: DotStore[D]): DotStore[D] = dotStore
 
-  given dotSetDotStore: DotStore[DotSet] = new DotStore[DotSet] {
-    override def dots(dotStore: DotSet): DotSet = dotStore
-
-    override def empty: DotSet = Dots.empty
+  given dotStoreCompat[A](using bot: Bottom[A], hd: HasDots[A]): DotStore[A] = new DotStore[A] {
+    override def dots(dotStore: A): DotSet = hd.dots(dotStore)
+    export bot.empty
   }
 
-  // Todo: V should be a SemiLattice according to paper
-  given dotFunDotStore[V]: DotStore[DotFun[V]] = new DotStore[DotFun[V]] {
-    override def dots(dotFun: DotFun[V]): DotSet = Dots.from(dotFun.keySet)
-
-    override def empty: DotFun[V] = Map.empty
-  }
-
-  given dotMapDotStore[K, V: DotStore]: DotStore[DotMap[K, V]] = new DotStore[DotMap[K, V]] {
-    override def dots(dotStore: DotMap[K, V]): DotSet =
-      dotStore.values.map(DotStore[V].dots(_)).reduce((l, r) => l.union(r))
-
-    override def empty: DotMap[K, V] = Map.empty
-  }
 }
