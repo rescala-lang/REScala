b75caec6042bfcfc5f8dd57556b9c13dace5b334
ragnar <git@rmgk.de>
2024-06-01 14:24:11 +0200

    reinstate removed datatypes

---
+++ Modules/Local-first Access Control/src/main/scala/lofi_acl/ardt/datatypes/MultiValueRegister.scala
@@ -0,0 +1,33 @@
+package lofi_acl.ardt.datatypes
+
+import lofi_acl.ardt.base.Causal
+import lofi_acl.ardt.base.Causal
+import rdts.syntax.LocalUid
+import rdts.time.{Dot, Dots}
+
+import scala.language.implicitConversions
+
+opaque type MultiValueRegister[V] = Causal[Map[Dot, V]]
+
+object MultiValueRegister:
+  extension [V](reg: MultiValueRegister[V])
+    def read: Set[V] = {
+      reg.dotStore.values.toSet
+    }
+
+  private[datatypes] given regToCausal[V]: Conversion[MultiValueRegister[V], Causal[Map[Dot, V]]] = identity
+  private[datatypes] given causalToReg[V]: Conversion[Causal[Map[Dot, V]], MultiValueRegister[V]] = identity
+
+  object mutators:
+    def write[V](register: MultiValueRegister[V], value: V, replicaId: LocalUid): MultiValueRegister[V] =
+      val dot = register.causalContext.nextDot(replicaId.uid)
+      Causal(
+        Map(dot -> value),
+        Dots.from(register.dotStore.keySet + dot)
+      )
+
+    def clear[V](register: MultiValueRegister[V]): MultiValueRegister[V] =
+      Causal(
+        Map.empty,
+        Dots.from(register.dotStore.keySet)
+      )
