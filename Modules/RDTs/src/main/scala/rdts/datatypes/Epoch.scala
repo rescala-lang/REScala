2e57d80ff8f9bfa4ea7a01f13578c9a2fbc525a8
ragnar <git@rmgk.de>
2024-06-22 20:29:56 +0200

    desyntax epoche

--- Modules/RDTs/src/main/scala/rdts/datatypes/Epoch.scala
+++ Modules/RDTs/src/main/scala/rdts/datatypes/Epoch.scala
@@ -2,10 +2,20 @@ package rdts.datatypes
 
 import rdts.base.{Bottom, Lattice}
 import rdts.dotted.HasDots
-import rdts.syntax.{OpsSyntaxHelper, PermMutate, PermQuery}
 import rdts.time.{Dots, Time}
 
-case class Epoch[E](counter: Time, value: E)
+case class Epoch[E](counter: Time, value: E) {
+
+  type Delta = Epoch[E]
+
+  def read: E = value
+
+  def write(value: E): Delta = copy(value = value)
+
+  def epocheWrite(value: E): Delta = Epoch(counter + 1, value)
+
+  def map(f: E => E): Delta = write(f(value))
+}
 
 object Epoch {
 
@@ -18,19 +28,6 @@ object Epoch {
     extension (dotted: Epoch[E])
       def dots: Dots                               = dotted.value.dots
       def removeDots(dots: Dots): Option[Epoch[E]] = dotted.value.removeDots(dots).map(nv => dotted.copy(value = nv))
-  }
-
-  extension [C, E](container: C)
-    def epoche: syntax[C, E] = syntax(container)
-
-  implicit class syntax[C, E](container: C)
-      extends OpsSyntaxHelper[C, Epoch[E]](container) {
-    def read(using IsQuery): E = current.value
-
-    def write(using IsMutator)(value: E): C       = current.copy(value = value).mutator
-    def epocheWrite(using IsMutator)(value: E): C = Epoch(current.counter + 1, value).mutator
-
-    def map(using IsMutator)(f: E => E): C = write(f(current.value))
   }
 
   given latticeInstance[E: Lattice]: Lattice[Epoch[E]] = new Lattice[Epoch[E]] {
