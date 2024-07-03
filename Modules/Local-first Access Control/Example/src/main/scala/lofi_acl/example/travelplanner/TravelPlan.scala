dae8668872f1d0f5dc6877288feab09c41f9b8d2
Christian Kuessner <17302779+ckuessner@users.noreply.github.com>
2024-07-03 17:40:29 +0200

    Change TravelPlan ARDT

--- Modules/Local-first Access Control/Example/src/main/scala/lofi_acl/example/travelplanner/TravelPlan.scala
+++ Modules/Local-first Access Control/Example/src/main/scala/lofi_acl/example/travelplanner/TravelPlan.scala
@@ -5,28 +5,27 @@ import com.github.plokhotnyuk.jsoniter_scala.macros.JsonCodecMaker
 import lofi_acl.access.Filter
 import lofi_acl.ardt.datatypes.LWW.filter
 import lofi_acl.ardt.datatypes.ORMap.stringKeyORMapFilter
-import lofi_acl.example.travelplanner.TravelPlan.emptyStringAsBottom
 import rdts.base.{Bottom, Lattice}
 import rdts.datatypes.LastWriterWins
 import rdts.datatypes.contextual.ObserveRemoveMap
 import rdts.dotted.HasDots
 
 case class TravelPlan(
-    title: LastWriterWins[String],
+    title: LastWriterWins[Title],
     bucketList: ObserveRemoveMap[String, LastWriterWins[String]],
     expenses: ObserveRemoveMap[String, Expense]
 ) derives Lattice, Bottom, Filter
 
 case class Expense(
     // time: LastWriterWins[LocalDateTime], // For what do we need time?
-    description: LastWriterWins[String],
+    description: LastWriterWins[Option[String]],
     amount: LastWriterWins[Option[Float]],
     comments: ObserveRemoveMap[String, LastWriterWins[String]]
 ) derives Lattice, HasDots, Bottom, Filter
 
 object TravelPlan {
-  given emptyStringAsBottom: Bottom[String] = Bottom.provide("")
-  // given zeroFloatAsBottom: Bottom[Float] = Bottom.provide(0.0f)
-
   given jsonCodec: JsonValueCodec[TravelPlan] = JsonCodecMaker.make[TravelPlan]
+
+  type Title = String
+  given Bottom[Title] = Bottom.provide("")
 }
