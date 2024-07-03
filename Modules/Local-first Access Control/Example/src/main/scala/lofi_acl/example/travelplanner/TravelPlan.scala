454f8439614bbff24c30b6b0c1382dd022f9025a
Christian Kuessner <17302779+ckuessner@users.noreply.github.com>
2024-07-03 14:29:47 +0200

    Refactor TravelPlan typeclass derivation

--- Modules/Local-first Access Control/Example/src/main/scala/lofi_acl/example/travelplanner/TravelPlan.scala
+++ Modules/Local-first Access Control/Example/src/main/scala/lofi_acl/example/travelplanner/TravelPlan.scala
@@ -1,30 +1,21 @@
 package lofi_acl.example.travelplanner
 
+import com.github.plokhotnyuk.jsoniter_scala.core.JsonValueCodec
+import com.github.plokhotnyuk.jsoniter_scala.macros.JsonCodecMaker
 import lofi_acl.access.Filter
 import lofi_acl.ardt.datatypes.LWW.filter
 import lofi_acl.ardt.datatypes.ORMap.stringKeyORMapFilter
 import lofi_acl.example.travelplanner.TravelPlan.emptyStringAsBottom
 import rdts.base.{Bottom, Lattice}
 import rdts.datatypes.LastWriterWins
-import rdts.datatypes.alternatives.ObserveRemoveSet
 import rdts.datatypes.contextual.ObserveRemoveMap
 import rdts.dotted.HasDots
 
 case class TravelPlan(
     title: LastWriterWins[String],
-    locations: ObserveRemoveSet[String],
+    bucketList: ObserveRemoveMap[String, LastWriterWins[String]],
     expenses: ObserveRemoveMap[String, Expense]
-)
-
-object TravelPlan {
-  import rdts.datatypes.contextual.ObserveRemoveMap.lattice
-
-  given emptyStringAsBottom: Bottom[String] = Bottom.provide("")
-  // given zeroFloatAsBottom: Bottom[Float] = Bottom.provide(0.0f)
-
-  given expenseLattice: Lattice[TravelPlan] = Lattice.derived
-  given lattice: Lattice[TravelPlan]        = Lattice.derived
-}
+) derives Lattice, Bottom, Filter
 
 case class Expense(
     // time: LastWriterWins[LocalDateTime], // For what do we need time?
@@ -32,3 +23,10 @@ case class Expense(
     amount: LastWriterWins[Option[Float]],
     comments: ObserveRemoveMap[String, LastWriterWins[String]]
 ) derives Lattice, HasDots, Bottom, Filter
+
+object TravelPlan {
+  given emptyStringAsBottom: Bottom[String] = Bottom.provide("")
+  // given zeroFloatAsBottom: Bottom[Float] = Bottom.provide(0.0f)
+
+  given jsonCodec: JsonValueCodec[TravelPlan] = JsonCodecMaker.make[TravelPlan]
+}
