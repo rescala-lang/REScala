599a1ed6acaf1ea93d9b5497fc8c96538114fef0
ragnar <git@rmgk.de>
2024-07-29 18:56:41 +0200

    add experimental sse handler

---
+++ Modules/Channels/jvm/src/main/scala/channels/SunHttpSSE.scala
@@ -0,0 +1,35 @@
+package channels
+
+import com.sun.net.httpserver.{HttpExchange, HttpHandler}
+import de.rmgk.delay.Async
+
+import scala.concurrent.ExecutionContext
+
+object SunHttpSSE {
+
+  class SSEServer(addHandler: HttpHandler => Unit, ec: ExecutionContext) extends LatentConnection[MessageBuffer] {
+    def prepare(incomingHandler: Handler[MessageBuffer]): Async[Abort, Connection[MessageBuffer]] = Async.fromCallback {
+
+      val handler = new HttpHandler {
+
+        override def handle(exchange: HttpExchange): Unit = {
+          val headers = exchange.getResponseHeaders
+          headers.add("Content-Type", "text/event-stream")
+          headers.add("Connection", "keep-alive")
+
+          exchange.sendResponseHeaders(200, 0)
+          val outstream = exchange.getResponseBody
+
+          val instream = exchange.getRequestBody
+
+          val conn = JIOStreamConnection(instream, outstream, () => exchange.close())
+
+          ec.execute(() => conn.loopHandler(incomingHandler))
+        }
+
+      }
+
+    }
+  }
+
+}
