1bea6e4b48b5e585355a8402789810682cca1d87
Lukas Holst <lukas@familie-holst.de>
2024-07-29 10:33:40 -0400

    Monitoring-Server now streams all messages to the according files.

--- Modules/DTN/jvm/src/main/scala/dtn/Monitoring.scala
+++ Modules/DTN/jvm/src/main/scala/dtn/Monitoring.scala
@@ -1,22 +1,62 @@
 package dtn
 
-import io.bullet.borer.Cbor
-import rdts.time.Dots
+import io.bullet.borer.Json
+
+import java.io.BufferedOutputStream
+import java.nio.charset.StandardCharsets
+import java.nio.file.Files
+import java.nio.file.Paths
+import java.nio.file.StandardOpenOption
+import java.time.ZoneId
+import java.time.ZonedDateTime
+import scala.util.Using
 
 class MonitoringServer(server: TCPReadonlyServer) {
   def run(): Unit = {
-    try {
-      while true do {
-        val (connection, data)          = server.queue.take()
-        val received: MonitoringMessage = Cbor.decode(data).to[MonitoringMessage].value
-      }
-    } catch {
-      case e: Exception =>
-        println(s"monitoring server ran into exception: $e")
-        server.stop()
-    }
-  }
+    val dir = Paths.get("/shared/monitoring")
 
+    Using.Manager { use =>
+      val streamReceived =
+        use(BufferedOutputStream(Files.newOutputStream(dir.resolve("received.data"), StandardOpenOption.APPEND)))
+      val streamForwarded =
+        use(BufferedOutputStream(Files.newOutputStream(dir.resolve("forwarded.data"), StandardOpenOption.APPEND)))
+      val streamDelivered =
+        use(BufferedOutputStream(Files.newOutputStream(dir.resolve("delivered.data"), StandardOpenOption.APPEND)))
+      val streamCreated =
+        use(BufferedOutputStream(Files.newOutputStream(dir.resolve("created.data"), StandardOpenOption.APPEND)))
+
+      try {
+        while true do {
+          val (connection, data) = server.queue.take()
+
+          val now: ZonedDateTime = ZonedDateTime.now(ZoneId.of("UTC"))
+
+          Json.decode(data).to[MonitoringMessage].value match
+            case m: MonitoringMessage.BundleReceivedAtRouter =>
+              streamReceived.write(Json.encode[MonitoringMessage](m.copy(time = Option(now))).toByteArray)
+              streamReceived.write("\n".getBytes())
+              streamReceived.flush()
+            case m: MonitoringMessage.BundleForwardedAtRouter =>
+              streamForwarded.write(Json.encode[MonitoringMessage](m.copy(time = Option(now))).toByteArray)
+              streamForwarded.write("\n".getBytes())
+              streamForwarded.flush()
+            case m: MonitoringMessage.BundleDeliveredAtClient =>
+              streamDelivered.write(Json.encode[MonitoringMessage](m.copy(time = Option(now))).toByteArray)
+              streamDelivered.write("\n".getBytes())
+              streamDelivered.flush()
+            case m: MonitoringMessage.BundleCreatedAtClient =>
+              streamCreated.write(Json.encode[MonitoringMessage](m.copy(time = Option(now))).toByteArray)
+              streamCreated.write("\n".getBytes())
+              streamCreated.flush()
+        }
+      } catch {
+        case e: Exception =>
+          println(s"monitoring server ran into exception: $e")
+          server.stop()
+      }
+    }
+    ()
+  }
 }
 object MonitoringServer {
   def apply(interface: String, port: Int): MonitoringServer = {
@@ -29,7 +69,7 @@ object MonitoringServer {
 
 class MonitoringClient(connection: TCPConnection) extends MonitoringClientInterface {
   override def send(message: MonitoringMessage): Unit = {
-    connection.send(Cbor.encode[MonitoringMessage](message).toByteArray)
+    connection.send(Json.encode[MonitoringMessage](message).toByteArray)
   }
 }
 object MonitoringClient {
--- Modules/DTN/shared/src/main/scala/dtn/Monitoring.scala
+++ Modules/DTN/shared/src/main/scala/dtn/Monitoring.scala
@@ -3,12 +3,14 @@ package dtn
 import io.bullet.borer.Codec
 import io.bullet.borer.derivation.MapBasedCodecs.*
 import rdts.time.Dots
+import java.time.ZonedDateTime
+import io.bullet.borer.{Encoder, Decoder}
 
 enum MonitoringMessage derives Codec.All:
-  case BundleReceivedAtRouter(nodeId: String, bundleId: String)
-  case BundleForwardedAtRouter(nodeId: String, bundleId: String)
-  case BundleDeliveredAtClient(clientId: String, bundleId: String, dots: Dots)
-  case BundleCreatedAtClient(clientId: String, bundleId: String, dots: Dots)
+  case BundleReceivedAtRouter(nodeId: String, bundleId: String, time: Option[ZonedDateTime] = None)
+  case BundleForwardedAtRouter(nodeId: String, bundleId: String, time: Option[ZonedDateTime] = None)
+  case BundleDeliveredAtClient(clientId: String, bundleId: String, dots: Dots, time: Option[ZonedDateTime] = None)
+  case BundleCreatedAtClient(clientId: String, bundleId: String, dots: Dots, time: Option[ZonedDateTime] = None)
 
 trait MonitoringClientInterface {
   def send(message: MonitoringMessage): Unit
@@ -16,4 +18,12 @@ trait MonitoringClientInterface {
 
 object NoMonitoringClient extends MonitoringClientInterface {
   override def send(message: MonitoringMessage): Unit = ()
+}
+
+// default formatter of ZonedDateTime should suffice here
+given Encoder[ZonedDateTime] = Encoder { (writer, zonedDateTime) =>
+  writer.write(zonedDateTime.toString())
+}
+given Decoder[ZonedDateTime] = Decoder { reader =>
+  ZonedDateTime.parse(reader.readString())
 }
