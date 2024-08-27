7fa1520036f062b16bf0611efdc8faa859a2a4bd
ragnar <git@rmgk.de>
2024-08-27 16:38:34 +0200

    add obremmap test

---
+++ Modules/RDTs/src/test/scala/test/rdts/bespoke/ObserveRemoveMapTest.scala
@@ -0,0 +1,33 @@
+package test.rdts.bespoke
+
+import rdts.base.{Lattice, LocalUid}
+import rdts.datatypes.contextual.ObserveRemoveMap
+import rdts.dotted.Obrem
+import rdts.time.Dot
+
+class ObserveRemoveMapTest extends munit.FunSuite {
+
+  given Lattice[Dot] = Lattice.assertEquals
+
+  test("basic usage") {
+    val obremmap = Obrem(ObserveRemoveMap.empty[String, Dot])
+
+    given replicaId: LocalUid = LocalUid.gen()
+
+    val added = obremmap.mod{ ctx ?=> current =>
+      val nextDot = ctx.nextDot(replicaId.uid)
+      current.update("Hi!", nextDot)
+    }
+
+    assert(added.data.contains("Hi!"))
+
+
+    val remove = added.mod{ ctx ?=> current =>
+      current.remove("Hi!")
+    }
+
+    val merged = added `merge` remove
+
+    assertEquals(merged.data.entries.toMap, Map.empty)
+  }
+}
