d81390396bb7382e7fc54dfdded0aa3821c7a4d3
ragnar <git@rmgk.de>
2024-09-10 14:12:39 +0200

    dare acl wip example …

---
+++ Modules/Examples/Misc JVM/src/main/scala/ex2024dare/ACL.scala
@@ -0,0 +1,55 @@
+package ex2024dare
+
+import rdts.base.{LocalUid, Uid}
+import rdts.dotted.Dotted
+import rdts.time.{Dot, Dots}
+
+object ACL {
+
+  trait Operation(id: Dot, signer: User, predecessors: Dots)
+
+  object Operation {
+    case class Create(signer: User, id: Dot, roomid: Uid)
+        extends Operation(id, signer, Dots.empty)
+    case class Add(signer: User, added: User, id: Dot, predecessors: Dots)
+        extends Operation(id, signer, predecessors)
+    case class Remove(signer: User, removed: User, id: Dot, predecessors: Dots)
+        extends Operation(id, signer, predecessors)
+  }
+
+  case class User(uid: Uid)
+
+  case class OpGraph(ops: Map[Dot, Operation]) {
+
+    def dots: Dots = Dots.from(ops.keys)
+
+    def create()(using context: Dots, replicaId: LocalUid): Dotted[OpGraph] = {
+      val next = context.nextDot(replicaId.uid)
+
+      val op = Operation.Create(User(replicaId.uid), next, Uid.gen())
+
+      Dotted(OpGraph(Map(next -> op)), Dots.single(next))
+    }
+
+    def add(user: User)(using context: Dots, replicaId: LocalUid): Dotted[OpGraph] = {
+      val next = context.nextDot(replicaId.uid)
+
+      val op = Operation.Add(User(replicaId.uid), user, next, dots)
+
+      Dotted(OpGraph(Map(next -> op)), Dots.single(next))
+    }
+
+    def remove(user: User)(using context: Dots, replicaId: LocalUid): Dotted[OpGraph] = {
+      val next = context.nextDot(replicaId.uid)
+
+      val op = Operation.Remove(User(replicaId.uid), user, next, dots)
+
+      Dotted(OpGraph(Map(next -> op)), Dots.single(next))
+    }
+
+
+  }
+
+
+
+}
