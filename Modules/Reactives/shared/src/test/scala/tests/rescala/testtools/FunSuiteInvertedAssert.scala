e0c2f66785e80872c32a0767acce8f35a2dd289b
ragnar <git@rmgk.de>
2025-01-31 13:26:51 +0100

    fix assertions subclass …

--- Modules/Reactives/shared/src/test/scala/tests/rescala/testtools/FunSuiteInvertedAssert.scala
+++ Modules/Reactives/shared/src/test/scala/tests/rescala/testtools/FunSuiteInvertedAssert.scala
@@ -1,25 +1,32 @@
 package tests.rescala.testtools
 
+import munit.diff.DiffOptions
 import munit.{Assertions, Compare, Location}
 
 abstract class FunSuiteInvertedAssert extends munit.FunSuite {
 
-  override def assertEquals[A, B](obtained: A, expected: B, clue: => Any)(using
+  override def assertEquals[A, B](
+      obtained: A,
+      expected: B,
+      clue: => Any = "values are not the same",
+  )(implicit
       loc: Location,
-      compare: Compare[A, B]
-  ): Unit = super.assertEquals(expected, obtained, clue)(
-    loc,
-    new Compare[B, A] {
-      override def isEqual(obtained: B, expected: A): Boolean = compare.isEqual(expected, obtained)
+      compare: Compare[A, B],
+      diffOptions: DiffOptions,
+  ): Unit =
+    super.assertEquals(expected, obtained, clue)(
+      loc,
+      new Compare[B, A] {
+        override def isEqual(obtained: B, expected: A): Boolean = compare.isEqual(expected, obtained)
 
-      override def failEqualsComparison(
-          obtained: B,
-          expected: A,
-          title: Any,
-          loc: Location,
-          assertions: Assertions
-      ): Nothing = compare.failEqualsComparison(expected, obtained, title, loc, assertions)
-    }
-  )
+        override def failEqualsComparison(
+            obtained: B,
+            expected: A,
+            title: Any,
+            assertions: Assertions,
+        )(implicit loc: Location, options: DiffOptions): Nothing =
+          compare.failEqualsComparison(expected, obtained, title, assertions)(using loc, options)
+      }
+    )
 
 }
