b1a424cd1db5d1f5d2223df2b9409a66c1b8312f
Christian Kuessner <17302779+ckuessner@users.noreply.github.com>
2025-02-25 23:21:32 +0100

    Improve BftAclOpGraph

--- Modules/Replication/jvm/src/main/scala/lofi_acl/sync/acl/bft/BftAclOpGraph.scala
+++ Modules/Replication/jvm/src/main/scala/lofi_acl/sync/acl/bft/BftAclOpGraph.scala
@@ -1,16 +1,15 @@
 package lofi_acl.sync.acl.bft
 
-import com.github.plokhotnyuk.jsoniter_scala.core.{JsonValueCodec, readFromArray, writeToArray}
+import com.github.plokhotnyuk.jsoniter_scala.core.{JsonValueCodec, readFromArray, writeToArray, writeToArrayReentrant}
 import com.github.plokhotnyuk.jsoniter_scala.macros.{CodecMakerConfig, JsonCodecMaker}
 import lofi_acl.access.PermissionTree
-import lofi_acl.crypto.{Ed25519Util, PublicIdentity}
-import lofi_acl.sync.acl.bft.BftAcl.{Delegation, EncodedDelegation, opCodec}
+import lofi_acl.crypto.{Ed25519Util, PrivateIdentity, PublicIdentity}
+import lofi_acl.sync.acl.bft.BftAclOpGraph.{Delegation, EncodedDelegation, Signature, opCodec}
 
 import java.security.PrivateKey
 import java.util.Base64
 import scala.collection.mutable
-
-type Signature = String
+import scala.util.{Failure, Success, Try}
 
 case class Acl(read: Map[PublicIdentity, PermissionTree], write: Map[PublicIdentity, PermissionTree]):
   def addPermissions(user: PublicIdentity, read: PermissionTree, write: PermissionTree): Acl =
@@ -25,28 +24,28 @@ case class Acl(read: Map[PublicIdentity, PermissionTree], write: Map[PublicIdent
       }
     )
 
-case class BftAcl(ops: Map[Signature, Delegation], heads: Set[Signature]) {
+case class BftAclOpGraph(ops: Map[Signature, Delegation], heads: Set[Signature]) {
   def delegateAccess(
       delegator: PublicIdentity,
       delegatorKey: PrivateKey,
       delegatee: PublicIdentity,
       read: PermissionTree,
       write: PermissionTree
-  ): (BftAcl, EncodedDelegation) =
+  ): (BftAclOpGraph, EncodedDelegation) =
     require(read <= write) // Write access implies read access! (Not really enforced)
     val op = Delegation(delegator, delegatee, read, write, parents = heads)
     require(isDelegationLegal(op))
     val opBytes     = writeToArray(op)
     val sig         = Ed25519Util.sign(opBytes, delegatorKey)
     val sigAsString = Base64.getEncoder.encodeToString(sig)
-    (BftAcl(ops + (sigAsString -> op), Set(sigAsString)), EncodedDelegation(sig, opBytes))
+    (BftAclOpGraph(ops + (sigAsString -> op), Set(sigAsString)), EncodedDelegation(sig, opBytes))
 
   def isDelegationLegal(op: Delegation): Boolean =
     val referenceVersion = reconstruct(op.parents).get
     op.read <= referenceVersion.read.getOrElse(op.delegator, PermissionTree.empty) &&
     op.write <= referenceVersion.write.getOrElse(op.delegator, PermissionTree.empty)
 
-  def receive(signature: Array[Byte], encodedOp: Array[Byte]): Either[Set[Signature], BftAcl] =
+  def receive(signature: Array[Byte], encodedOp: Array[Byte]): Either[Set[Signature], BftAclOpGraph] =
     val signatureAsString = Base64.getEncoder.encodeToString(signature)
     if ops.contains(signatureAsString) then return Right(this)
     readFromArray[Delegation](encodedOp) match
@@ -58,7 +57,7 @@ case class BftAcl(ops: Map[Signature, Delegation], heads: Set[Signature]) {
         val missing = parents.filterNot(ops.contains)
         if missing.nonEmpty then return Left(missing)
         // Track new op, remove heads that op references as predecessors and add new op as head
-        Right(BftAcl(ops + (signatureAsString -> delegation), (heads -- parents) + signatureAsString))
+        Right(BftAclOpGraph(ops + (signatureAsString -> delegation), (heads -- parents) + signatureAsString))
 
   def reconstruct(heads: Set[Signature]): Option[Acl] =
     require(heads.forall(ops.contains))
@@ -80,7 +79,22 @@ case class BftAcl(ops: Map[Signature, Delegation], heads: Set[Signature]) {
     Some(resultAcl)
 }
 
-object BftAcl:
+object BftAclOpGraph:
+  type Signature = String
+
+  def createSelfSignedRoot(rootIdentity: PrivateIdentity): EncodedDelegation = {
+    val rootAclDelta: Delegation = Delegation(
+      delegator = rootIdentity.getPublic,
+      delegatee = rootIdentity.getPublic,
+      read = PermissionTree.allow,
+      write = PermissionTree.allow,
+      parents = Set.empty
+    )
+    val opBytes = writeToArray(rootAclDelta)
+    val sig = Ed25519Util.sign(opBytes, rootIdentity.identityKey.getPrivate)
+    EncodedDelegation(sig, opBytes)
+  }
+
   given opCodec: JsonValueCodec[Delegation] = JsonCodecMaker.make(
     CodecMakerConfig.withAllowRecursiveTypes(true) // Required for PermissionTree
   )
@@ -91,8 +105,23 @@ object BftAcl:
       read: PermissionTree,
       write: PermissionTree,
       parents: Set[Signature]
-  )
+  ) {
+    def encode(signature: Signature)(using JsonValueCodec[Delegation]): EncodedDelegation = {
+      val delegationBytes = writeToArray(this)
+      val signatureBytes = Base64.getDecoder.decode(signature)
+      assert(Ed25519Util.checkEd25519Signature(delegationBytes, signatureBytes, delegator))
+      EncodedDelegation(signatureBytes, delegationBytes)
+    }
+  }
 
-  case class EncodedDelegation(sig: Array[Byte], op: Array[Byte])
+  case class EncodedDelegation(sig: Array[Byte], op: Array[Byte]) {
+    def decode(using JsonValueCodec[Delegation]): Try[(Signature, Delegation)] = {
+      val delegation: Delegation = readFromArray(op)
+      val signer                 = delegation.delegator.publicKey
+      if !Ed25519Util.checkEd25519Signature(op, sig, signer)
+      then Failure(InvalidSignatureException)
+      else Success((Base64.getEncoder.encodeToString(sig), delegation))
+    }
+  }
 
 object InvalidSignatureException extends RuntimeException
