8f582e15c7dd4938c624d80751fc282db830d9a4
Robin Breitenbach <robin.breitenbach@stud.tu-darmstadt.de>
2025-04-03 16:46:13 +0200

    Implement simple generation aspects (wip)

--- Modules/LoRe Compiler Plugin/src/main/scala/loreCompilerPlugin/codegen/DafnyGen.scala
+++ Modules/LoRe Compiler Plugin/src/main/scala/loreCompilerPlugin/codegen/DafnyGen.scala
@@ -65,207 +65,199 @@ object DafnyGen {
     * @return The generated Dafny code.
     */
   private def generateFromTAbs(node: TAbs): String = {
-    ""
+    // TODO: This is not properly functional, as it only generates the body, but not the definition!
+    generate(node.body)
   }
 
-  // TODO: Implement
   /** Generates Dafny code for the given LoRe TNum.
     *
     * @param node The LoRe TNum node.
     * @return The generated Dafny code.
     */
   private def generateFromTNum(node: TNum): String = {
-    ""
+    // Transforming to string may seem odd but in reality it'll be a number
+    // in code because it's not surrounded by quotes, like a string would be
+    node.value.toString
   }
 
-  // TODO: Implement
   /** Generates Dafny code for the given LoRe TString.
     *
     * @param node The LoRe TString node.
     * @return The generated Dafny code.
     */
   private def generateFromTString(node: TString): String = {
-    ""
+    // Surround by quotes so it's an actual string within the code
+    s"\"${node.value}\""
   }
 
-  // TODO: Implement
   /** Generates Dafny code for the given LoRe TTrue.
     *
     * @param node The LoRe TTrue node.
     * @return The generated Dafny code.
     */
   private def generateFromTTrue(node: TTrue): String = {
-    ""
+    true.toString
   }
 
-  // TODO: Implement
   /** Generates Dafny code for the given LoRe TFalse.
     *
     * @param node The LoRe TFalse node.
     * @return The generated Dafny code.
     */
   private def generateFromTFalse(node: TFalse): String = {
-    ""
+    false.toString
   }
 
-  // TODO: Implement
   /** Generates Dafny code for the given LoRe TVar.
     *
     * @param node The LoRe TVar node.
     * @return The generated Dafny code.
     */
   private def generateFromTVar(node: TVar): String = {
-    ""
+    // Just place the variable name in the code
+    node.name
   }
 
-  // TODO: Implement
   /** Generates Dafny code for the given LoRe TNeg.
     *
     * @param node The LoRe TNeg node.
     * @return The generated Dafny code.
     */
   private def generateFromTNeg(node: TNeg): String = {
-    ""
+    s"!${generate(node.body)}"
   }
 
-  // TODO: Implement
   /** Generates Dafny code for the given LoRe TFCall.
     *
     * @param node The LoRe TFCall node.
     * @return The generated Dafny code.
     */
   private def generateFromTFCall(node: TFCall): String = {
-    ""
+    // TODO: 0-argument methods vs properties?
+    if node.args.nonEmpty then {
+      val args: Seq[String] = node.args.map(arg => generate(arg))
+      s"${generate(node.parent)}.${node.field}(${args.mkString(", ")})"
+    } else {
+      s"${generate(node.parent)}.${node.field}"
+    }
   }
 
-  // TODO: Implement
   /** Generates Dafny code for the given LoRe TAdd.
     *
     * @param node The LoRe TAdd node.
     * @return The generated Dafny code.
     */
   private def generateFromTAdd(node: TAdd): String = {
-    ""
+    s"${generate(node.left)} + ${generate(node.right)}"
   }
 
-  // TODO: Implement
   /** Generates Dafny code for the given LoRe TSub.
     *
     * @param node The LoRe TSub node.
     * @return The generated Dafny code.
     */
   private def generateFromTSub(node: TSub): String = {
-    ""
+    s"${generate(node.left)} - ${generate(node.right)}"
   }
 
-  // TODO: Implement
   /** Generates Dafny code for the given LoRe TMul.
     *
     * @param node The LoRe TMul node.
     * @return The generated Dafny code.
     */
   private def generateFromTMul(node: TMul): String = {
-    ""
+    s"${generate(node.left)} * ${generate(node.right)}"
   }
 
-  // TODO: Implement
   /** Generates Dafny code for the given LoRe TDiv.
     *
     * @param node The LoRe TDiv node.
     * @return The generated Dafny code.
     */
   private def generateFromTDiv(node: TDiv): String = {
-    ""
+    s"${generate(node.left)} / ${generate(node.right)}"
   }
 
-  // TODO: Implement
   /** Generates Dafny code for the given LoRe TConj.
     *
     * @param node The LoRe TConj node.
     * @return The generated Dafny code.
     */
   private def generateFromTConj(node: TConj): String = {
-    ""
+    s"${generate(node.left)} && ${generate(node.right)}"
   }
 
-  // TODO: Implement
   /** Generates Dafny code for the given LoRe TDisj.
     *
     * @param node The LoRe TDisj node.
     * @return The generated Dafny code.
     */
   private def generateFromTDisj(node: TDisj): String = {
-    ""
+    s"${generate(node.left)} || ${generate(node.right)}"
   }
 
-  // TODO: Implement
   /** Generates Dafny code for the given LoRe TLt.
     *
     * @param node The LoRe TLt node.
     * @return The generated Dafny code.
     */
   private def generateFromTLt(node: TLt): String = {
-    ""
+    s"${generate(node.left)} < ${generate(node.right)}"
   }
 
-  // TODO: Implement
   /** Generates Dafny code for the given LoRe TGt.
     *
     * @param node The LoRe TGt node.
     * @return The generated Dafny code.
     */
   private def generateFromTGt(node: TGt): String = {
-    ""
+    s"${generate(node.left)} > ${generate(node.right)}"
   }
 
-  // TODO: Implement
   /** Generates Dafny code for the given LoRe TLeq.
     *
     * @param node The LoRe TLeq node.
     * @return The generated Dafny code.
     */
   private def generateFromTLeq(node: TLeq): String = {
-    ""
+    s"${generate(node.left)} <= ${generate(node.right)}"
   }
 
-  // TODO: Implement
   /** Generates Dafny code for the given LoRe TGeq.
     *
     * @param node The LoRe TGeq node.
     * @return The generated Dafny code.
     */
   private def generateFromTGeq(node: TGeq): String = {
-    ""
+    s"${generate(node.left)} >= ${generate(node.right)}"
   }
 
-  // TODO: Implement
   /** Generates Dafny code for the given LoRe TEq.
     *
     * @param node The LoRe TEq node.
     * @return The generated Dafny code.
     */
   private def generateFromTEq(node: TEq): String = {
-    ""
+    s"${generate(node.left)} == ${generate(node.right)}"
   }
 
-  // TODO: Implement
   /** Generates Dafny code for the given LoRe TIneq.
     *
     * @param node The LoRe TIneq node.
     * @return The generated Dafny code.
     */
   private def generateFromTIneq(node: TIneq): String = {
-    ""
+    s"${generate(node.left)} != ${generate(node.right)}"
   }
 
-  // TODO: Implement
   /** Generates Dafny code for the given LoRe TFunC.
     *
     * @param node The LoRe TFunc node.
     * @return The generated Dafny code.
     */
   private def generateFromTFunC(node: TFunC): String = {
-    ""
+    val args: Seq[String] = node.args.map(arg => generate(arg))
+    s"${node.name}(${args.mkString(", ")})"
   }
 
   // TODO: Implement
